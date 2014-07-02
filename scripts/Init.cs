c1c080c3a24c7f4306c6f20ae5eb03974e7c4835
Pedro Morais <Pedro.de.Faria.Autran.e.Morais@autodesk.com>
2014-07-02 15:12:30 -0700

    Reuploaded everything
    seemed to fix it

---
+++ scripts/Init.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+	
+	void generateCubes(int amt)
+	{
+		GameObject tmp;
+		for (int i = 0; i < amt; i++) 
+		{
+			tmp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			tmp.transform.parent = this.transform;
+			tmp.transform.position = new Vector3(0,0,0);
+			tmp.name = "part" + i;
+		}
+	}
+
+	void Start () {
+		string[] filepaths = {"C:\\Users\\t_defap\\Documents\\part2_1.bxda","C:\\Users\\t_defap\\Documents\\movement_1.bxda"};
+		generateCubes (filepaths.Length);
+		for (int i = 0; i < filepaths.Length; i++) 
+		{
+			HandleMeshes.loadBXDA (filepaths[i],transform.GetChild(i));
+		}
+		HandleJoints.loadBXDJ ("C:/Users/t_waggn/Documents/bxdj/skeleton.bxdj", transform.GetChild(0), transform.GetChild(1));
+	}
+
+	void Update () {
+	
+	}
+}
