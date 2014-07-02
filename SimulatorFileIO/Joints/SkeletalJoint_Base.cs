43626a1058f4ed7916925ca7e6e0798ff50e7689
Westin Miller <westin.miller@autodesk.com>
2014-07-02 13:58:57 -0700

    Added simulator API project
    Includes abstract classes representing the model tree that don't rely on
    the Inventor API.
    Includes file I/O for these classes.

---
+++ SimulatorFileIO/Joints/SkeletalJoint_Base.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+public enum SkeletalJointType
+{
+    ROTATIONAL = 1, LINEAR = 2
+}
+
+public interface SkeletalJointFactory
+{
+     SkeletalJoint_Base create(SkeletalJointType type);
+}
+
+public class BaseSkeletalJointFactory : SkeletalJointFactory {
+    public SkeletalJoint_Base create(SkeletalJointType type){
+        switch(type){
+            case SkeletalJointType.ROTATIONAL:
+                return new RotationalJoint_Base();
+            case SkeletalJointType.LINEAR:
+                return new LinearJoint_Base();
+            default:
+                return null;
+        }
+    }
+}
+
+public abstract class SkeletalJoint_Base
+{
+    public static SkeletalJointFactory baseFactory = new BaseSkeletalJointFactory();
+
+    public JointDriver cDriver;
+
+    public abstract SkeletalJointType getJointType();
+
+    public abstract void writeJoint(System.IO.BinaryWriter writer);
+
+    protected abstract void readJoint(System.IO.BinaryReader reader);
+
+    public static SkeletalJoint_Base readJointFully(System.IO.BinaryReader reader)
+    {
+        SkeletalJointType type = (SkeletalJointType)reader.ReadByte();
+        SkeletalJoint_Base joint = baseFactory.create(type);
+        joint.readJoint(reader);
+        return joint;
+    }
+
+    protected virtual string ToString_Internal()
+    {
+        return Enum.GetName(typeof(SkeletalJointType), getJointType());
+    }
+
+    public override string ToString() {
+        string info = ToString_Internal();
+        if (cDriver != null)
+        {
+            info += " driven by " + Enum.GetName(typeof(JointDriverType), cDriver.getDriveType()).Replace('_', ' ').ToLowerInvariant() + " (" + cDriver.portA + (JointDriver.hasTwoPorts(cDriver.getDriveType())?","+cDriver.portB:"")+")";
+        }
+        return info;
+    }
+}
---
+++ SimulatorFileIO/Math/Math3D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BXDVector3
+{
+    public double x, y, z;
+    public BXDVector3() { }
+    public BXDVector3(double x, double y, double z) { this.x = x; this.y = y; this.z = z; }
+    public String toString()
+    {
+        return "[" + x + "," + y + "," + z + "]";
+    }
+}
