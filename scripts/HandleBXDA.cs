8c5a2104431d2d9cbc5eb5fa806433da69b9cc23
Pedro Morais <Pedro.de.Faria.Autran.e.Morais@autodesk.com>
2014-07-02 13:39:56 -0700

    Initial BXDA Handler

---
+++ scripts/HandleBXDA.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class HandleBXDA : MonoBehaviour 
+{
+	void loadBXDA(string filepath, Transform trans)
+	{
+		int vCount;
+		int tCount;
+		
+		BinaryReader bxda = new BinaryReader(File.Open(filepath,FileMode.Open));
+		
+		vCount = bxda.ReadInt32();
+		
+		Vector3[] vertices = new Vector3[vCount];
+		Vector3[] normals = new Vector3[vCount];
+		Color32[] colors = new Color32[vCount];
+		Vector2[] uvs = new Vector2[vCount];
+		
+		for (int i = 0; i < vCount; i++)
+		{
+			vertices[i] = new Vector3((float) bxda.ReadDouble(),(float) bxda.ReadDouble(),(float) bxda.ReadDouble());
+			
+			normals[i] = new Vector3((float) bxda.ReadDouble(),(float) bxda.ReadDouble(),(float) bxda.ReadDouble());
+			
+			colors[i] = new Color32(bxda.ReadByte(),bxda.ReadByte(),bxda.ReadByte(),bxda.ReadByte());
+			
+			uvs[i] = new Vector2((float) bxda.ReadDouble(), (float) bxda.ReadDouble());
+		}
+		
+		tCount = bxda.ReadInt32();
+		int[] triangles = new int[tCount*3];
+		
+		for (int i = 0; i < tCount*3; i++) 
+		{
+			triangles[i] = bxda.ReadInt32();
+		}
+		
+		Mesh mesh = new Mesh ();
+		trans.gameObject.AddComponent("Mesh Filter");
+		trans.gameObject.AddComponent ("MeshRender");
+		trans.GetComponent<MeshFilter>().mesh = mesh;
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.normals = normals;
+		mesh.colors32 = colors;
+		mesh.uv = uvs;
+
+	}
+
+	//creates primitive meshes/objects as children of this.transform and removes their colliders
+	//these are later turned into robot part meshes
+	void generateCubes(int amt)
+	{
+		GameObject tmp;
+		for (int i = 0; i < amt; i++) 
+		{
+			tmp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			tmp.transform.parent = this.transform;
+			tmp.transform.position = new Vector3(0,0,0);
+			Destroy(tmp.gameObject.collider);
+			tmp.name = "part" + i;
+		}
+	}
+
+	//attaches convex mesh colliders to all children of specified transform object
+	//pass this.transform to get all parts
+	void attachMeshColliders(Transform parent)
+	{
+		MeshCollider tmp;
+		foreach (Transform child in parent) 
+		{
+			child.gameObject.AddComponent<MeshCollider>();
+			tmp = child.gameObject.GetComponent<MeshCollider>();
+			tmp.convex = true;
+		}
+	}
+
+	void attachRigidBodies(Transform parent)
+	{
+		foreach (Transform child in parent) 
+		{
+			child.gameObject.AddComponent<Rigidbody>();
+		}
+	}
+
+	void betaHinge(Transform owner, Transform connected)// Vector3 anchorCoor)
+	{
+		HingeJoint OwnerHinge;
+		OwnerHinge = owner.gameObject.AddComponent<HingeJoint> ();
+		OwnerHinge.connectedBody = connected.gameObject.rigidbody;
+		//OwnerHinge.anchor = anchorCoor;
+	}
+
+	void Start () 
+	{
+		//string[] filepaths = {"C:\\Users\\t_defap\\Documents\\part2_1.bxda","C:\\Users\\t_defap\\Documents\\movement_1.bxda"};
+		string[] filepaths = {"C:\\Users\\t_defap\\Documents\\skele_andy_mark_chassis\\skele_andy_mark_chassis\\node_AndyMarkWheelEdgeRightTop.bxda","C:\\Users\\t_defap\\Documents\\skele_andy_mark_chassis\\skele_andy_mark_chassis\\node_AndyMarkWheelEdgeRightBottom.bxda","C:\\Users\\t_defap\\Documents\\skele_andy_mark_chassis\\skele_andy_mark_chassis\\node_AndyMarkWheelEdgeLeftTop.bxda","C:\\Users\\t_defap\\Documents\\skele_andy_mark_chassis\\skele_andy_mark_chassis\\node_AndyMarkWheelEdgeLeftBottom.bxda","C:\\Users\\t_defap\\Documents\\skele_andy_mark_chassis\\skele_andy_mark_chassis\\node_AndyMarkWheelCenterRight.bxda","C:\\Users\\t_defap\\Documents\\skele_andy_mark_chassis\\skele_andy_mark_chassis\\node_AndyMarkWheelCenterLeft.bxda","C:\\Users\\t_defap\\Documents\\skele_andy_mark_chassis\\skele_andy_mark_chassis\\node_frameouterplateleft_x81.bxda"};
+		generateCubes (filepaths.Length);
+
+		for (int i = 0; i < filepaths.Length; i++) 
+		{
+			loadBXDA (filepaths[i],transform.GetChild(i));
+		}
+		for (int i = 0; i < 5; i++) 
+		{
+			betaHinge(transform.GetChild(6), transform.GetChild(i));
+		}
+
+		attachMeshColliders (this.transform);
+		//attachRigidBodies (this.transform);
+		//betaHinge(transform.GetChild(0),transform.GetChild(1),new Vector3(0,0,0))
+
+
+	}
+	
+	void Update () 
+	{
+	}
+}
