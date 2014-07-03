53140a3ad0467b9bf0d065f344cb61059a936237
Ryan Milem <ryan.milem@autodesk.com>
2014-07-03 14:51:50 -0700

    Revert "Changed finding rotational joint axes"
    This reverts commit f74b9d8f97881874ccc6931db0923edbc27200e6.

--- JointResolver-Rev2/SkeletalStructure/RotationalJoint.cs
+++ JointResolver-Rev2/SkeletalStructure/RotationalJoint.cs
@@ -23,17 +23,19 @@ public class RotationalJoint : RotationalJoint_Base
         return false;
     }
 
-    private static void getRotationalInfo(dynamic geom, out Point groupABase)
+    private static void getRotationalInfo(dynamic geom, out UnitVector groupANormal, out Point groupABase)
     {
         if (geom is EdgeProxy)
         {
             EdgeProxy edge = (EdgeProxy)geom;
             if (edge.GeometryType == CurveTypeEnum.kCircularArcCurve || edge.GeometryType == CurveTypeEnum.kCircleCurve)
             {
+                groupANormal = geom.Geometry.Normal;
                 groupABase = geom.Geometry.Center;
             }
             else if (edge.GeometryType == CurveTypeEnum.kLineSegmentCurve || edge.GeometryType == CurveTypeEnum.kLineCurve)
             {
+                groupANormal = edge.Geometry.Direction;
                 groupABase = edge.Geometry.MidPoint;
                 // mid points look right...
             }
@@ -48,11 +50,13 @@ public class RotationalJoint : RotationalJoint_Base
             Console.WriteLine("FaceType: " + Enum.GetName(typeof(SurfaceTypeEnum), face.SurfaceType));
             if (face.SurfaceType == SurfaceTypeEnum.kPlaneSurface)
             {
+                groupANormal = face.Geometry.Normal;
                 groupABase = face.Geometry.RootPoint;
             }
             else if (face.SurfaceType == SurfaceTypeEnum.kCylinderSurface)
             {
                 groupABase = face.Geometry.BasePoint;
+                groupANormal = face.Geometry.AxisVector;
                 // face.Geometry.Radius
             }
             else
@@ -72,65 +76,31 @@ public class RotationalJoint : RotationalJoint_Base
             throw new Exception("Not a rotational joint");
         wrapped = new SkeletalJoint(parent, rigidJoint);
 
-        UnitVector jointInvNormal; //Stores invetors vector of the axis of rotation.
+        UnitVector groupANormal = null;
+        UnitVector groupBNormal = null;
         Point groupABase = null;
         Point groupBBase = null;
         Console.WriteLine("O1: " + Enum.GetName(typeof(ObjectTypeEnum), wrapped.asmJoint.OriginOne.Type) + "\t" +
             Enum.GetName(typeof(ObjectTypeEnum), wrapped.asmJoint.OriginOne.Geometry.Type));
         Console.WriteLine("O2: " + Enum.GetName(typeof(ObjectTypeEnum), wrapped.asmJoint.OriginTwo.Type) + "\t" +
             Enum.GetName(typeof(ObjectTypeEnum), wrapped.asmJoint.OriginTwo.Geometry.Type));
-        int translationDegrees;
-        ObjectsEnumerator translationAxes;
-        int rotationDegrees;
-        ObjectsEnumerator rotationAxes;
-        Point center;
-        IEnumerator axesEnumerator;
 
-        //Tries to find the axis of rotation from the first occurrence.
-        wrapped.asmJointOccurrence.AffectedOccurrenceOne.GetDegreesOfFreedom(out translationDegrees, out translationAxes,
-            out rotationDegrees, out rotationAxes, out center);
 
-        //Tries to find the axis of rotation from the second occurrence.
-        if (rotationDegrees == 0)
-        {
-            wrapped.asmJointOccurrence.AffectedOccurrenceTwo.GetDegreesOfFreedom(out translationDegrees, out translationAxes,
-            out rotationDegrees, out rotationAxes, out center);
-        }
-
-        //Tried and failed.  Gives up.
-        if (rotationDegrees == 0)
-        {
-            throw new Exception("No axis of rotation found for rotational joint.");
-        }
-
-        if (translationDegrees == 1)
-        {
-            axesEnumerator = translationAxes.GetEnumerator();
-            axesEnumerator.MoveNext();
-            jointInvNormal = ((Vector)axesEnumerator.Current).AsUnitVector();
-        }
-        else if (translationDegrees == 0)
-        {
-            jointInvNormal = null;
-        }
-        else
-        {
-            throw new Exception("More than one linear axis of freedom found on linear joint.");
-        }
-
-        getRotationalInfo(wrapped.asmJoint.OriginOne.Geometry, out groupABase);
-        getRotationalInfo(wrapped.asmJoint.OriginTwo.Geometry, out groupBBase);
+        getRotationalInfo(wrapped.asmJoint.OriginOne.Geometry, out groupANormal, out groupABase);
+        getRotationalInfo(wrapped.asmJoint.OriginTwo.Geometry, out groupBNormal, out groupBBase);
 
         if (wrapped.childIsTheOne)
         {
-            jointNormal = Utilities.toBXDVector(jointInvNormal);
+            childNormal = Utilities.toBXDVector(groupANormal);
             childBase = Utilities.toBXDVector(groupABase);
+            parentNormal = Utilities.toBXDVector(groupBNormal);
             parentBase = Utilities.toBXDVector(groupBBase);
         }
         else
         {
-            jointNormal = Utilities.toBXDVector(jointInvNormal);
+            childNormal = Utilities.toBXDVector(groupBNormal);
             childBase = Utilities.toBXDVector(groupBBase);
+            parentNormal = Utilities.toBXDVector(groupANormal);
             parentBase = Utilities.toBXDVector(groupABase);
         }
 
