d4fd9c9cd66ed601e0c51181f2614d272f0f3479
Ryan Milem <ryan.milem@autodesk.com>
2014-07-07 08:31:22 -0700

    Planar joints now store write base points, similar to other joints.  Will need to fix points later.

--- SimulatorFileIO/Joints/PlanarJoint_Base.cs
+++ SimulatorFileIO/Joints/PlanarJoint_Base.cs
@@ -13,6 +13,8 @@ public class PlanarJoint_Base : SkeletalJoint_Base
     //Contain the normal to the plane of motion.  Since both define the same plane, maybe we only need one?
     public BXDVector3 parentNormal;
     public BXDVector3 childNormal;
+    public BXDVector3 parentBase;
+    public BXDVector3 childBase;
 
     public override SkeletalJointType getJointType()
     {
@@ -28,11 +30,22 @@ public class PlanarJoint_Base : SkeletalJoint_Base
         writer.Write(childNormal.x);
         writer.Write(childNormal.y);
         writer.Write(childNormal.z);
+
+        writer.Write(parentBase.x);
+        writer.Write(parentBase.y);
+        writer.Write(parentBase.z);
+
+        writer.Write(childBase.x);
+        writer.Write(childBase.y);
+        writer.Write(childBase.z);
     }
 
     protected override void readJoint(System.IO.BinaryReader reader)
     {
         parentNormal = new BXDVector3(reader.ReadDouble(), reader.ReadDouble(), reader.ReadDouble());
         childNormal = new BXDVector3(reader.ReadDouble(), reader.ReadDouble(), reader.ReadDouble());
+
+        parentBase = new BXDVector3(reader.ReadDouble(), reader.ReadDouble(), reader.ReadDouble());
+        childBase = new BXDVector3(reader.ReadDouble(), reader.ReadDouble(), reader.ReadDouble());
     }
 }
