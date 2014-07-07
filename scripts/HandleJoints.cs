bb666a3cce83276c0b569148c04caba8207e4653
Nathaniel Waggoner <nathan.waggoner@autodesk.com>
2014-07-07 13:16:38 -0700

    HandleJoints Updated and Revised
    It has been commented and had a couple tweaks so that it can interact in
    a more modular fashion.

--- scripts/HandleJoints.cs
+++ scripts/HandleJoints.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Collections.Generic; 
 public class HandleJoints : MonoBehaviour
 {
+	//enums for quick ID'ing within conditionals
 	public enum SkeletalJointType:byte
 	{
 		ROTATIONAL = 1, 
@@ -18,39 +19,46 @@ public class HandleJoints : MonoBehaviour
 		BUMPER_PNEUMATIC = 4,
 		RELAY_PNEUMATIC = 5
 	}
-	
-	public static void loadBXDJ(string filePath = null, Transform jointO, Transform jointC)
-	{
 
+	//temp variable until wheels are included in the bxdj file format
+	public static bool isWheel = true;
+
+	public static void loadBXDJ(string filePath, Transform jointO, Transform jointC)
+	{
+		//this loads the skeleton for the object model
 		List<RigidNode_Base> nodes = new List<RigidNode_Base>();
-		RigidNode_Base skeleton = SkeletonIO.readSkeleton ("C:/Users/t_waggn/Documents/bxdj/skeleton.bxdj");
+		RigidNode_Base skeleton = SkeletonIO.readSkeleton (filePath);
 		skeleton.listAllNodes(nodes); 
 
 		foreach (RigidNode_Base node in nodes) {
-
+			//returns skeletonjointtype to then be used as required joint type
 			SkeletalJoint_Base nodeX = node.getSkeletalJoint();
 			if (nodeX != null){
+
+				var rigid = jointC;
+				var owner = jointO;
+
 				if ((int)nodeX.getJointType() == (int)SkeletalJointType.ROTATIONAL){
+
 					RotationalJoint_Base nodeR = (RotationalJoint_Base)nodeX;
-					var rigid = jointC;
-					var owner = jointO;
+
 
 					Rigidbody rigidB = rigid.gameObject.AddComponent<Rigidbody>();
 					var ownerB = owner.gameObject.AddComponent<ConfigurableJoint>();
 					ownerB.connectedBody = rigidB;
-					rigidB.useGravity = false;
+		
 
-
+					//takes the x, y, and z axis information from a custom vector class to unity's vector class
 					Vector3 parentC = new Vector3((float)nodeR.parentBase.x,(float)nodeR.parentBase.y,(float)nodeR.parentBase.z);
 					Vector3 parentN = new Vector3((float)nodeR.parentNormal.x, (float)nodeR.parentNormal.y,(float)nodeR.parentNormal.z);
 					Vector3 childC = new Vector3((float)nodeR.childBase.x,(float)nodeR.childBase.y,(float)nodeR.childBase.z);
 					Vector3 childN = new Vector3((float)nodeR.childNormal.x,(float)nodeR.childNormal.y,(float)nodeR.childNormal.z);
 
-					
+					//configures the joint
 					ownerB.anchor = parentC;
 					ownerB.connectedAnchor = childC;
 					ownerB.axis = parentN;
-					//ownerB.secondaryAxis = new Vector3 (0, 0, 1);
+					ownerB.secondaryAxis = new Vector3 (0, 0, 1);
 					
 					ownerB.angularXMotion = ConfigurableJointMotion.Limited;
 					ownerB.angularYMotion = ConfigurableJointMotion.Locked;
@@ -58,11 +66,17 @@ public class HandleJoints : MonoBehaviour
 					ownerB.xMotion = ConfigurableJointMotion.Locked;
 					ownerB.yMotion = ConfigurableJointMotion.Locked;
 					ownerB.zMotion = ConfigurableJointMotion.Locked;
+
+				//this is the conditional for Identified wheels
+				}else if((int)nodeX.getJointType() == (int)SkeletalJointType.ROTATIONAL && isWheel == true){
+
+
+
 				}
 				
 			}
 
-			Debug.Log("Alive!");
+			//Debug.Log("Alive!");
 				}
 		//Rigidbody rigidB = rigid.gameObject.AddComponent<Rigidbody> ();
 		//owner.gameObject.AddComponent<Rigidbody> ();
@@ -86,10 +100,6 @@ public class HandleJoints : MonoBehaviour
 		//ownerB.yMotion = ConfigurableJointMotion.Locked;
 		//ownerB.zMotion = ConfigurableJointMotion.Locked;
 		//Debug.Log (jointType);
-	}
-	void Start(){
-		loadBXDJ ();
-
 	}
 
 }
