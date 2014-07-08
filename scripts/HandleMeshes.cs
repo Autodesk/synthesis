bbee26c7d220b412c27ee2122f7690079fa96a06
Pedro Morais <Pedro.de.Faria.Autran.e.Morais@autodesk.com>
2014-07-08 15:37:44 -0700

    Handlemeshes now uses API
    yolo

--- scripts/HandleMeshes.cs
+++ scripts/HandleMeshes.cs
@@ -2,72 +2,68 @@ using UnityEngine;
 using System.Collections;
 using System.IO;
 
-public class HandleMeshes : MonoBehaviour 
+public class HandleMeshes : MonoBehaviour
 {
-	public static void loadBXDA(string filepath, Transform trans)
-	{
-		int vCount;
-		int tCount;
-		
-		BinaryReader bxda = new BinaryReader(File.Open(filepath,FileMode.Open));
-		
-		vCount = bxda.ReadInt32();
-		
-		Vector3[] vertices = new Vector3[vCount];
-		Vector3[] normals = new Vector3[vCount];
-		Color32[] colors = new Color32[vCount];
-		Vector2[] uvs = new Vector2[vCount];
-		
-		for (int i = 0; i < vCount; i++)
-		{
-			vertices[i] = new Vector3((float) bxda.ReadDouble(),(float) bxda.ReadDouble(),(float) bxda.ReadDouble());
-			
-			normals[i] = new Vector3((float) bxda.ReadDouble(),(float) bxda.ReadDouble(),(float) bxda.ReadDouble());
-			
-			colors[i] = new Color32(bxda.ReadByte(),bxda.ReadByte(),bxda.ReadByte(),bxda.ReadByte());
-			
-			uvs[i] = new Vector2((float) bxda.ReadDouble(), (float) bxda.ReadDouble());
-		}
-		
-		tCount = bxda.ReadInt32();
-		int[] triangles = new int[tCount*3];
-		
-		for (int i = 0; i < tCount*3; i++) 
-		{
-			triangles[i] = bxda.ReadInt32();
-		}
-		
-		Mesh mesh = new Mesh ();
-		trans.gameObject.AddComponent("Mesh Filter");
-		trans.gameObject.AddComponent ("MeshRender");
-		trans.GetComponent<MeshFilter>().mesh = mesh;
-		
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-		mesh.normals = normals;
-		mesh.colors32 = colors;
-		mesh.uv = uvs;
-		
-	}
+    public static void loadBXDA(string filepath, Transform trans)
+    {
+        BXDAMesh mesh = new BXDAMesh();
+        mesh.ReadBXDA(filepath);
 
-	public static void attachMeshColliders(Transform parent)
-	{
-		MeshCollider tmp;
-		foreach (Transform child in parent) 
-		{
-			child.gameObject.AddComponent<MeshCollider>();
-			tmp = child.gameObject.GetComponent<MeshCollider>();
-			tmp.convex = true;
-		}
-	}
+        int mCount = mesh.meshes.Count;
 
-	public static void attachRigidBodies(Transform parent)
-	{
-		foreach (Transform child in parent) 
-		{
-			child.gameObject.AddComponent<Rigidbody>();
-		}
-	}
+
+        Init.generateCubes(mCount, trans, trans.name + " Subpart");
+
+        for (int j = 0; j < mCount; j++)
+        {
+            BXDAMesh.BXDASubMesh sub = mesh.meshes[j];
+            GameObject parentCube = trans.gameObject;
+
+            Vector3[] vertices = ArrayUtilities.WrapArray<Vector3>(
+                delegate(double x, double y, double z) { return new Vector3((float)x, (float)y, (float)z); }, sub.verts);
+            Vector3[] normals = ArrayUtilities.WrapArray<Vector3>(
+                delegate(double x, double y, double z) { return new Vector3((float)x, (float)y, (float)z); }, sub.norms);
+            Color32[] colors = ArrayUtilities.WrapArray<Color32>(
+                delegate(byte r, byte g, byte b, byte a) { return new Color32(r, g, b, a); }, sub.colors);
+            Vector2[] uvs = ArrayUtilities.WrapArray<Vector2>(
+                delegate(double x, double y)
+                {
+                    return new Vector2((float)x, (float)y);
+                }, sub.textureCoords);
+
+            Mesh unityMesh = new Mesh();
+
+            Debug.Log(vertices[2]);
+            trans.GetChild(j).gameObject.AddComponent("Mesh Filter");
+            trans.GetChild(j).gameObject.AddComponent("MeshRender");
+            trans.GetChild(j).GetComponent<MeshFilter>().mesh = unityMesh;
+
+            unityMesh.vertices = vertices;
+            unityMesh.triangles = sub.indicies;
+            unityMesh.normals = normals;
+            unityMesh.colors32 = colors;
+            unityMesh.uv = uvs;
+        }
+    }
+
+    public static void attachMeshColliders(Transform parent)
+    {
+        MeshCollider tmp;
+        foreach (Transform child in parent)
+        {
+            child.gameObject.AddComponent<MeshCollider>();
+            tmp = child.gameObject.GetComponent<MeshCollider>();
+            tmp.convex = true;
+        }
+    }
+
+    public static void attachRigidBodies(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            child.gameObject.AddComponent<Rigidbody>();
+        }
+    }
 
 
 }
