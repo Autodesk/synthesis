48962611f98015ad30c773a30f15fdd93ea16664
Westin Miller <westin.miller@autodesk.com>
2014-07-09 16:04:49 -0700

    Version 0.0.3.1

--- SimulatorFileIO/Properties/AssemblyInfo.cs
+++ SimulatorFileIO/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.3.1")]
+[assembly: AssemblyFileVersion("0.0.3.1")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
