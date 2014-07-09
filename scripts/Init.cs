7a86b5a1727fa4e05b5e62be9a3952007e8d6c55
Nathaniel Waggoner <nathan.waggoner@autodesk.com>
2014-07-09 09:12:10 -0700

    New GenCube and overhaul of how files are loaded.
    The generatecubes has added parameters while bxda files are now loaded
    procedurally.

--- scripts/Init.cs
+++ scripts/Init.cs
@@ -1,55 +1,72 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class Init : MonoBehaviour
-{
-	
-		public void generateCubes (int amt)
+{		
+		
+		public static void generateCubes (int amt, Transform parent, string name)
 		{
 				GameObject tmp;
 				for (int i = 0; i < amt; i++) {
 						tmp = GameObject.CreatePrimitive (PrimitiveType.Cube);
-						tmp.transform.parent = this.transform;
+						tmp.transform.parent = parent;
 						tmp.transform.position = new Vector3 (0, 0, 0);
 						Destroy (tmp.collider);
-						tmp.name = "part" + i;
+						tmp.name = name + i;
 				}
+		
+		}
+		//converts custom vector format to unity vector3's
+		public static Vector3 ConvertV3(BXDVector3 vector)	
+		{
+			Vector3 vectorUnity = new Vector3 ((float)vector.x, (float)vector.y, (float)vector.z);
+			return vectorUnity;
 		}
 
 		void Start ()
 		{
-				string[] filepaths = {"C:/Users/t_waggn/Documents/Skeleton/node_0.bxda",
-							  "C:/Users/t_waggn/Documents/Skeleton/node_1.bxda",
-			 				  "C:/Users/t_waggn/Documents/Skeleton/node_2.bxda",
-							  "C:/Users/t_waggn/Documents/Skeleton/node_3.bxda", 
-							  "C:/Users/t_waggn/Documents/Skeleton/node_4.bxda", 
-			 				  "C:/Users/t_waggn/Documents/Skeleton/node_5.bxda",
-							  "C:/Users/t_waggn/Documents/Skeleton/node_6.bxda"};
+				//Now you can use a default directory to load all of the files
+				string path = "C:/Users/t_waggn/Documents/Skeleton/";
+				
+				List<string> filepaths = new List<string>();
+				List<RigidNode_Base> names = new List<RigidNode_Base> ();
+				HandleJoints.genSkeleton ("C:/Users/t_waggn/Documents/Skeleton/skeleton.bxdj", names);
+				foreach (RigidNode_Base node in names) { 
+						filepaths.Add(path + node.modelName);
+						
+				}
 
-				generateCubes (filepaths.Length);
 
-				for (int i = 0; i < filepaths.Length; i++) {
+				//generateCubes (filepaths.Count);
+				
+				for (int i = 0; i < filepaths.Count; i++) {
 						HandleMeshes.loadBXDA (filepaths [i], transform.GetChild (i));
 				}
 				HandleMeshes.attachMeshColliders (transform);
-				HandleJoints.loadBXDJ ("C:/Users/t_waggn/Documents/Skeleton/skeleton.bxdj", transform);
+				HandleJoints.loadBXDJ (transform);
 				//HandleMeshes.attachRigidBodies (transform);
 
+				
 
 
 		}
-	/*
+
 		void FixedUpdate ()
 		{
-
+		/*
 				for (int i = 7; i < 10; i++) {
 						WheelCollider tmp = transform.GetChild (i).GetComponent<WheelCollider> ();
 						tmp.motorTorque = Input.GetAxis ("Vertical") * 5 - Input.GetAxis ("Horizontal") * 5;
 				}
-				for (int i = 10; i < 13; i++) {
+				for (int i = 10; i < 12; i++) {
 						WheelCollider tmp = transform.GetChild (i).GetComponent<WheelCollider> ();
 						tmp.motorTorque = Input.GetAxis ("Vertical") * 5 + Input.GetAxis ("Horizontal") * 5;
 				}
-		}
 */
 }
+		
+
+}
+
