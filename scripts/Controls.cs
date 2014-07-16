3fc51504d9d525b467c94a8c426661c11707fc84
Jacob Paul Crisan <jacob.crisan@autodesk.com>
2014-07-16 16:05:04 -0700

    Temporary Drive code and Error Handling
    I created a file called 'FakeInit.cs' which is meant to contain some
    simple drive code that other members of the Unity team can access (it is
    by no means a permanent thing, however, because it may change or may not
    be used at all because the user will upload there own drivetrain code).
    I also created a file called ErrorStuff.cs. It, like its name, is very
    simple and is meant to hold any custom exceptions the Unity team wants
    to make. At the moment, it has a PWMAssignedException, which is meant to
    signify that a PWM port that the user attempts to sign has already been
    used already (see the PWM port dictionary in Controls.cs). I did have a
    problem, however, with one of the wheels. I don't know if its the wheel
    collider, but one of the configurable joints rotates a wheel backwards.
    I assume it was a bug and that it will be fixed in the coming updates. I
    am 50% confident that the wheel collider was not reversed, however. I
    also updated Controls.cs to make FakeInit work correctly.
    
    Note: You can always use the "old reliable" drivetrain code if the
    equation method does not work.

--- scripts/Controls.cs
+++ scripts/Controls.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using ErrorHandling;
 
-public class Controls : MonoBehaviour 
+public class Controls : MonoBehaviour
 {
-        // For four wheel drive - might be outdated
-        private static KeyCode forward;
-        private static KeyCode backward;
-        private static KeyCode left;
-        private static KeyCode right;
+		// For four wheel drive - might be outdated
+		public static KeyCode forward;
+		public static KeyCode backward;
+		public static KeyCode left;
+		public static KeyCode right;
 
-        /*  This is a bit complicated.
+		/*  This is a bit complicated.
             In order for Unity to recognize input, the user has to press a keyboard button that corresponds to a KeyCode
             This actually makes it harder for the user to select custom keys.
             For example, what if you want to set the F key to forward?
@@ -25,80 +26,92 @@ public class Controls : MonoBehaviour
 
             Credit for this one goes to Westin Miller (Double check with him if I get this explanation wrong)
         */
-        public static KeyCode GetKeyCodeFormString(string userInput) 
-        {
-                // Firstly, we are going to grab an array of all of the different KeyCode objects
-                System.Array codes = System.Enum.GetValues (typeof(KeyCode));
+		public static KeyCode GetKeyCodeFormString (string userInput)
+		{
+				// Firstly, we are going to grab an array of all of the different KeyCode objects
+				System.Array codes = System.Enum.GetValues (typeof(KeyCode));
                 
-                // for example, it will create a string "Enter").
-                // It then compares it to the input the user typed in.
-                foreach (KeyCode code in codes) {
-                        // If the name of the object matches the user string, it will return the KeyCode object that we need.
-                        if (System.Enum.GetName (typeof(KeyCode), code).Equals (userInput)) 
-                        {
-                                return code;
-                        }
-                }
-                return KeyCode.None;
-        }
+				// for example, it will create a string "Enter").
+				// It then compares it to the input the user typed in.
+				foreach (KeyCode code in codes) {
+						// If the name of the object matches the user string, it will return the KeyCode object that we need.
+						if (System.Enum.GetName (typeof(KeyCode), code).Equals (userInput)) {
+								return code;
+						}
+				}
+				return KeyCode.None;
+		}
 
 
-        // Allows you to set custom buttons, but sadly, this is not dynamic yet, and it based on a basic forward, back, left, and right principal.
-        public static void setControls(string vForward, string vBackward, string vLeft, string vRight) 
-        {
-                forward = GetKeyCodeFormString(vForward);
-                backward = GetKeyCodeFormString(vBackward);
-                left = GetKeyCodeFormString(vLeft);
-                right = GetKeyCodeFormString(vRight);
+		// Allows you to set custom buttons, but sadly, this is not dynamic yet, and it based on a basic forward, back, left, and right principal.
+		public static void setControls (string vForward, string vBackward, string vLeft, string vRight)
+		{
+				forward = GetKeyCodeFormString (vForward);
+				backward = GetKeyCodeFormString (vBackward);
+				left = GetKeyCodeFormString (vLeft);
+				right = GetKeyCodeFormString (vRight);
 
-                // Will be used to make sure that all of the variables are assigned correctly
-                Dictionary<string, KeyCode> temp = new Dictionary<string, KeyCode>(){{"forward", forward}, {"backward", backward}, {"left", left}, {"right", right}};
+				// Will be used to make sure that all of the variables are assigned correctly
+				Dictionary<string, KeyCode> temp = new Dictionary<string, KeyCode> (){{"forward", forward}, {"backward", backward}, {"left", left}, {"right", right}};
 
-                // If any of the keys are not assigned correctly (that is, they have no set value), it print an error message to the console
-                foreach (KeyValuePair<string, KeyCode> key in temp) {
-                        if (key.Value == KeyCode.None) {
-                                Debug.Log ("Error, Key "  + key.Key + " was not set correctly");
-                        }
-                }
-        }
+				// If any of the keys are not assigned correctly (that is, they have no set value), it print an error message to the console
+				foreach (KeyValuePair<string, KeyCode> key in temp) {
+						if (key.Value == KeyCode.None) {
+								Debug.Log ("Error, Key " + key.Key + " was not set correctly");
+						}
+				}
+		}
 
-        // Combs through all of the skeleton data and finds the pwm port numbers that each wheelCollider in the skeleton is attatched to. It is returned in the form of a dictionary with an integer as its key, and a List of wheelColliders as its value
-        // So you can reference each motor easily using syntax like this: wheelDictionary[1][0].motorTorque = 5 (or something along those lines)
-        public static Dictionary<int, List<WheelCollider>> assignMotors(RigidNode_Base skeleton) 
-        {
-                // This dictionary will hold all of the data we gather from the skeleton and store it
-                Dictionary<int, List<WheelCollider>> functionOutput = new Dictionary<int, List<WheelCollider>>();
+		public static void setMotor (UnityRigidNode wheel, float speed)
+		{
+				wheel.GetWheelCollider ().motorTorque = speed;
+				wheel.GetConfigJoint ().targetAngularVelocity = new Vector3 (wheel.GetWheelCollider ().rpm * 6 * Time.deltaTime, 0, 0);
+		}
 
-                // We will need this to grab and comb through all of the nodes (oarts) or the skeleton
-                List<RigidNode_Base> listAllNodes = new List<RigidNode_Base> ();
-                skeleton.ListAllNodes (listAllNodes);
+		public static void setSetOfMotors (Dictionary<int, UnityRigidNode> dictionary, int speed, params int[] pwmPorts)
+		{
+				foreach (int pwmPort in pwmPorts) {
+						setMotor (dictionary [pwmPort], speed);
+				}
+		}
 
-                foreach (RigidNode_Base dropTheBase in listAllNodes) 
-                {
-                        // Unity rigid nodes have the functions we need (it inherits from RigidNodeBase so we can typecast it)
-                        UnityRigidNode unityNode = (UnityRigidNode)dropTheBase;
+		// Combs through all of the skeleton data and finds the pwm port numbers of each wheel (by looking through the UnityRigidNode that it is attatched to)
+		public static Dictionary<int, UnityRigidNode> assignMotors (RigidNode_Base skeleton)
+		{
+				// This dictionary will hold all of the data we gather from the skeleton and store it
+				Dictionary<int, UnityRigidNode> functionOutput = new Dictionary<int, UnityRigidNode> ();
+
+				// We will need this to grab and comb through all of the nodes (parts) or the skeleton
+				List<RigidNode_Base> listAllNodes = new List<RigidNode_Base> ();
+				skeleton.ListAllNodes (listAllNodes);
+
+				foreach (RigidNode_Base dropTheBase in listAllNodes) {
+						// UnityRigidNodes have the functions we need (it inherits from RigidNodeBase so we can typecast it)
+						UnityRigidNode unityNode = (UnityRigidNode)dropTheBase;
                        
-                        // If it finds a wheelCollider
-                        if (unityNode.GetWheelCollider() != null) 
-                        {
-                                // As I hear on the internet: "It is easier to ask for forgiveness than it is to ask for permisson" (A.K.A: try-catch = more efficient than if-else)
-                                try 
-                                {
-                                        // It will assume that there is already an entry for the pwm port of the wheelCollider and try to add the wheel collider to the list of wheelColliders in the dictionary entry
-                                        functionOutput[unityNode.GetPortA()].Add(unityNode.GetWheelCollider());
-                                }
-                                catch 
-                                {
-                                        // If it does not work, it will create an entry for the pwm port and then add the wheelCollider to the list in the dictionary entry
-                                        functionOutput.Add(unityNode.GetPortA(), new List<WheelCollider>());
-                                        functionOutput[unityNode.GetPortA()].Add(unityNode.GetWheelCollider());
-                                }
-                        }
-                }
+						// If it finds a wheelCollider
+						if (unityNode.GetWheelCollider () != null) {
+								// The code will now attempt to assign each unityNode containing a wheelCollider a unique PWM port value. If it does not, however, it will throw an exception (see ErrorStuff.CS)
+								try {
+										// If its a unique port, we won't have problems
+										if (!functionOutput.ContainsKey (unityNode.GetPortA ())) {
+												functionOutput.Add (unityNode.GetPortA (), unityNode);
+												Debug.Log (unityNode.GetPortA ());
+												Debug.Log (string.Format ("Wheel Name: {0}, PWM Port {1}.", unityNode.GetWheelCollider ().name, unityNode.GetPortA ()));
 
-                // Finally, it is done
-                return functionOutput;
-        }
+												// If it isn't however, the user will have issues
+										} else {
+												throw new PWMAssignedException (string.Format ("Error, PWM port {0} has already been assigned! (A.K.A: your trying to break the rules)", unityNode.GetPortA ()));
+										}
+								} catch (PWMAssignedException e) {
+										Debug.Log (e);
+								}
+						}
+				}
+
+				// Finally, it is done
+				return functionOutput;
+		}
 
 
 }
---
+++ scripts/FakeInit.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class FakeInit : MonoBehaviour
+{		
+
+	public Dictionary<int, UnityRigidNode> PWMAssignments = new Dictionary<int, UnityRigidNode> ();
+	public float speed = 10;
+	
+	void Start ()
+	{
+		string homePath = (System.Environment.OSVersion.Platform == PlatformID.Unix || System.Environment.OSVersion.Platform == PlatformID.MacOSX) ? System.Environment.GetEnvironmentVariable ("HOME") : System.Environment.ExpandEnvironmentVariables ("%HOMEDRIVE%%HOMEPATH%");
+		//Now you can use a default directory to load all of the files
+		Directory.CreateDirectory (homePath + "/Documents/Skeleton/Skeleton");
+		string path = homePath + "/Documents/Skeleton/Skeleton/";
+		
+		
+		List<RigidNode_Base> names = new List<RigidNode_Base> ();
+		RigidNode_Base.NODE_FACTORY = new UnityRigidNodeFactory ();
+		RigidNode_Base skeleton = BXDJSkeleton.ReadSkeleton (homePath + "/Documents/Skeleton/Skeleton/skeleton.bxdj");
+		
+		skeleton.ListAllNodes (names);
+		foreach (RigidNode_Base node in names) {
+			UnityRigidNode uNode = (UnityRigidNode)node;
+			uNode.CreateTransform (transform);
+			transform.localScale = new Vector3 (0.01f, 0.01f, 0.01f);			
+			uNode.CreateMesh (path + uNode.GetModelFileName ());
+			uNode.CreateJoint ();
+			
+			
+			
+			
+		}
+		transform.Rotate (new Vector3 (-90.0f, 0.0f, 0.0f));
+		PWMAssignments = Controls.assignMotors (skeleton);
+		
+		Controls.setControls ("W", "S", "A", "D");
+		
+	}
+	
+	void FixedUpdate ()
+	{
+
+		// Old Reliable
+		/*if (Input.anyKey) {
+			if (Input.GetKey (Controls.forward)) {
+				Controls.setMotor (PWMAssignments [1], speed);
+				Controls.setMotor (PWMAssignments [2], speed);
+				Controls.setMotor (PWMAssignments [3], speed);
+				Controls.setMotor (PWMAssignments [4], speed);
+			}
+			if (Input.GetKey (Controls.backward)) {
+				Controls.setMotor (PWMAssignments [1], -speed);
+				Controls.setMotor (PWMAssignments [2], -speed);
+				Controls.setMotor (PWMAssignments [3], -speed);
+				Controls.setMotor (PWMAssignments [4], -speed);
+			}
+			if (Input.GetKey (Controls.left)) {
+				Controls.setMotor (PWMAssignments [4], -speed);
+				Controls.setMotor (PWMAssignments [1], -speed);
+				Controls.setMotor (PWMAssignments [2], speed);
+				Controls.setMotor (PWMAssignments [3], speed);
+			}
+			if (Input.GetKey (Controls.right)) {
+				Controls.setMotor (PWMAssignments [4], speed);
+				Controls.setMotor (PWMAssignments [1], speed);
+				Controls.setMotor (PWMAssignments [2], -speed);
+				Controls.setMotor (PWMAssignments [3], -speed);
+			}
+		} else {
+			Controls.setMotor (PWMAssignments [1], 0);
+			Controls.setMotor (PWMAssignments [2], 0);
+			Controls.setMotor (PWMAssignments [3], 0);
+			Controls.setMotor (PWMAssignments [4], 0);
+		}*/
+
+		// Using Equations
+		Controls.setMotor (PWMAssignments[4], -10*(Input.GetAxis("Vertical")+Input.GetAxis("Horizontal")));
+		Controls.setMotor (PWMAssignments[3], 10*(Input.GetAxis("Vertical")-Input.GetAxis("Horizontal")));
+		Controls.setMotor (PWMAssignments[1], 10*(Input.GetAxis("Vertical")+Input.GetAxis("Horizontal")));
+		Controls.setMotor (PWMAssignments[2], 10*(Input.GetAxis("Vertical")-Input.GetAxis("Horizontal")));			}
+		
+	}
+}
+
+
+}
+
