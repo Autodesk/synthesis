ab5cff70bb2c9d27d6b4a782ec1f7734cc8460da
Westin Miller <westin.miller@autodesk.com>
2014-07-21 13:14:10 -0700

    Edge decimation
    Edge decimation is much better.

--- JointResolver-Rev2/Exporter/ConvexHullCalculator.cs
+++ JointResolver-Rev2/Exporter/ConvexHullCalculator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Yay or nay to fancy things
+#define FANCY_SIMPLIFY_GRAPHICS
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +12,7 @@ using System.Collections;
 
 public class ConvexHullCalculator
 {
+
     public static List<BXDAMesh.BXDASubMesh> GetHull(BXDAMesh mesh, bool decompose = false)
     {
         return GetHull(mesh.meshes, decompose);
@@ -35,70 +39,39 @@ public class ConvexHullCalculator
     private class SimplificationFace : IComparable<SimplificationFace>
     {
         public SimplificationVertex[] verts = new SimplificationVertex[3];
-        public float area;
-        public float partnerNorms;
-        public float[] norm;
-
-        public bool containsVert(SimplificationVertex v)
-        {
-            foreach (SimplificationVertex j in verts)
-            {
-                if (j == v)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+        public float[] edgeLengths = new float[3];
+        public int minEdge = 0;
+        public int tmpHead;
 
         public void updateInfo()
         {
-            // Calc area
-            float[] p1 = new float[] { verts[0].pos[0] - verts[1].pos[0], verts[0].pos[0] - verts[1].pos[1], verts[0].pos[2] - verts[1].pos[2] };
-            float[] p2 = new float[] { verts[2].pos[0] - verts[1].pos[0], verts[2].pos[0] - verts[1].pos[1], verts[2].pos[2] - verts[1].pos[2] };
-            float[] cross = new float[]{(p1[1]*p2[2])-(p1[2]*p2[1]),
-                (p1[2]*p2[0])-(p1[0]*p2[2]),
-                (p1[0]*p2[1])-(p1[1]*p2[0])};
-            float normMag = (float) Math.Sqrt((cross[0] * cross[0]) + (cross[1] * cross[1]) + (cross[2] * cross[2]));
-            area = normMag;
-            norm = cross;
-            norm[0] /= normMag;
-            norm[1] /= normMag;
-            norm[2] /= normMag;
-        }
-
-        public void updateNeighbors()
-        {
-            partnerNorms = 0.05f;    // Every time you divide by zero you kill a kitten
-            List<SimplificationFace> yay = new List<SimplificationFace>();
-            for (int v = 0; v < 3; v++)
+            float best = 999999999;
+            for (int i = 0; i < 3; i++)
             {
-                foreach (SimplificationFace face in verts[v].faces)
+                SimplificationVertex a = verts[i];
+                SimplificationVertex b = verts[(i + 1) % verts.Length];
+                float dx = a.pos[0] - b.pos[0];
+                float dy = a.pos[1] - b.pos[1];
+                float dz = a.pos[2] - b.pos[2];
+                edgeLengths[i] = (dx * dx) + (dy * dy) + (dz * dz);
+                if (edgeLengths[i] < best)
                 {
-                    for (int v2 = 0; v2 < 3 && face != this; v2++)
-                    {
-                        if (v2 != v && face.verts[v2].faces.Contains(this))
-                        {
-                            yay.Add(face);
-                        }
-                    }
+                    best = edgeLengths[i];
+                    minEdge = i;
                 }
-            }
-            foreach (SimplificationFace ff in yay)
-            {
-                // Dot them!
-                partnerNorms += Math.Abs((ff.norm[0] * norm[0]) + (ff.norm[1] * norm[1]) + (ff.norm[2] * norm[2]));
             }
         }
 
         int IComparable<SimplificationFace>.CompareTo(SimplificationFace other)
         {
             // The best ones have coplanar neighbors.
-            return (area/partnerNorms).CompareTo(area / other.partnerNorms);
+            return edgeLengths[minEdge].CompareTo(other.edgeLengths[other.minEdge]);
         }
     }
+
     private static void Simplify(ref float[] verts, ref uint vertCount, ref uint[] inds, ref  uint trisCount)
     {
+        // Setup edge relations, compute min edge lengths...
         List<SimplificationVertex> simplVerts = new List<SimplificationVertex>();
         for (int i = 0; i < vertCount; i++)
         {
@@ -121,37 +94,144 @@ public class ConvexHullCalculator
         {
             face.updateInfo();
         }
-        foreach (SimplificationFace face in simplFace)
-        {
-            face.updateNeighbors();
-        }
 
         simplFace.Sort();
         Console.WriteLine("Starts at " + simplFace.Count);
-        // Time for shenanigans!  We are going to naively pick the smallest triangle and then pretend it isn't there.
-        for (int i = 0; i < 2000 && simplFace.Count > 50; i++)
-        {
-            Console.WriteLine("Simply from " + simplFace.Count);
-            SimplificationFace remove = simplFace[0];
-            float[] center = new float[3];
-            foreach (SimplificationVertex vert in remove.verts)
+
+        #region FANCY_SIMPLIFY_GRAPHICS
+#if FANCY_SIMPLIFY_GRAPHICS
+        double[] major, minor;
+        GraphicsCoordinateSet majorCoordSet, minorCoordSet;
+
+            major = new double[9 * simplFace.Count];
+            for (int a = 0; a < simplFace.Count; a++)
             {
-                center[0] += vert.pos[0] / 3.0f;
-                center[1] += vert.pos[1] / 3.0f;
-                center[2] += vert.pos[2] / 3.0f;
+                int off = a * 9;
+                simplFace[a].tmpHead = off;
+                Array.Copy(simplFace[a].verts[0].pos, 0, major, off, 3);
+                Array.Copy(simplFace[a].verts[1].pos, 0, major, off + 3, 3);
+                Array.Copy(simplFace[a].verts[2].pos, 0, major, off + 6, 3);
+            }
+            minor = new double[9];
+            AssemblyDocument part = ((AssemblyDocument) Program.INVENTOR_APPLICATION.ActiveDocument);
+            GraphicsDataSets dataSets;
+            try
+            {
+                dataSets = part.GraphicsDataSetsCollection.AddNonTransacting("convexHULL");
+            }
+            catch
+            {
+                dataSets = part.GraphicsDataSetsCollection["convexHULL"];
+                dataSets.Delete();
+                dataSets = part.GraphicsDataSetsCollection.AddNonTransacting("convexHULL");
+            }
+            ClientGraphics graphics;
+            try
+            {
+                graphics = part.ComponentDefinition.ClientGraphicsCollection.AddNonTransacting("convexHULL");
+            }
+            catch
+            {
+                graphics = part.ComponentDefinition.ClientGraphicsCollection["convexHULL"];
+                graphics.Delete();
+                graphics = part.ComponentDefinition.ClientGraphicsCollection.AddNonTransacting("convexHULL");
+            }
+            {
+                GraphicsNode node = graphics.AddNode(graphics.Count + 1);
+                majorCoordSet =
+                dataSets.CreateCoordinateSet(dataSets.Count + 1);
+                majorCoordSet.PutCoordinates(major);
+                LineGraphics primitive = node.AddLineGraphics();
+                primitive.BurnThrough = false;
+                //Create Coordinate Index Set 
+                GraphicsIndexSet indexSetCoords =
+                dataSets.CreateIndexSet(dataSets.Count + 1);
+                for (int i = 0; i < major.Length; i += 3)
+                {
+                    indexSetCoords.Add(indexSetCoords.Count + 1, i + 1); //from point 1 
+                    indexSetCoords.Add(indexSetCoords.Count + 1, i + 2); //connect to point 2 
+                    indexSetCoords.Add(indexSetCoords.Count + 1, i + 2); //from point 2 
+                    indexSetCoords.Add(indexSetCoords.Count + 1, i + 3); //connect to point 3 
+                    indexSetCoords.Add(indexSetCoords.Count + 1, i + 3); //from point 3 
+                    indexSetCoords.Add(indexSetCoords.Count + 1, i + 1); //connect to point 1
+                }
+                primitive.CoordinateSet = majorCoordSet;
+                primitive.CoordinateIndexSet = indexSetCoords;
+            }
+            {
+                GraphicsNode node = graphics.AddNode(graphics.Count + 1);
+                minorCoordSet =
+                dataSets.CreateCoordinateSet(dataSets.Count + 1);
+                minorCoordSet.PutCoordinates(minor);
+                TriangleGraphics primitive = node.AddTriangleGraphics();
+                primitive.DepthPriority = 19999;
+                //Create Coordinate Index Set 
+                GraphicsColorSet colorSet = dataSets.CreateColorSet(dataSets.Count + 1);
+                for (int i = 0; i < minor.Length; i += 3)
+                {
+                    colorSet.Add(colorSet.Count + 1, 255, 0, 0);
+                }
+                primitive.CoordinateSet = minorCoordSet;
+                primitive.ColorSet = colorSet;
+                primitive.ColorBinding = ColorBindingEnum.kPerItemColors;
+                //primitive.CoordinateIndexSet = indexSetCoords;
+            }
+            Program.INVENTOR_APPLICATION.ActiveView.Update();
+#endif
+        #endregion
+
+        // Time for shenanigans!  We are going to naively pick the shortest edge and then remove it.  Destroys two faces per loop typically.
+        // Give it 2000 tries at most.
+        for (int i = 0; i < 2000 && simplFace.Count > 250; i++)
+        {
+            SimplificationFace bFace = simplFace[0];
+            // This is the edge to remove
+            SimplificationVertex[] remove = new SimplificationVertex[] {
+                bFace.verts[bFace.minEdge], bFace.verts[(bFace.minEdge + 1) % bFace.verts.Length] };
+
+           
+        #region FANCY_SIMPLIFY_GRAPHICS
+#if FANCY_SIMPLIFY_GRAPHICS
+                // Highlight
+                Array.Copy(bFace.verts[0].pos, 0, minor, 0, 3);
+                Array.Copy(bFace.verts[1].pos, 0, minor, 3, 3);
+                Array.Copy(bFace.verts[2].pos, 0, minor, 6, 3);
+                minorCoordSet.PutCoordinates(minor);
+                Program.INVENTOR_APPLICATION.ActiveView.Update();
+#endif
+            #endregion
+
+            // Find the center point of the edge.  One edge -> one vertex
+            float[] center = new float[3];
+            foreach (SimplificationVertex vert in remove)
+            {
+                center[0] += vert.pos[0] / 2.0f;
+                center[1] += vert.pos[1] / 2.0f;
+                center[2] += vert.pos[2] / 2.0f;
             }
             SimplificationVertex newVertex = new SimplificationVertex(center[0], center[1], center[2]);
+
+            // Ineffeciently check each face to see if it shares a vertex with the edge
             for (int k = simplFace.Count - 1; k >= 0; k--)
             {
                 int matched = 0;
                 SimplificationFace face = simplFace[k];
                 for (int j = 0; j < face.verts.Length; j++)
                 {
-                    if (face.verts[j] == remove.verts[0] ||
-                        face.verts[j] == remove.verts[1] ||
-                        face.verts[j] == remove.verts[2])
+                    if (face.verts[j] == remove[0] ||
+                        face.verts[j] == remove[1])
                     {
                         face.verts[j] = newVertex;
+
+                        #region FANCY_SIMPLIFY_GRAPHICS
+#if FANCY_SIMPLIFY_GRAPHICS
+                            Array.Copy(face.verts[j].pos, 0, major, face.tmpHead + (3 * j), 3);
+                            int index = (face.tmpHead / 3) + j + 1;
+                            majorCoordSet.Remove(index);
+                            majorCoordSet.Add(index, Program.INVENTOR_APPLICATION.TransientGeometry.CreatePoint(face.verts[j].pos[0], face.verts[j].pos[1], face.verts[j].pos[2]));
+#endif
+                        #endregion
+
                         if (matched == 0)
                         {
                             newVertex.faces.Add(face);
@@ -159,6 +239,8 @@ public class ConvexHullCalculator
                         matched++;
                     }
                 }
+                // If we share at least two verts with the edge we are dead, since the triangle ends up with two
+                // of the same vertex
                 if (matched >= 2)
                 {
                     // Degenerate
@@ -170,44 +252,36 @@ public class ConvexHullCalculator
                 }
                 else if (matched == 1)
                 {
+                    // We changed, so update edge lengths
                     face.updateInfo();
                 }
             }
             simplVerts.Add(newVertex);
-            foreach (SimplificationFace face in simplFace)
-            {
-                face.updateNeighbors();
-            }
+            // Resort by edge length
             simplFace.Sort();
         }
 
         simplVerts.RemoveAll((vert) => vert.faces.Count <= 0);
 
         // Rebuild arrays
-        System.IO.StreamWriter writer = new System.IO.StreamWriter(new System.IO.FileStream("C:/Temp/test.obj", System.IO.FileMode.Create));
-
         vertCount = (uint) simplVerts.Count;
         verts = new float[vertCount * 3];
         for (int i = 0; i < simplVerts.Count; i++)
         {
             int off = i * 3;
-            simplVerts[i].finalIndex = (uint) (i + 1);
+            simplVerts[i].finalIndex = (uint) (i + 1);  // We chose to make indicies 1-based?  What type of programmers are we?
             Console.WriteLine(simplVerts[i].finalIndex);
             Array.Copy(simplVerts[i].pos, 0, verts, off, 3);
-            writer.WriteLine("v " + simplVerts[i].pos[0] + " " + simplVerts[i].pos[1] + "  " + simplVerts[i].pos[2]);
         }
         trisCount = (uint) simplFace.Count;
         inds = new uint[trisCount * 3];
-        for (int i = 0; i < simplFace.Count; i++ )
+        for (int i = 0; i < simplFace.Count; i++)
         {
             int off = i * 3;
             inds[off] = simplFace[i].verts[0].finalIndex;
-            inds[off+1] = simplFace[i].verts[1].finalIndex;
-            inds[off+2] = simplFace[i].verts[2].finalIndex;
-            writer.WriteLine("f " + inds[off] + "  " + inds[off+1] + "  " + inds[off+2]);
+            inds[off + 1] = simplFace[i].verts[1].finalIndex;
+            inds[off + 2] = simplFace[i].verts[2].finalIndex;
         }
-
-        writer.Close();
     }
 
     private static BXDAMesh.BXDASubMesh ExportMeshInternal(float[] verts, uint vertCount, uint[] inds, uint trisCount)
