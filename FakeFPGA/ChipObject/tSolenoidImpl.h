d61df7f1005ec6d43a161159c0a305e7b8254891
Westin Miller <westin.miller@autodesk.com>
2014-07-25 13:48:25 -0700

    tSolenoid backed by shared memory.

--- FakeFPGA/ChipObject/tSolenoidImpl.h
+++ FakeFPGA/ChipObject/tSolenoidImpl.h
@@ -15,9 +15,15 @@ namespace nFPGA {
 
 	class tSolenoid_Impl: public nFPGA::nFRC_2012_1_6_4::tSolenoid {
 	private:
+#pragma region ADDRESSES
+		static const int kDO7_0_ElementSize = 8;
+		static const int kDO7_0_ElementMask = 0xFF;
+		static const int kSolenoid_DO7_0_Address = 0x8144;
+#pragma endregion
+	private:
 		NiFpgaState *state;
 	public:
-		unsigned char solenoidState[kNumDO7_0Elements];
+		uint32_t *solenoidState;
 
 		tSolenoid_Impl(NiFpgaState *state);
 		virtual tSystemInterface* getSystemInterface();
