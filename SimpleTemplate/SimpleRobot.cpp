bcf3e78664fa839d85e40bb0453d4db5c8222fc8
Westin Miller <westin.miller@autodesk.com>
2014-07-29 17:03:17 -0700

    Massive testing
    All things seem to be working.
    NOTE:  If two channels collide a WPIError is thrown.  This seems to make
    the program self destruct.
    This is issue #4.

--- SimpleTemplate/SimpleRobot.cpp
+++ SimpleTemplate/SimpleRobot.cpp
@@ -1,4 +1,5 @@
 #include <WPILib.h>
+#include <AnalogPotentiometer.h>
 #include <emulator.h>
 
 
@@ -10,6 +11,32 @@ private:
 	Joystick joy;
 public:
 	RobotDemo(void): drive(1,2), joy(1) {
+		// Does it all work?
+		(new Accelerometer(2))->GetAcceleration();
+		(new AnalogChannel(4))->GetValue();
+		(new AnalogPotentiometer(3))->Get();
+		(new AnalogTrigger(6))->GetTriggerState();
+		// AnalogTriggerOutput can't be created
+		(new Compressor(1,1))->GetPressureSwitchValue();
+		(new Counter(2))->Get();
+		// Dashboard can't be created
+		(new DigitalInput(3))->Get();
+		(new DigitalOutput(4))->Set(1);
+		// DigitalSource can't be created
+		(new DoubleSolenoid(1,2))->Set(DoubleSolenoid::kForward);
+		// DriverStation* can't be created
+		(new Encoder(5,6))->GetRaw();
+		(new GearTooth(7))->Get();
+		(new Gyro(1))->GetAngle();
+		(new Jaguar(5))->Set(5);
+		(new Joystick(1))->GetRawButton(1);
+		(new Relay(2))->Set(Relay::kForward);
+		(new Servo(6))->Set(1);
+		(new Solenoid(3))->Set(true);
+		(new Talon(3))->Set(1);
+		(new Timer())->Get();
+		(new Ultrasonic(8,9))->GetRangeMM();
+		(new Victor(4))->Set(2);
 	}
 	void Autonomous(void) {
 		printf("Entering autonomous!\n");
