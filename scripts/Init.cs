e4163cbe2cb2442b24e65adb844631c1fcfe1db8
Pedro Morais <Pedro.de.Faria.Autran.e.Morais@autodesk.com>
2014-08-08 09:01:16 -0700

    Added camera functionality and dynamic importation

--- scripts/Init.cs
+++ scripts/Init.cs
@@ -3,6 +3,8 @@ using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Windows.Forms;
+
 public class Init : MonoBehaviour
 {		
 	// We will need these
@@ -13,8 +15,7 @@ public class Init : MonoBehaviour
 	RigidNode_Base skeleton;
 	unityPacket udp = new unityPacket();
 	List<Vector3> unityWheelData = new List<Vector3>();
-	
-	
+
 	public enum WheelPositions
 	{
 		FL = 1,
@@ -23,37 +24,48 @@ public class Init : MonoBehaviour
 		BR = 4
 	}
 
+	[STAThread]
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (10, 10, 90, 30), "Load Model")) 
+		{
+			String filePath;
+			
+			FolderBrowserDialog fbd = new FolderBrowserDialog ();
+			
+			
+			if (DialogResult.OK == fbd.ShowDialog ()) 
+			{
+				filePath = fbd.SelectedPath;
 
+				List<RigidNode_Base> names = new List<RigidNode_Base>();
+				RigidNode_Base.NODE_FACTORY = new UnityRigidNodeFactory();
+				skeleton = BXDJSkeleton.ReadSkeleton(filePath + "/skeleton.bxdj");
+				skeleton.ListAllNodes(names);
+				foreach (RigidNode_Base node in names)
+				{
+					UnityRigidNode uNode = (UnityRigidNode)node;
+					
+					uNode.CreateTransform(transform);		
+					uNode.CreateMesh(filePath +"/" + uNode.modelFileName);
+					uNode.FlipNorms();
+					uNode.CreateJoint();
+					
+					if (uNode.IsWheel)
+					{
+						unityWheelData.Add(uNode.GetWheelCenter());
+					}
+				}
+				Quaternion rotation = auxFunctions.FlipRobot(unityWheelData, transform);
+				transform.localRotation *= rotation;
+				GameObject.Find("Camera").AddComponent<Camera>();
+			}
+		}
+	}
 
 	void Start()
 	{
 		Physics.gravity = new Vector3(0,-980f,0);
-		string homePath = (System.Environment.OSVersion.Platform == PlatformID.Unix || System.Environment.OSVersion.Platform == PlatformID.MacOSX) ? System.Environment.GetEnvironmentVariable("HOME") : System.Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
-		//Now you can use a default directory to load all of the files
-		Directory.CreateDirectory(homePath + "/Documents/Skeleton/Skeleton/");
-		string path = homePath + "/Documents/Skeleton/Skeleton/";
-				
-	
-		List<RigidNode_Base> names = new List<RigidNode_Base>();
-		RigidNode_Base.NODE_FACTORY = new UnityRigidNodeFactory();
-		skeleton = BXDJSkeleton.ReadSkeleton(homePath + "/Documents/Skeleton/Skeleton/skeleton.bxdj");
-		skeleton.ListAllNodes(names);
-		foreach (RigidNode_Base node in names)
-		{
-			UnityRigidNode uNode = (UnityRigidNode)node;
-
-			uNode.CreateTransform(transform);		
-			uNode.CreateMesh(path + uNode.modelFileName);
-			uNode.FlipNorms();
-			uNode.CreateJoint();
-			
-			if (uNode.IsWheel)
-			{
-				unityWheelData.Add(uNode.GetWheelCenter());
-			}
-		}
-		Quaternion rotation = auxFunctions.FlipRobot(unityWheelData, transform);
-		transform.localRotation *= rotation;
 		//byte test = (byte)2;		
 		//DriveJoints.updateSolenoids(skeleton, test);
 	}
