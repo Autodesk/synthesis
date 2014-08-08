c94fcb9e76c4ff5e5c7fe1f15de02f56d3f340c8
Jacob Paul Crisan <jacob.crisan@autodesk.com>
2014-08-08 13:46:24 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	scripts/UnityRigidNode.cs

--- scripts/Init.cs
+++ scripts/Init.cs
@@ -27,7 +27,25 @@ public class Init : MonoBehaviour
 	[STAThread]
 	void OnGUI ()
 	{
+
+		Physics.gravity = new Vector3(0,-9.8f,0);
+		string homePath = (System.Environment.OSVersion.Platform == PlatformID.Unix || System.Environment.OSVersion.Platform == PlatformID.MacOSX) ? System.Environment.GetEnvironmentVariable("HOME") : System.Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+		//Now you can use a default directory to load all of the files
+		Directory.CreateDirectory(homePath + "/Documents/Skeleton/Skeleton/");
+		string path = homePath + "/Documents/Skeleton/Skeleton/";
+				
+	
+		List<RigidNode_Base> names = new List<RigidNode_Base>();
+		RigidNode_Base.NODE_FACTORY = delegate()
+		{
+			return new UnityRigidNode();
+		};
+		skeleton = BXDJSkeleton.ReadSkeleton(homePath + "/Documents/Skeleton/Skeleton/skeleton.bxdj");
+		skeleton.ListAllNodes(names);
+		foreach (RigidNode_Base node in names)
+
 		if (GUI.Button (new Rect (10, 10, 90, 30), "Load Model")) 
+
 		{
 			String filePath;
 			FolderBrowserDialog fbd = new FolderBrowserDialog ();
@@ -37,7 +55,6 @@ public class Init : MonoBehaviour
 			{
 				filePath = fbd.SelectedPath;
 
-				List<RigidNode_Base> names = new List<RigidNode_Base>();
 				RigidNode_Base.NODE_FACTORY = delegate() {
 					return new UnityRigidNode();
 				};
@@ -85,6 +102,9 @@ public class Init : MonoBehaviour
 	
 	void FixedUpdate()
 	{
+		unityPacket.OutputStatePacket packet = udp.getLastPacket();
+		
+		DriveJoints.UpdateAllWheels(skeleton ,packet.dio[0].pwmValues);
 		
 	}
 }
