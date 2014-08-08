74a45372caa45dffef4714955faa841f555174ab
Westin Miller <westin.miller@autodesk.com>
2014-08-07 17:00:55 -0700

    Useless property getters, obsolete comment

--- SimulatorFileIO/Joints/Driver/PneumaticDriverMeta.cs
+++ SimulatorFileIO/Joints/Driver/PneumaticDriverMeta.cs
@@ -1,6 +1,6 @@
 using System.IO;
 
-﻿//find proper specs for these
+﻿// TODO find proper specs for these
 public enum PneumaticDiameter : byte
 {
     HIGH = 0,
@@ -16,43 +16,20 @@ public enum PneumaticPressure : byte
 }
 
 
-
-
+/// <summary>
+/// Stores the variables concerning a pneumatic cylinder.
+/// </summary>
 public class PneumaticDriverMeta : JointDriverMeta
 {
-    /// <summary>
-    /// Stores the variables concerning a wheel, such as its position (which may be removed later) and radius.  
-    /// </summary>
-
-    /*public JointDriverType type
-    {
-        get;
-        set;
-    }*/
-
-    public float widthMM
-    {
-        get;
-        set;
-    }
-
-    public float pressurePSI
-    {
-        get;
-        set;
-    }
+    public float widthMM;
+    public float pressurePSI;
 
 
-    //Writes the position of the wheel to the file.
     protected override void WriteDataInternal(BinaryWriter writer)
     {
-        //writer.Write((byte)((int)type));
-
         writer.Write(widthMM);
         writer.Write(pressurePSI);
     }
-
-    //Reads the position of the wheel from the file.
     protected override void ReadDataInternal(BinaryReader reader)
     {
         widthMM = reader.ReadSingle();
