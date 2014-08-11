31a1d91ce38ad796256780060f50ca0f3dc2fad5
Philip Arola <philip.arola@autodesk.com>
2014-08-11 14:19:07 -0700

    Added Type to the Pneumatic meta

--- SimulatorFileIO/Joints/Driver/PneumaticDriverMeta.cs
+++ SimulatorFileIO/Joints/Driver/PneumaticDriverMeta.cs
@@ -24,11 +24,11 @@ public class PneumaticDriverMeta : JointDriverMeta
     /// Stores the variables concerning a wheel, such as its position (which may be removed later) and radius.  
     /// </summary>
 
-    /*public JointDriverType type
+    public JointDriverType type
     {
         get;
         set;
-    }*/
+    }
 
     public float widthMM
     {
@@ -46,7 +46,7 @@ public class PneumaticDriverMeta : JointDriverMeta
     //Writes the position of the wheel to the file.
     protected override void WriteDataInternal(BinaryWriter writer)
     {
-        //writer.Write((byte)((int)type));
+        writer.Write((byte)((int)type));
 
         writer.Write(widthMM);
         writer.Write(pressurePSI);
@@ -55,6 +55,8 @@ public class PneumaticDriverMeta : JointDriverMeta
     //Reads the position of the wheel from the file.
     protected override void ReadDataInternal(BinaryReader reader)
     {
+        type = (JointDriverType) reader.ReadByte();
+
         widthMM = reader.ReadSingle();
         pressurePSI = reader.ReadSingle();
     }
