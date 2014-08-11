dc39141af9b945462be7f728c166f655444bc10f
Philip Arola <philip.arola@autodesk.com>
2014-08-11 16:19:43 -0700

    Removed as per Request
    Doesn't need the JointTypeDriver type either
    Small possibility that it won't work with the removal of the lines from
    WriteDataInternal and ReadDataInternal.

--- SimulatorFileIO/Joints/Driver/PneumaticDriverMeta.cs
+++ SimulatorFileIO/Joints/Driver/PneumaticDriverMeta.cs
@@ -17,25 +17,17 @@ public enum PneumaticPressure : byte
 
 
 
-
+/// <summary>
+/// Stores the variables concerning a wheel, such as its position (which may be removed later) and radius.  
+/// </summary>
 public class PneumaticDriverMeta : JointDriverMeta
 {
-    /// <summary>
-    /// Stores the variables concerning a wheel, such as its position (which may be removed later) and radius.  
-    /// </summary>
-
-    public JointDriverType type;
-
     public float widthMM;
     public float pressurePSI;
-
-
 
     //Writes the position of the wheel to the file.
     protected override void WriteDataInternal(BinaryWriter writer)
     {
-        writer.Write((byte)((int)type));
-
         writer.Write(widthMM);
         writer.Write(pressurePSI);
     }
@@ -43,8 +35,6 @@ public class PneumaticDriverMeta : JointDriverMeta
     //Reads the position of the wheel from the file.
     protected override void ReadDataInternal(BinaryReader reader)
     {
-        type = (JointDriverType) reader.ReadByte();
-
         widthMM = reader.ReadSingle();
         pressurePSI = reader.ReadSingle();
     }
