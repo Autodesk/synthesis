b81aa349c0746fa7b9a3c1a4a68543a2a9f11b61
Jamie Graham <jamie.graham@autodesk.com>
2014-08-14 09:37:17 -0700

    Update Init.cs
    Conflicts didn't resolve the first time...

--- scripts/Init.cs
+++ scripts/Init.cs
@@ -8,30 +8,6 @@ using System.Windows.Forms;
 
 public class Init : MonoBehaviour
 {
-<<<<<<< HEAD
-=======
-    // We will need these
-    public List<List<UnityRigidNode>> PWMAssignments;
-    public Dictionary<List<int>, UnityRigidNode> SolenoidAssignments;
-    public float speed = 5;
-    public int[] motors = { 1, 2, 3, 4 };
-    RigidNode_Base skeleton;
-    unityPacket udp = new unityPacket();
-    List<Vector3> unityWheelData = new List<Vector3>();
-
-    int robots = 0;
-    string filePath = "C:/Users/t_crisj/Desktop/Skeleton/";
-
-
-    public enum WheelPositions
-    {
-        FL = 1,
-        FR = 2,
-        BL = 3,
-        BR = 4
-    }
->>>>>>> 9531905157d82d8aa0c701930e78ad27e465da88
-
 	// We will need these
 	public List<List<UnityRigidNode>> PWMAssignments;
 	public Dictionary<List<int>, UnityRigidNode> SolenoidAssignments;
@@ -67,7 +43,6 @@ public class Init : MonoBehaviour
 		
 	}
 
-<<<<<<< HEAD
 */
 	
 	
@@ -84,21 +59,7 @@ public class Init : MonoBehaviour
 
 			GameObject robot = new GameObject("Robot");
 			robot.transform.parent = transform;
-=======
-    void TryLoad()
-    {
-        if (filePath != null && skeleton == null)
-        {
-            UnityRigidNode nodeThing = new UnityRigidNode();
-            nodeThing.modelFileName = "field.bxda";
-            nodeThing.CreateTransform(transform);
-			nodeThing.CreateMesh("C:/Users/t_crisj/Desktop/Skeleton/field.bxda");
-            nodeThing.unityObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
-
-            GameObject robot = new GameObject("Robot");
-            robot.transform.parent = transform;
->>>>>>> 9531905157d82d8aa0c701930e78ad27e465da88
 
 			List<RigidNode_Base> names = new List<RigidNode_Base>();
 			RigidNode_Base.NODE_FACTORY = delegate()
@@ -150,8 +111,7 @@ public class Init : MonoBehaviour
 	}
 
 
-<<<<<<< HEAD
-	void FixedUpdate()
+/*	void FixedUpdate()
 	{
     	
     	unityPacket.OutputStatePacket packet = udp.GetLastPacket();
@@ -161,8 +121,8 @@ public class Init : MonoBehaviour
 			DriveJoints.UpdateAllWheels(skeleton, packet.dio);
 		}
 	}
+*/
 
-=======
     void FixedUpdate()
     {
         if (skeleton != null)
@@ -171,5 +131,5 @@ public class Init : MonoBehaviour
             DriveJoints.UpdateAllMotors(skeleton, packet.dio[0].pwmValues);
         }
     }
->>>>>>> 9531905157d82d8aa0c701930e78ad27e465da88
+
 }
