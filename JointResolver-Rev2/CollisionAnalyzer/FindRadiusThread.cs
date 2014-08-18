a51e594b183fbeab4eae11f5e359f0023c1c53fd
Ryan Milem <ryan.milem@autodesk.com>
2014-08-18 15:29:07 -0700

    Removed unused code.

--- JointResolver-Rev2/CollisionAnalyzer/FindRadiusThread.cs
+++ JointResolver-Rev2/CollisionAnalyzer/FindRadiusThread.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Inventor;
 using System.Threading;
-using System.Runtime.Caching;
 
 /// <summary>
 /// Handles a single thread to find the radius of a single component.
