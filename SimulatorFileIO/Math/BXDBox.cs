310696e9241071217b7767ab57610e3b761f23f4
Ryan Milem <ryan.milem@autodesk.com>
2014-08-18 15:20:07 -0700

    Removed System, fixed stuff that broke.

--- SimulatorFileIO/Math/BXDBox.cs
+++ SimulatorFileIO/Math/BXDBox.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-public class BXDBox : IComparable<BXDBox>
+public class BXDBox : System.IComparable<BXDBox>
 {
     BXDVector3 minPoint;
     BXDVector3 maxPoint;
