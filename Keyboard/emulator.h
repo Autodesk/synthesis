059d3cdbed4c43846b315e3213ad5b2bb144e899
Jake Springer <Jake.Springer@autodesk.com>
2015-06-24 16:05:54 -0700

    Updated emulator file to accompany Keyboard_BasicControl.cpp

---
+++ Keyboard/emulator.h
@@ -0,0 +1,64 @@
+#ifndef __EMULATOR_H
+#define __EMULATOR_H
+#include <stdint.h>
+#include <WPILib.h>	 // Ensure we have the START_ROBOT_CLASS Macro once so we can override it.
+
+// Used to specify your team ID
+#ifdef JAVA_BUILD
+extern int TEAM_ID;
+#else
+extern const int TEAM_ID;
+#endif
+
+extern "C" {
+#ifdef JAVA_BUILD
+	__declspec(dllexport) void SetEmulatedTeam(int team);
+#endif
+	__declspec(dllexport) int StartEmulator();
+#ifndef JAVA_BUILD
+	extern int32_t FRC_UserProgram_StartupLibraryInit();
+#endif
+}
+
+#ifndef JAVA_BUILD
+
+#ifndef START_ROBOT_CLASS
+#error "emulator.h needs to be included AFTER WPILib"
+#endif
+
+#undef START_ROBOT_CLASS // Squash warnings
+
+// Override the default FRC robot class start macro.
+#define START_ROBOT_CLASS(_ClassName_) \
+	void *FRC_userClassFactory() \
+{ \
+	return new _ClassName_(); \
+} \
+	extern "C" { \
+	int32_t FRC_UserProgram_StartupLibraryInit() \
+{ \
+	RobotBase::startRobotTask(FRC_userClassFactory); \
+	return 0; \
+} \
+	int main(int argc, char ** argv) { StartEmulator(); } \
+}
+
+#define START_ROBOT_CLASS_WINMAIN(_ClassName_, _MainFunc_) \
+	void *FRC_userClassFactory() \
+{ \
+	return new _ClassName_(); \
+} \
+	extern "C" { \
+	int32_t FRC_UserProgram_StartupLibraryInit() \
+{ \
+	RobotBase::startRobotTask(FRC_userClassFactory); \
+	return 0; \
+} \
+	int main() /*INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)*/ { \
+		_MainFunc_(); \
+		StartEmulator(); \
+		return 0; \
+	} \
+}
+#endif
+#endif
