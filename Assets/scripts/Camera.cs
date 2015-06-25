a10074730b4bbcd77bbbb6a3192eee906e0a1067
Pedro Morais <Pedro.de.Faria.Autran.e.Morais@autodesk.com>
2015-06-25 10:31:27 -0700

    unity files added

---
+++ Assets/scripts/Camera.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Camera : MonoBehaviour {
+
+	Vector3 COM;
+	Vector3 rotateVect;
+	GameObject gameobj;
+	static float camMag = 4;
+	Vector3 rotateXZ(Vector3 vector, Vector3 origin, float theta, float mag)
+	{
+		vector -= origin;
+		Vector3 output = vector;
+		output.x = Mathf.Cos (theta) * (vector.x ) - Mathf.Sin (theta) * (vector.z ) ;
+		output.z = Mathf.Sin (theta) * (vector.x ) + Mathf.Cos (theta) * (vector.z ) ;
+
+		return output.normalized*mag + origin;
+	}
+
+	Vector3 rotateYZ(Vector3 vector, Vector3 origin, float theta, float mag)
+	{
+		vector -= origin;
+		Vector3 output = vector;
+		output.y = Mathf.Cos (theta) * (vector.y ) - Mathf.Sin (theta) * (vector.z ) ;
+		output.z = Mathf.Sin (theta) * (vector.y ) + Mathf.Cos (theta) * (vector.z ) ;
+		output.y = output.y > .1f ? output.y : .1f;
+		return output.normalized*mag + origin;
+	}
+
+	void Update ()
+	{
+
+		camMag -= Input.GetAxis ("Mouse ScrollWheel")*10;
+		rotateVect = rotateXZ (rotateVect, COM, 0, camMag);
+		rotateVect = rotateYZ (rotateVect, COM, 0, camMag);
+		transform.position = rotateVect;
+
+		if (Input.GetMouseButton (2)) 
+		{
+			rotateVect = rotateXZ (rotateVect, COM, Input.GetAxis("Mouse X")/5f, camMag);
+			rotateVect = rotateYZ (rotateVect, COM, Input.GetAxis("Mouse Y")/5f, camMag);
+			transform.position = rotateVect;
+
+			COM = auxFunctions.TotalCenterOfMass (gameobj);
+			Debug.Log(COM);
+			transform.LookAt (COM);
+		}
+
+	}
+	
+	void Start ()
+	{
+		rotateVect = - transform.position;
+		gameobj = GameObject.Find("GameObject");
+	}
+}
---
+++ Assets/scripts/Init.cs
@@ -0,0 +1,220 @@
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Init : MonoBehaviour
+{
+    // We will need these
+    public const float PHYSICS_MASS_MULTIPLIER = 0.001f;
+
+    private GUIController gui;
+
+    private RigidNode_Base skeleton;
+    private GameObject activeRobot;
+
+    private unityPacket udp = new unityPacket();
+    private string filePath = BXDSettings.Instance.LastSkeletonDirectory + "\\";
+
+    /// <summary>
+    /// Frames before the robot gets reloaded, or -1 if no reload is queued.
+    /// </summary>
+    /// <remarks>
+    /// This allows reloading the robot to be delayed until a "Loading" dialog can be drawn.
+    /// </remarks>
+    private volatile int reloadInFrames = -1;
+
+    public Init()
+    {
+    }
+
+    [STAThread]
+    void OnGUI()
+    {
+        if (gui == null)
+        {
+            gui = new GUIController();
+            gui.AddWindow("Load Model", new FileBrowser(), (object o) =>
+            {
+                string fileLocation = (string) o;
+                // If dir was selected...
+                if (File.Exists(fileLocation + "\\skeleton.bxdj"))
+                    fileLocation += "\\skeleton.bxdj";
+                DirectoryInfo parent = Directory.GetParent(fileLocation);
+                if (parent != null && parent.Exists && File.Exists(parent.FullName + "\\skeleton.bxdj"))
+                {
+                    this.filePath = parent.FullName + "\\";
+                    reloadInFrames = 2;
+                }
+                else
+                {
+                    UserMessageManager.Dispatch("Invalid selection!");
+                }
+            });
+            gui.AddAction("Orient Robot", () =>
+            {
+                OrientRobot();
+            });
+            if (!File.Exists(filePath + "\\skeleton.bxdj"))
+            {
+                gui.DoAction("Load Model");
+            }
+        }
+        gui.Render();
+
+        if (reloadInFrames >= 0)
+        {
+            GUI.backgroundColor = new Color(1, 1, 1, 0.5f);
+            GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Loading... Please Wait", gui.BlackBoxStyle);
+        }
+    }
+
+    /// <summary>
+    /// Repositions the robot so it is aligned at the center of the field, and resets all the
+    /// joints, velocities, etc..
+    /// </summary>
+    private void OrientRobot()
+    {
+        if (activeRobot != null && skeleton != null)
+        {
+            var unityWheelData = new List<GameObject>();
+            // Invert the position of the root object
+            activeRobot.transform.localPosition = Vector3.zero;
+            activeRobot.transform.localRotation = Quaternion.identity;
+            var nodes = skeleton.ListAllNodes();
+            foreach (RigidNode_Base node in nodes)
+            {
+                UnityRigidNode uNode = (UnityRigidNode) node;
+                uNode.unityObject.transform.localPosition = Vector3.zero;
+                uNode.unityObject.transform.localRotation = Quaternion.identity;
+                if (uNode.unityObject.rigidbody != null)
+                {
+                    uNode.unityObject.rigidbody.velocity = Vector3.zero;
+                    uNode.unityObject.rigidbody.angularVelocity = Vector3.zero;
+                }
+                if (uNode.HasDriverMeta<WheelDriverMeta>() && uNode.wheelCollider != null)
+                {
+                    unityWheelData.Add(uNode.wheelCollider);
+                }
+            }
+            if (unityWheelData.Count > 0)
+            {
+               auxFunctions.OrientRobot(unityWheelData, activeRobot.transform);
+            }
+        }
+    }
+
+    private void TryLoad()
+    {
+        if (activeRobot != null)
+        {
+            skeleton = null;
+            UnityEngine.Object.Destroy(activeRobot);
+        }
+        if (filePath != null && skeleton == null)
+        {
+            List<Collider> meshColliders = new List<Collider>();
+            activeRobot = new GameObject("Robot");
+            activeRobot.transform.parent = transform;
+
+            List<RigidNode_Base> names = new List<RigidNode_Base>();
+            RigidNode_Base.NODE_FACTORY = delegate()
+            {
+                return new UnityRigidNode();
+            };
+
+            skeleton = BXDJSkeleton.ReadSkeleton(filePath + "skeleton.bxdj");
+            skeleton.ListAllNodes(names);
+            foreach (RigidNode_Base node in names)
+            {
+                UnityRigidNode uNode = (UnityRigidNode) node;
+
+                uNode.CreateTransform(activeRobot.transform);
+                uNode.CreateMesh(filePath + uNode.modelFileName);
+                uNode.CreateJoint();
+
+                meshColliders.AddRange(uNode.unityObject.GetComponentsInChildren<Collider>());
+            }
+
+            {   // Add some mass to the base object
+                UnityRigidNode uNode = (UnityRigidNode) skeleton;
+                uNode.unityObject.transform.rigidbody.mass += 20f * PHYSICS_MASS_MULTIPLIER; // Battery'
+                Vector3 vec = uNode.unityObject.rigidbody.centerOfMass;
+                vec.y *= 0.9f;
+                uNode.unityObject.rigidbody.centerOfMass = vec;
+            }
+
+            auxFunctions.IgnoreCollisionDetection(meshColliders);
+            OrientRobot();
+        }
+        else
+        {
+            Debug.Log("unityWheelData is null...");
+        }
+        gui.guiVisible = false;
+    }
+
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, -9.8f, 0);
+        Physics.solverIterationCount = 15;
+        Physics.minPenetrationForPenalty = 0.001f;
+
+        // Load Field
+
+        UnityRigidNode nodeThing = new UnityRigidNode();
+        nodeThing.modelFileName = "field.bxda";
+        nodeThing.CreateTransform(transform);
+        nodeThing.CreateMesh(UnityEngine.Application.dataPath + "\\Resources\\field.bxda");
+        nodeThing.unityObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+        reloadInFrames = 2;
+    }
+
+    void OnEnable()
+    {
+        udp.Start();
+    }
+
+    void OnDisable()
+    {
+        udp.Stop();
+    }
+
+    void Update()
+    {
+        if (reloadInFrames >= 0 && reloadInFrames-- == 0)
+        {
+            reloadInFrames = -1;
+            TryLoad();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (skeleton != null)
+        {
+            unityPacket.OutputStatePacket packet = udp.GetLastPacket();
+            DriveJoints.UpdateAllMotors(skeleton, packet.dio);
+            DriveJoints.UpdateSolenoids(skeleton, packet.solenoid);
+            List<RigidNode_Base> nodes = skeleton.ListAllNodes();
+            InputStatePacket sensorPacket = new InputStatePacket();
+            foreach (RigidNode_Base node in nodes)
+            {
+                if (node.GetSkeletalJoint() == null)
+                    continue;
+                foreach (RobotSensor sensor in node.GetSkeletalJoint().attachedSensors)
+                {
+                    if (sensor.type == RobotSensorType.POTENTIOMETER && node.GetSkeletalJoint() is RotationalJoint_Base)
+                    {
+                        UnityRigidNode uNode = (UnityRigidNode) node;
+                        float angle = DriveJoints.GetAngleBetweenChildAndParent(uNode) + ((RotationalJoint_Base) uNode.GetSkeletalJoint()).currentAngularPosition;
+                        sensorPacket.ai[sensor.module - 1].analogValues[sensor.port - 1] = (int) sensor.equation.Evaluate(angle);
+                    }
+                }
+            }
+            udp.WritePacket(sensorPacket);
+        }
+    }
+}
