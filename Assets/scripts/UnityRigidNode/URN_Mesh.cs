a6733518d31738fb6110ae6471207ed10d62b963
Patrick Johnson <patrick.johnson@autodesk.com>
2015-07-06 16:22:12 -0700

    Fixed script compilation errors
    Former-commit-id: 7cb1a87789844614180769c2fc0d492f204d9dcf

--- Assets/scripts/UnityRigidNode/URN_Mesh.cs
+++ Assets/scripts/UnityRigidNode/URN_Mesh.cs
@@ -12,7 +12,7 @@ public partial class UnityRigidNode : RigidNode_Base
     public void CreateMesh(string filePath)
     {
         BXDAMesh mesh = new BXDAMesh();
-        mesh.ReadFromFile(filePath);
+        mesh.ReadFromFile(filePath, null);
 
         // Create all submesh objects
         auxFunctions.ReadMeshSet(mesh.meshes, delegate(int id, BXDAMesh.BXDASubMesh sub, Mesh meshu)
