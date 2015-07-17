300501b32bd01ef9531eee71710808ef8e6f81c3
Patrick Johnson <patrick.johnson@autodesk.com>
2015-07-17 11:10:18 -0700

    just cleaning up a few things, no functionality change

--- Assets/scripts/Init.cs
+++ Assets/scripts/Init.cs
@@ -52,7 +52,7 @@ public class Init : MonoBehaviour
 	private bool time_stop;
 	private float oldSpeed;
 	private bool showStatWindow;
-	Quaternion rotation;
+
 
     /// <summary>
     /// Frames before the robot gets reloaded, or -1 if no reload is queued.
@@ -235,10 +235,9 @@ public class Init : MonoBehaviour
 
             gui.AddAction("Reset Robot", () =>
             {
-				Debug.Log (rotation + " reset");
                 resetRobot();
             });
-			//button to manually orient the robot
+			//shows button to manually orient the robot
 			ShowOrient();
 
             if (!File.Exists(filePath + "\\skeleton.bxdj"))
