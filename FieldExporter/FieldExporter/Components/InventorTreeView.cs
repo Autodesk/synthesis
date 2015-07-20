8bb4c6ce1ef379f36dd37ad7c4403baa6d660185
Mackinnon Buck <mackinnonman@icloud.com>
2015-07-20 09:06:05 -0700

    Custom InventorTreeView component. Code cleaned.
    I have implemented a custom .net component called "InventorTreevView"
    which allows easy communication between inventor and the treeview
    component. I have also cleaned up my code and separated different files
    into organized folders.

---
+++ FieldExporter/FieldExporter/Components/InventorTreeView.cs
@@ -0,0 +1,151 @@
+﻿using Inventor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+class InventorTreeView : TreeView
+{
+    /// <summary>
+    /// Adds children to a TreeNode based on its Inventor component correspondant.
+    /// </summary>
+    /// <param name="component"></param>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private TreeNode AddComponentChildren(ComponentOccurrence component, TreeNode node)
+    {
+        IEnumerator enumerator = component.SubOccurrences.GetEnumerator();
+        ComponentOccurrence subOccurrence;
+
+        while (enumerator.MoveNext())
+        {
+            subOccurrence = (ComponentOccurrence)enumerator.Current;
+
+            TreeNode currentNode = new TreeNode(subOccurrence.Name);
+            currentNode.Name = currentNode.Text;
+            currentNode.Tag = subOccurrence;
+
+            node.Nodes.Add(currentNode);
+
+            AddComponentChildren(subOccurrence, currentNode);
+        }
+
+        return node;
+    }
+
+    /// <summary>
+    /// Adds parents to a TreeNode based on its Inventor component correspondant.
+    /// </summary>
+    /// <param name="component"></param>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private TreeNode AddComponentParents(ComponentOccurrence component, TreeNode node)
+    {
+        ComponentOccurrence parentOccurrence;
+
+        if (component.ParentOccurrence != null)
+        {
+            parentOccurrence = component.ParentOccurrence;
+
+            TreeNode currentNode = new TreeNode(parentOccurrence.Name);
+            currentNode.Name = currentNode.Text;
+            currentNode.Tag = parentOccurrence;
+
+            currentNode.Nodes.Add(node);
+
+            AddComponentParents(parentOccurrence, currentNode);
+        }
+
+        return node;
+    }
+
+    /// <summary>
+    /// Adds the supplied component as a node with its corresponding parents and children.
+    /// </summary>
+    /// <param name="component"></param>
+    public void AddComponent(ComponentOccurrence component)
+    {
+        TreeNode node = new TreeNode(component.Name);
+        node.Name = node.Text;
+        node.Tag = component;
+
+        node = AddComponentChildren(component, node);
+        node = AddComponentParents(component, node);
+
+        while (true)
+        {
+            TreeNode[] searchResults = Nodes.Find(node.Name, true);
+            if (searchResults.Length > 0)
+            {
+                if (searchResults[0].Parent != null)
+                {
+                    //MessageBox.Show("Object and parent exist, so I'll add to the existing parent and remove the existing object.");
+                    searchResults[0].Parent.Nodes.Insert(searchResults[0].Index, node);
+                }
+                else
+                {
+                    //MessageBox.Show("Object exists but parent does not, so I'll add the component as a root and remove the existing object.");
+                    Nodes.Add(node);
+                }
+                searchResults[0].Remove();
+                break;
+            }
+            else
+            {
+                if (node.Parent != null)
+                {
+                    //MessageBox.Show("Object does not exist, but potential parent does, so I'll reiterate as the parent.");
+                    node = node.Parent;
+                }
+                else
+                {
+                    //MessageBox.Show("Neither object nor parent exist, so I'll just add the component as a root.");
+                    Nodes.Add(node);
+                    break;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Adds the supplied components as nodes and sends progress updates.
+    /// </summary>
+    /// <param name="components"></param>
+    /// <param name="progressUpdate"></param>
+    public void AddComponents(ObjectsEnumerator components, Action<int, int> progressUpdate)
+    {
+        for (int i = 0; i < components.Count; i++)
+        {
+            AddComponent(components[i + 1]);
+            progressUpdate(i + 1, components.Count);
+        }
+    }
+
+    /// <summary>
+    /// Resets the InventorTreeView.
+    /// </summary>
+    public void Reset()
+    {
+        Nodes.Clear();
+    }
+
+    /// <summary>
+    /// Removes a node when [delete] is pressed.
+    /// </summary>
+    /// <param name="e"></param>
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        base.OnKeyDown(e);
+
+        if (e.KeyData == Keys.Delete)
+        {
+            if (SelectedNode != null)
+            {
+                Nodes.Remove(SelectedNode);
+            }
+        }
+    }
+}
---
+++ FieldExporter/FieldExporter/Forms/ProgressWindow.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FieldExporter
+{
+    partial class ProgressWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressWindow));
+            this.ProcessProgressBar = new System.Windows.Forms.ProgressBar();
+            this.ProcessInfoLabel = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProcessProgressBar
+            // 
+            this.ProcessProgressBar.Location = new System.Drawing.Point(12, 40);
+            this.ProcessProgressBar.Name = "ProcessProgressBar";
+            this.ProcessProgressBar.Size = new System.Drawing.Size(278, 23);
+            this.ProcessProgressBar.TabIndex = 0;
+            // 
+            // ProcessInfoLabel
+            // 
+            this.ProcessInfoLabel.AutoSize = true;
+            this.ProcessInfoLabel.Location = new System.Drawing.Point(13, 13);
+            this.ProcessInfoLabel.Name = "ProcessInfoLabel";
+            this.ProcessInfoLabel.Size = new System.Drawing.Size(90, 17);
+            this.ProcessInfoLabel.TabIndex = 1;
+            this.ProcessInfoLabel.Text = "Processing...";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(194, 69);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(96, 32);
+            this.CancelButton.TabIndex = 2;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // ProgressWindow
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(302, 113);
+            this.ControlBox = false;
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.ProcessInfoLabel);
+            this.Controls.Add(this.ProcessProgressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressWindow";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Processing...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ProgressBar ProcessProgressBar;
+        public System.Windows.Forms.Label ProcessInfoLabel;
+        public System.Windows.Forms.Button CancelButton;
+    }
+}
