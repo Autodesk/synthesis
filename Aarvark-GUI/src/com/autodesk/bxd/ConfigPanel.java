57de52d129a253f4d16a494f340fb03cd1d1b673
Shawn Hice <shawn.hice@autodesk.com>
2015-07-24 16:18:09 -0700

    Changed to properties scheme
    made it run with config panel, minor changes to the aardvarkframe code

--- Aarvark-GUI/src/com/autodesk/bxd/ConfigPanel.java
+++ Aarvark-GUI/src/com/autodesk/bxd/ConfigPanel.java
@@ -7,6 +7,7 @@ package com.autodesk.bxd;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.io.File;
 import javax.swing.JFrame;
 
 /**
@@ -26,6 +27,7 @@ public class ConfigPanel extends javax.swing.JFrame {
         ConfigFile config = new ConfigFile();
         config.readLine(ConfigFile.Delims.Version);
     }
+    
     
      private void setWindowPosition(JFrame window, int screen) {
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -61,6 +63,13 @@ public class ConfigPanel extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
+        teamNumber = new javax.swing.JTextField();
+        loopBack = new javax.swing.JTextField();
+        version = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -71,21 +80,61 @@ public class ConfigPanel extends javax.swing.JFrame {
             }
         });
 
+        teamNumber.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                teamNumberActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Team Number:");
+
+        jLabel2.setText("Loopback Name:");
+
+        jLabel3.setText("Version Number:");
+
+        jLabel4.setText("Emulator Setup");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(158, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addContainerGap(43, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(teamNumber)
+                            .addComponent(loopBack)
+                            .addComponent(version))))
                 .addGap(157, 157, 157))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(202, Short.MAX_VALUE)
+                .addContainerGap(26, Short.MAX_VALUE)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(teamNumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(loopBack, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(version, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(31, 31, 31)
                 .addComponent(jButton1)
-                .addGap(73, 73, 73))
+                .addGap(65, 65, 65))
         );
 
         pack();
@@ -94,10 +143,16 @@ public class ConfigPanel extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         ConfigPanel.this.setVisible(false);
-        OpenPanel oPanel = new OpenPanel();
-        
-        
+        PropertiesConfig propsfile = new PropertiesConfig();
+        propsfile.setProp("Loopback_Adapter",loopBack.getText());
+        propsfile.setProp("TeamNumber",teamNumber.getText());
+        propsfile.setProp("Version",version.getText());
+        AardvarkFrame aaPanel = new AardvarkFrame();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void teamNumberActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_teamNumberActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_teamNumberActionPerformed
 
     /**
      * @param args the command line arguments
@@ -129,12 +184,24 @@ public class ConfigPanel extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ConfigPanel cPanel = new ConfigPanel();
+                File f = new File("config.properties");
+                if (f.exists() == false){
+                    ConfigPanel cPanel = new ConfigPanel();
+                }else{
+                    AardvarkFrame aaPanel = new AardvarkFrame();
+                }
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField loopBack;
+    private javax.swing.JTextField teamNumber;
+    private javax.swing.JTextField version;
     // End of variables declaration//GEN-END:variables
 }
