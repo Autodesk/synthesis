06ebf15ad2dddbec86a02b75a7fed2d9d4003f90
Mackinnon Buck <mackinnonman@icloud.com>
2015-07-29 11:26:38 -0700

    Fixed inverted normals. Game piece functionality :D
    The weird flipped normals bug was fixed. This commit also contains code
    for the updated field exporter, and can now load field elements that
    behave like field elements! Out with the old totes, in with the new. 3DS
    tote implementation was removed.
    
    
    Former-commit-id: 9596b9825cec713be268fd74b95e42ca59c229f3

--- Assets/scripts/Init.cs
+++ Assets/scripts/Init.cs
@@ -60,7 +60,6 @@ public class Init : MonoBehaviour
     public Init()
     {
 		udp = new unityPacket ();
-		filePath = BXDSettings.Instance.LastSkeletonDirectory + "\\";
 		Debug.Log (filePath);
 
 		statsWindowRect = new Rect (Screen.width - 320, 20, 300, 150);
@@ -495,7 +494,7 @@ public class Init : MonoBehaviour
 
 		totes = new List<GameObject> ();
 
-		filePath = Application.dataPath + "\\resources\\FieldOutput\\";
+		filePath = Application.dataPath + "\\Assets\\resources\\FieldOutput\\";
 
         reloadRobotInFrames = 2;
 		reloadFieldInFrames = 2;
@@ -525,23 +524,6 @@ public class Init : MonoBehaviour
 			reloadFieldInFrames = -1;
 			TryLoadField();
 			resetRobot();
-		}
-
-		// Only allow camera moving if gui is not showing
-		if (gui != null && !gui.guiVisible) 
-		{
-			if (Input.GetKeyDown (KeyCode.Z)) {
-				totes.Add (Tote.Create (new Vector3 (-3.619f, 0.742f, -8.183f), new Vector3 (0f, 323.3176f, 247.9989f), new Vector3 (FORMAT_3DS_SCALE, FORMAT_3DS_SCALE, FORMAT_3DS_SCALE)));
-			}
-			if (Input.GetKeyDown (KeyCode.X)) {
-				totes.Add (Tote.Create (new Vector3 (3.619f, 0.742f, -8.183f), new Vector3 (0f, 216.2776f, 247.9989f), new Vector3 (FORMAT_3DS_SCALE, FORMAT_3DS_SCALE, FORMAT_3DS_SCALE)));
-			}
-			if (Input.GetKeyDown (KeyCode.C)) {
-				totes.Add (Tote.Create (new Vector3 (-3.619f, 0.742f, 8.183f), new Vector3 (0f, 36.2776f, 247.9989f), new Vector3 (FORMAT_3DS_SCALE, FORMAT_3DS_SCALE, FORMAT_3DS_SCALE)));
-			}
-			if (Input.GetKeyDown (KeyCode.V)) {
-				totes.Add (Tote.Create (new Vector3 (3.619f, 0.742f, 8.183f), new Vector3 (0f, 143.3176f, 247.9989f), new Vector3 (FORMAT_3DS_SCALE, FORMAT_3DS_SCALE, FORMAT_3DS_SCALE)));
-			}
 		}
 
 		// Orient Robot
