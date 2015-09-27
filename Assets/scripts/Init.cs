acd05c5d41484aa9c11c864832c13bb3c862065d
Mackinnon Buck <mackinnonman@icloud.com>
2015-09-26 21:27:22 -0700

    New BXDF file format support.
    The new BXDF file format is now supported and can be read from.  I have
    already seen stability improvements.

--- Assets/scripts/Init.cs
+++ Assets/scripts/Init.cs
@@ -33,7 +33,6 @@ public class Init : MonoBehaviour
 	private Rect helpWindowRect;
 	private GUIContent helpButtonContent;
 	private Rect helpButtonRect;
-	//robot stats for stats window
 	private float acceleration;
 	private float angvelo;
 	private float speed;
@@ -41,14 +40,10 @@ public class Init : MonoBehaviour
 	private float time;
 	private bool time_stop;
 	private float oldSpeed;
-	//Display windows or not
 	private bool showStatWindow;
 	private bool showHelpWindow;
-	//for orienting the robot
 	private Quaternion rotation;
 	private bool oriented;
-	//start/stop button on the stats window timer
-	private string label;
 
     /// <summary>
     /// Frames before the robot gets reloaded, or -1 if no reload is queued.
@@ -77,15 +72,11 @@ public class Init : MonoBehaviour
 		showStatWindow = false;
 		showHelpWindow = false;
 		rotation = Quaternion.identity;
-		label = "Stop";
     }
 
-	/// <summary>
-	/// Displays stats window.
-	/// </summary>
-	/// <param name="windowID">Window I.</param>
+	//displays stats like speed and acceleration
 	public void StatsWindow(int windowID) {
-
+		
 		GUI.Label (new Rect (10, 20, 300, 50), "Speed: " + Math.Round(speed, 1).ToString() + " m/s");
 		GUI.Label (new Rect (150, 20, 300, 50),Math.Round(speed*3.28084, 1).ToString() + " ft/s");
 		GUI.Label (new Rect (10, 40, 300, 50), "Acceleration: " + Math.Round(acceleration, 1).ToString() + " m/s^2");
@@ -93,25 +84,21 @@ public class Init : MonoBehaviour
 		GUI.Label (new Rect (10, 60, 300, 50), "Angular Velocity: " + Math.Round(angvelo, 1).ToString() + " rad/s");
 		GUI.Label (new Rect (10, 80, 300, 50), "Weight: " + weight.ToString() + " lbs");
 		GUI.Label (new Rect (10, 120, 300, 50), "Timer: " + Math.Round (time, 1).ToString() + " sec");
-
+		if(GUI.Button (new Rect (120, 120, 80, 25), "Start/Stop"))
+		{
+			time_stop = !time_stop;
+		}
+		
 		if (GUI.Button (new Rect (210, 120, 80, 25), "Reset")) 
 		{
 			time = 0;
 		}
 
-		if(GUI.Button (new Rect (120, 120, 80, 25), label))
-		{
-			time_stop = !time_stop;
-			
-			if(time_stop)
-				label = "Start";
-			else
-				label = "Stop";
-		}
+		GUI.DragWindow (new Rect (0, 0, 10000, 10000));
 	}
 
 	/// <summary>
-	/// Displays help windows.
+	/// Exits the window.
 	/// </summary>
 	/// <param name="windowID">Window I.</param>
 	public void HelpWindow(int windowID)
@@ -147,9 +134,7 @@ public class Init : MonoBehaviour
 		GUI.Label (new Rect (leftXOffset, 7 * heightGap, 300, 50), "[Arrow Keys]", labelSkin);
 		GUI.Label (new Rect (leftXOffset, 8 * heightGap, 300, 50), "[S]", labelSkin);
 	}
-	/// <summary>
-	/// Opens windows to orient the robot
-	/// </summary>
+
 	public void ShowOrient()
 	{
 		List<string> titles = new List<string> ();
@@ -174,26 +159,23 @@ public class Init : MonoBehaviour
 		                                     new string[0], new Rect[0], titles.ToArray (), rects.ToArray ());
 		//The directional buttons lift the robot to avoid collison with objects, rotates it, and saves the applied rotation to a vector3
 		gui.AddWindow("Orient Robot", oWindow, (object o)=>{
+			mainNode.transform.position = new Vector3(mainNode.transform.position.x, 1, mainNode.transform.position.z);
 			switch((int)o)
 			{
 			case 0:
-				mainNode.transform.position = new Vector3(mainNode.transform.position.x, 1, mainNode.transform.position.z);
-				mainNode.transform.Rotate(new Vector3(0, 0, 45));
+				mainNode.transform.Rotate(new Vector3(0, 0, 90));
 				oriented = true;
                 break;
 			case 1:	
-				mainNode.transform.position = new Vector3(mainNode.transform.position.x, 1, mainNode.transform.position.z);
-				mainNode.transform.Rotate (new Vector3(0, 0, -45));
+				mainNode.transform.Rotate (new Vector3(0, 0, -90));
 				oriented = true;
 				break;
 			case 2:	
-				mainNode.transform.position = new Vector3(mainNode.transform.position.x, 1, mainNode.transform.position.z);
-				mainNode.transform.Rotate(new Vector3( 45, 0, 0));
+				mainNode.transform.Rotate(new Vector3( 90, 0, 0));
 				oriented = true;
 				break;
-			case 3:
-				mainNode.transform.position = new Vector3(mainNode.transform.position.x, 1, mainNode.transform.position.z);
-				mainNode.transform.Rotate(new Vector3( -45, 0, 0));
+			case 3:;
+				mainNode.transform.Rotate(new Vector3( -90, 0, 0));
 				oriented = true;
 				break;
 			case 4:
@@ -204,8 +186,7 @@ public class Init : MonoBehaviour
 				oWindow.Active = false;
 				break;
 			case 6:
-				rotation = Quaternion.identity;
-				mainNode.transform.rotation = rotation;
+				mainNode.transform.rotation = Quaternion.identity;
 				break;
 
 			}			
@@ -405,7 +386,6 @@ public class Init : MonoBehaviour
 			activeRobot.transform.localPosition = new Vector3(1f, 1f, -0.5f);
 			activeRobot.transform.rotation = Quaternion.identity;
 			activeRobot.transform.localRotation = Quaternion.identity;
-			mainNode.transform.rotation = Quaternion.identity;
             var nodes = skeleton.ListAllNodes();
             foreach (RigidNode_Base node in nodes)
             {
@@ -479,7 +459,6 @@ public class Init : MonoBehaviour
 			{
 			//Resets robot to user saved orientation
 			mainNode.transform.rotation = rotation;
-				mainNode.rigidbody.inertiaTensorRotation = Quaternion.identity;
 			}
 			//makes sure robot spawns in the correct place
 			mainNode.transform.position = new Vector3(-2f, 1f, -3f);
@@ -494,9 +473,6 @@ public class Init : MonoBehaviour
 		totes.Clear ();
     }
 
-	/// <summary>
-	/// Loads a robot from file into the simulator.
-	/// </summary>
     private void TryLoadRobot()
     {
 		//resets rotation for new robot
@@ -521,7 +497,6 @@ public class Init : MonoBehaviour
 																																																																																																																																																																																																																																																																																																																					
             skeleton = BXDJSkeleton.ReadSkeleton(filePath + "skeleton.bxdj");
 			//Debug.Log(filePath + "skeleton.bxdj");
-			if( skeleton != null)
             skeleton.ListAllNodes(names);
             foreach (RigidNode_Base node in names)
             {
@@ -538,7 +513,6 @@ public class Init : MonoBehaviour
 
             {   // Add some mass to the base object
                 UnityRigidNode uNode = (UnityRigidNode) skeleton;
-				if(uNode != null)
                 uNode.unityObject.transform.rigidbody.mass += 20f * PHYSICS_MASS_MULTIPLIER; // Battery'
             }
 
@@ -556,20 +530,17 @@ public class Init : MonoBehaviour
 		HideGuiSidebar();
     }
 
-	/// <summary>
-	/// Loads a field from file into the simulator.
-	/// </summary>
 	private void TryLoadField()
 	{
 		activeField = new GameObject("Field");
-		
-		FieldDefinition_Base.FIELDDEFINITION_FACTORY = delegate()
+
+		FieldDefinition.Factory = delegate(Guid guid, string name)
 		{
-			return new UnityFieldDefinition();
+			return new UnityFieldDefinition(guid, name);
 		};
-		
+
 		Debug.Log (filePath);
-		field = (UnityFieldDefinition)BXDFProperties.ReadProperties(filePath + "definition.bxdf");
+		field = BXDFProperties.ReadProperties(filePath + "definition.bxdf") as UnityFieldDefinition;
 		field.CreateTransform(activeField.transform);
 		field.CreateMesh(filePath + "mesh.bxda");
 		fieldLoaded = true;
@@ -663,14 +634,9 @@ public class Init : MonoBehaviour
 			//stops robot while menu is open
 			if(gui.guiVisible)
 			{
-				mainNode.rigidbody.isKinematic = true;
-			}
-			else 
-			{
-				mainNode.rigidbody.isKinematic = false;
+				packet = null;
 			}
 
-			if(skeleton != null)
 			DriveJoints.UpdateAllMotors (skeleton, packet.dio);
 			//TODO put this code in drivejoints, figure out nullreference problem with cDriver
 			foreach(RigidNode_Base node in nodes)
@@ -763,6 +729,14 @@ public class Init : MonoBehaviour
 				oldSpeed = speed;
 				if (!time_stop)
 					time += Time.deltaTime;
+
+				if(gui.guiVisible)
+				{
+					udp = null;
+					//mainNode.rigidbody.isKinematic = true;
+					//mainNode.rigidbody.constraints = RigidbodyConstraints.FreezeAll;// | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationY;
+				}
+
 
 			}
 		}
