416d48e454c0b955d8bc01954028398de8fb3897
Mackinnon Buck <mackinnon.buck@autodesk.com>
2015-11-25 13:24:27 -0800

    Minor changes to improve elevator physics.
    Constant force multiplier and damping to prevent the elevator from
    derping all over the place.

--- Assets/scripts/ElevatorScript.cs
+++ Assets/scripts/ElevatorScript.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class ElevatorScript : MonoBehaviour {
 
+	public const float FORCE_MULTIPLIER = 5f;
 	public float currentTorque;
 	public ElevatorType eType;
 	public bool breakOn = false;
+
 	// Use this for initialization
 	void Start () {
         rigidbody.useGravity = true;
@@ -15,7 +17,7 @@ public class ElevatorScript : MonoBehaviour {
 	{
 		//TODO this is mostly placeholder stuff
 		Vector3 forceDirection = Vector3.up;
-		Vector3 force = forceDirection * currentTorque;// * 5;
+		Vector3 force = forceDirection * currentTorque * FORCE_MULTIPLIER;
 		int stageOffset = (int)eType;// 1 + ((int)eType)%2;
 		for (int i = 0; i < transform.parent.childCount; i++) 
 		{
@@ -23,9 +25,10 @@ public class ElevatorScript : MonoBehaviour {
 			if(rbody.GetComponent<ConfigurableJoint>()!=null && rbody.GetComponent<ConfigurableJoint>().connectedBody == rigidbody)
 			{
 				rbody.useGravity = false;
-				rbody.AddForce(force*3+Physics.gravity*stageOffset, ForceMode.Acceleration);
+				rbody.drag = FORCE_MULTIPLIER;
+				rbody.AddForce(force+Physics.gravity*stageOffset, ForceMode.Acceleration);
 			}
 		}
-		rigidbody.AddForce (force*3+Physics.gravity*stageOffset, ForceMode.Acceleration);
+		rigidbody.AddForce (force+Physics.gravity*stageOffset, ForceMode.Acceleration);
 	}
 }
