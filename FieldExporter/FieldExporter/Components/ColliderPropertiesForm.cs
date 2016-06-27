7b30bcfc415e9146e374f7533a4b47fb318de1b5
Mackinnon Buck <mackinnon.buck@autodesk.com>
2016-06-27 10:49:20 -0700

    Moved field exporter to its own repository.
    The field exporter is being moved to its own repository.

---
+++ FieldExporter/FieldExporter/Components/ColliderPropertiesForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FieldExporter.Components
+{
+    public interface ColliderPropertiesForm
+    {
+        /// <summary>
+        /// Used for getting a PropertySetCollider from information contained in the control.
+        /// </summary>
+        /// <returns></returns>
+        PropertySet.PropertySetCollider GetCollider();
+    }
+}
---
+++ FieldExporter/FieldExporter/Forms/EnterFrictionDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FieldExporter.Forms
+{
+    public partial class EnterFrictionDialog : Form
+    {
+        /// <summary>
+        /// Used for getting the friction value entered in the dialog.
+        /// </summary>
+        public int Friction
+        {
+            get
+            {
+                return (int)frictionNumericUpDown.Value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the EnterFrictionDialog class.
+        /// </summary>
+        public EnterFrictionDialog(int initalValue)
+        {
+            InitializeComponent();
+
+            frictionNumericUpDown.Value = initalValue;
+        }
+    }
+}
