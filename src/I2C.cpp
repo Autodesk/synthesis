d3ed1b71a3016f5991a7ea2e61a0c5cf482bd2ab
jadon belezos <jadon.belezos@autodesk.com>
2016-06-29 10:52:29 -0700

    add vs project files
    add db files to ignore list for gitignore.
    add return statements to i2c file.
    it compiles! with a workaround using frc simulator define

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -4,12 +4,16 @@ void i2CInitialize(uint8_t port, int32_t* status){}
 
 int32_t i2CTransaction(uint8_t port, uint8_t deviceAddress, uint8_t* dataToSend,
                        uint8_t sendSize, uint8_t* dataReceived,
-                       uint8_t receiveSize){
+                       uint8_t receiveSize)
+{
 	//call i2c read and i2c write for transactions.
+	return 0;
 }
 int32_t i2CWrite(uint8_t port, uint8_t deviceAddress, uint8_t* dataToSend,
-                 uint8_t sendSize){
+                 uint8_t sendSize)
+{
 // write are always successful because there is no where for the data to go
+	return 0;
 }
 
 
@@ -17,6 +21,7 @@ int32_t i2CRead(uint8_t port, uint8_t deviceAddress, uint8_t* buffer,
                 uint8_t count)
 {
 // rewrite buffer with 0's
+	return 0;
 }
 	void i2CClose(uint8_t port){
 		//not implemented because 
