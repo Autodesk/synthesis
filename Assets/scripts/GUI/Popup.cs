5f2ec2e60c953c8b8e6e5d3571184aad7b906f82
Thariq Ridha <thariq.ridha@autodesk.com>
2016-07-15 10:47:27 -0700

    Revert "Revert "Added Graphics and Input Settings (WIP)""
    This reverts commit 3b719298df4e85bbdf5820774496ce98ef8fcf76.

---
+++ Assets/scripts/GUI/Popup.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Popup
+{
+    static int popupListHash = "PopupList".GetHashCode();
+    static bool popupActive = false;
+
+    public static bool List(Rect position, ref bool showList, ref int listEntry, GUIContent buttonContent, GUIContent[] listContent,
+                             GUIStyle listStyle)
+    {
+        return List(position, ref showList, ref listEntry, buttonContent, listContent, "button", "box", listStyle);
+    }
+
+    public static bool List(Rect position, ref bool showList, ref int listEntry, GUIContent buttonContent, GUIContent[] listContent,
+                             GUIStyle buttonStyle, GUIStyle boxStyle, GUIStyle listStyle)
+    {
+        int controlID = GUIUtility.GetControlID(popupListHash, FocusType.Passive);
+        bool done = false;
+        if (Event.current.GetTypeForControl(controlID) == EventType.mouseDown)
+        {
+            if (!showList && position.Contains(Event.current.mousePosition) && !popupActive)
+            {
+                GUIUtility.hotControl = controlID;
+                showList = true;
+                popupActive = true;
+            }
+            else if (showList)
+            {
+                done = true;
+                popupActive = false;
+            }
+
+
+        }
+
+        if (showList)
+        {
+            Rect listRect = new Rect(position.x, position.y, position.width, listStyle.CalcHeight(listContent[0], 1.0f) * listContent.Length);
+            GUI.Box(listRect, "", boxStyle);
+            listEntry = GUI.SelectionGrid(listRect, listEntry, listContent, 1, listStyle);
+        }
+        else GUI.Label(position, buttonContent, buttonStyle);
+        if (done)
+        {
+            showList = false;
+        }
+        return done;
+    }
+}
