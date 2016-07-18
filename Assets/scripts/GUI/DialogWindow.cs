bec432d1d76ee343f9e9c7af25a17ddf37704dab
Umair Zaidi <umair.zaidi@autodesk.com>
2016-07-18 13:42:13 -0700

    Switch View/Quit Sim GUI Update

--- Assets/scripts/GUI/DialogWindow.cs
+++ Assets/scripts/GUI/DialogWindow.cs
@@ -37,13 +37,39 @@ public class DialogWindow : OverlayWindow
 	/// A list of the button labels.
 	/// </summary>
 	private readonly string[] labels;
-	
-	/// <summary>
-	/// Creates a dialog with the given title and buttons with their corresponding labels.
+
+    /// <summary>
+	/// Default textures.
 	/// </summary>
-	/// <param name="title">The overlay title</param>
-	/// <param name="labels">The button labels</param> 
-	public DialogWindow(string title, params string[] labels)
+	private Texture2D buttonTexture;
+    private Texture2D greyWindowTexture;
+    private Texture2D darkGreyWindowTexture;
+    private Texture2D lightGreyWindowTexture;
+    private Texture2D transparentWindowTexture;
+    /// <summary>
+	/// Selected button texture.
+	/// </summary>
+	private Texture2D buttonSelected;
+    /// <summary>
+	/// Gravity-Regular font.
+	/// </summary>
+	private Font gravityRegular;
+    private Font russoOne;
+    /// <summary>
+	/// Custom GUIStyle for windows.
+	/// </summary>
+	private GUIStyle window;
+    /// <summary>
+    /// Custom GUIStyle for buttons.
+    /// </summary>
+    private GUIStyle button;
+
+    /// <summary>
+    /// Creates a dialog with the given title and buttons with their corresponding labels.
+    /// </summary>
+    /// <param name="title">The overlay title</param>
+    /// <param name="labels">The button labels</param> 
+    public DialogWindow(string title, params string[] labels)
 	{
 		this.title = title;
 		this.labels = labels;
@@ -54,7 +80,30 @@ public class DialogWindow : OverlayWindow
 	/// </summary>
 	public void Render()
 	{
-		windowRect = new Rect ((Screen.width / 2 - (50 + labels.Length * 250) / 2), Screen.height / 2 - 100, 50 + labels.Length * 250, 200);
+        //Loads textures and fonts
+        buttonTexture = Resources.Load("Images/greyButton") as Texture2D;
+        buttonSelected = Resources.Load("Images/selectedbuttontexture") as Texture2D;
+        gravityRegular = Resources.Load("Fonts/Gravity-Regular") as Font;
+        greyWindowTexture = Resources.Load("Images/greyBackground") as Texture2D;
+
+        //Custom style for windows
+        window = new GUIStyle(GUI.skin.window);
+        window.normal.background = greyWindowTexture;
+        window.onNormal.background = greyWindowTexture;
+        window.font = gravityRegular;
+        window.fontSize = 16;
+
+        //Custom style for buttons
+        button = new GUIStyle(GUI.skin.button);
+        button.normal.background = buttonTexture;
+        button.hover.background = buttonSelected;
+        button.active.background = buttonSelected;
+        button.onNormal.background = buttonSelected;
+        button.onHover.background = buttonSelected;
+        button.onActive.background = buttonSelected;
+        button.fontSize = 13;
+
+        windowRect = new Rect ((Screen.width / 2 - (50 + labels.Length * 250) / 2), Screen.height / 2 - 100, 50 + labels.Length * 250, 200);
 
 		if (_active)
 		{
@@ -62,14 +111,14 @@ public class DialogWindow : OverlayWindow
 			{
 				for (int i = 0; i < labels.Length; i++)
 				{
-					if (GUI.Button(new Rect(50 + i * 250, 50, 200, 100), labels[i]))
+					if (GUI.Button(new Rect(50 + i * 250, 50, 200, 100), labels[i], button))
 					{
 						_active = false;
 						if (OnComplete != null)
 							OnComplete(i);
 					}
 				}
-			}, title);
+			}, title, window);
 		}
 	}
 
