f51999c342d59f7a93f50fc26692c77978d2c2e0
Toby Hodges <toby.hodges@autodesk.com>
2016-07-19 08:58:34 -0700

    added opentkbuild for development

---
+++ OpenTKbuild/OpenTKbuild/Scene.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace OpenTKbuild
+{
+    class Scene : GameWindow
+    {
+        public string vShaderPath = "resources/Shaders/vertexShader.vs", fShaderPath = "resources/Shaders/fragmentshader.frag";
+
+        //openTK variables
+        static OpenTK.Graphics.GraphicsMode gMode = new OpenTK.Graphics.GraphicsMode(32, 34, 8, 4);
+
+        //drawing variables
+        static int width = 1200, height = 800;
+        
+        public Scene() : base (width, height, gMode, "Synthesis R&D")
+        {
+            //drawing variables
+            GL.Viewport(0, 0, width, height);
+            GL.Enable(EnableCap.DepthTest);
+            Shader shader = new Shader(vShaderPath, fShaderPath);
+
+            float[] vertices =
+            {
+                -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+                 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+                -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+                -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+            };
+
+            Vector3[] positions =
+            {
+                new Vector3( 0.0f,  0.0f,  0.0f),
+                new Vector3( 2.0f,  5.0f, -15.0f),
+                new Vector3(-1.5f, -2.2f, -2.5f),
+                new Vector3(-3.8f, -2.0f, -12.3f),
+                new Vector3( 2.4f, -0.4f, -3.5f),
+                new Vector3(-1.7f,  3.0f, -7.5f),
+                new Vector3( 1.3f, -2.0f, -2.5f),
+                new Vector3( 1.5f,  2.0f, -2.5f),
+                new Vector3( 1.5f,  0.2f, -1.5f),
+                new Vector3(-1.3f,  1.0f, -1.5f)
+            };
+
+            int VBO = GL.GenBuffer();
+            int VAO = GL.GenVertexArray();
+            GL.BindVertexArray(VAO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            
+            GL.ClearColor(Color.FromArgb(100, 100, 100, 255));
+            
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            
+        }
+ 
+    }
+}
---
+++ OpenTKbuild/OpenTKbuild/Shader.cs
@@ -0,0 +1,150 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTKbuild
+{
+    class Shader
+    {
+
+        public int Program;
+
+        public Shader() : base()
+        {
+        }
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            #region OLD
+            //string vertexCode = null, fragmentCode = null;
+
+            //System.IO.FileStream vShaderFile = new System.IO.FileStream(vertexPath, System.IO.FileMode.Open);
+            //System.IO.FileStream fShaderFile = new System.IO.FileStream(fragmentPath, System.IO.FileMode.Open);
+            #endregion
+
+            string vCode = null, fCode = null;
+
+            try
+            {
+                #region OLD
+                //System.IO.StreamReader vShaderRead = new System.IO.StreamReader(vShaderFile);
+                //System.IO.StreamReader fShaderRead = new System.IO.StreamReader(fShaderFile);
+                //vShaderFile.Close();
+                //fShaderFile.Close();
+
+                //vertexCode = vShaderRead.ToString();
+                //fragmentCode = fShaderRead.ToString();
+                #endregion
+                vCode = System.IO.File.ReadAllText(@"resources/Shaders/vertexShader.vs");
+                fCode = System.IO.File.ReadAllText(@"resources/Shaders/fragmentShader.frag");
+            }
+            catch
+            {
+                System.Console.WriteLine("FILE NOT READ SUCCESSFULLY\n");
+            }
+
+            #region OLD
+            //char[] vShaderCodeChar = vertexCode.ToCharArray();
+            //char[] fShaderCodeChar = fragmentCode.ToCharArray();
+
+            //string[] vShaderCode = new string[vShaderCodeChar.Length];
+            //string[] fShaderCode = new string[fShaderCodeChar.Length];
+
+            //for (int i = 0; i < vShaderCodeChar.Length; i++)
+            //{
+            //    vShaderCode[i] = vShaderCodeChar[i].ToString();
+            //}
+
+            //for (int i = 0; i < vShaderCodeChar.Length; i++)
+            //{
+            //    vShaderCode[i] = vShaderCodeChar[i].ToString();
+            //}
+            #endregion
+
+            int vertex, fragment;
+
+            vertex = GL.CreateShader(ShaderType.VertexShader);
+            GL.CompileShader(vertex);
+
+            fragment = GL.CreateShader(ShaderType.FragmentShader);
+            GL.CompileShader(fragment);
+
+            compileShader(vertex, vCode);
+            compileShader(fragment, fCode);
+
+            int ShaderProgram = GL.CreateProgram();
+            GL.AttachShader(ShaderProgram, vertex);
+            GL.AttachShader(ShaderProgram, fragment);
+            GL.LinkProgram(ShaderProgram);
+
+            string info;
+            GL.GetProgramInfoLog(ShaderProgram, out info);
+            System.Console.WriteLine(info);
+
+            #region OLD
+
+            //int success;
+            //char[] infolog = new char[512];
+
+            //int[] vcount = null;
+            //int[] fcount = null;
+            //int nullint = 0;
+
+            //System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            //sb.Append(infolog);
+
+            //vertex = GL.CreateShader(ShaderType.VertexShader);
+            //GL.CompileShader(vertex);
+
+            //GL.GetShader(vertex, ShaderParameter.CompileStatus, out success);
+            //if (success == 0)
+            //{
+            //    GL.GetShaderInfoLog(vertex, 512, out nullint, sb);
+            //    System.Console.WriteLine("Error: Shader : Vertex : Compilation Failed\n" + infolog);
+            //}
+
+            //fragment = GL.CreateShader(ShaderType.FragmentShader);
+            //GL.CompileShader(fragment);
+
+            //GL.GetShader(fragment, ShaderParameter.CompileStatus, out success);
+            //if (success == 0)
+            //{
+            //    GL.GetShaderInfoLog(fragment, 512, out nullint, sb);
+            //    System.Console.WriteLine("Error: Shader : Fragment : Compilation Failed\n" + infolog);
+            //}
+
+            //Program = GL.CreateProgram();
+            //GL.AttachShader(Program, vertex);
+            //GL.AttachShader(Program, fragment);
+            //GL.LinkProgram(Program);
+
+            //GL.DeleteShader(vertex);
+            //GL.DeleteShader(fragment);
+
+            #endregion
+
+
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Program);
+        }
+
+        private void compileShader(int shader, string source)
+        {
+            GL.ShaderSource(shader, source);
+            GL.CompileShader(shader);
+
+            string info;
+            GL.GetShaderInfoLog(shader, out info);
+            System.Console.WriteLine(info);
+
+            int compileResult;
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out compileResult);
+            if (compileResult != 1)
+            {
+                System.Console.WriteLine("CompileError : " + source);
+            }
+        }
+    }
+}
