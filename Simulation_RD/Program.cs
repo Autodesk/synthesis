446a0b77e21c06058d0f4ea4f9e75247ac179d06
Shawn Hice <shawn@thelogiccore.com>
2016-07-22 08:04:58 -0700

    removed the MMMMaster branch

---
+++ Simulation_RD/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Simulation_RD
+{
+    class Program
+    {
+        //static GameWindow game;
+
+        static void Main(string[] args)
+        {
+            Scene scene = new Scene();
+
+            Shader shader = new Shader(@"\resources\vertexShader.vs\", @"\resources\fragmentShader.frag");
+            
+            scene.Run();
+        }
+    }
+}
