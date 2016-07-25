f070e2b5e70c5cacac3442b42cffcda2e6776def
HiceS <shawnhice@gmail.com>
2016-07-25 11:41:00 -0700

    Added the Aardvark libraries - didnt need to be a subtree

---
+++ exporters/exporter_research/Aardvark-Libraries/SimulatorFileIO/Joints/AngularDOF.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Contains information about a rotational degree of freedom.
+/// </summary>
+public interface AngularDOF
+{
+    float currentPosition
+    {
+        get;
+    }
+    float upperLimit
+    {
+        get;
+        set;
+    }
+    float lowerLimit
+    {
+        get;
+        set;
+    }
+
+    BXDVector3 rotationAxis
+    {
+        get;
+        set;
+    }
+    BXDVector3 basePoint
+    {
+        get;
+        set;
+    }
+}
+
+public static class AngularDOFExtensions
+{
+    public static bool hasAngularLimits(this AngularDOF dof)
+    {
+        return !float.IsPositiveInfinity(dof.upperLimit) || !float.IsNegativeInfinity(dof.lowerLimit);
+    }
+}
