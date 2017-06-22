86f3785a61cbc84462b2ff7c75058262447966b1
Mackinnon Buck <mackinnon.buck@autodesk.com>
2017-06-22 11:35:10 -0700

    Added object tracking for replay, created FSM
    Added a tracker component that stores transform information for its
    parent object for a set number of past frames. The tracker keeps this
    information in a fixed queue, which I created as a structure to quickly
    add elements to a collection and overwrite the oldest element when a new
    one is added to a full queue. I also created a finite state machine in
    preparation for the replay analysis mode. An FSM will make the code a
    whole lot cleaner and less bug prone, and it increases scalability if we
    want to add more modes in the future.

---
+++ engine/unity5/Assets/Scripts/FEA/Tracker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.FEA
+{
+    public class Tracker : MonoBehaviour
+    {
+        private const float QueueTimespan = 3.0f;
+
+        private FixedQueue<StateDescriptor> statesQueue;
+
+        /// <summary>
+        /// Returns a StateDescriptor based on the current frame.
+        /// </summary>
+        private StateDescriptor State
+        {
+            get
+            {
+                return new StateDescriptor
+                {
+                    Position = gameObject.transform.position,
+                    Rotation = gameObject.transform.rotation.eulerAngles
+                };
+            }
+        }
+
+        /// <summary>
+        /// Called when the Tracker is initialized.
+        /// </summary>
+        void Start()
+        {
+            statesQueue = new FixedQueue<StateDescriptor>((int)(QueueTimespan / Time.fixedDeltaTime), State);
+            //Debug.Log(statesQueue.Length);
+
+            // TODO: Find and store joint instance
+        }
+
+        /// <summary>
+        /// Adds the current state to the states queue.
+        /// </summary>
+        void FixedUpdate()
+        {
+            statesQueue.Add(State);
+
+            Vector3 lastPoint = Vector3.zero;
+            int i = 0;
+
+            foreach (StateDescriptor state in statesQueue)
+            {
+                if (lastPoint != Vector3.zero)
+                {
+                    float age = (float)i / statesQueue.Length;
+                    Debug.DrawLine(lastPoint, state.Position, new Color(1.0f, age, 0.0f, 1.0f - age));
+                }
+
+                lastPoint = state.Position;
+                i++;
+            }
+        }
+    }
+}
---
+++ engine/unity5/Assets/Scripts/FSM/SimState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.FSM
+{
+    public abstract class SimState
+    {
+        public virtual void Start() { }
+        public virtual void End() { }
+        public virtual void Pause() { }
+        public virtual void Resume() { }
+        public virtual void Update() { }
+        public virtual void FixedUpdate() { }
+        public virtual void OnGUI() { }
+        public virtual void Awake() { }
+    }
+}
