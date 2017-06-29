3d4f680aff66b0e3dcaaf5904a80c0f380a41302
LuckyQXW <q1475963@gmail.com>
2017-06-29 15:43:01 -0700

    Add resetting state and allow rotation in start, fix orient conflicts
    Orientation reset mess up when orient robot and keep resetting. Works for the first few times then the matrix change the rotation of the reference part randomly so everything rotates to the wrong way

--- engine/unity5/Assets/scripts/DynamicCamera.cs
+++ engine/unity5/Assets/scripts/DynamicCamera.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class DynamicCamera : MonoBehaviour
 {
@@ -125,6 +126,7 @@ public class DynamicCamera : MonoBehaviour
                 {
                     targetVector = robot.transform.GetChild(0).transform.position;//AuxFunctions.TotalCenterOfMass(robot);
 
+
                     if (Input.GetMouseButton(0))
                     {
                         cameraAngle = Mathf.Max(Mathf.Min(cameraAngle - Input.GetAxis("Mouse Y") * 5f, 90f), 0f);
@@ -244,22 +246,19 @@ public class DynamicCamera : MonoBehaviour
     }
 
     //This state locates directly above the field and looks straight down on the field in order for robot positioning
+    //Not working with 2016&2017 field because they are not centered
     public class OverviewState : CameraState
     {
         Vector3 positionVector;
         Vector3 rotationVector;
         Vector3 fieldVector;
-        Vector3 targetVector;
         GameObject field;
-        GameObject target;
 
         public OverviewState(MonoBehaviour mono)
         {
             this.mono = mono;
             field = GameObject.Find("Field");
             fieldVector = field.transform.position;
-            target = GameObject.Find("Robot");
-            targetVector = target.transform.position;
         }
 
         public override void Init()
@@ -271,8 +270,7 @@ public class DynamicCamera : MonoBehaviour
         }
         public override void Update()
         {
-            targetVector = target.transform.position;
-            mono.transform.position = targetVector + new Vector3(0f, 9f, 0f);
+            
         }
         
         public override void End()
@@ -281,6 +279,42 @@ public class DynamicCamera : MonoBehaviour
         }
     }
 
+    //This state locates directly above the robot and follows it
+    public class SateliteState : CameraState
+    {
+        Vector3 targetPosition;
+        Vector3 rotationVector;
+        GameObject target;
+
+        public SateliteState(MonoBehaviour mono)
+        {
+            this.mono = mono;
+        }
+
+        public override void Init()
+        {
+            target = GameObject.Find("Robot");
+            targetPosition = target.transform.position;
+            rotationVector = new Vector3(90f, 90f, 0f);
+            mono.transform.rotation = Quaternion.Euler(rotationVector);
+        }
+
+        public override void Update()
+        {
+            if(target != null && target.transform.childCount > 0)
+            {
+                targetPosition = target.transform.GetChild(0).transform.position;
+                
+            }
+            mono.transform.position = targetPosition + new Vector3(0f, 6f, 0f);
+            mono.transform.LookAt(targetPosition);
+        }
+
+        public override void End()
+        {
+
+        }
+    }
     void Start()
     {
         SwitchCameraState(new OrbitState(this));
