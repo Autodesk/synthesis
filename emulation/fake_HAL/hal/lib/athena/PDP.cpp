6f9904ccc9d73bb00b294f7b644bcf15f7f69253
Benjamin Lee <benjamin.fik.lee@gmail.com>
2017-07-10 23:50:53 +0100

    Completed PDP.cpp
    Former-commit-id: 09ef7af0078919146a45cc5a2fa266aa02b20d40

--- emulation/fake_HAL/hal/lib/athena/PDP.cpp
+++ emulation/fake_HAL/hal/lib/athena/PDP.cpp
@@ -20,7 +20,7 @@ using namespace hal;
 static std::unique_ptr<PDP> pdp[kNumPDPModules];
 
 static inline bool checkPDPInit(int32_t module, int32_t* status) {
-  if (!HAL_CheckPDPModule(module)) {
+  /*if (!HAL_CheckPDPModule(module)) {
     *status = RESOURCE_OUT_OF_RANGE;
     return false;
   }
@@ -28,19 +28,19 @@ static inline bool checkPDPInit(int32_t module, int32_t* status) {
     *status = INCOMPATIBLE_STATE;
     return false;
   }
-  return true;
+  return true;*/
 }
 
 extern "C" {
 
 void HAL_InitializePDP(int32_t module, int32_t* status) {
-  if (!HAL_CheckPDPModule(module)) {
+  /*if (!HAL_CheckPDPModule(module)) {
     *status = RESOURCE_OUT_OF_RANGE;
     return;
   }
   if (!pdp[module]) {
     pdp[module] = std::make_unique<PDP>(module);
-  }
+  }*/
 }
 
 HAL_Bool HAL_CheckPDPModule(int32_t module) {
@@ -52,76 +52,82 @@ HAL_Bool HAL_CheckPDPChannel(int32_t channel) {
 }
 
 double HAL_GetPDPTemperature(int32_t module, int32_t* status) {
-  if (!checkPDPInit(module, status)) return 0;
+  /*if (!checkPDPInit(module, status)) return 0;
 
   double temperature;
 
   *status = pdp[module]->GetTemperature(temperature);
 
-  return temperature;
+  return temperature;*/
+  return 0.0;
 }
 
 double HAL_GetPDPVoltage(int32_t module, int32_t* status) {
-  if (!checkPDPInit(module, status)) return 0;
+  /*if (!checkPDPInit(module, status)) return 0;
 
   double voltage;
 
   *status = pdp[module]->GetVoltage(voltage);
 
-  return voltage;
+  return voltage;*/
+  return 0.0;
 }
 
 double HAL_GetPDPChannelCurrent(int32_t module, int32_t channel,
                                 int32_t* status) {
-  if (!checkPDPInit(module, status)) return 0;
+  /*if (!checkPDPInit(module, status)) return 0;
 
   double current;
 
   *status = pdp[module]->GetChannelCurrent(channel, current);
 
-  return current;
+  return current;*/
+  return 0.0;
 }
 
 double HAL_GetPDPTotalCurrent(int32_t module, int32_t* status) {
-  if (!checkPDPInit(module, status)) return 0;
+  /*if (!checkPDPInit(module, status)) return 0;
 
   double current;
 
   *status = pdp[module]->GetTotalCurrent(current);
 
-  return current;
+  return current;*/
+  return 0.0;
 }
 
 double HAL_GetPDPTotalPower(int32_t module, int32_t* status) {
-  if (!checkPDPInit(module, status)) return 0;
+  /*if (!checkPDPInit(module, status)) return 0;
 
   double power;
 
   *status = pdp[module]->GetTotalPower(power);
 
-  return power;
+  return power;*/
+  return 0.0;
 }
 
 double HAL_GetPDPTotalEnergy(int32_t module, int32_t* status) {
-  if (!checkPDPInit(module, status)) return 0;
+  /*if (!checkPDPInit(module, status)) return 0;
 
   double energy;
 
   *status = pdp[module]->GetTotalEnergy(energy);
 
-  return energy;
+  return energy;*/
+  return 0.0;
 }
 
 void HAL_ResetPDPTotalEnergy(int32_t module, int32_t* status) {
-  if (!checkPDPInit(module, status)) return;
+  /*if (!checkPDPInit(module, status)) return;
 
-  *status = pdp[module]->ResetEnergy();
+  *status = pdp[module]->ResetEnergy();*/
 }
 
 void HAL_ClearPDPStickyFaults(int32_t module, int32_t* status) {
-  if (!checkPDPInit(module, status)) return;
+  /*if (!checkPDPInit(module, status)) return;
 
-  *status = pdp[module]->ClearStickyFaults();
+  *status = pdp[module]->ClearStickyFaults();*/
 }
 
 }  // extern "C"
