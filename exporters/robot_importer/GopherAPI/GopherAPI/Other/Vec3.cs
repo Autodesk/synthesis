f61f552469b64ea4a55749d7fed937de34f3049c
Ben Howard <thebenjhoward@gmail.com>
2017-07-13 08:22:07 -0700

    Unstable Alpha of GopherAPI completed
    The field reader can successfully break up a .field file into its constituent sections and process the .stl and thumbnail sections. Attributes and joints are untested due the fact that the field exporter can't seem to write them correctly. The robot exporter is completely untested and definitely will not work rn.

---
+++ exporters/robot_importer/GopherAPI/GopherAPI/Other/Vec3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GopherAPI.Other
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Vec3
+    {
+        private float[] values;
+        public object this[int index]
+        {
+            get { return values[index]; }
+            set { values[index] = (float)value; }
+        }
+        public Vec3()
+        {
+            values = new float[] { 0f, 0f, 0f };
+        }
+        public Vec3(float[] vec)
+        {
+            if (vec.Length == 3)
+                values = vec;
+            else
+                throw new Exception("ERROR: Expected 3 floats but got " + vec.Length.ToString());
+        }
+        public Vec3(float x, float y, float z)
+        {
+            values = new float[] { x, y, z };
+        }
+    }
+}
