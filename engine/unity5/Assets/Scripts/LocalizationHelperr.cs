927d40afc7241580c16e7944c02c6060fb67f5a5
IsabelleCordova <isabelleloveschocolate@gmail.com>
2017-07-13 15:37:25 -0700

    Unity UI
    Former-commit-id: 98457be2acc38a26856961ad0714820a50aaba03

---
+++ engine/unity5/Assets/Scripts/LocalizationHelperr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizationHelperr : MonoBehaviour
+{
+    [Tooltip("The drop down box to populate with camera view icons")]
+    public Dropdown dropDown;
+
+    [Tooltip("The icons of the different camera views")]
+    public Sprite[] icons;
+
+    void Start()
+    {
+        dropDown.ClearOptions();
+
+        List<Dropdown.OptionData> iconItems = new List<Dropdown.OptionData>();
+
+        foreach (var icon in icons)
+        {
+
+            string iconName = icon.name;
+            int dot = icon.name.IndexOf('.');
+            if (dot>=0)
+            {
+                iconName = iconName.Substring(dot + 1);
+            }
+
+            var iconOption = new Dropdown.OptionData(icon.name, icon);
+            iconItems.Add(iconOption);
+        }
+
+        dropDown.AddOptions(iconItems);
+    }
+
+}
