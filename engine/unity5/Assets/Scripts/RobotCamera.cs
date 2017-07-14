9ca871da5dc1b39494dd2e208a32ee384785c106
LuckyQXW <q1475963@gmail.com>
2017-07-14 09:13:28 -0700

    Change camera toggle so it can go to robot view
    Former-commit-id: 1e588f19e1da20daaedac2694854b3c101925507

--- engine/unity5/Assets/Scripts/RobotCamera.cs
+++ engine/unity5/Assets/Scripts/RobotCamera.cs
@@ -6,7 +6,10 @@ public class RobotCamera : MonoBehaviour
     private List<GameObject> robotCameraList = new List<GameObject>();
     public GameObject CurrentCamera { get; set; }
     
-
+    /// <summary>
+    /// Switching between different cameras on robot given the specific camera
+    /// </summary>
+    /// <param name="robotCamera"></param> the specific camera on the robot camera list
     public void SwitchCamera(GameObject robotCamera)
     {
         CurrentCamera.SetActive(false);
@@ -14,6 +17,22 @@ public class RobotCamera : MonoBehaviour
         CurrentCamera.SetActive(true);
     }
 
+    /// <summary>
+    /// Switch to the next camera on the list
+    /// </summary>
+    /// <param name="currentCamera"></param>
+    public void ToggleCamera()
+    {
+        SwitchCamera(robotCameraList[(robotCameraList.IndexOf(CurrentCamera) + 1) % robotCameraList.Count]);
+    }
+
+    /// <summary>
+    /// Add a new camera to the robot
+    /// </summary>
+    /// <param name="anchor"></param> The robot node to which the camera attaches
+    /// <param name="positionOffset"></param> 
+    /// <param name="rotationOffset"></param>
+    /// <returns></returns>
     public GameObject AddCamera(Transform anchor, Vector3 positionOffset, Vector3 rotationOffset)
     {
         GameObject newCamera = new GameObject("RobotCamera_" + robotCameraList.Count);
@@ -31,6 +50,10 @@ public class RobotCamera : MonoBehaviour
         return newCamera;
     }
 
+    /// <summary>
+    /// Add a new camera to the robot using the default position and rotation
+    /// </summary>
+    /// <returns></returns>
     public GameObject AddCamera()
     {
         GameObject newCamera = new GameObject("RobotCamera_" + robotCameraList.Count);
@@ -46,13 +69,13 @@ public class RobotCamera : MonoBehaviour
         return newCamera;
     }
 
-
-    void LateUpdate()
+    /// <summary>
+    /// Return true if the current camera is the last on the list
+    /// </summary>
+    /// <returns></returns> true if the current camera is the last on the list
+    public bool IsLastCamera()
     {
-        if (Input.GetKeyDown(Controls.ControlKey[(int)Controls.Control.CameraToggle]))
-        {
-            SwitchCamera(robotCameraList[(robotCameraList.IndexOf(CurrentCamera) + 1) % robotCameraList.Count]);
-        }
+        return robotCameraList.IndexOf(CurrentCamera) == robotCameraList.Count - 1;
     }
     
 }
