01d5652dacfe132877d3ce62f14e013dd4d9103c
Lauren Choquer <choquerlauren@gmail.com>
2017-07-17 08:18:25 -0700

    Added qemu-emulator folder
    Former-commit-id: b6bffa8f6cd53b340e956f6c4c0574ca15c164f8

---
+++ emulation/fake_HAL/qemu-emulator/start.bat
@@ -0,0 +1,17 @@
+@echo off
+
+call properties.bat
+
+qemu-2.6.0-win64\qemu-system-arm.exe^
+ -machine xilinx-zynq-a9^
+ -cpu cortex-a9^
+ -m %RAM_SIZE%^
+ -kernel linux/uImage^
+ -dtb linux/devicetree.dtb^
+ -display none^
+ -serial null^
+ -serial mon:stdio^
+ -localtime^
+ -append "console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0 rw"^
+ -redir tcp:%LOCAL_SSH_PORT%::22^
+ -sd %IMG_FILE%
