233c416418923df83a0ebc288f4362b184096cf5
LuckyQXW <q1475963@gmail.com>
2017-07-19 16:42:47 -0700

    Improve freeroam to fps-ish + tool tips
    Hold right mouse and it works exactly like fps :)
    
    
    Former-commit-id: 5a31d99164ed2ba2b31a2d3fe5c55e2d23397db2

--- engine/unity5/Assets/scripts/DynamicCamera.cs
+++ engine/unity5/Assets/scripts/DynamicCamera.cs
@@ -196,6 +196,7 @@ public class DynamicCamera : MonoBehaviour
 
         public override void Init()
         {
+            mono.transform.position = new Vector3(0f, 1f, 0f);
             positionVector = new Vector3(0f, 1f, 0f);
             lagPosVector = positionVector;
             rotationVector = Vector3.zero;
@@ -203,7 +204,7 @@ public class DynamicCamera : MonoBehaviour
             zoomValue = 60f;
             lagZoom = zoomValue;
             rotationSpeed = 3f;
-            transformSpeed = 0.25f;
+            transformSpeed = 2.5f;
             scrollWheelSensitivity = 40f;
         }
 
@@ -211,29 +212,23 @@ public class DynamicCamera : MonoBehaviour
         {
             if (MovingEnabled)
             {
-                if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
-                {
-                    positionVector += (Input.GetAxis("Mouse Y") * mono.transform.up) * transformSpeed;
-                    positionVector += (Input.GetAxis("Mouse X") * mono.transform.right) * transformSpeed;
-                }
-                else if (Input.GetMouseButton(0))
-                {
-                    rotationVector.y += Input.GetAxis("Mouse X") * rotationSpeed;
-                    positionVector += (Input.GetAxis("Mouse Y") * mono.transform.forward) * transformSpeed;
-                }
-                else if (Input.GetMouseButton(1))
+                if (Input.GetMouseButton(1))
                 {
                     rotationVector.x -= Input.GetAxis("Mouse Y") * rotationSpeed;
                     rotationVector.y += Input.GetAxis("Mouse X") * rotationSpeed;
                 }
 
+                positionVector += Input.GetAxis("CameraHorizontal") * mono.transform.right * transformSpeed * Time.deltaTime;
+                positionVector += Input.GetAxis("CameraVertical") * mono.transform.forward * transformSpeed * Time.deltaTime;
+
                 zoomValue = Mathf.Max(Mathf.Min(zoomValue - Input.GetAxis("Mouse ScrollWheel") * scrollWheelSensitivity, 60.0f), 10.0f);
 
-                lagPosVector = CalculateLagVector(lagPosVector, positionVector, lagResponsiveness);
+                //lagPosVector = CalculateLagVector(lagPosVector, positionVector, lagResponsiveness);
                 lagRotVector = CalculateLagVector(lagRotVector, rotationVector, lagResponsiveness);
                 lagZoom = CalculateLagScalar(lagZoom, zoomValue, lagResponsiveness);
 
-                mono.transform.position = lagPosVector;
+                mono.transform.position += positionVector;
+                positionVector = Vector3.zero;
                 mono.transform.eulerAngles = lagRotVector;
                 mono.GetComponent<Camera>().fieldOfView = lagZoom;
             }
@@ -264,7 +259,7 @@ public class DynamicCamera : MonoBehaviour
 
         public override void Init()
         {
-            positionVector = new Vector3(0f, 15f, 0f) + fieldVector;
+            positionVector = new Vector3(0f, 9f, 0f) + fieldVector;
             mono.transform.position = positionVector;
             rotationVector = new Vector3(90f, 90f, 0f);
             mono.transform.rotation = Quaternion.Euler(rotationVector);
@@ -334,8 +329,7 @@ public class DynamicCamera : MonoBehaviour
     {
         if (currentCameraState.GetType().Equals(typeof(DriverStationState))) SwitchCameraState(new OrbitState(this));
         else if (currentCameraState.GetType().Equals(typeof(OrbitState))) SwitchCameraState(new FreeroamState(this));
-        else if (currentCameraState.GetType().Equals(typeof(FreeroamState))) SwitchCameraState(new OverviewState(this));
-        else if (currentCameraState.GetType().Equals(typeof(OverviewState))) SwitchCameraState(new DriverStationState(this));
+        else if (currentCameraState.GetType().Equals(typeof(FreeroamState))) SwitchCameraState(new DriverStationState(this));
         if (_cameraState != null) _cameraState.Update();
     }
 
