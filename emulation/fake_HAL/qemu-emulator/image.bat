19755c8d3918032416a2cd5d0a3f0ca773520ce7
Benjamin Lee <benjamin.fik.lee@gmail.com>
2017-07-21 09:23:12 -0700

    Got a cygwin based makefile working for the HAL

--- emulation/fake_HAL/qemu-emulator/image.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo TODO: figure out if it's even possible to build disk images in Windows
-REM call properties.bat
-REM cscript //nologo unzip.vbs FRC_roboRIO_2016_v19.zip
-REM qemu-2.6.0-win64\qemu-system-arm.exe create -f qcow2 %IMG_FILE% %HDD_SIZE%
--- emulation/fake_HAL/qemu-emulator/properties.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set IMG_FILE=roborio.img
-set RAM_SIZE=256
-set HDD_SIZE=10G
-set LOCAL_SSH_PORT=10022
