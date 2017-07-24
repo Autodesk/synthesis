08b606aae9716ed3f6948c7ad1d085433751e33a
LuckyQXW <q1475963@gmail.com>
2017-07-24 14:39:35 -0700

    Camera tool tip improved
    Former-commit-id: 6ed8c6e9579a9fea2dfa030a1a1bf30e73ff5245

--- engine/unity5/Assets/Scripts/GUI/SimulatorUI.cs
+++ engine/unity5/Assets/Scripts/GUI/SimulatorUI.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class SimulatorUI : MonoBehaviour
 {
     private DynamicCamera dynamicCamera;
@@ -11,12 +13,14 @@ public class SimulatorUI : MonoBehaviour
     public MainState mainState;
 
     private GameObject canvas;
+    private GameObject cameraToolTip;
 
     // Use this for initialization
     void Start()
     {
 
         canvas = GameObject.Find("Canvas");
+        cameraToolTip = GameObject.Find("TooltipText (9)");
     }
 
     // Update is called once per frame
@@ -24,15 +28,12 @@ public class SimulatorUI : MonoBehaviour
     {
         if (dynamicCamera == null)
         {
-            dynamicCamera = GameObject.Find("Main Camera").AddComponent<DynamicCamera>();
+            dynamicCamera = GameObject.Find("Main Camera").GetComponent<DynamicCamera>();
         }
-        //dynamicCamera.cameraState
-        //if (Input.GetKey(Controls.ControlKey[Controls.Control.CameraToggle])) {
-        //  if (dynamicCamera.cameraState ) { 
-
-
-
-        //  }
+        //if (mainState == null)
+        //{
+        //    mainState = transform.GetComponent<StateMachine>().MainState;
+        //}
     }
 
     public void showControlPanel(bool show)
@@ -61,7 +62,7 @@ public class SimulatorUI : MonoBehaviour
     }
 
     //In game UI resets robot using UI icons
-    public void resetRobotClick()
+    public void ResetRobotClick()
     {
         mainState = stateMachine.GetComponent<StateMachine>().MainState;
         mainState.BeginReset();
@@ -75,6 +76,7 @@ public class SimulatorUI : MonoBehaviour
         {
             case 1:
                 dynamicCamera.SwitchCameraState(new DynamicCamera.DriverStationState(dynamicCamera));
+                DynamicCamera.MovingEnabled = true;
                 break;
             case 2:
                 dynamicCamera.SwitchCameraState(new DynamicCamera.OrbitState(dynamicCamera));
@@ -82,6 +84,11 @@ public class SimulatorUI : MonoBehaviour
                 break;
             case 3:
                 dynamicCamera.SwitchCameraState(new DynamicCamera.FreeroamState(dynamicCamera));
+                DynamicCamera.MovingEnabled = true;
+                break;
+            case 4:
+                dynamicCamera.SwitchCameraState(new DynamicCamera.OverviewState(dynamicCamera));
+                DynamicCamera.MovingEnabled = true;
                 break;
         }
     }
@@ -93,5 +100,39 @@ public class SimulatorUI : MonoBehaviour
         Application.OpenURL("http://bxd.autodesk.com/tutorials.html");
     }
 
+    public void CameraToolTips()
+    {
+        if (dynamicCamera.cameraState.GetType().Equals(typeof(DynamicCamera.DriverStationState)))
+        {
+            cameraToolTip.GetComponent<Text>().text = "Driver Station";
+            Debug.Log(cameraToolTip.GetComponent<Text>().text);
+        }
+        else if (dynamicCamera.cameraState.GetType().Equals(typeof(DynamicCamera.FreeroamState)))
+            cameraToolTip.GetComponent<Text>().text = "Freeroam";
+        else if (dynamicCamera.cameraState.GetType().Equals(typeof(DynamicCamera.OrbitState)))
+            cameraToolTip.GetComponent<Text>().text = "Orbit Robot";
+        else if (dynamicCamera.cameraState.GetType().Equals(typeof(DynamicCamera.OverviewState)))
+            cameraToolTip.GetComponent<Text>().text = "Overview";
+    }
+
+    public void ResetToolTips()
+    {
+        
+    }
+
+    public void ChooseResetMode(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                mainState.BeginReset();
+                mainState.EndReset();
+                break;
+            case 1:
+                mainState.IsResetting = true;
+                mainState.BeginReset();
+                break; 
+        }
+    }
 }
    
