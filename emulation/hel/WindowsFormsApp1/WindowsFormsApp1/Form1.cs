d02ef607ded11b8e8b845492f041e38bfa0990ec
Lauren Choquer <choquerlauren@gmail.com>
2017-07-31 14:10:42 -0700

    Added Windows form
    Former-commit-id: 98d372f74d44e6f26f4c5adb9f4ca432bcb452fe

---
+++ emulation/hel/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            String number = txtNumber.Text;
+            String path = txtBrowse.Text;
+
+            this.Close();
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtBrowse.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+       
