56e4368000179119bb56f37ab4b8e42499ae9eaf
Jasmine Cheng <jasminecheng@live.com>
2017-08-01 16:59:18 -0700

    Fixing stuff
    Former-commit-id: 275b163f235d67a9b20ba113e12a25079c1d1725

---
+++ engine/unity5/Assets/Scripts/GUI/QuickSwapScroll.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GUI
+{
+    class QuickSwapScroll : MonoBehaviour
+    {
+        GameObject GObject;
+        Vector2 TargetPosition;
+
+        public QuickSwapScroll(GameObject gObject, Vector2 targetPosition)
+        {
+            GObject = gObject;
+            TargetPosition = targetPosition;
+        }
+
+        void Update()
+        {
+            if (GObject.activeSelf == true && Math.Abs(GObject.GetComponent<RectTransform>().anchoredPosition.x - TargetPosition.x) > 3)
+            {
+                GObject.GetComponent<RectTransform>().anchoredPosition = (GObject.GetComponent<RectTransform>().anchoredPosition.x - TargetPosition.x > 0) ? (Vector3)GObject.GetComponent<RectTransform>().anchoredPosition + new Vector3(-3f, 0f, 0f) : (Vector3)GObject.GetComponent<RectTransform>().anchoredPosition + new Vector3(3f, 0f, 0f);
+            }
+        }
+
+    }
+}
