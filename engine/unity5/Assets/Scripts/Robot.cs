a9aaa5f03283d6b6493c83a161763ce0ad1b581c
Mackinnon Buck <mackinnonman@icloud.com>
2017-08-11 10:22:42 -0700

    Fixed collision tracking issue
    Fixed an issue with collision tracking in which there was a 1/6 chance of the tracker skipping a collision. That is now resolved.

--- engine/unity5/Assets/Scripts/Robot.cs
+++ engine/unity5/Assets/Scripts/Robot.cs
@@ -182,7 +182,7 @@ public class Robot : MonoBehaviour
                 node.MainObject.AddComponent<Tracker>().Trace = true;
         }
 
-        foreach (BRaycastRobot r in FindObjectsOfType<BRaycastRobot>())
+        foreach (BRaycastRobot r in GetComponentsInChildren<BRaycastRobot>())
             r.RaycastRobot.EffectiveMass = collectiveMass;
 
         RotateRobot(robotStartOrientation);
@@ -472,7 +472,7 @@ public class Robot : MonoBehaviour
             Debug.Log(t);
         }
 
-        foreach (BRaycastRobot r in GetComponentsInChildren<BRaycastRobot>())
+        foreach (BRaycastRobot r in manipulatorObject.GetComponentsInChildren<BRaycastRobot>())
             r.RaycastRobot.EffectiveMass = collectiveMass;
 
         RotateRobot(robotStartOrientation);
