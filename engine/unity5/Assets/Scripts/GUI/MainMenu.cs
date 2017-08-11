d89991691eb52296d47ba1f54f58d5f0530b0f17
LuckyQXW <q1475963@gmail.com>
2017-08-10 17:30:10 -0700

    Change file browser to highlight target folder
    The file browser will highlight 3 levels of parent folder containing
    target file (bxda, bxdj, bxdf), more levels will slow down performance
    when user click into a huge folder
    
    
    Former-commit-id: 3ec41af4eca4de35c7dafdd36d068376a3ccd7d6

--- engine/unity5/Assets/Scripts/GUI/MainMenu.cs
+++ engine/unity5/Assets/Scripts/GUI/MainMenu.cs
@@ -813,7 +813,7 @@ public class MainMenu : MonoBehaviour
         robotDirectory = PlayerPrefs.GetString("RobotDirectory", (System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "//synthesis//Robots"));
         robotDirectory = (Directory.Exists(robotDirectory)) ? robotDirectory : robotDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments); //If the robot directory no longer exists, set it to the default application path.
         fieldDirectory = PlayerPrefs.GetString("FieldDirectory", (System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "//synthesis//Fields"));
-        fieldDirectory = (Directory.Exists(fieldDirectory)) ? fieldDirectory : robotDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments); //if the field directory no longer exists, set it to the default application path.
+        fieldDirectory = (Directory.Exists(fieldDirectory)) ? fieldDirectory : fieldDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments); //if the field directory no longer exists, set it to the default application path.
 
         simSelectedField = PlayerPrefs.GetString("simSelectedField");
         simSelectedFieldName = (Directory.Exists(simSelectedField)) ? PlayerPrefs.GetString("simSelectedFieldName", "No Field Selected!") : "No Field Selected!";
