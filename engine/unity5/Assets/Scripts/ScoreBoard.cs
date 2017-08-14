50d13103853c41390ac385f0676a63deb83f5eab
Unknown <mesadog14@gmail.com>
2017-08-14 09:46:26 -0700

    set up high score function and database for future uses

--- engine/unity5/Assets/Scripts/ScoreBoard.cs
+++ engine/unity5/Assets/Scripts/ScoreBoard.cs
@@ -6,14 +6,29 @@ using UnityEngine.UI;
 public class ScoreBoard : MonoBehaviour {
 
     [SerializeField]
+
     private Text score = null; //this allows us to set the text feild in the editor.
 
+    private void Update()
+    {   
+        
+    }
+   
     // To change text do : score.text = "";
+
     int totalPoints = 0;
-    void addPoints(int points)
+
+    public void addPoints(int points)
     {
         totalPoints = totalPoints + points;
         string displayPoints = totalPoints.ToString();
         score.text = displayPoints;
+    }
+
+    int oldHighscore = PlayerPrefs.GetInt("highscore", 0);
+
+    public void StoreHighscore(int newHighscore)
+    {
+            PlayerPrefs.SetInt("highscore", newHighscore);
     }
 }
