911ee674f36cfc2dae8b2d699c5250a2404e4917
Unknown <mesadog14@gmail.com>
2017-08-14 09:27:39 -0700

    Added script to add points to the scoreboard

---
+++ engine/unity5/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+    [SerializeField]
+    private Text score = null; //this allows us to set the text feild in the editor.
+
+    // To change text do : score.text = "";
+    int totalPoints = 0;
+    void addPoints(int points)
+    {
+        totalPoints = totalPoints + points;
+        string displayPoints = totalPoints.ToString();
+        score.text = displayPoints;
+    }
+}
