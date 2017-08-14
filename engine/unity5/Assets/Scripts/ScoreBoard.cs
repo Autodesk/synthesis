25cf10b62f9c29d3999e61859702a114501343db
Unknown <mesadog14@gmail.com>
2017-08-14 16:32:31 -0700

    added game timer

--- engine/unity5/Assets/Scripts/ScoreBoard.cs
+++ engine/unity5/Assets/Scripts/ScoreBoard.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreBoard : MonoBehaviour {
+public class ScoreBoard : MonoBehaviour
+{
 
     [SerializeField]
 
-    private Text score = null; //this allows us to set the text feild in the editor.
-
+    private Text score = null; 
     private void Update()
-    {   
-        
+    {
+
     }
-   
+
     // To change text do : score.text = "";
 
     int totalPoints = 0;
@@ -29,6 +29,29 @@ public class ScoreBoard : MonoBehaviour {
 
     public void StoreHighscore(int newHighscore)
     {
-            PlayerPrefs.SetInt("highscore", newHighscore);
+        PlayerPrefs.SetInt("highscore", newHighscore);
+    }
+
+    public void GameStart()
+    {
+        GameBehaviour.GameStart();
+    }
+
+}
+public static class GameBehaviour : object
+{
+    public static void GameStart()
+    {
+        Image img = GameObject.Find("ScoreTextPanel").GetComponent<Image>();
+        img.color = UnityEngine.Color.green;
+        GameplayTimer.timeRunning = true;
+    }
+
+    public static void GameEnd()
+    {
+        GameplayTimer.timeLeft = 135.0;
+        GameplayTimer.timeRunning = false;
+        Image img = GameObject.Find("ScoreTextPanel").GetComponent<Image>();
+        img.color = UnityEngine.Color.red;
     }
 }
