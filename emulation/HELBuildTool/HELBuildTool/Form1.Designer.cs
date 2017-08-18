d2873554d910bd6b30828fd97fc86f2038890f6b
Benjamin Lee <benjamin.fik.lee@gmail.com>
2017-08-18 12:27:32 -0700

    Started work on java

--- emulation/HELBuildTool/HELBuildTool/Form1.Designer.cs
+++ emulation/HELBuildTool/HELBuildTool/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBrowseJava = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.javaButton = new System.Windows.Forms.RadioButton();
+            this.cppButton = new System.Windows.Forms.RadioButton();
             this.btnBrowse = new System.Windows.Forms.Button();
             this.txtBrowse = new System.Windows.Forms.TextBox();
             this.lbBrowse = new System.Windows.Forms.Label();
@@ -42,6 +46,10 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txtBrowseJava);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.javaButton);
+            this.groupBox1.Controls.Add(this.cppButton);
             this.groupBox1.Controls.Add(this.btnBrowse);
             this.groupBox1.Controls.Add(this.txtBrowse);
             this.groupBox1.Controls.Add(this.lbBrowse);
@@ -51,10 +59,52 @@
             this.groupBox1.Controls.Add(this.lbNumber);
             this.groupBox1.Location = new System.Drawing.Point(34, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(202, 207);
+            this.groupBox1.Size = new System.Drawing.Size(202, 330);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Build Information";
+            // 
+            // txtBrowseJava
+            // 
+            this.txtBrowseJava.Location = new System.Drawing.Point(21, 194);
+            this.txtBrowseJava.Name = "txtBrowseJava";
+            this.txtBrowseJava.Size = new System.Drawing.Size(100, 20);
+            this.txtBrowseJava.TabIndex = 11;
+            this.txtBrowseJava.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Enter the main Java class path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // javaButton
+            // 
+            this.javaButton.AutoSize = true;
+            this.javaButton.Location = new System.Drawing.Point(21, 254);
+            this.javaButton.Name = "javaButton";
+            this.javaButton.Size = new System.Drawing.Size(48, 17);
+            this.javaButton.TabIndex = 9;
+            this.javaButton.TabStop = true;
+            this.javaButton.Text = "Java";
+            this.javaButton.UseVisualStyleBackColor = true;
+            // 
+            // cppButton
+            // 
+            this.cppButton.AutoSize = true;
+            this.cppButton.Checked = true;
+            this.cppButton.Location = new System.Drawing.Point(21, 231);
+            this.cppButton.Name = "cppButton";
+            this.cppButton.Size = new System.Drawing.Size(44, 17);
+            this.cppButton.TabIndex = 8;
+            this.cppButton.TabStop = true;
+            this.cppButton.Text = "C++";
+            this.cppButton.UseVisualStyleBackColor = true;
+            this.cppButton.CheckedChanged += new System.EventHandler(this.cppButton_CheckedChanged);
             // 
             // btnBrowse
             // 
@@ -64,7 +114,7 @@
             this.btnBrowse.TabIndex = 7;
             this.btnBrowse.Text = "Browse";
             this.btnBrowse.UseVisualStyleBackColor = true;
-            this.btnBrowse.Click += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // txtBrowse
             // 
@@ -84,7 +134,7 @@
             // 
             // btnSetup
             // 
-            this.btnSetup.Location = new System.Drawing.Point(103, 170);
+            this.btnSetup.Location = new System.Drawing.Point(103, 291);
             this.btnSetup.Name = "btnSetup";
             this.btnSetup.Size = new System.Drawing.Size(75, 23);
             this.btnSetup.TabIndex = 3;
@@ -94,7 +144,7 @@
             // 
             // btnRunCode
             // 
-            this.btnRunCode.Location = new System.Drawing.Point(21, 170);
+            this.btnRunCode.Location = new System.Drawing.Point(21, 291);
             this.btnRunCode.Name = "btnRunCode";
             this.btnRunCode.Size = new System.Drawing.Size(75, 23);
             this.btnRunCode.TabIndex = 2;
@@ -117,13 +167,12 @@
             this.lbNumber.Size = new System.Drawing.Size(122, 13);
             this.lbNumber.TabIndex = 0;
             this.lbNumber.Text = "Enter your team number:";
-            //this.lbNumber.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(275, 238);
+            this.ClientSize = new System.Drawing.Size(275, 354);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "HELBuildTool";
@@ -144,6 +193,10 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.TextBox txtBrowse;
         private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.RadioButton javaButton;
+        private System.Windows.Forms.RadioButton cppButton;
+        private System.Windows.Forms.TextBox txtBrowseJava;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- emulation/HELBuildTool/HELBuildTool/Form1.cs
+++ emulation/HELBuildTool/HELBuildTool/Form1.cs
@@ -7,60 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FolderSelect;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        OpenFileDialog openFileDialog;
+        FolderSelect.FolderSelectDialog folderSelectDialog;
+        OpenFileDialog fileSelectDialog;
 
         public Form1()
         {
-            openFileDialog = new OpenFileDialog();
+            folderSelectDialog = new FolderSelectDialog();
+            fileSelectDialog = new OpenFileDialog();
             InitializeComponent();
+
+            txtBrowseJava.Enabled = false;
         }
 
         private void btnSetup_Click(object sender, EventArgs e)
         {
-          String number = txtNumber.Text;
-          String firstDigits = number.Substring(0, number.Length - 2);
-          String secondDigits = number.Substring(number.Length - 2, 2);
+            String number = txtNumber.Text;
+            String firstDigits = number.Substring(0, number.Length - 2);
+            String secondDigits = number.Substring(number.Length - 2, 2);
 
-          String deviceName;
+            String deviceName;
 
-          //get all loopback adapters to see if one already exists
-          System.Management.ManagementObjectCollection results = (new System.Management.ManagementObjectSearcher(
-              "SELECT Name, NetConnectionID, PNPDeviceID, ConfigManagerErrorCode, NetConnectionStatus " +
-              "FROM Win32_NetworkAdapter " +
-              "WHERE ServiceName='msloop'")).Get();
+            //get all loopback adapters to see if one already exists
+            System.Management.ManagementObjectCollection results = (new System.Management.ManagementObjectSearcher(
+                        "SELECT Name, NetConnectionID, PNPDeviceID, ConfigManagerErrorCode, NetConnectionStatus " +
+                        "FROM Win32_NetworkAdapter " +
+                        "WHERE ServiceName='msloop'")).Get();
 
-         if(results.Count > 0) {
-            deviceName = results.GetEnumerator().Current.Properties.Cast<System.Management.PropertyData>()
-              .Single(p => p.Name == "Name").Value.ToString();
-          }
-          else {
+            if(results.Count > 0) {
+                deviceName = results.GetEnumerator().Current.Properties.Cast<System.Management.PropertyData>()
+                    .Single(p => p.Name == "Name").Value.ToString();
+            }
+            else {
 
-            //enable a loopback adapter
-            System.Diagnostics.Process.Start("cmd", "/C devcon install %WINDIR%\\Inf\\NetLoop.inf *MSLOOP")
-              .WaitForExit(0);
+                //enable a loopback adapter
+                System.Diagnostics.Process.Start("cmd", "/C devcon install %WINDIR%\\Inf\\NetLoop.inf *MSLOOP")
+                    .WaitForExit(0);
 
-            results = (new System.Management.ManagementObjectSearcher(
-                "SELECT Name, NetConnectionID, PNPDeviceID, ConfigManagerErrorCode, NetConnectionStatus " +
-                "FROM Win32_NetworkAdapter " +
-                "WHERE ServiceName='msloop'")).Get();
-            deviceName = results.GetEnumerator().Current.Properties.Cast<System.Management.PropertyData>()
-              .Single(p => p.Name == "Name").Value.ToString();
-          }
+                results = (new System.Management.ManagementObjectSearcher(
+                            "SELECT Name, NetConnectionID, PNPDeviceID, ConfigManagerErrorCode, NetConnectionStatus " +
+                            "FROM Win32_NetworkAdapter " +
+                            "WHERE ServiceName='msloop'")).Get();
+                deviceName = results.GetEnumerator().Current.Properties.Cast<System.Management.PropertyData>()
+                    .Single(p => p.Name == "Name").Value.ToString();
+            }
 
-          //set the IP address of the loopback adapter
-          System.Diagnostics.Process.Start("netsh", "int ip set address name=\"" + deviceName +
-              "\" static 10." + firstDigits + "." + secondDigits + ".2 255.255.255.0");
+            //set the IP address of the loopback adapter
+            System.Diagnostics.Process.Start("netsh", "int ip set address name=\"" + deviceName +
+                    "\" static 10." + firstDigits + "." + secondDigits + ".2 255.255.255.0");
         }
 
         private void btnRunCode_Click(object sender, EventArgs e)
         {
             String number = txtNumber.Text;
+            String entryPoint = txtBrowseJava.Text;
             String path = txtBrowse.Text;
+
+            bool isJava = javaButton.Checked;
 
             Console.WriteLine(path);
             String currentDir = System.IO.Directory.GetCurrentDirectory();
@@ -68,48 +76,81 @@ namespace WindowsFormsApp1
             //convert currentDir to a cygwin path
             currentDir = currentDir.Replace("C:\\", "/cygdrive/c/").Replace("\\", "/");
             System.IO.Directory.SetCurrentDirectory(path);
-            Console.WriteLine(currentDir);
+            Console.WriteLine(entryPoint);
 
 #if DEBUG
             String cygwinPath = "C:\\cygwin64";
             String buildTool = currentDir + "/../../..";
             String makeArgs = "SYNTHESIS_LIBS=" + currentDir + "/../../../../../emulation/hel/build " +
-              "SYNTHESIS_INCLUDES=" + currentDir + "/../../../../../emulation/hel/ " +
-              "TEAM_ID_FILE=" + currentDir + "/../../../teamID.cpp";
+                "SYNTHESIS_INCLUDES=" + currentDir + "/../../../../../emulation/hel " +
+                "SYNTHESIS_JARS=" + currentDir + "/../../../../../emulation/hel/build/java " +
+                "TEAM_ID_FILE=" + currentDir + "/../../../teamID.cpp " +
+                "ENTRY_POINT=\"" + entryPoint + "\"";
 #else
             String cygwinPath = "C:\\Program Files (x86)\\Autodesk\\Synthesis\\cygwin64";
             String buildTool = "/cygdrive/c/Program Files (x86)/Autodesk/Synthesis/SynthesisDrive/HELBuildTool";
-            String makeArgs = "";
+            String makeArgs = "ENTRY_POINT=\"" + entryPoint + "\"";
 #endif
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo(
-                cygwinPath + "\\bin\\mintty.exe",
-                "'" + cygwinPath + "\\bin\\bash.exe' " +
-                "-c \"mount -c /cygdrive && make -f " +
-                "'" + buildTool + "/Makefile' " +
-                "TEAM_ID=" + number + " " +
-                makeArgs + " " +
-                "&& echo 'Starting robot code' && ./build/FRC_UserProgram " +
-                "|| read -p 'Press enter to exit'\"");
-            startInfo.EnvironmentVariables["PATH"] = cygwinPath + "\\bin";
+            System.Diagnostics.ProcessStartInfo startInfo;
+            if (isJava) {
+                startInfo = new System.Diagnostics.ProcessStartInfo(
+                        cygwinPath + "\\bin\\mintty.exe",
+                        "'" + cygwinPath + "\\bin\\bash.exe' " +
+                        "-c \"mount -c /cygdrive && make -f " +
+                        "'" + buildTool + "/Makefile.java' " +
+                        "TEAM_ID=" + number + " " +
+                        makeArgs + " run " +
+                        "|| read -p 'Press enter to exit'\"");
+            }
+            else {
+                startInfo = new System.Diagnostics.ProcessStartInfo(
+                        cygwinPath + "\\bin\\mintty.exe",
+                        "'" + cygwinPath + "\\bin\\bash.exe' " +
+                        "-c \"mount -c /cygdrive && make -f " +
+                        "'" + buildTool + "/Makefile' " +
+                        "TEAM_ID=" + number + " " +
+                        makeArgs + " " +
+                        "&& echo 'Starting robot code' && ./build/FRC_UserProgram " +
+                        "|| read -p 'Press enter to exit'\"");
+            }
+            String envPath = System.Environment.GetEnvironmentVariable("PATH");
+            startInfo.EnvironmentVariables["PATH"] = cygwinPath + "\\bin;" + envPath;
             startInfo.EnvironmentVariables["TEAM_ID"] = "" + number;
             startInfo.UseShellExecute = false;
 
             System.Diagnostics.Process.Start(startInfo);
+#if DEBUG
             System.IO.Directory.SetCurrentDirectory(windowsCurrentDir);
+#endif
         }
 
-        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        private void btnBrowse_Click(object sender, EventArgs e)
         {
             //openFileDialog.ShowDialog();
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            if (folderSelectDialog.ShowDialog())
             {
-              txtBrowse.Text = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+                txtBrowse.Text = folderSelectDialog.FileName;
             }
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void cppButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (javaButton.Checked)
+            {
+                txtBrowseJava.Enabled = true;
+            }
+            else
+            {
+                txtBrowseJava.Enabled = false;
+            }
         }
     }
 }
