279571c8b8eb7e8cd656982d3345af7f272b6b1f
JustinFlint <justin.flint1999@gmail.com>
2017-09-01 09:29:33 -0700

    Finished documentation and removed unused and empty forms

--- exporters/BxDFieldExporter/BxDFieldExporter/Forms/EnterName.cs
+++ exporters/BxDFieldExporter/BxDFieldExporter/Forms/EnterName.cs
@@ -25,7 +25,7 @@ namespace BxDFieldExporter
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        private void NameTextBox_TextChanged(object sender, EventArgs e)
         {
             if (nameTextBox.Text.Length > 0)
             {
@@ -36,6 +36,11 @@ namespace BxDFieldExporter
                 okButton.Enabled = false;
             }
         }
+        /// <summary>
+        /// Submits the name to the standard add-in server to create a component.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OKButton_OnClick(object sender, EventArgs e)
         {
             name = nameTextBox.Text;
@@ -43,11 +48,21 @@ namespace BxDFieldExporter
             Dispose(true);
             StandardAddInServer.AddComponent(name);
         }
+        /// <summary>
+        /// Closes the form and doesn't add a new component.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CancleButton_OnClick(object sender, EventArgs e)
         {
             Dispose(true);
         }
 
+        /// <summary>
+        /// Checks if the enter key is pressed whenever there is a keypress, and closes if it has been pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void EnterName_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
@@ -59,7 +74,12 @@ namespace BxDFieldExporter
                 Dispose(true);
             }
         }
-
+        /// <summary>
+        /// If the box is selected and escape is pressed, it closes the form and doesn't add a new component
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="keyData"></param>
+        /// <returns></returns>
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
             if (keyData == Keys.Escape && nameTextBox.Focused)
