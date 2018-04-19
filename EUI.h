46dab52ff8985792af7c066ed103eff101a9e023
Shawn Hice <shawnhice@gmail.com>
2018-04-19 13:53:39 -0700

    Removed the uneccessary files from the source, we cna have handlers in the main UI

--- EUI.h
+++ EUI.h
@@ -15,35 +15,42 @@ using namespace std;
 
 #ifndef EUI_h
 #define EUI_h
+namespace Synthesis{
+    
+    class EUI{
+    public:
+        EUI();
+        EUI(Ptr<UserInterface>, Ptr<Application>);
+        ~EUI();
+        
+    private:
+        Ptr<Application> _APP;
+        Ptr<UserInterface> _UI;
+        Ptr<Workspace> _WorkSpace;
+        Ptr<Workspaces> _WorkSpaces;
+        Ptr<ToolbarPanel> _ToolbarPanel;
+        Ptr<ToolbarControls> _ToolbarControls;
+        
+        Ptr<CommandDefinition> _AddWheelCommandDef;
+        Ptr<CommandDefinition> _ExportCommandDef;
+        
+        void configButtonWheel();
+        void configButtonExporter();
+        bool CreateWorkspace();
+        
+    };
 
-class EUI{
-public:
-    EUI();
-    EUI(Ptr<UserInterface>, Ptr<Application>);
-    ~EUI();
-    
-    
-    
-    
-    
-private:
-    Ptr<Application> _APP;
-    Ptr<UserInterface> _UI;
-    Ptr<Workspace> _WorkSpace;
-    Ptr<Workspaces> _WorkSpaces;
-    Ptr<ToolbarPanel> _ToolbarPanel;
-    Ptr<ToolbarControls> _ToolbarControls;
-    
-    Ptr<CommandDefinition> _AddWheelCommandDef;
-    Ptr<CommandDefinition> _ExportCommandDef;
-    
-    void configButtonWheel();
-    void configButtonExporter();
-    
-    
-    bool CreateWorkspace();
-    
-};
+    class CommandCreatedEventHandler : public adsk::core::CommandCreatedEventHandler{
+    public:
+        void notify(const Ptr<CommandCreatedEventArgs>& eventArgs) override;
+    private:
+    };
+
+    class CommandEventHandler : public adsk::core::CommandEventHandler{
+    public:
+        void notify(const Ptr<CommandEventArgs>& eventArgs) override;
+    };
+}
 
 
 #endif /* EUI_h */
