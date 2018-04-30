660bf3e02dfaa7abe18ebb97d33ae7a27a68e37b
Shawn Hice <shawnhice@gmail.com>
2018-04-30 08:05:29 -0700

    Maybe fix for windows
    Former-commit-id: 62cb13efdffe1a024f0286a1c54d765d635d7594

--- EUI.cpp
+++ EUI.cpp
@@ -40,7 +40,7 @@ bool EUI::CreateWorkspace(){
         
         
         //GUID = sha256(Synthesis:workspace) = 58D6985C9E7C0A2D731CC2141775F86F163FBA96CA871E8EC2840DF1FBEA2C0D
-        _WorkSpace = _UI->workspaces()->add(_APP->supportedProductTypes()[0], "10001", "Synthesis", "Resources");
+        _WorkSpace = _UI->workspaces()->add(_APP->activeProduct()->productType(), "10001", "Synthesis", "Resources");
         
         _WorkSpace->tooltip("Workspace for exporting fusion robot files");
         Ptr<ToolbarPanels> toolbarPanels = _WorkSpace->toolbarPanels();
