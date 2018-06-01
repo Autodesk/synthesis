3c3071c789143642b90d648f55da1f31fd1a30fc
Shawn Hice <shawnhice@gmail.com>
2018-05-31 23:45:38 -0700

    Actually added custom handlers
    Former-commit-id: feab4fe458555ebfa8d363bf0f0a1246618c9b20

--- CustomHandlers.h
+++ CustomHandlers.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "Exporter.h"
+
+using namespace adsk::core;
+using namespace adsk::fusion;
+using namespace adsk::cam;
+using namespace std;
+
+class EUI_Handlers {
+public:
+	EUI_Handlers(Ptr<Application>);
+	~EUI_Handlers();
+
+	MyCommandCreatedEventHandler _commandCreated;
+private:
+};
+
+class MyCommandCreatedEventHandler : public adsk::core::CommandCreatedEventHandler {
+public:
+	void notify(const Ptr<CommandCreatedEventArgs>& eventArgs) override;
+	Ptr<Application> _app;
+private:
+
+};
