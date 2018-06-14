a58ab0618fda9cdf534333ca506dd2ef380533f1
Shawn Hice <shawnhice@gmail.com>
2018-06-14 13:30:33 -0700

    Added a way to recursively add items to the vector3 List

--- LVector3.cpp
+++ LVector3.cpp
@@ -1,7 +1,13 @@
 #include "LVector3.h"
 
+#define NULL 0
+
 LVector3::LVector3() {
 
+}
+
+LVector3::LVector3(const LVector3* v) {
+	this->Head = v->Head;
 }
 
 LVector3::~LVector3() {
@@ -9,10 +15,21 @@ LVector3::~LVector3() {
 }
 
 //Add a Vector3 to the List using operator overloading
-LVector3 LVector3::operator+ (const Vector3* v) const {
-	
+LVector3* LVector3::operator+ (const Vector3* v) const {
+	Vector3 *v2 = new Vector3(v->x, v->y, v->z);  //should have just made a const copy constructor
+	LVector3 * a = new LVector3(this);
+
+	if(Head != NULL)
+		a->add(v2, Head);
+
+	if (Head == NULL) {
+		a->Head = v2;
+	}
 }
 
-void LVector3::add(Vector3* v) {
+void LVector3::add(Vector3* v, Vector3* tempHead) {
+	if (tempHead->next != NULL)
+		return(add(v, tempHead->next));
 
+	tempHead->next = v;
 }
