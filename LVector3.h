ea622b9a905fb475889afa6a4a1be099fc14681c
Shawn Hice <shawnhice@gmail.com>
2018-06-14 12:36:05 -0700

    Added more data

---
+++ LVector3.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Vector3.h" 
+
+//List equivalent for the Vector3 data type
+class LVector3 {
+public:
+	LVector3();
+	~LVector3();
+
+
+
+	//add [] operator
+	//add >> operator
+
+private:
+	Vector3 * Head;
+};
--- Vector3.h
+++ Vector3.h
@@ -2,7 +2,7 @@
 
 #define EPSILON 1.0E-6F;					//This is a tool that will come in handy later
 
-//Data Layer
+//Data point <x, y, z>
 class Vector3 {
 public:
 	Vector3();
@@ -10,9 +10,10 @@ public:
 	Vector3(Vector3*);
 	~Vector3();
 
-	bool add(float, float, float);			//Add a new vector to the dynamic list
+	//Add + operator
+	//Add * operator
+	//Add >> operator
 
-	//add operators for multiply and add and write
 private:
 	Vector3 * next;
 	Vector3 * prev;
