8ccea7d2cb01e6c0fcf62c59b4375a8e0d4e66fd
Shawn Hice <shawnhice@gmail.com>
2018-06-14 11:54:25 -0700

    More changes to data layer

---
+++ Vector3.cpp
@@ -0,0 +1,16 @@
+#include "Vector3.h"
+
+//default constructor
+Vector3::Vector3() {
+
+}
+
+//first item constructor JIC
+Vector3::Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {
+
+}
+
+//copy constructor just in case 
+Vector3::Vector3(Vector3*) {
+
+}
