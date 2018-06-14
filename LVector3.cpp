e42b04ceab7427bebb2b275d1696b8c221b364d5
Shawn Hice <shawnhice@gmail.com>
2018-06-14 13:11:16 -0700

    started making the data interface
    Former-commit-id: c4a830aeeb67a53a4457154ee0c0f1be1140f401

---
+++ LVector3.cpp
@@ -0,0 +1,18 @@
+#include "LVector3.h"
+
+LVector3::LVector3() {
+
+}
+
+LVector3::~LVector3() {
+
+}
+
+//Add a Vector3 to the List using operator overloading
+LVector3 LVector3::operator+ (const Vector3* v) const {
+	
+}
+
+void LVector3::add(Vector3* v) {
+
+}
