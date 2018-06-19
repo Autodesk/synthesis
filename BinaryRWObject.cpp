e2b76f11d811565c6d7e7990d0043f784d5720b7
Shawn Hice <shawnhice@gmail.com>
2018-06-19 12:54:14 -0700

    binary file writing

--- BinaryRWObject.cpp
+++ BinaryRWObject.cpp
@@ -3,24 +3,64 @@
 using namespace BXDATA;
 
 BinaryWriter::BinaryWriter(string file) {
-	OUT.open(file, ios::out | ios::binary);
+	ofs.open(file, ios::out | ios::binary);
 }
 
 BinaryWriter::~BinaryWriter() {
-	OUT.close();
+	ofs.close();
 }
 
 bool BinaryWriter::Write(BXDA * bxda) {
+	/*
+			int vertCount = verts.Length / 3;
+            byte meshFlags = (byte)((norms != null ? 1 : 0));
+
+            writer.Write(meshFlags);
+            writer.WriteArray(verts, 0, vertCount * 3);
+            if (norms != null)
+            {
+                writer.WriteArray(norms, 0, vertCount * 3);
+            }
+
+            writer.Write(surfaces.Count);
+            foreach (BXDASurface surface in surfaces)
+            {
+                surface.WriteData(writer);
+            }
+	*/
+
 	try {
-		OUT.write((char*)&bxda, sizeof(BXDA));
-		//OUT.write((char*)bxda->Version, sizeof(unsigned int));
-		//for (Submesh * s : bxda->meshes) {
-		//	//int count = s->verts->count();
-		//	unsigned char meshflag = (unsigned char)(s->norms != nullptr ? 1 : 0);
+		//OUT.write((char*)&bxda, sizeof(BXDA));
+		ofs.write((char*)bxda->Version, sizeof(unsigned int));
+		int countV, countN, countI;
+		int countS = bxda->meshes.size();
+		for (int i = 0; i < countS; i++) {
+			Submesh * temp = bxda->meshes[i];
+			//int count = s->verts->count();
+			countV = temp->verts.size() / 3;
+			countN = temp->norms.size() / 3;
+			countI = 0;
 
-		//	OUT.write((char*)meshflag, sizeof(unsigned char));
+			unsigned char meshflag = countN > 0 ? 1 : 0;
 
-		//}
+			ofs.write((char*)meshflag, sizeof(unsigned char));
+
+			//byte * buf = new byte[(countV*3) * sizeof(double)];
+
+			ofs.write((char*)(countV * 3), sizeof(int));
+			for (double d : temp->verts) {
+				//buf[countI] = d;
+				ofs.write(((char*)((int)d)), sizeof(double));		//loss of precision
+			}
+
+			ofs.write((char*)(countN * 3), sizeof(int));
+			for (double n : temp->norms) {
+				//buf[countI] = d;
+				ofs.write(((char*)((int)n)), sizeof(double));		//loss of precision
+			}
+			delete temp;
+
+		}
 
 		return 1;
 	}
