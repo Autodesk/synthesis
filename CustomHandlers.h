9a0c7ac13b2b2440623bc99b9fadea55364ac705
Shawn Hice <shawnhice@gmail.com>
2018-06-20 14:20:06 -0700

    Added prototypes for event handlers
    Former-commit-id: 97711c43f3d093cd0701e581509b5458b4047e13

--- CustomHandlers.h
+++ CustomHandlers.h
@@ -8,6 +8,9 @@ using namespace adsk::cam;
 using namespace std;
 
 namespace Synthesis {
+
+	//____________________Button Events_________________________
+
 	class ExportCommandCreatedEventHandler : public adsk::core::CommandCreatedEventHandler {
 	public:
 		void notify(const Ptr<CommandCreatedEventArgs>& eventArgs) override;
@@ -22,5 +25,28 @@ namespace Synthesis {
 		Ptr<Application> _APP;
 	private:
 
+	};
+
+
+	//________________Palette Events__________________________
+
+	class MyCloseEventHandler : public adsk::core::UserInterfaceGeneralEventHandler{
+	public:
+		void notify(const Ptr<UserInterfaceGeneralEventArgs>& eventArgs) override;
+		Ptr<Application> _APP;
+	};
+
+	class MyHTMLEventHandler : public adsk::core::HTMLEventHandler
+	{
+	public:
+		void notify(const Ptr<HTMLEventArgs>& eventArgs) override;
+		Ptr<Application> _APP;
+	};
+
+	class ShowPaletteCommandExecuteHandler : public adsk::core::CommandEventHandler
+	{
+	public:
+		void notify(const Ptr<CommandEventArgs>& eventArgs) override;
+		Ptr<Application> _APP;
 	};
 }
