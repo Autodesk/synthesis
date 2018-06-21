5eaedc4347fbce339a597a74ff1365a8ff3f6ed1
copernicium <logan.traffas@gmail.com>
2018-06-21 14:33:07 -0700

    Begin work on wpilib-like HAL calls for testing

---
+++ emulation/Minerva/Minerva/Test.h
@@ -0,0 +1,19 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "HAL/Relay.h"
+
+namespace minerva{
+	struct Relay{
+		enum Value { kOff, kOn, kForward, kReverse };
+		enum Direction { kBothDirections, kForwardOnly, kReverseOnly };
+		
+		void Set(Value);
+		
+		Direction m_direction;
+		HAL_RelayHandle m_forwardHandle = HAL_kInvalidHandle;
+		HAL_RelayHandle m_reverseHandle = HAL_kInvalidHandle;
+	};
+}
+
+#endif
