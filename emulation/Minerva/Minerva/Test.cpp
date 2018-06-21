a386edaceab8f20eac66128612cfffe129f7eec4
copernicium <logan.traffas@gmail.com>
2018-06-21 15:56:50 -0700

    Reformat wpilib test functions and handle an overload error

--- emulation/Minerva/Minerva/Test.cpp
+++ emulation/Minerva/Minerva/Test.cpp
@@ -3,49 +3,49 @@
 using namespace std;
 
 void minerva::Relay::Set(Relay::Value value) {
-//if (StatusIsFatal()) return;
+	//if (StatusIsFatal()) return;
 
-int32_t status = 0;
+	int32_t status = 0;
 
-switch (value) {
-	case kOff:
-		if (m_direction == kBothDirections || m_direction == kForwardOnly) {
-			HAL_SetRelay(m_forwardHandle, false, &status);
-		}
-		if (m_direction == kBothDirections || m_direction == kReverseOnly) {
-			HAL_SetRelay(m_reverseHandle, false, &status);
-		}
-		break;
-	case kOn:
-		if (m_direction == kBothDirections || m_direction == kForwardOnly) {
-			HAL_SetRelay(m_forwardHandle, true, &status);
-		}
-		if (m_direction == kBothDirections || m_direction == kReverseOnly) {
-			HAL_SetRelay(m_reverseHandle, true, &status);
-		}
-		break;
-	case kForward:
-		if (m_direction == kReverseOnly) {
+	switch (value) {
+		case kOff:
+			if (m_direction == kBothDirections || m_direction == kForwardOnly) {
+				HAL_SetRelay(m_forwardHandle, false, &status);
+			}
+			if (m_direction == kBothDirections || m_direction == kReverseOnly) {
+				HAL_SetRelay(m_reverseHandle, false, &status);
+			}
 			break;
-		}
-		if (m_direction == kBothDirections || m_direction == kForwardOnly) {
-			HAL_SetRelay(m_forwardHandle, true, &status);
-		}
-		if (m_direction == kBothDirections) {
-			HAL_SetRelay(m_reverseHandle, false, &status);
-		}
-		break;
-	case kReverse:
-		if (m_direction == kForwardOnly) {
+		case kOn:
+			if (m_direction == kBothDirections || m_direction == kForwardOnly) {
+				HAL_SetRelay(m_forwardHandle, true, &status);
+			}
+			if (m_direction == kBothDirections || m_direction == kReverseOnly) {
+				HAL_SetRelay(m_reverseHandle, true, &status);
+			}
 			break;
-		}
-		if (m_direction == kBothDirections) {
-			HAL_SetRelay(m_forwardHandle, false, &status);
-		}
-		if (m_direction == kBothDirections || m_direction == kReverseOnly) {
-			HAL_SetRelay(m_reverseHandle, true, &status);
-		}
-		break;
+		case kForward:
+			if (m_direction == kReverseOnly) {
+				break;
+			}
+			if (m_direction == kBothDirections || m_direction == kForwardOnly) {
+				HAL_SetRelay(m_forwardHandle, true, &status);
+			}
+			if (m_direction == kBothDirections) {
+				HAL_SetRelay(m_reverseHandle, false, &status);
+			}
+			break;
+		case kReverse:
+			if (m_direction == kForwardOnly) {
+				break;
+			}
+			if (m_direction == kBothDirections) {
+				HAL_SetRelay(m_forwardHandle, false, &status);
+			}
+			if (m_direction == kBothDirections || m_direction == kReverseOnly) {
+				HAL_SetRelay(m_reverseHandle, true, &status);
+			}
+			break;
 	}
 }
 
