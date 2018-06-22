96caa768da2a1ffce33483aeb8561a8d3e2be2e1
copernicium <logan.traffas@gmail.com>
2018-06-22 15:19:37 -0700

    Move HALType and StatusFrame to new file types.h an avoid potential namespace problems

---
+++ emulation/Minerva/Minerva/include/types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <HAL/Encoder.h>
+#include <HAL/HAL.h>
+#include <HAL/handles/HandlesInternal.h>
+
+#include <variant>
+#include <string>
+
+namespace minerva{
+	using HALType = std::variant<int, unsigned int, int*, unsigned long int, double, char*, bool, HAL_EncoderEncodingType, long, HAL_RuntimeType, const char *, hal::HAL_HandleEnum, HAL_AllianceStationID>;
+	using StatusFrame = std::map<std::string, HALType>;
+}
+
+#endif
