cb6a117110179e53381c40445fb0cebbd6f55140
matthew17754 <matmoradi17@gmail.com>
2018-06-24 17:33:38 -0700

    Implemented JSON Deserializer
    Includes Newtonsoft JSON.NET NuGet Packages

---
+++ emulation/Minerva/SimBridge/DeserializerJSON.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace SimBridge
+{
+    class DeserializerJSON
+    {
+        public void deserializeJSON()
+        {
+            try
+            {
+                //var json = JsonConvert.DeserializeObject<dynamic>(strJSON);
+                using (StreamReader r = new StreamReader("file.json"))
+                {
+                    string strJSON = r.ReadToEnd();
+                    var jItems = JsonConvert.DeserializeObject<JSONdata>(strJSON);
+                }
+            }
+            catch(Exception ex)
+            {
+                string error = ex.Message.ToString();
+            }
+        }
+    }
+}
---
+++ emulation/Minerva/SimBridge/JSONdata.cs
@@ -0,0 +1,11 @@
+﻿namespace SimBridge
+{
+    public class JSONdata
+    {
+        public int thing1 { get; set; }
+        public string thing2 { get; set; }
+        public string thing3 { get; set; }
+        public float thing4 { get; set; }
+        public float thing5 { get; set; }
+    }
+}
