a3baa395da9a754d9a159afeceb8225d56746807
matthew17754 <matmoradi17@gmail.com>
2018-06-25 14:55:48 -0700

    Disabled Robot Exporter Forms Resizability
    Disables the maximize and minimize features on the robot exporter forms as well as their resizability.

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/AboutDialog.Designer.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/AboutDialog.Designer.cs
@@ -40,7 +40,7 @@
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.pictureBox1.Image = global::JointResolver.Properties.Resources.SmallIcon;
             this.pictureBox1.Location = new System.Drawing.Point(82, 24);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(48, 52);
             this.pictureBox1.TabIndex = 0;
@@ -49,7 +49,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(146, 179);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
@@ -63,7 +63,7 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(9, 110);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(194, 19);
             this.textBox1.TabIndex = 4;
@@ -78,8 +78,11 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "AboutDialog";
             this.ShowIcon = false;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
