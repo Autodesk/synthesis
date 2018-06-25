c616dd0dc76e917459e8aacb49d998832bf29b84
copernicium <logan.traffas@gmail.com>
2018-06-25 15:25:13 -0700

    Begin work on PWM interface

--- emulation/Minerva/Minerva/include/handler.h
+++ emulation/Minerva/Minerva/include/handler.h
@@ -8,6 +8,8 @@
 #include "types.h"
 #include "function_signature.h"
 #include "channel.h"
+#include "constants.h"
+#include "handle.h"
 
 unsigned constexpr hasher(char const *input) {
     return *input ?
@@ -18,7 +20,6 @@ unsigned constexpr hasher(char const *input) {
 void callFunc(std::string function_name, std::vector<minerva::FunctionSignature::ParameterValueInfo> params);
 template<typename T>
 void callFunc(std::string function_name, std::vector<minerva::FunctionSignature::ParameterValueInfo> params, minerva::Channel<T> &chan);
-
 
 minerva::StatusFrame __current_status_frame;
 
@@ -59,21 +60,283 @@ void printType(minerva::HALType ht) {
 
 void callFunc(const char * function_name, std::vector<minerva::FunctionSignature::ParameterValueInfo> params) {
     switch(hasher(function_name)) {
-        case(hasher("HAL_SetRelay")):
-            std::string array_index = "Relay ";
-            array_index += std::get<int>(params[0].value);
-            __current_status_frame[array_index] = params[1].value;
-            return;
-    }
+        case hasher("HAL_SetRelay"):
+            {
+				std::string array_index = "Relay ";
+            	array_index += std::to_string(std::get<int>(params[0].value));
+            	__current_status_frame[array_index] = params[1].value;
+            	std::cout<<"\n\""<<array_index<<"\"\n";
+				return;
+			}
+		case hasher("HAL_FreeRelayPort"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_SetPWMDisabled"):
+			{
+				HAL_SetPWMRaw(std::get<int>(params[0].value),minerva::constants::HAL::kPwmDisabled,std::get<int*>(params[1].value));
+				return;//TODO
+			}
+		case hasher("HAL_SetPWMElimintateDeadband"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_SetPWMRaw"):
+			{
+				minerva::Handle handle;
+				handle.packed = {std::get<int>(params[0].value)};
+				if(handle.unpacked.channel < minerva::constants::HAL::kNumPWMHeaders){
+					std::string array_index = "PWM ";
+					array_index += std::to_string(handle.unpacked.channel);
+					__current_status_frame[array_index] = params[1].value;
+				} else {
+					throw "MXP Unsupported";
+				}
+				return;
+			}
+   		case hasher("HAL_SetPWMPosition"):
+			{
+				int32_t port_handle = std::get<int32_t>(params[0].value);
+				double pos = std::get<double>(params[1].value);
+				if(pos < 0.0){
+					pos = 0.0;
+				} else if(pos > 1.0){
+					pos = 1.0;
+				}
+				int32_t raw_value = 0;//TODO math
+				HAL_SetPWMRaw(port_handle,raw_value,std::get<int32_t*>(params[2].value));
+				return;
+			}
+		case hasher("HAL_FreePWMPort"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_SetPWMSpeed"):
+			{
+				int32_t port_handle = std::get<int32_t>(params[0].value);
+				double speed = std::get<double>(params[1].value);
+				if(speed < -1.0){
+					speed = -1.0;
+				} else if(speed > 1.0){
+					speed = 1.0;
+				} else if(!std::isfinite(speed)){
+					speed = 0.0;
+				}
+				int32_t raw_value = 0;//TODO math
+				HAL_SetPWMRaw(port_handle,raw_value,std::get<int32_t*>(params[2].value));
+				return;
+			}
+		case hasher("HAL_SetPWMPeriodicScale"):
+			{
+				minerva::Handle handle;
+				handle.packed = {std::get<int>(params[0].value)};
+				if(handle.unpacked.channel < minerva::constants::NI_FPGA::PWM::kNumPeriodScaleHdrElements){
+					//TODO
+				} else {
+					throw "MXP Unsupported";
+				}
+				return;
+			}
+		case hasher("HAL_LatchPWMZero"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_SetPWMConfig"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_SetPWMConfigRaw"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_GetPWMConfigRaw"):
+			{
+				return;//TODO
+			}
+	}
+}
+
+template<typename T,typename U>
+void channelReturn(minerva::Channel<T>& channel, U val){
+	minerva::HALType a = val;
+	channel.put(std::get<T>(a));
 }
 
 template<typename T>
 void callFunc(const char *function_name, std::vector<minerva::FunctionSignature::ParameterValueInfo> params, minerva::Channel<T> &chan) {
     switch(hasher(function_name)) {
-        case(hasher("HAL_GetRelay")):
-            std::string array_index = "Relay ";
-            array_index += std::get<HAL_Bool>(params[0].value);
-            chan.put(std::get<T>(__current_status_frame[array_index]));
-            return;
-    }
+        case hasher("HAL_GetRelay"):
+            {
+				std::string array_index = "Relay ";
+            	array_index += std::to_string(std::get<HAL_Bool>(params[0].value));
+            	chan.put(std::get<T>(__current_status_frame[array_index]));
+            	std::cout<<"\n\""<<array_index<<"\"\n";
+				return;
+			}
+    	case hasher("HAL_CheckRelayChannel"):
+			{
+				int32_t channel = std::get<int>(params[0].value);
+				channelReturn(chan,channel < minerva::constants::HAL::kNumRelayHeaders && channel >= 0);
+				return;
+			}
+		case hasher("HAL_GetPort"):
+			{
+				int32_t channel = std::get<int>(params[0].value);
+				if(channel < 0 || channel >= 255){
+					channelReturn(chan,HAL_kInvalidHandle);
+				} else {
+					channelReturn(chan,channel);
+				}
+				return;
+			}
+		case hasher("HAL_InitializePWMPort"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_GetErrorMessage"):
+			{
+				const char* message = [&]{
+					switch(std::get<int>(params[0].value)){
+						case 0:
+							return "";
+					}
+					return "";//TODO error handling
+				}();
+				channelReturn(chan,message);
+				return;
+			}
+		case hasher("HAL_Report"):
+			{
+				channelReturn(chan,0);//definition hidden in NI FPGA
+				return;
+			}	
+		case hasher("HAL_GetPWMRaw"):
+			{
+				minerva::Handle handle;
+				handle.packed = {std::get<int>(params[0].value)};
+				if(handle.unpacked.channel < minerva::constants::HAL::kNumPWMHeaders){
+					std::string array_index = "PWM ";
+					array_index += std::to_string(handle.unpacked.channel);
+					chan.put(std::get<T>(__current_status_frame[array_index]));
+				} else {
+					throw "MXP Unsupported";
+				}
+				return;
+			}
+		case hasher("HAL_GetPWMPosition"):
+			{
+				int32_t port_handle = {std::get<int>(params[0].value)};
+				
+				int32_t value = HAL_GetPWMRaw(port_handle,std::get<int32_t*>(params[1].value));
+
+				channelReturn(chan,(double)value);//TODO math
+				return;
+			}
+		case hasher("HAL_GetPWMSpeed"):
+			{
+				minerva::Handle handle;
+				handle.packed = {std::get<int>(params[0].value)};
+				
+				int32_t value = HAL_GetPWMRaw(handle.packed,std::get<int32_t*>(params[1].value));
+
+				channelReturn(chan,(double)value);//TODO math
+				
+				return;
+			}
+		case hasher("HAL_InitializeRelayPort"):
+			{
+				return;//TODO
+			}
+		case hasher("HAL_GetNumAccumulators"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumAccumulators);
+				return;
+			}
+		case hasher("HAL_GetNumAnalogTriggers"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumAnalogTriggers);
+				return;
+			}
+		case hasher("HAL_GetNumAnalogInputs"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumAnalogInputs);
+				return;
+			}
+		case hasher("HAL_GetNumAnalogOutputs"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumAnalogOutputs);
+				return;
+			}
+		case hasher("HAL_GetNumCounters"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumCounters);
+				return;
+			}
+		case hasher("HAL_GetNumDigitalHeaders"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumDigitalHeaders);
+				return;
+			}
+		case hasher("HAL_GetNumPWMHeaders"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumPWMHeaders);
+				return;
+			}
+		case hasher("HAL_GetNumDigitalChannels"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumDigitalChannels);
+				return;
+			}
+		case hasher("HAL_GetNumPWMChannels"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumPWMChannels);
+				return;
+			}
+		case hasher("HAL_GetNumDigitalPWMOutputs"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumDigitalPWMOutputs);
+				return;
+			}
+		case hasher("HAL_GetNumEncoders"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumEncoders);
+				return;
+			}
+		case hasher("HAL_GetNumInterrupts"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumInterrupts);
+				return;
+			}
+		case hasher("HAL_GetNumRelayChannels"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumRelayChannels);
+				return;
+			}
+		case hasher("HAL_GetNumRelayHeaders"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumRelayHeaders);
+				return;
+			}
+		case hasher("HAL_GetNumPCMModules"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumPCMModules);
+				return;
+			}
+		case hasher("HAL_GetNumSolenoidChannels"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumSolenoidChannels);
+				return;
+			}
+		case hasher("HAL_GetNumPDPModules"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumPDPModules);
+				return;
+			}
+		case hasher("HAL_GetNumPDPChannels"):
+			{
+				channelReturn(chan,minerva::constants::HAL::kNumPDPChannels);
+				return;
+			}
+	}
 }
+
