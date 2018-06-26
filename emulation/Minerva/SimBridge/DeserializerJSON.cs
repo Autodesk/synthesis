f3793f20cca4d49910bc5d4fa3d0486c506288ac
matthew17754 <matmoradi17@gmail.com>
2018-06-26 14:52:04 -0700

    Implemented Network Reciever Over UDP Socket
    Recieves the JSON data over a raw socket and deserializes it to a cusom .NET object.

--- emulation/Minerva/SimBridge/DeserializerJSON.cs
+++ emulation/Minerva/SimBridge/DeserializerJSON.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Newtonsoft.Json;
 using System.IO;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
 
 namespace SimBridge
 {
@@ -8,21 +11,42 @@ namespace SimBridge
     {
         public void deserializeJSON()
         {
-            try
-            {
-                //var json = JsonConvert.DeserializeObject<dynamic>(strJSON);
-                using (StreamReader r = new StreamReader("file.json"))
+                //This constructor arbitrarily assigns the local port number.
+                UdpClient udpClient = new UdpClient();
+                udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 11000));
+                try
                 {
-                    string strJSON = r.ReadToEnd();
-                    var jItems = JsonConvert.DeserializeObject<JSONdata>(strJSON);
+                    //IPEndPoint object will allow us to read datagrams sent from any source.
+                    IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                    string strJSON = String.Empty;
+                    do
+                    {
+                        //Blocks until a message returns on this socket from a remote host.
+                        Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                        strJSON = Encoding.ASCII.GetString(receiveBytes);
+                        var jItems = JsonConvert.DeserializeObject<JSONdata>(strJSON);
                 }
-            }
+                    while (strJSON[strJSON.Length] != '\0');
+                    udpClient.Close();
+                }
+
             catch(Exception ex)
             {
                 string error = ex.Message.ToString();
             }
         }
     }
+
+    //var json = JsonConvert.DeserializeObject<dynamic>(strJSON);
+    /*
+    using (StreamReader r = new StreamReader("file.json"))
+    {
+        string strJSON = r.ReadToEnd();
+        var jItems = JsonConvert.DeserializeObject<JSONdata>(strJSON);
+    }
+    */
 
     public class JSONdata
     {
