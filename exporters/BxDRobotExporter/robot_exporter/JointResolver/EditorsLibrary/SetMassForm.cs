94266ab8b5483585e8405e4670914e22c5c1968d
Nathan Walker <nathanwalker1208@gmail.com>
2018-06-27 08:42:47 -0700

    Added basic mass settings window. Will be fleshed out more in next commit

---
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/SetMassForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorsLibrary
+{
+    public partial class SetMassForm : Form
+    {
+        public float TotalMass = 0;
+
+        public SetMassForm()
+        {
+            InitializeComponent();
+
+            TotalMass = SynthesisGUI.Instance.TotalMass;
+            MassBox.Value = (decimal) TotalMass;
+        }
+
+        private void MassBox_ValueChanged(object sender, EventArgs e)
+        {
+            TotalMass = (float) MassBox.Value;
+        }
+    }
+}
