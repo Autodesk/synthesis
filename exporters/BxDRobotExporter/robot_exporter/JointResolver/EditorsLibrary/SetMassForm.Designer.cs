946ed81f629ed7865fc4dd91e6a77018b56d0527
Nathan Walker <nathanwalker1208@gmail.com>
2018-06-27 09:55:18 -0700

    Added a metric units checkbox
    Former-commit-id: 13ef40e29d16cda7985be0e6c7a4e356110b50de

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/SetMassForm.Designer.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/SetMassForm.Designer.cs
@@ -35,6 +35,8 @@
             this.CancelButton = new System.Windows.Forms.Button();
             this.SaveButton = new System.Windows.Forms.Button();
             this.MassLabel = new System.Windows.Forms.Label();
+            this.UnitLabel = new System.Windows.Forms.Label();
+            this.MetricBox = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.MassBox)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -42,7 +44,7 @@
             // 
             // MassBox
             // 
-            this.MassBox.Location = new System.Drawing.Point(64, 4);
+            this.MassBox.Location = new System.Drawing.Point(42, 4);
             this.MassBox.Margin = new System.Windows.Forms.Padding(4);
             this.MassBox.Maximum = new decimal(new int[] {
             200,
@@ -57,44 +59,48 @@
             // 
             this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.Controls.Add(this.MassBox, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.MassLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.UnitLabel, 2, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(268, 57);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(272, 57);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.AutoSize = true;
             this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel2.ColumnCount = 3;
-            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 2);
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 3);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel2.Controls.Add(this.CancelButton, 2, 0);
-            this.tableLayoutPanel2.Controls.Add(this.SaveButton, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.CancelButton, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.MetricBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.SaveButton, 2, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 28);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(268, 29);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(272, 29);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // CancelButton
             // 
             this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CancelButton.Location = new System.Drawing.Point(190, 3);
+            this.CancelButton.Location = new System.Drawing.Point(194, 3);
             this.CancelButton.Name = "CancelButton";
             this.CancelButton.Size = new System.Drawing.Size(75, 23);
             this.CancelButton.TabIndex = 1;
@@ -103,7 +109,7 @@
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(109, 3);
+            this.SaveButton.Location = new System.Drawing.Point(113, 3);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(75, 23);
             this.SaveButton.TabIndex = 2;
@@ -118,10 +124,34 @@
             this.MassLabel.Location = new System.Drawing.Point(3, 3);
             this.MassLabel.Margin = new System.Windows.Forms.Padding(3);
             this.MassLabel.Name = "MassLabel";
-            this.MassLabel.Size = new System.Drawing.Size(54, 22);
+            this.MassLabel.Size = new System.Drawing.Size(32, 22);
             this.MassLabel.TabIndex = 1;
-            this.MassLabel.Text = "Mass (lbs)";
+            this.MassLabel.Text = "Mass";
             this.MassLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // UnitLabel
+            // 
+            this.UnitLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.UnitLabel.Location = new System.Drawing.Point(249, 3);
+            this.UnitLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.UnitLabel.Name = "UnitLabel";
+            this.UnitLabel.Size = new System.Drawing.Size(20, 22);
+            this.UnitLabel.TabIndex = 3;
+            this.UnitLabel.Text = "lbs";
+            this.UnitLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MetricBox
+            // 
+            this.MetricBox.AutoSize = true;
+            this.MetricBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MetricBox.Location = new System.Drawing.Point(3, 3);
+            this.MetricBox.Name = "MetricBox";
+            this.MetricBox.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.MetricBox.Size = new System.Drawing.Size(58, 23);
+            this.MetricBox.TabIndex = 3;
+            this.MetricBox.Text = "Metric";
+            this.MetricBox.UseVisualStyleBackColor = true;
+            this.MetricBox.CheckedChanged += new System.EventHandler(this.MetricBox_CheckedChanged);
             // 
             // SetMassForm
             // 
@@ -130,7 +160,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(278, 69);
+            this.ClientSize = new System.Drawing.Size(288, 73);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -142,6 +172,7 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,5 +186,7 @@
         private System.Windows.Forms.Button CancelButton;
         private System.Windows.Forms.Button SaveButton;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label UnitLabel;
+        private System.Windows.Forms.CheckBox MetricBox;
     }
 }
--- exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/SetMassForm.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/SetMassForm.cs
@@ -12,6 +12,8 @@ namespace EditorsLibrary
 {
     public partial class SetMassForm : Form
     {
+        static bool IsMetric = false;
+
         public float TotalMass = 0;
 
         public SetMassForm()
@@ -19,12 +21,32 @@ namespace EditorsLibrary
             InitializeComponent();
 
             TotalMass = SynthesisGUI.Instance.TotalMass;
-            MassBox.Value = (decimal) TotalMass;
+            
+            if (IsMetric)
+                MassBox.Value = (decimal)(TotalMass / 2.20462);
+            else
+                MassBox.Value = (decimal)TotalMass;
+
+            MetricBox.Checked = IsMetric;
+        }
+
+        private void MetricBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (MetricBox.Checked)
+                UnitLabel.Text = "kg";
+            else
+                UnitLabel.Text = "lbs";
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            TotalMass = (float)MassBox.Value;
+            IsMetric = MetricBox.Checked;
+
+            if (IsMetric)
+                TotalMass = (float)MassBox.Value * 2.20462f;
+            else
+                TotalMass = (float)MassBox.Value;
+
             DialogResult = DialogResult.OK;
             Close();
         }
