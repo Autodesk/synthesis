a201c6b10fd7ac961e887d58e94ba1485e3b0c0c
copernicium <logan.traffas@gmail.com>
2018-07-02 15:58:21 -0700

    Begin work on tPWM and tDIO implementation

--- emulation/Minerva/Minerva/include/roborio.h
+++ emulation/Minerva/Minerva/include/roborio.h
@@ -13,19 +13,20 @@ namespace minerva{
 
 	struct RoboRIO{
 		struct AnalogOutput{
-				uint16_t mxp_value;
+			uint16_t mxp_value;
 		};
-        struct AnalogInputs {
-            struct AnalogInput{
-                uint8_t oversample_bits;
-                uint8_t average_bits;
-                uint8_t scan_list;
-            };
+		
+		struct AnalogInputs {
+			struct AnalogInput{
+				uint8_t oversample_bits;
+				uint8_t average_bits;
+				uint8_t scan_list;
+			};
 
-            void setConfig(tAI::tConfig value);
-            tAI::tConfig getConfig();
+			void setConfig(tAI::tConfig value);
+			tAI::tConfig getConfig();
 
-            void setReadSelect(tAI::tReadSelect);
+			void setReadSelect(tAI::tReadSelect);
             tAI::tReadSelect getReadSelect();
 
             void setOversampleBits(uint8_t, uint8_t);
@@ -42,9 +43,33 @@ namespace minerva{
             tAI::tReadSelect read_select;
         };
 
+
+		struct PWMSystem{
+			tPWM::tConfig config; // configuration for PWM IO
+			std::array<uint32_t, tPWM::kNumPeriodScaleHdrElements> hdr_period_scale; //2-bit mask for signal masking frequency, effectively scaling the PWM value (0 = 1x 1, = 2x, 3 = 4x)
+			std::array<uint32_t, tPWM::kNumPeriodScaleMXPElements> mxp_period_scale;
+			std::array<uint16_t, tPWM::kNumPeriodScaleHdrElements> hdr_values; //these are the pwm values 
+			std::array<uint16_t, tPWM::kNumPeriodScaleMXPElements> mxp_values;
+		};
+		
+		struct DIOSystem{
+			tDIO::tDO outputs;
+			tDIO::tOutputEnable enabled_outputs;
+			
+			struct PWMData{
+				uint8_t id;
+				uint8_t hardware_channel;
+				uint8_t duty_cycle;			
+			};
+			
+			std::array<PWMData, hal::kNumDigitalPWMOutputs> pwm_data;
+		};
+
 		std::array<AnalogOutput, tAO::kNumMXPRegisters> analog_outputs;
         AnalogInputs analog_inputs;
-    };
+		PWMSystem pwm_system;
+		DIOSystem digital_system;
+	};
 
 	static RoboRIO roborio_state;
 }
