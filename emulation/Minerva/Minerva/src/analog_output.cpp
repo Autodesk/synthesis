c83064c213da198da55237cfc0c7013bc2e8f12c
copernicium <logan.traffas@gmail.com>
2018-07-02 15:58:21 -0700

    Begin work on tPWM and tDIO implementation
    Former-commit-id: d51dac92deedd08933799f5fa788e2d0b5d82653

--- emulation/Minerva/Minerva/src/analog_output.cpp
+++ emulation/Minerva/Minerva/src/analog_output.cpp
@@ -1,5 +1,17 @@
 #include "roborio.h"
 
+namespace nFPGA{
+	namespace nRoboRIO_FPGANamespace{
+		void tAO::writeMXP(uint8_t reg_index, uint16_t value, tRioStatusCode* status){
+			minerva::roborio.analog_outputs[reg_index].mxp_value = value;
+		}
+
+		uint16_t tAO::readMXP(uint8_t reg_index, tRioStatusCode* status){
+			return minerva::roborio.analog_outputs[reg_index].mxp_value;
+		}
+	}
+}
+
 #ifdef ANALOG_OUTPUT_TEST
 
 int main(){
