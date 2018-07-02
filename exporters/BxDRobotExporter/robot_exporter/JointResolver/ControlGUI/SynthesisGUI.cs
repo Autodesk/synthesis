706e8603dc0f956bae4ff03844aa774e5a103ef9
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-02 10:55:29 -0700

    Removed unused settings

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/ControlGUI/SynthesisGUI.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/ControlGUI/SynthesisGUI.cs
@@ -405,7 +405,7 @@ public partial class SynthesisGUI : Form
             RMeta.FieldName = field;
 
             PluginSettings.GeneralUseFancyColors = colors;
-            PluginSettings.OnSettingsChanged(PluginSettings.InventorChildColor, PluginSettings.GeneralUseFancyColors, PluginSettings.GeneralSaveLocation);
+            PluginSettings.OnSettingsChanged();
 
             return true;
         }
--- exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/ExporterSettingsForm.Designer.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/ExporterSettingsForm.Designer.cs
@@ -31,23 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PluginSettingsForm));
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonOK = new System.Windows.Forms.Button();
-            this.groupboxColor = new System.Windows.Forms.GroupBox();
             this.ChildHighlight = new System.Windows.Forms.Button();
             this.ChildLabel = new System.Windows.Forms.Label();
-            this.groupboxGeneral = new System.Windows.Forms.GroupBox();
-            this.UseFancyColorsCheckBox = new System.Windows.Forms.CheckBox();
-            this.SaveLocationLabel = new System.Windows.Forms.Label();
-            this.buttonBrowse = new System.Windows.Forms.Button();
-            this.SaveLocationTextBox = new System.Windows.Forms.TextBox();
-            this.groupboxColor.SuspendLayout();
-            this.groupboxGeneral.SuspendLayout();
+            this.MainLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.MainLayout.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(16, 163);
+            this.buttonCancel.Location = new System.Drawing.Point(2, 28);
             this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(120, 29);
@@ -57,8 +50,7 @@
             // 
             // buttonOK
             // 
-            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonOK.Location = new System.Drawing.Point(155, 163);
+            this.buttonOK.Location = new System.Drawing.Point(126, 28);
             this.buttonOK.Margin = new System.Windows.Forms.Padding(2);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(120, 29);
@@ -66,29 +58,14 @@
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
             // 
-            // groupboxColor
-            // 
-            this.groupboxColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.groupboxColor.Controls.Add(this.ChildHighlight);
-            this.groupboxColor.Controls.Add(this.ChildLabel);
-            this.groupboxColor.ForeColor = System.Drawing.Color.Gray;
-            this.groupboxColor.Location = new System.Drawing.Point(11, 106);
-            this.groupboxColor.Margin = new System.Windows.Forms.Padding(2);
-            this.groupboxColor.Name = "groupboxColor";
-            this.groupboxColor.Padding = new System.Windows.Forms.Padding(2);
-            this.groupboxColor.Size = new System.Drawing.Size(264, 53);
-            this.groupboxColor.TabIndex = 12;
-            this.groupboxColor.TabStop = false;
-            this.groupboxColor.Text = "Inventor";
-            // 
             // ChildHighlight
             // 
             this.ChildHighlight.BackColor = System.Drawing.Color.Black;
+            this.ChildHighlight.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ChildHighlight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ChildHighlight.Location = new System.Drawing.Point(126, 21);
-            this.ChildHighlight.Margin = new System.Windows.Forms.Padding(2);
+            this.ChildHighlight.Location = new System.Drawing.Point(127, 3);
             this.ChildHighlight.Name = "ChildHighlight";
-            this.ChildHighlight.Size = new System.Drawing.Size(76, 20);
+            this.ChildHighlight.Size = new System.Drawing.Size(118, 20);
             this.ChildHighlight.TabIndex = 9;
             this.ChildHighlight.UseVisualStyleBackColor = false;
             this.ChildHighlight.Click += new System.EventHandler(this.ChildHighlight_Click);
@@ -96,76 +73,45 @@
             // ChildLabel
             // 
             this.ChildLabel.AutoSize = true;
+            this.ChildLabel.Dock = System.Windows.Forms.DockStyle.Left;
             this.ChildLabel.ForeColor = System.Drawing.Color.Black;
-            this.ChildLabel.Location = new System.Drawing.Point(2, 24);
-            this.ChildLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ChildLabel.Location = new System.Drawing.Point(3, 3);
+            this.ChildLabel.Margin = new System.Windows.Forms.Padding(3);
             this.ChildLabel.Name = "ChildLabel";
-            this.ChildLabel.Size = new System.Drawing.Size(101, 13);
+            this.ChildLabel.Size = new System.Drawing.Size(75, 20);
             this.ChildLabel.TabIndex = 6;
-            this.ChildLabel.Text = "Child Highlight Color";
+            this.ChildLabel.Text = "Highlight Color";
+            this.ChildLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // groupboxGeneral
+            // MainLayout
             // 
-            this.groupboxGeneral.Controls.Add(this.UseFancyColorsCheckBox);
-            this.groupboxGeneral.Controls.Add(this.SaveLocationLabel);
-            this.groupboxGeneral.Controls.Add(this.buttonBrowse);
-            this.groupboxGeneral.Controls.Add(this.SaveLocationTextBox);
-            this.groupboxGeneral.Location = new System.Drawing.Point(11, 12);
-            this.groupboxGeneral.Name = "groupboxGeneral";
-            this.groupboxGeneral.Size = new System.Drawing.Size(264, 88);
-            this.groupboxGeneral.TabIndex = 13;
-            this.groupboxGeneral.TabStop = false;
-            this.groupboxGeneral.Text = "General";
-            // 
-            // UseFancyColorsCheckBox
-            // 
-            this.UseFancyColorsCheckBox.AutoSize = true;
-            this.UseFancyColorsCheckBox.Location = new System.Drawing.Point(6, 65);
-            this.UseFancyColorsCheckBox.Name = "UseFancyColorsCheckBox";
-            this.UseFancyColorsCheckBox.Size = new System.Drawing.Size(119, 17);
-            this.UseFancyColorsCheckBox.TabIndex = 3;
-            this.UseFancyColorsCheckBox.Text = "Fancy Colors (Slow)";
-            this.UseFancyColorsCheckBox.UseVisualStyleBackColor = true;
-            this.UseFancyColorsCheckBox.CheckedChanged += new System.EventHandler(this.UseFancyColorsCheckBox_CheckedChanged);
-            // 
-            // SaveLocationLabel
-            // 
-            this.SaveLocationLabel.AutoSize = true;
-            this.SaveLocationLabel.Location = new System.Drawing.Point(7, 21);
-            this.SaveLocationLabel.Name = "SaveLocationLabel";
-            this.SaveLocationLabel.Size = new System.Drawing.Size(108, 13);
-            this.SaveLocationLabel.TabIndex = 2;
-            this.SaveLocationLabel.Text = "Robot Save Location";
-            // 
-            // buttonBrowse
-            // 
-            this.buttonBrowse.Location = new System.Drawing.Point(232, 40);
-            this.buttonBrowse.Name = "buttonBrowse";
-            this.buttonBrowse.Size = new System.Drawing.Size(26, 20);
-            this.buttonBrowse.TabIndex = 1;
-            this.buttonBrowse.Text = "...";
-            this.buttonBrowse.UseVisualStyleBackColor = true;
-            this.buttonBrowse.Click += new System.EventHandler(this.ButtonBrowse_Click);
-            // 
-            // SaveLocationTextBox
-            // 
-            this.SaveLocationTextBox.Location = new System.Drawing.Point(7, 40);
-            this.SaveLocationTextBox.Name = "SaveLocationTextBox";
-            this.SaveLocationTextBox.ReadOnly = true;
-            this.SaveLocationTextBox.Size = new System.Drawing.Size(219, 20);
-            this.SaveLocationTextBox.TabIndex = 0;
+            this.MainLayout.AutoSize = true;
+            this.MainLayout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MainLayout.ColumnCount = 2;
+            this.MainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.MainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.MainLayout.Controls.Add(this.ChildHighlight, 1, 0);
+            this.MainLayout.Controls.Add(this.buttonCancel, 0, 1);
+            this.MainLayout.Controls.Add(this.ChildLabel, 0, 0);
+            this.MainLayout.Controls.Add(this.buttonOK, 1, 1);
+            this.MainLayout.Location = new System.Drawing.Point(3, 3);
+            this.MainLayout.Name = "MainLayout";
+            this.MainLayout.RowCount = 2;
+            this.MainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.MainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.MainLayout.Size = new System.Drawing.Size(248, 59);
+            this.MainLayout.TabIndex = 13;
             // 
             // PluginSettingsForm
             // 
             this.AcceptButton = this.buttonOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(286, 203);
-            this.Controls.Add(this.groupboxGeneral);
-            this.Controls.Add(this.groupboxColor);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.buttonOK);
+            this.ClientSize = new System.Drawing.Size(277, 115);
+            this.Controls.Add(this.MainLayout);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -173,11 +119,10 @@
             this.MinimizeBox = false;
             this.Name = "PluginSettingsForm";
             this.Text = "Exporter Settings";
-            this.groupboxColor.ResumeLayout(false);
-            this.groupboxColor.PerformLayout();
-            this.groupboxGeneral.ResumeLayout(false);
-            this.groupboxGeneral.PerformLayout();
+            this.MainLayout.ResumeLayout(false);
+            this.MainLayout.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -185,13 +130,8 @@
 
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonOK;
-        private System.Windows.Forms.GroupBox groupboxColor;
         private System.Windows.Forms.Button ChildHighlight;
         private System.Windows.Forms.Label ChildLabel;
-        private System.Windows.Forms.GroupBox groupboxGeneral;
-        private System.Windows.Forms.Label SaveLocationLabel;
-        private System.Windows.Forms.Button buttonBrowse;
-        private System.Windows.Forms.TextBox SaveLocationTextBox;
-        private System.Windows.Forms.CheckBox UseFancyColorsCheckBox;
+        private System.Windows.Forms.TableLayoutPanel MainLayout;
     }
 }
