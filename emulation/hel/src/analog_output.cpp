bb73e685b386a0afc99dba60972208ef49769987
Nicholas Burrell <zero_frost@protonmail.com>
2018-07-03 16:43:12 -0700

    Rename cerebrum to hel

---
+++ emulation/hel/src/analog_output.cpp
@@ -0,0 +1,44 @@
+#include "roborio.h"
+
+using namespace nFPGA;
+using namespace nRoboRIO_FPGANamespace;
+
+namespace cerebrum{
+	uint16_t RoboRIO::AnalogOutputs::getMXPOutput(uint8_t index)const{
+		return mxp_outputs[index];
+	}
+
+	void RoboRIO::AnalogOutputs::setMXPOutput(uint8_t index, uint16_t value){
+		mxp_outputs[index] = value;
+	}
+}
+
+struct AnalogOutputManager: public tAO{
+	tSystemInterface* getSystemInterface(){
+		return nullptr;
+	}
+
+	void writeMXP(uint8_t reg_index, uint16_t value, tRioStatusCode* /*status*/){
+		cerebrum::roborio_state.analog_outputs.setMXPOutput(reg_index, value);
+	}
+
+	uint16_t readMXP(uint8_t reg_index, tRioStatusCode* /*status*/){
+		return cerebrum::roborio_state.analog_outputs.getMXPOutput(reg_index);
+	}
+};
+
+namespace nFPGA{
+	namespace nRoboRIO_FPGANamespace{
+		tAO* tAO::create(tRioStatusCode* /*status*/){
+			return new AnalogOutputManager();
+		}
+	}
+}
+
+#ifdef ANALOG_OUTPUT_TEST
+
+int main(){
+	//tAO* a = tAO::create(nullptr);
+}
+
+#endif
