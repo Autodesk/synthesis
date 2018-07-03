ea203c5b5d23381cdbc393bdceb8f833b92f8355
Nicholas Burrell <zero_frost@protonmail.com>
2018-07-03 15:07:06 -0700

    Add RoboRIOManager for lock base global state
    Former-commit-id: db1ddf4ccbc5541b38ae3049a60adbd6e969e481

--- emulation/cerebrum/include/roborio.h
+++ emulation/cerebrum/include/roborio.h
@@ -1,20 +1,21 @@
 #ifndef _ROBORIO_H_
 #define _ROBORIO_H_
 
+#include <array>
+#include <memory>
+#include <mutex>
+
 #include "HAL/ChipObject.h"
 #include "athena/PortsInternal.h"
 
-#include<array>
-
 namespace cerebrum{
+    using namespace nFPGA;
+    using namespace nRoboRIO_FPGANamespace;
 
-	using namespace nFPGA;
- 	using namespace nRoboRIO_FPGANamespace;
-
-	struct RoboRIO{
-		struct AnalogOutput{
-				uint16_t mxp_value;
-		};
+    struct RoboRIO{
+        struct AnalogOutput{
+            uint16_t mxp_value;
+        };
         struct AnalogInputs {
             struct AnalogInput{
                 uint8_t oversample_bits;
@@ -42,39 +43,65 @@ namespace cerebrum{
             tAI::tReadSelect read_select;
         };
 
-		struct PWMSystem{
-			tPWM::tConfig config; // configuration for PWM IO
-			std::array<uint32_t, tPWM::kNumPeriodScaleHdrElements> hdr_period_scale; //2-bit mask for signal masking frequency, effectively scaling the PWM value (0 = 1x 1, = 2x, 3 = 4x)
-			std::array<uint32_t, tPWM::kNumPeriodScaleMXPElements> mxp_period_scale;
-			std::array<uint16_t, tPWM::kNumPeriodScaleHdrElements> hdr_values; //these are the pwm values 
-			std::array<uint16_t, tPWM::kNumPeriodScaleMXPElements> mxp_values;
-		};
-		
-		struct DIOSystem{
-			tDIO::tDO outputs;
-			tDIO::tOutputEnable enabled_outputs;
-			tDIO::tPulse pulses;
-			tDIO::tDI inputs;
+        struct PWMSystem{
+            tPWM::tConfig config; // configuration for PWM IO
+            std::array<uint32_t, tPWM::kNumPeriodScaleHdrElements> hdr_period_scale; //2-bit mask for signal masking frequency, effectively scaling the PWM value (0 = 1x 1, = 2x, 3 = 4x)
+            std::array<uint32_t, tPWM::kNumPeriodScaleMXPElements> mxp_period_scale;
+            std::array<uint16_t, tPWM::kNumPeriodScaleHdrElements> hdr_values; //these are the pwm values 
+            std::array<uint16_t, tPWM::kNumPeriodScaleMXPElements> mxp_values;
+        };
 
-			uint8_t pulse_length;
+        struct DIOSystem{
+            tDIO::tDO outputs;
+            tDIO::tOutputEnable enabled_outputs;
+            tDIO::tPulse pulses;
+            tDIO::tDI inputs;
 
-			struct PWMData{
-				uint8_t id;
-				uint8_t hardware_channel;
-				uint8_t duty_cycle;			
-			};
-			
-			std::array<PWMData, hal::kNumDigitalPWMOutputs> pwm_data;
-			//std::array<uint8_t, 
-		};
+            uint8_t pulse_length;
 
-		std::array<AnalogOutput, tAO::kNumMXPRegisters> analog_outputs;
-		PWMSystem pwm_system;
-		DIOSystem digital_system;
+            struct PWMData{
+                uint8_t id;
+                uint8_t hardware_channel;
+                uint8_t duty_cycle;
+            };
+
+            std::array<PWMData, hal::kNumDigitalPWMOutputs> pwm_data;
+            //std::array<uint8_t, 
+        };
+
+        std::array<AnalogOutput, tAO::kNumMXPRegisters> analog_outputs;
+        PWMSystem pwm_system;
+        DIOSystem digital_system;
         AnalogInputs analog_inputs;
-	};
 
-	static RoboRIO roborio_state;
+        explicit RoboRIO() = default;
+
+        friend class RoboRIOManager;
+    private:
+        RoboRIO(RoboRIO const&) = default;
+    };
+
+    class RoboRIOManager {
+
+    public:
+        static std::shared_ptr<RoboRIO> getInstance(std::mutex& m) {
+            std::unique_lock<std::mutex> lock(m);
+            static std::shared_ptr<RoboRIO> instance;
+            return instance;
+        }
+        static RoboRIO getCopy() {
+            std::mutex m;
+            return RoboRIO((*RoboRIOManager::getInstance(m)));
+        }
+    private:
+        RoboRIOManager() {}
+    public:
+        RoboRIOManager(RoboRIOManager const&) = delete;
+        void operator=(RoboRIOManager const&) = delete;
+    };
+
+    static RoboRIO roborio_state;
+
 }
 
 #endif
