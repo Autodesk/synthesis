0b73d31d79b007bb65ac6370297049ab388f6854
copernicium <logan.traffas@gmail.com>
2018-07-03 13:34:48 -0700

    Implement more tDIO functions, fix some warnings, and update .gitignore
    Former-commit-id: 59583de12bb86b17d823f94f81d963f7b654e296

--- emulation/cerebrum/src/pwm.cpp
+++ emulation/cerebrum/src/pwm.cpp
@@ -4,79 +4,79 @@
 
 namespace nFPGA{
 	namespace nRoboRIO_FPGANamespace{
-		uint32_t tPWM::readCycleStartTime(tRioStatusCode* status){
+		uint32_t tPWM::readCycleStartTime(tRioStatusCode* /*status*/){
 			return 0;
 		}
 		
-		void tPWM::writeConfig(tPWM::tConfig value, tRioStatusCode* status){
+		void tPWM::writeConfig(tPWM::tConfig value, tRioStatusCode* /*status*/){
 			cerebrum::roborio_state.pwm_system.config = value;
 		}
 
-		void tPWM::writeConfig_Period(uint16_t value, tRioStatusCode* status){
+		void tPWM::writeConfig_Period(uint16_t value, tRioStatusCode* /*status*/){
 			cerebrum::roborio_state.pwm_system.config.Period = value;
 		}
 	
-		void tPWM::writeConfig_MinHigh(uint16_t value, tRioStatusCode* status){
+		void tPWM::writeConfig_MinHigh(uint16_t value, tRioStatusCode* /*status*/){
 			cerebrum::roborio_state.pwm_system.config.MinHigh = value;
 		}
 
-		tPWM::tConfig tPWM::readConfig(tRioStatusCode* status){
+		tPWM::tConfig tPWM::readConfig(tRioStatusCode* /*status*/){
 			return cerebrum::roborio_state.pwm_system.config;
 		}
 		
-		uint16_t tPWM::readConfig_Period(tRioStatusCode* status){
+		uint16_t tPWM::readConfig_Period(tRioStatusCode* /*status*/){
 			return cerebrum::roborio_state.pwm_system.config.Period;
 		}
 		
-		uint16_t tPWM::readConfig_MinHigh(tRioStatusCode* status){
+		uint16_t tPWM::readConfig_MinHigh(tRioStatusCode* /*status*/){
 			return cerebrum::roborio_state.pwm_system.config.MinHigh;
 		}
 
-		uint32_t readCycleStartTimeUpper(tRioStatusCode* status){
+		uint32_t readCycleStartTimeUpper(tRioStatusCode* /*status*/){
 			return 0;
 		}
 
-		uint16_t readLoopTiming(tRioStatusCode* status){
+		uint16_t readLoopTiming(tRioStatusCode* /*status*/){
 			return hal::kExpectedLoopTiming;
 		}
 
-		void tPWM::writePeriodScaleMXP(uint8_t bitfield_index, uint8_t value, tRioStatusCode* status){
+		void tPWM::writePeriodScaleMXP(uint8_t bitfield_index, uint8_t value, tRioStatusCode* /*status*/){
 			cerebrum::roborio_state.pwm_system.mxp_period_scale[bitfield_index] = value;
 		}
 
-		uint8_t tPWM::readPeriodScaleMXP(uint8_t bitfield_index, tRioStatusCode* status){
+		uint8_t tPWM::readPeriodScaleMXP(uint8_t bitfield_index, tRioStatusCode* /*status*/){
 			return cerebrum::roborio_state.pwm_system.hdr_period_scale[bitfield_index];
 		}
 
-		void tPWM::writePeriodScaleHdr(uint8_t bitfield_index, uint8_t value, tRioStatusCode* status){
+		void tPWM::writePeriodScaleHdr(uint8_t bitfield_index, uint8_t value, tRioStatusCode* /*status*/){
 			cerebrum::roborio_state.pwm_system.hdr_period_scale[bitfield_index] = value;
 		}
 		
-		uint8_t tPWM::readPeriodScaleHdr(uint8_t bitfield_index, tRioStatusCode* status){
+		uint8_t tPWM::readPeriodScaleHdr(uint8_t bitfield_index, tRioStatusCode* /*status*/){
 			return cerebrum::roborio_state.pwm_system.hdr_period_scale[bitfield_index];
 		}
 
-		void tPWM::writeZeroLatch(uint8_t bitfield_index, bool value, tRioStatusCode* status){
+		void tPWM::writeZeroLatch(uint8_t bitfield_index, bool value, tRioStatusCode* /*status*/){
 			//TODO
 		}
 	
-		bool tPWM::readZeroLatch(uint8_t bitfield_index, tRioStatusCode* status){
+		bool tPWM::readZeroLatch(uint8_t bitfield_index, tRioStatusCode* /*status*/){
 			//TODO
 		}
 
-		void tPWM::writeHdr(uint8_t reg_index, uint16_t value, tRioStatusCode* status){
+		void tPWM::writeHdr(uint8_t reg_index, uint16_t value, tRioStatusCode* /*status*/){
 			cerebrum::roborio_state.pwm_system.hdr_values[reg_index] = value;
 		}
 		
-		uint16_t tPWM::readHdr(uint8_t reg_index, tRioStatusCode* status){
+		uint16_t tPWM::readHdr(uint8_t reg_index, tRioStatusCode* /*status*/){
 			return cerebrum::roborio_state.pwm_system.hdr_values[reg_index];
 		}
 
-		void tPWM::writeMXP(uint8_t reg_index, uint16_t value, tRioStatusCode* status){
+		void tPWM::writeMXP(uint8_t reg_index, uint16_t value, tRioStatusCode* /*status*/){
 			cerebrum::roborio_state.pwm_system.mxp_values[reg_index] = value;
 		}
 		
-		uint16_t tPWM::readMXP(uint8_t reg_index, tRioStatusCode* status){
+		uint16_t tPWM::readMXP(uint8_t reg_index, tRioStatusCode* /*status*/){
 			return cerebrum::roborio_state.pwm_system.mxp_values[reg_index];
 		}
 	}
