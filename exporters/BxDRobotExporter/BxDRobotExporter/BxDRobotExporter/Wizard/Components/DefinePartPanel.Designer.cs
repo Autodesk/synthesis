b1af08a71b0bf8ad6539840cbc3ba90c04af3892
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-03 19:29:18 -0700

    Improved highlighting of parts in inventor

--- exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Wizard/Components/DefinePartPanel.Designer.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Wizard/Components/DefinePartPanel.Designer.cs
@@ -65,6 +65,7 @@
             // 
             this.NodeGroupBox.AutoSize = true;
             this.NodeGroupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.NodeGroupBox.BackColor = System.Drawing.SystemColors.Control;
             this.NodeGroupBox.Controls.Add(this.JointLimitGroupBox);
             this.NodeGroupBox.Controls.Add(this.MetaTabControl);
             this.NodeGroupBox.Controls.Add(this.PortsGroupBox);
@@ -337,10 +338,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.NodeGroupBox);
             this.Name = "DefinePartPanel";
             this.Size = new System.Drawing.Size(443, 205);
-            this.MouseHover += new System.EventHandler(this.HighlightNode);
             this.NodeGroupBox.ResumeLayout(false);
             this.NodeGroupBox.PerformLayout();
             this.JointLimitGroupBox.ResumeLayout(false);
