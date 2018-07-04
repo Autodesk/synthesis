408a7fcd2747a470487863eb1bc8c70d4b4cb63f
Nicholas Burrell <zero_frost@protonmail.com>
2018-07-04 15:09:45 -0700

    Add URL to download image from Dropbox (temporary and developer only)

--- emulation/hel/download_vm.sh
+++ emulation/hel/download_vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O synthesis_image.tar.gz #INSERT URL HERE
+wget -O synthesis_image.tar.gz https://uc1047ee6720d098b469c6f3b731.dl.dropboxusercontent.com/cd/0/get/AKjGidGE2hBx8Jx2DQX3E1OWVSjxCNXbIcraSeaUty-rOoiEcHOTQeTP5amIUw8YGYwFrowQaGAiT9s8LyRCRr3mXw06WmVd3DmqHZI0WJOUA7MXI4-tMfFqwfPkfMXobTx0jgRpVH9TzAx2V8c4x6ZXX0gfm4nFHxWY_2h-1J-wp0PtbKhtBWWLkGfzg2PAnEk/file?dl=1
 mkdir synthesis_image -p;
 tar -xvzf synthesis_image.tar.gz --directory synthesis_image;
 cd synthesis_image;
