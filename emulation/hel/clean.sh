09ffc04c436794bdf7c579bec2f5790264185851
Nicholas Burrell <zero_frost@protonmail.com>
2018-07-05 10:34:30 -0700

    Update clean.sh
    Former-commit-id: 95239a3be4079b5baeb5e2d8b0133c60feb773c5

--- emulation/hel/clean.sh
+++ emulation/hel/clean.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-killall qemu-system-arm && rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake GoogleTest-prefix synthesis_image synthesis_image.tar.gz bin/tests/* build/* lib/* docs/html/* docs/latex/* vm_lock
+killall qemu-system-arm;
+rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake GoogleTest-prefix synthesis_image synthesis_image.tar.gz bin/tests/* build/* lib/* docs/html/* docs/latex/* vm_lock
