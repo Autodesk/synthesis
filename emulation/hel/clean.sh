8b0446a44ee699660760299dfe6ae13f9c1eed53
Nicholas Burrell <zero_frost@protonmail.com>
2018-07-05 09:28:30 -0700

    Update CMakeList.txt with VM support. Added SCP to VM automatically
    Former-commit-id: 7cbd0fd76c3926705dfd36aaf830d4f6ede53d23

---
+++ emulation/hel/clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+killall qemu-system-arm && rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake GoogleTest-prefix synthesis_image synthesis_image.tar.gz bin/tests/* build/* lib/* docs/html/* docs/latex/* vm_lock
