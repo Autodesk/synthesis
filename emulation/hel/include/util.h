e8e5266128f2202fca1387796a9d347f984c7345
copernicium <logan.traffas@gmail.com>
2018-07-08 17:06:23 -0700

    Add doxygen comments to util.h
    Former-commit-id: 92403a930b161055fa7572b1a0a17c2f58c2470d

--- emulation/hel/include/util.h
+++ emulation/hel/include/util.h
@@ -3,7 +3,16 @@
 
 namespace hel{
 
-	template<typename T>
+    /**
+     * \fn unsigned findMostSignificantBit(T value)
+     * \brief Finds the most significant bit in an integer
+     * Finds the index of the high bit of the greatest value; returns zero if zero is passed in
+     * \tparam T the type of integer
+     * \param value the integer to analyze
+     * \return the zero-indexed index of the most significant bit
+     */
+	
+    template<typename T>
 	unsigned findMostSignificantBit(T value){
 		unsigned most_significant_bit = 0;
 		
@@ -14,6 +23,15 @@ namespace hel{
 		
 		return most_significant_bit;
 	}
+
+    /**
+     * \fn bool checkBitHigh(T value, unsigned index)
+     * \brief Check if the bit in place \b index of integer \b value is high
+     * \tparam T the type of integer
+     * \param value the integer to analyze
+     * \param index the zero-indexed index to check
+     * \return true if the bit is high
+     */
 
 	template<typename T>
 	bool checkBitHigh(T value,unsigned index){
