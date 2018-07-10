9d74e802c21daf482ae76fb4000dd0e27b4df46c
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-10 14:54:40 -0700

    Fixed typo
    Former-commit-id: 109af4df27d40a2a683688257fc19ae4f7e431c6

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/DriveChooser.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/EditorsLibrary/DriveChooser.cs
@@ -61,14 +61,14 @@ public partial class DriveChooser : Form
 
             if (joint.cDriver.portA < txtPortA.Minimum)
                 txtPortA.Value = txtPortA.Minimum;
-            if (joint.cDriver.portA > txtPortA.Maximum)
+            else if (joint.cDriver.portA > txtPortA.Maximum)
                 txtPortA.Value = txtPortA.Maximum;
             else
                 txtPortA.Value = joint.cDriver.portA;
 
             if (joint.cDriver.portB < txtPortB.Minimum)
                 txtPortB.Value = txtPortB.Minimum;
-            if (joint.cDriver.portB > txtPortB.Maximum)
+            else if (joint.cDriver.portB > txtPortB.Maximum)
                 txtPortB.Value = txtPortB.Maximum;
             else
                 txtPortB.Value = joint.cDriver.portB;
