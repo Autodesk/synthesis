d5631534a766c011c0f75b41a718abdb21584fe2
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-10 13:19:54 -0700

    Removed background image from wizard

--- exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Wizard/WizardForm.Designer.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Wizard/WizardForm.Designer.cs
@@ -45,8 +45,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(484, 711);
             this.Controls.Add(this.WizardPages);
             this.DoubleBuffered = true;
