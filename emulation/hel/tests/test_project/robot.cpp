6ec651a13768b1a08d3b386f10907a6bd2b43130
Nicholas Burrell <zero_frost@protonmail.com>
2018-07-11 14:32:31 -0700

    Update test robot code

--- emulation/hel/tests/test_project/robot.cpp
+++ emulation/hel/tests/test_project/robot.cpp
@@ -9,7 +9,10 @@
 #include <IterativeRobot.h>
 #include <Joystick.h>
 #include <Spark.h>
-
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <Timer.h>
 /**
  * This is a demo program showing the use of the DifferentialDrive class.
  * Runs the motors with arcade steering.
@@ -20,11 +23,25 @@ class Robot : public frc::IterativeRobot {
 	frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
 	frc::Joystick m_stick{0};
 
+    float newPWMR = 0.0;
+    float newPWML = 0.0;
+
    public:
 	void TeleopPeriodic() {
-		// drive with arcade style
-		m_robotDrive.ArcadeDrive(m_stick.GetY(), m_stick.GetX());
-	}
+        std::srand(std::time(nullptr));
+        // drive with arcade style
+        newPWML = ((float)((std::rand())/((RAND_MAX + 1u)/2000)-1000)/1000.0f);
+        newPWMR = ((float)((std::rand())/((RAND_MAX + 1u)/2000)-1000)/1000.0f);
+        //m_robotDrive.ArcadeDrive(m_stick.GetY(), m_stick.GetX());
+
+        m_leftMotor.Set(newPWML);
+        m_rightMotor.Set(newPWMR);
+
+        std::cout << "Excepted Left: " << newPWML << "\nExcepted Right: " << newPWMR << "\n";
+        std::cout << "Actual Left: " << m_leftMotor.Get() << "\nActual Right: " << m_rightMotor.Get() << "\n";
+
+        Wait(0.045);
+    }
 };
 
 START_ROBOT_CLASS(Robot)
