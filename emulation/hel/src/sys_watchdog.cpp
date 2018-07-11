3507f04b2d8eef5802a31dadca4417c6a08eb807
copernicium <logan.traffas@gmail.com>
2018-07-11 15:29:04 -0700

    Implement used tSysWatchdog functions

--- emulation/hel/src/sys_watchdog.cpp
+++ emulation/hel/src/sys_watchdog.cpp
@@ -4,65 +4,68 @@ using namespace nFPGA;
 using namespace nRoboRIO_FPGANamespace;
 
 namespace hel{
+    tSysWatchdog::tStatus RoboRIO::SysWatchdog::getStatus()const{
+        return status;
+    }
+
+    void RoboRIO::SysWatchdog::setStatus(tSysWatchdog::tStatus s){
+        status = s;
+    }
+    
     struct SysWatchdogManager: public tSysWatchdog{
-        tSystemInterface* getSystemInterface(){
-           //TODO
+        tSystemInterface* getSystemInterface(){ //unnecessary for emulation
+           return nullptr;
         }
 
-        tStatus readStatus(tRioStatusCode* status){
-           //TODO
+        tStatus readStatus(tRioStatusCode* /*status*/){
+           return RoboRIOManager::getInstance()->watchdog.getStatus();
         }
 
-        bool readStatus_SystemActive(tRioStatusCode* status){
-           //TODO
+        bool readStatus_SystemActive(tRioStatusCode* /*status*/){
+           return RoboRIOManager::getInstance()->watchdog.getStatus().SystemActive;
         }
 
-        bool readStatus_PowerAlive(tRioStatusCode* status){
-           //TODO
+        bool readStatus_PowerAlive(tRioStatusCode* /*status*/){
+           return RoboRIOManager::getInstance()->watchdog.getStatus().PowerAlive;
         }
 
-        unsigned short readStatus_SysDisableCount(tRioStatusCode* status){
-           //TODO
+        uint16_t readStatus_SysDisableCount(tRioStatusCode* /*status*/){
+           return RoboRIOManager::getInstance()->watchdog.getStatus().SysDisableCount;
         }
 
-        unsigned short readStatus_PowerDisableCount(tRioStatusCode* status){
-           //TODO
+        uint16_t readStatus_PowerDisableCount(tRioStatusCode* /*status*/){
+           return RoboRIOManager::getInstance()->watchdog.getStatus().PowerDisableCount;
         }
 
-        void writeCommand(unsigned short value, tRioStatusCode* status){
-           //TODO
+        void writeCommand(uint16_t /*value*/, tRioStatusCode* /*status*/){} //unnecessary for emulation
+
+        uint16_t readCommand(tRioStatusCode* /*status*/){ //unnecessary for emulation
+            return 0;
         }
 
-        unsigned short readCommand(tRioStatusCode* status){
-           //TODO
+        uint8_t readChallenge(tRioStatusCode* /*status*/){ //unnecessary for emulation
+            return 0;
         }
 
-        unsigned char readChallenge(tRioStatusCode* status){
-           //TODO
+        void writeActive(bool /*value*/, tRioStatusCode* /*status*/){} //unnecessary for emulation
+
+        bool readActive(tRioStatusCode* /*status*/){ //unnecessary for emulation
+            return 0;
         }
 
-        void writeActive(bool value, tRioStatusCode* status){
-           //TODO
+        uint32_t readTimer(tRioStatusCode* /*status*/){ //unnecessary for emulation
+            return 0;
         }
 
-        bool readActive(tRioStatusCode* status){
-           //TODO
-        }
-
-        unsigned int readTimer(tRioStatusCode* status){
-           //TODO
-        }
-
-        unsigned short readForcedKills(tRioStatusCode* status){
-           //TODO
+        uint16_t readForcedKills(tRioStatusCode* /*status*/){ //unnecessary for emulation
+            return 0;
         }
     };
 }
 
 namespace nFPGA{
     namespace nRoboRIO_FPGANamespace{
-        tSysWatchdog* tSysWatchdog::create(tRioStatusCode* status){
-            *status = 0;
+        tSysWatchdog* tSysWatchdog::create(tRioStatusCode* /*status*/){
             return new hel::SysWatchdogManager();
         }
     }
