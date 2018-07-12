3e8bde911c9d2ca4790ce686fda90e2a0aeabb49
copernicium <logan.traffas@gmail.com>
2018-07-12 15:56:35 -0700

    Add relays, analog outputs, and digital mxp outputs to interface

--- emulation/hel/include/roborio_interface.h
+++ emulation/hel/include/roborio_interface.h
@@ -5,8 +5,33 @@
 
 namespace hel{
     struct RoboRIOInterface{
-        std::array<double, tPWM::kNumHdrRegisters> pwm_hdrs;
+        struct MXPData{
+            enum class Config{
+                DIO, PWM, SPI, I2C
+            };
 
+            Config config;
+
+            double value;
+        };
+
+        enum class RelayState{
+            _00, //Corresponds to off
+            _01, //Corresponds to reverse only
+            _10, //Corresponds to forward only
+            _11  //Corresponds to forward and reverse, an error
+        };
+
+    private:
+        std::array<double, tPWM::kNumHdrRegisters> pwm_hdrs;
+        
+        std::array<RelayState,hal::kNumRelayHeaders> relays;
+
+        std::array<double, hal::kNumAnalogOutputs> analog_outputs;
+
+        std::array<MXPData, hal::kNumDigitalMXPChannels> digital_mxp;
+
+    public:
         void update();
 
         std::string toString()const;
--- emulation/hel/src/roborio_interface.cpp
+++ emulation/hel/src/roborio_interface.cpp
@@ -8,4 +8,47 @@ void hel::RoboRIOInterface::update(){
     for(unsigned i = 0; i < pwm_hdrs.size(); i++){
         pwm_hdrs[i] = HAL_GetPWMSpeed(i, &status);
     }
+
+    for(unsigned i = 0; i < relays.size(); i++){
+        relays[i] = [&](){
+                        int forward_handle = i;
+                        int reverse_handle = i + hal::kNumRelayHeaders;
+                        if(HAL_GetRelay(forward_handle, &status)){
+                            if(HAL_GetRelay(reverse_handle, &status)){
+                                return RelayState::_11; //TODO error handling
+                            } else{
+                                return RelayState::_10;
+                            }
+                        } else if(HAL_GetRelay(reverse_handle, &status)){
+                            return RelayState::_01;
+                        }
+                        return RelayState::_00;
+                    }();
+    }
+
+    for(unsigned i = 0; i < analog_outputs.size(); i++){
+        analog_outputs[i] = HAL_GetAnalogOutput(i, &status);
+    }
+
+    for(unsigned i = 0; i < digital_mxp.size(); i++){
+        digital_mxp[i].config = [&](){
+                                 return MXPData::Config::DIO; //TODO
+                             }();
+        switch(digital_mxp[i].config){
+        case MXPData::Config::DIO:
+            digital_mxp[i].value = 0; //TODO
+        case MXPData::Config::PWM:
+            digital_mxp[i].value = 0; //TODO
+        case MXPData::Config::SPI:
+            digital_mxp[i].value = 0; //TODO
+        case MXPData::Config::I2C:
+            digital_mxp[i].value = 0; //TODO
+        default:
+            ; //TODO error handling
+        }
+    }
+}
+
+std::string hel::RoboRIOInterface::toString()const{
+    return ""; //TODO
 }
