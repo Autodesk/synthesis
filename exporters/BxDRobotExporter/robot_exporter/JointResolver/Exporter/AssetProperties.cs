03642025a95a18e2f5a78d91c807d4deac271828
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-13 13:55:54 -0700

    Optimizations and actually fixed broken color

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/Exporter/AssetProperties.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/Exporter/AssetProperties.cs
@@ -4,10 +4,56 @@ using Inventor;
 
 public class AssetProperties
 {
-    public Color color = null;
-    public double transparency;
-    public double translucency;
+    public uint color = 0;
+    public double transparency = 0;
+    public double translucency = 0;
     public double specular = 0;
+
+    public AssetProperties()
+    {
+        
+    }
+
+    public AssetProperties(Asset asset)
+    {
+        CopyAsset(asset);
+    }
+
+    public void CopyAsset(Asset asset)
+    {
+        foreach (AssetValue val in asset)
+        {
+            //is this one supposed to be different from the others? "DisplayName vs Name"
+            if (val.DisplayName.Equals("Color") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeColor)
+            {
+                Color tempColor = ((ColorAssetValue)val).Value;
+                color = ((uint)tempColor.Red << 0) | ((uint)tempColor.Green << 8) | ((uint)tempColor.Blue << 16) | ((((uint)(tempColor.Opacity * 255)) & 0xFF) << 24);
+            }
+            /*    //I am unable to find any reference to gloss in the API, and I've found the value changes from material to material
+            else if (val.Name.Equals("generic_glossiness") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
+            {
+                generic_glossiness = ((FloatAssetValue)val).Value;
+            }
+            */
+            //opacity is a double
+            else if (val.DisplayName.Equals("Transparency") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
+            {
+                transparency = ((FloatAssetValue)val).Value;
+            }
+            else if (val.DisplayName.Equals("Translucency") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
+            {
+                translucency = ((FloatAssetValue)val).Value;
+            }
+            else if (val.ValueType == AssetValueTypeEnum.kAssetValueTextureType)
+            {
+                AssetTexture tex = ((TextureAssetValue)val).Value;
+            }
+            else if (val.Name.Contains("reflectivity") && val.Name.Contains("0deg") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
+            {
+                specular = ((FloatAssetValue)val).Value;
+            }
+        }
+    }
 
     public static AssetProperties Create(dynamic surf)
     {
@@ -35,38 +81,11 @@ public class AssetProperties
         }
     }
 
-    public AssetProperties(Asset asset)
+    public override bool Equals(object other)
     {
-        foreach (AssetValue val in asset)
-        {
-            //is this one supposed to be different from the others? "DisplayName vs Name"
-            if (val.DisplayName.Equals("Color") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeColor)
-            {
-                color = ((ColorAssetValue) val).Value;
-            }
-            /*    //I am unable to find any reference to gloss in the API, and I've found the value changes from material to material
-            else if (val.Name.Equals("generic_glossiness") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
-            {
-                generic_glossiness = ((FloatAssetValue)val).Value;
-            }
-            */
-            //opacity is a double
-            else if (val.DisplayName.Equals("Transparency") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
-            {
-                transparency = ((FloatAssetValue) val).Value;
-            }
-            else if (val.DisplayName.Equals("Translucency") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
-            {
-                translucency = ((FloatAssetValue) val).Value;
-            }
-            else if (val.ValueType == AssetValueTypeEnum.kAssetValueTextureType)
-            {
-                AssetTexture tex = ((TextureAssetValue) val).Value;
-            }
-            else if (val.Name.Contains("reflectivity") && val.Name.Contains("0deg") && val.ValueType == AssetValueTypeEnum.kAssetValueTypeFloat)
-            {
-                specular = ((FloatAssetValue) val).Value;
-            }
-        }
+        if (other is AssetProperties otherAsset)
+            return color == otherAsset.color && transparency == otherAsset.transparency && translucency == otherAsset.translucency && specular == otherAsset.specular;
+        else
+            return base.Equals(other);
     }
 }
