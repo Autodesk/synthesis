4d212752ba7cc11ca7b7efdc1e40eac416b1b018
copernicium <logan.traffas@gmail.com>
2018-07-13 09:27:05 -0700

    Finish serializer for current roborio state output

--- emulation/hel/src/roborio_interface.cpp
+++ emulation/hel/src/roborio_interface.cpp
@@ -1,6 +1,7 @@
 #include "roborio_interface.h"
 
 #include "HAL/HAL.h"
+#include "util.h"
 
 void hel::RoboRIOInterface::update(){
     int32_t status = 0;
@@ -32,7 +33,37 @@ void hel::RoboRIOInterface::update(){
 
     for(unsigned i = 0; i < digital_mxp.size(); i++){
         digital_mxp[i].config = [&](){
-            return MXPData::Config::DIO; //TODO
+            if(checkBitHigh(RoboRIOManager::getInstance()->digital_system.getMXPSpecialFunctionsEnabled(), i)){
+                unsigned mxp_remapped_i = i + hal::kNumDigitalHeaders;
+                if(
+                    mxp_remapped_i == 0 || 
+                    mxp_remapped_i == 1 || 
+                    mxp_remapped_i == 2 || 
+                    mxp_remapped_i == 3 || 
+                    mxp_remapped_i == 8 || 
+                    mxp_remapped_i == 9 || 
+                    mxp_remapped_i == 10 || 
+                    mxp_remapped_i == 11 || 
+                    mxp_remapped_i == 12 || 
+                    mxp_remapped_i == 13
+                ){
+                    return MXPData::Config::PWM;
+                }
+                if(
+                    mxp_remapped_i == 5 || 
+                    mxp_remapped_i == 6 || 
+                    mxp_remapped_i == 7
+                ){
+                    return MXPData::Config::SPI;
+                }
+                if(
+                    mxp_remapped_i == 14 || 
+                    mxp_remapped_i == 15
+                ){
+                    return MXPData::Config::I2C;
+                }
+            }
+            return MXPData::Config::DIO;
         }();
         switch(digital_mxp[i].config){
         case MXPData::Config::DIO:
@@ -51,26 +82,72 @@ void hel::RoboRIOInterface::update(){
             ; //TODO error handling
         }
     }
+
+    for(unsigned i = 0; i < digital_hdrs.size(); i++){
+        digital_hdrs[i] = HAL_GetDIO(i, &status);
+    }
+}
+
+std::string hel::to_string(hel::RoboRIOInterface::RelayState r){
+    switch(r){
+    case hel::RoboRIOInterface::RelayState::OFF:
+        return "OFF";
+    case hel::RoboRIOInterface::RelayState::REVERSE:
+        return "REVERSE";
+    case hel::RoboRIOInterface::RelayState::FORWARD:
+        return "FORWARD";
+    case hel::RoboRIOInterface::RelayState::ERROR:
+        return "ERROR";
+    default:
+        return ""; //TODO error handling
+    }
 }
 
 std::string hel::RoboRIOInterface::toString()const{
     return ""; //TODO implement function in readable print-out
 }
 
-std::string hel::RoboRIOInterface::serialize()const{
-    std::string s = "roborio[";
-    
-    std::string += "pwm_hdrs:[";
-    for(unsigned i = 0; i < pwm_hdrs.size(); i++){
-        s += std::to_string(pwm_hdrs[i]);
-        if((i + 1) < pwm_hdrs.size()){
-            s += ",";
-        }
+std::string hel::to_string(hel::RoboRIOInterface::MXPData::Config config){
+    switch(config){
+    case hel::RoboRIOInterface::MXPData::Config::DIO:
+        return "DIO";
+    case hel::RoboRIOInterface::MXPData::Config::PWM:
+        return "PWM";
+    case hel::RoboRIOInterface::MXPData::Config::SPI:
+        return "SPI";
+    case hel::RoboRIOInterface::MXPData::Config::I2C:
+        return "I2C";
+    default:
+        return ""; //TODO error handling
     }
-    std::string += "]";
-    
+}
+
+std::string hel::RoboRIOInterface::serialize()const{
+    std::string s = "\"roborio\":{";
+
+    s += serializeArray("\"pwm_hdrs\"", pwm_hdrs, static_cast<std::string(*)(double)>(std::to_string));
+    s += ",";
+    s += serializeArray("\"relays\"", relays, static_cast<std::string(*)(RelayState)>(hel::to_string));
+    s += ",";
+    s += serializeArray("\"analog_outputs\"", analog_outputs, static_cast<std::string(*)(double)>(std::to_string));
+    s += ",";
+    s += serializeArray(
+        "\"digital_mxp\"",
+        digital_mxp,
+        std::function<std::string(MXPData)>([&](MXPData data){
+            return "\"config:\"" + hel::to_string(data.config) + ",\"value\":" + std::to_string(data.value);
+        })
+    );
+    s += ",";
+    s += serializeArray(
+        "\"digital_hdrs\"", 
+        digital_hdrs,
+        std::function<std::string(bool)>([&](bool b){
+            return b ? "1" : "0";
+        })
+    );
     //TODO finish
 
-    s += "]";
+    s += "}";
     return s;
 }
