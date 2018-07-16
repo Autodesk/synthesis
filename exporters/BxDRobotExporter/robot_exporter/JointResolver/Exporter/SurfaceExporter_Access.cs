aab79d6573739510e3beacceb4e147fbfc998f1b
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-16 09:08:41 -0700

    Multithreaded the exporter
    Former-commit-id: e4cfc0cce440443a971f8fe556ce18dfdbf8a316

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/Exporter/SurfaceExporter_Access.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/Exporter/SurfaceExporter_Access.cs
@@ -2,44 +2,50 @@
 using System.IO;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 public partial class SurfaceExporter
 {
-    /// <summary>
-    /// Clears the mesh structure and physical properties, 
-    /// preparing this exporter for another set of objects.
-    /// </summary>
-    public void Reset(Guid guid)
-    {
-        outputMesh = new BXDAMesh(guid);
-    }
-
-    /// <summary>
-    /// Gets the currently generated mesh object.
-    /// </summary>
-    /// <returns>a BXDA Mesh</returns>
-    public BXDAMesh GetOutput()
-    {
-        DumpMeshBuffer();
-        return outputMesh;
-    }
+    private const int MAX_WAITING_EVENTS = 64;
 
     /// <summary>
     /// Exports all the components in this group to the in-RAM mesh.
     /// </summary>
     /// <param name="group">Group to export from</param>
     /// <param name="reporter">Progress reporter</param>
-    public void ExportAll(CustomRigidGroup group, BXDIO.ProgressReporter reporter = null)
+    public BXDAMesh ExportAll(CustomRigidGroup group, Guid guid, BXDIO.ProgressReporter reporter = null)
     {
-        // Collect faces to export
-        List<SurfaceBody> plannedSurfaces = GenerateExportList(group);
+        // Create output mesh
+        BXDAMesh outputMesh = new BXDAMesh(guid);
 
-        // Export faces
+        // Collect faces to export
+        List<SurfaceBody> plannedSurfaces = GenerateExportList(group, outputMesh);
+
+        // Export faces, multithreaded
         reporter?.Invoke(0, plannedSurfaces.Count);
+
+        ManualResetEvent[] doneEvents = new ManualResetEvent[MAX_WAITING_EVENTS];
+        ExportJob[] jobs = new ExportJob[plannedSurfaces.Count];
+
+        // Create wait events
+        for (int i = 0; i < MAX_WAITING_EVENTS; i++)
+            doneEvents[i] = new ManualResetEvent(true); // Start with all events triggered to fill up event space
+
+        // Start jobs
         for (int i = 0; i < plannedSurfaces.Count; i++)
         {
-            AddFacets(plannedSurfaces[i], false, SynthesisGUI.PluginSettings.GeneralUseFancyColors);
+            int waitSlot = WaitHandle.WaitAny(doneEvents); // Get next available done event handle
+            doneEvents[waitSlot].Reset(); // Reset the event
+
+            jobs[i] = new ExportJob(plannedSurfaces[i], outputMesh, false, SynthesisGUI.PluginSettings.GeneralUseFancyColors);
+
+            ThreadPool.QueueUserWorkItem(jobs[i].ThreadPoolCallback, new ExportJob.JobContext { doneEvent = doneEvents[waitSlot] }); // Add the job to the queue
             reporter?.Invoke((i + 1), plannedSurfaces.Count);
         }
+
+        // Wait for all jobs to finish
+        WaitHandle.WaitAll(doneEvents);
+
+        return outputMesh;
     }
 }
