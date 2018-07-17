d1b1872927e0c1a0118b6cacce14fac1be420152
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-17 10:57:15 -0700

    Optimized multithreaded code

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/Exporter/SurfaceExporter_Access.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/Exporter/SurfaceExporter_Access.cs
@@ -2,16 +2,14 @@
 using System.IO;
 using System;
 using System.Collections.Generic;
-using System.Threading;
+using System.Threading.Tasks;
 
 public partial class SurfaceExporter
 {
     public SurfaceExporter()
     {
-        ExportJob.ResetAssets();
+        assets.Clear();
     }
-
-    private const int MAX_WAITING_JOBS = 32;
 
     /// <summary>
     /// Exports all the components in this group to the in-RAM mesh.
@@ -23,50 +21,27 @@ public partial class SurfaceExporter
         // Create output mesh
         MeshController outputMesh = new MeshController(guid);
 
-        // Collect faces to export
+        // Collect surfaces to export
         List<SurfaceBody> plannedSurfaces = GenerateExportList(group, outputMesh.Mesh);
 
         // Export faces, multithreaded
         reporter?.Invoke(0, plannedSurfaces.Count);
 
-        ManualResetEvent[] doneEvents = new ManualResetEvent[MAX_WAITING_JOBS];
-        ExportJob[] jobs = new ExportJob[plannedSurfaces.Count];
-
-        // Create wait events
-        for (int i = 0; i < MAX_WAITING_JOBS; i++)
-            doneEvents[i] = new ManualResetEvent(true); // Start with all events triggered to fill up event space
-
         // Start jobs
         int totalJobsFinished = 0;
         object finishLock = new object(); // Used to prevent multiple threads from updating progress bar at the same time.
 
-        for (int i = 0; i < plannedSurfaces.Count; i++)
+        Parallel.ForEach(plannedSurfaces, (SurfaceBody surface) =>
         {
-            int waitSlot = WaitHandle.WaitAny(doneEvents); // Get next available done event handle
-            doneEvents[waitSlot].Reset(); // Reset the event
+        //SurfaceBody surface = plan.surface;
+            CalculateSurfaceFacets(surface, outputMesh, SynthesisGUI.PluginSettings.GeneralUseFancyColors);// && !plan.simplify);
 
-            jobs[i] = new ExportJob(plannedSurfaces[i], outputMesh, SynthesisGUI.PluginSettings.GeneralUseFancyColors);
-
-            // Add the job to the queue
-            ThreadPool.QueueUserWorkItem(jobs[i].ThreadPoolCallback, new ExportJob.JobContext { doneEvent = doneEvents[waitSlot], onFinish = () =>
-                {
-                    // Update the progress bar
-                    lock (finishLock)
-                    {
-                        totalJobsFinished++;
-                        reporter?.Invoke(totalJobsFinished, plannedSurfaces.Count);
-                    }
-                }
-            });
-        }
-
-        // Wait for all jobs to finish
-        WaitHandle.WaitAll(doneEvents);
-
-        // Check for errors
-        foreach (ExportJob job in jobs)
-            if (job.error != null)
-                throw job.error;
+            lock (finishLock)
+            {
+                totalJobsFinished++;
+                reporter?.Invoke(totalJobsFinished, plannedSurfaces.Count);
+            }
+        });
 
         outputMesh.DumpOutput();
         return outputMesh.Mesh;
