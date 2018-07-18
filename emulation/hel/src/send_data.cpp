c39a65bd9dbac79c5e37953648437611c78b4b1b
copernicium <logan.traffas@gmail.com>
2018-07-18 08:36:55 -0700

    Fix overflow of error statuses when calling HAL

--- emulation/hel/src/send_data.cpp
+++ emulation/hel/src/send_data.cpp
@@ -11,59 +11,53 @@ void hel::SendData::update(){
     int32_t status = 0;
     for(unsigned i = 0; i < pwm_hdrs.size(); i++){
         pwm_hdrs[i] = HAL_GetPWMSpeed(i, &status);
-        printf("%f\n", HAL_GetPWMSpeed(i, &status));
+        printf("%f\n", pwm_hdrs[i]);
+        status = 0; //reset status between HAL calls
     }
 
     for(unsigned i = 0; i < relays.size(); i++){
         relays[i] = [&](){
-            int forward_handle = i;
-            int reverse_handle = i + hal::kNumRelayHeaders;
-            if(HAL_GetRelay(forward_handle, &status)){
-                if(HAL_GetRelay(reverse_handle, &status)){
+            bool forward = HAL_GetRelay(i, &status);
+            bool reverse = HAL_GetRelay(i + hal::kNumRelayHeaders, &status);
+
+            if(forward){
+                if(reverse){
                     return RelayState::ERROR;
                 } else{
                     return RelayState::FORWARD;
                 }
-            } else if(HAL_GetRelay(reverse_handle, &status)){
+            } else if(reverse){
                 return RelayState::REVERSE;
             }
             return RelayState::OFF;
         }();
+        status = 0; //reset status between HAL calls
     }
 
     for(unsigned i = 0; i < analog_outputs.size(); i++){
         analog_outputs[i] = HAL_GetAnalogOutput(i, &status);
+        status = 0; //reset status between HAL calls
     }
 
     for(unsigned i = 0; i < digital_mxp.size(); i++){
         digital_mxp[i].config = [&](){
             if(checkBitHigh(instance.first->digital_system.getMXPSpecialFunctionsEnabled(), i)){
                 if(
-                    i == 0 ||
-                    i == 1 ||
-                    i == 2 ||
-                    i == 3 ||
-                    i == 8 ||
-                    i == 9 ||
-                    i == 10 ||
-                    i == 11 ||
-                    i == 12 ||
-                    i == 13
+                    i == 0  || i == 1  ||
+                    i == 2  || i == 3  ||
+                    i == 8  || i == 9  ||
+                    i == 10 || i == 11 ||
+                    i == 12 || i == 13
                 ){
                     return hel::MXPData::Config::PWM;
                 }
                 if(
-                    i == 4 ||
-                    i == 5 ||
-                    i == 6 ||
-                    i == 7
+                    i == 4 || i == 5 ||
+                    i == 6 || i == 7
                 ){
                     return hel::MXPData::Config::SPI;
                 }
-                if(
-                    i == 14 ||
-                    i == 15
-                ){
+                if(i == 14 || i == 15){
                     return hel::MXPData::Config::I2C;
                 }
             }
@@ -73,6 +67,7 @@ void hel::SendData::update(){
             }
             return hel::MXPData::Config::DI;
         }();
+
         switch(digital_mxp[i].config){
         case hel::MXPData::Config::DO:
             digital_mxp[i].value = HAL_GetDIO(i + hal::kNumDigitalHeaders, &status);
@@ -89,14 +84,15 @@ void hel::SendData::update(){
         default:
             ; //TODO error handling
         }
+        status = 0; //reset status between HAL calls
     }
-
     {
         tDIO::tOutputEnable output_mode = instance.first->digital_system.getEnabledOutputs();
         for(unsigned i = 0; i < digital_hdrs.size(); i++){
             if(checkBitHigh(output_mode.MXP,i)){
                 HAL_SetDIO(digital_hdrs[i], i, &status);
             }
+            status = 0; //reset status between HAL calls
         }
     }
     instance.second.unlock();
