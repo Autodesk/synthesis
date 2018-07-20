cc3f164fc00347f60fb1e0e59efd3268a19150be
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-20 14:13:47 -0700

    Too allow the center of mass to use floats, Vector3s are now templates, do doubles or floats may be used

--- Source/Data/BXDA/Physics.h
+++ Source/Data/BXDA/Physics.h
@@ -12,15 +12,15 @@ namespace BXDA
 		Physics();
 
 		Physics(const Physics &);
-		Physics(Vector3 centerOfMass, double mass);
+		Physics(Vector3<float> centerOfMass, float mass);
 
 		Physics operator+=(const Physics &); // Averages the center of mass of a physics class with another
 
 		std::string toString();
 
 	private:
-		Vector3 centerOfMass;
-		double mass;
+		Vector3<float> centerOfMass;
+		float mass;
 
 		void write(BinaryWriter &) const;
 
--- Source/Data/BXDA/Vector3.h
+++ Source/Data/BXDA/Vector3.h
@@ -5,20 +5,21 @@
 
 namespace BXDA
 {
+	template<typename T = double>
 	class Vector3 : public BinaryWritable
 	{
 	public:
-		double x;
-		double y;
-		double z;
+		T x;
+		T y;
+		T z;
 		
 		Vector3();
-		Vector3(const Vector3 &);
-		Vector3(double x, double y, double z);
+		Vector3(const Vector3<T> &);
+		Vector3(T x, T y, T z);
 
-		Vector3 operator+(const Vector3 &) const;
-		Vector3 operator*(double) const;
-		Vector3 operator/(double) const;
+		Vector3<T> operator+(const Vector3 &) const;
+		template<typename S> Vector3<T> operator*(S) const;
+		template<typename S> Vector3<T> operator/(S) const;
 
 		std::string toString();
 
@@ -26,4 +27,50 @@ namespace BXDA
 		void write(BinaryWriter &) const;
 
 	};
+
+	template<typename T = double>
+	Vector3<T>::Vector3() : Vector3(0, 0, 0)
+	{}
+
+	template<typename T = double>
+	Vector3<T>::Vector3(const Vector3<T> & vector)
+	{
+		x = vector.x;
+		y = vector.y;
+		z = vector.z;
+	}
+
+	template<typename T = double>
+	Vector3<T>::Vector3(T x, T y, T z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+	template<typename T>
+	Vector3<T> Vector3<T>::operator+(const Vector3<T> & other) const { return Vector3<T>(x + other.x, y + other.y, x + other.z); }
+
+	template<typename T>
+	template<typename S>
+	Vector3<T> Vector3<T>::operator*(S scale) const { return Vector3<T>(x * scale, y * scale, z * scale); }
+
+	template<typename T>
+	template<typename S>
+	Vector3<T> Vector3<T>::operator/(S scale) const { return Vector3<T>(x / scale, y / scale, z / scale); }
+	
+	template<typename T>
+	std::string Vector3<T>::toString()
+	{
+		return "(" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ")";
+	}
+
+	template<typename T>
+	void Vector3<T>::write(BinaryWriter & output) const
+	{
+		output.write(x);
+		output.write(y);
+		output.write(z);
+	}
+
 }
