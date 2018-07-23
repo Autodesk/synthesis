68d98e4d07ac83806558e805903532143f7a7902
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-23 08:33:42 -0700

    Added basic implementation for rotational joint

--- Source/Data/BXDJ/Joints/RotationalJoint.cpp
+++ Source/Data/BXDJ/Joints/RotationalJoint.cpp
@@ -1,2 +1,8 @@
 #include "RotationalJoint.h"
 
+using namespace BXDJ;
+
+//RotationalJoint::RotationalJoint(const RigidNode & child, const adsk::fusion::RevoluteJointMotion & fusionJoint) : Joint(child)
+//{
+//	this->fusionJoint = NULL;
+//}
--- Source/Data/BXDJ/Joints/RotationalJoint.h
+++ Source/Data/BXDJ/Joints/RotationalJoint.h
@@ -1,11 +1,17 @@
 #pragma once
 
-#include "../RigidNode.h"
+#include <Fusion/Components/RevoluteJointMotion.h>
+#include "AngularJoint.h"
 
 namespace BXDJ
 {
-	class RotationalJoint : public Joint
+	class RotationalJoint : public AngularJoint
 	{
+	public:
+		//RotationalJoint(const RigidNode & child, const adsk::fusion::RevoluteJointMotion &);
+
+	private:
+		adsk::fusion::RevoluteJointMotion * fusionJoint;
 
 	};
 }
