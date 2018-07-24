dcc31286529e454c5a2661dbd932da7780834209
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-24 14:26:26 -0700

    Implemented XmlWriter and XmlWritable
    Former-commit-id: 32661d2411667d3cd3756c4cebe6a2b52fd92465

---
+++ Source/Data/BXDJ/XmlWriter.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <stack>
+#include <list>
+
+namespace BXDJ
+{
+	class XmlWritable;
+
+	class XmlWriter
+	{
+	public:
+		XmlWriter(std::string file, bool lightWeight = false);
+		~XmlWriter();
+
+		void startElement(std::string);
+		void writeAttribute(std::string, std::string);
+		void writeElement(std::string, std::string);
+		void endElement();
+		void write(const XmlWritable &);
+
+	private:
+		struct Attribute
+		{
+			std::string name;
+			std::string value;
+		};
+
+		struct XmlElement
+		{
+			std::string name;
+			std::list<Attribute> attributes;
+			std::string innerXml;
+		};
+
+		std::ofstream outputFile;
+		bool lightWeight;
+		std::stack<XmlElement *> elementStack;
+
+	};
+
+	// Classes that inherit from XmlWritable can be fed into a XmlWriter
+	class XmlWritable
+	{
+		friend XmlWriter;
+
+	private:
+		virtual void write(XmlWriter &) const = 0;
+
+	};
+
+	// Inline function for writing XmlWritables. Inlines must be defined in header, and after any classes they use
+	inline void XmlWriter::write(const XmlWritable & xml) { xml.write(*this); }
+}
