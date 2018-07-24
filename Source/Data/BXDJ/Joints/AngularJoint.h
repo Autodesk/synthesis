7b6199bcc1eabce40685fa2f2a21c5806ffb9f73
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-24 11:21:09 -0700

    Added empty functions for rotational joint
    Former-commit-id: 3d156bd9102c55958582e127c0b43493e4dcc7b6

--- Source/Data/BXDJ/Joints/AngularJoint.h
+++ Source/Data/BXDJ/Joints/AngularJoint.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "../../Vector3.h"
-#include "../RigidNode.h"
+#include "../Joint.h"
 
 namespace BXDJ
 {
 	class AngularJoint : public Joint
 	{
+	public:
+		AngularJoint(const RigidNode & child) : Joint(child) {}
+
 		virtual Vector3<float> getAxisOfRotation() = 0;
 		virtual float getCurrentAngle() = 0;
 		virtual float getUpperLimit() = 0;
--- Source/Data/BXDJ/Joints/RotationalJoint.cpp
+++ Source/Data/BXDJ/Joints/RotationalJoint.cpp
@@ -2,7 +2,32 @@
 
 using namespace BXDJ;
 
-//RotationalJoint::RotationalJoint(const RigidNode & child, const adsk::fusion::RevoluteJointMotion & fusionJoint) : Joint(child)
-//{
-//	this->fusionJoint = NULL;
-//}
+RotationalJoint::RotationalJoint(const RigidNode & child, core::Ptr<fusion::RevoluteJointMotion> fusionJoint) : AngularJoint(child)
+{
+	this->fusionJoint = fusionJoint;
+}
+
+RotationalJoint::RotationalJoint(const RotationalJoint & jointToCopy) : AngularJoint(jointToCopy)
+{
+	fusionJoint = jointToCopy.fusionJoint;
+}
+
+Vector3<float> RotationalJoint::getAxisOfRotation()
+{
+	return Vector3<float>(0, 0, 0);
+}
+
+float RotationalJoint::getCurrentAngle()
+{
+	return 0.0f;
+}
+
+float BXDJ::RotationalJoint::getUpperLimit()
+{
+	return 0.0f;
+}
+
+float BXDJ::RotationalJoint::getLowerLimit()
+{
+	return 0.0f;
+}
