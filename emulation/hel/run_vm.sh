6b115b45197b14ac01a82b14e45ec4f073442b60
Nicholas Burrell <zero_frost@protonmail.com>
2018-07-23 21:21:28 -0700

    Updated .clang_complete and run_vm.sh
    Former-commit-id: df105c5c502189c617b99738c015f04659e8d99f

--- emulation/hel/run_vm.sh
+++ emulation/hel/run_vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SSH_COMMAND="ssh -p 10022 -t synthesis@localhost rm -rf /home/synthesis/FRCUserProgram"
 SCP_COMMAND="scp -P 10022 ./user_code/FRCUserProgram synthesis@localhost:/home/synthesis"
-PASSWORD="synthesis"
+PASSWORD="adskbxd"
 
 ./scripts/download_vm.sh
 printf "Starting VM\n"
@@ -11,7 +11,8 @@ if ! mkdir ./vm_lock > /dev/null ; then
 	printf "VM Initialized\nCopying files.\n"
 	expect -c "spawn $SCP_COMMAND; expect \"assword:\"; send \"$PASSWORD\r\"; interact"
 else
-	(qemu-system-arm -M vexpress-a9 -dtb synthesis_image/vexpress-v2p-ca9.dtb -kernel synthesis_image/zImage -append "root=/dev/mmcblk0 rw roottype=ext4 console=ttyAMA0" --nographic -serial null -drive if=sd,driver=raw,cache=writeback,file=synthesis_image/rootfs.ext4 -redir tcp:10022::22 < /dev/null &> /dev/null; rm -rf ./vm_lock )&
+	#(qemu-system-arm -M vexpress-a9 -dtb vm-package/zynq-zed.dtb -kernel vm-package/zImage -append "root=/dev/mmcblk0 rw roottype=ext4 console=ttyAMA0" --nographic -serial null -drive if=sd,driver=raw,cache=writeback,file=vm-package/rootfs.ext4 -redir tcp:10022::22 < /dev/null &> /dev/null; rm -rf ./vm_lock )&
+	(qemu-system-arm -machine xilinx-zynq-a9 -cpu cortex-a9 -m 2048 -kernel vm-package/zImage -dtb vm-package/zynq-zed.dtb -display none -serial null -serial mon:stdio -localtime -append "console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0" -redir tcp:10022::22  -sd vm-package/rootfs.ext4 < /dev/null &> /dev/null; rm -rf ./vm_lock )&
 	printf "VM Successfully start. Please wait while it initializes\n"
 	sleep 30 && expect -c "spawn $SSH_COMMAND; expect \"assword:\"; send \"$PASSWORD\r\"; interact"
 	expect -c "spawn $SSH_COMMAND; expect \"assword:\"; send \"$PASSWORD\r\"; interact"
