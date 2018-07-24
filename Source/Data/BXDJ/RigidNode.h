ccf176b0cb481a65846d9340b813d575e0984518
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-24 09:34:51 -0700

    Bug fixes

--- Source/Data/BXDJ/RigidNode.h
+++ Source/Data/BXDJ/RigidNode.h
@@ -14,26 +14,40 @@ namespace BXDJ
 {
 	class Joint;
 
-	// RIGID NODE
+	// Stores the collection of component occurences that act as a single rigidbody in Synthesis
 	class RigidNode
 	{
 	public:
 		RigidNode();
 		~RigidNode();
 		RigidNode(core::Ptr<fusion::Component>);
-		RigidNode(core::Ptr<fusion::Occurrence>);
 
 		void getMesh(BXDA::Mesh &) const;
 		
 		template<typename JointVariant>
 		void addJoint(const JointVariant &);
+		
+		std::string log = "";
 
 	private:
+		// Used for storing information about which occurences are parents or children in joints
+		struct JointSummary
+		{
+			std::vector<core::Ptr<fusion::Occurrence>> children;
+			std::map<core::Ptr<fusion::Occurrence>, std::vector<core::Ptr<fusion::Occurrence>>> parents;
+		};
+
+		// Stores all component occurences that are grouped into this node
 		std::vector<core::Ptr<fusion::Occurrence>> fusionOccurrences;
-		std::vector<Joint *> childrenJoints;
+		// Stores the joints that lead to this node's children
+		std::vector<Joint *> childrenJoints; 
 
-		void buildTree(core::Ptr<fusion::Component>);
+		RigidNode(core::Ptr<fusion::Occurrence> occ, JointSummary & jSum) { buildTree(occ, jSum); }
 
+		JointSummary getJointSummary(core::Ptr<fusion::Component>);
+		void buildTree(core::Ptr<fusion::Occurrence>, JointSummary &);
+
+		// Gets the level of a component occurence in the heirarchy
 		static int levelOfOccurrence(core::Ptr<fusion::Occurrence>);
 
 	};
@@ -44,7 +58,7 @@ namespace BXDJ
 		childrenJoints.push_back(new JointVariant(joint));
 	}
 
-	// JOINT
+	// Links RigidNodes together
 	class Joint
 	{
 	public:
