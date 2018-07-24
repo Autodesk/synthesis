e08bdf206239c27cef10aa1134b992ab1f2f7e86
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-24 14:35:07 -0700

    Bug fixes
    Former-commit-id: 950f44e35b367133c9b6ecc13586da2c4a7784f5

--- Source/Data/BXDJ/XmlWriter.cpp
+++ Source/Data/BXDJ/XmlWriter.cpp
@@ -31,44 +31,72 @@ void XmlWriter::writeAttribute(std::string name, std::string value)
 
 void XmlWriter::writeElement(std::string name, std::string innerXml)
 {
-	elementStack.top()->innerXml += "<" + name + ">";
-	elementStack.top()->innerXml += innerXml;
-	elementStack.top()->innerXml += "</" + name + ">\n";
+	XmlElement * currentElement = NULL;
+
+	// Determine whether writing to file or parent element
+	if (!elementStack.empty())
+		currentElement = elementStack.top();
+	else
+		currentElement = new XmlElement();
+
+	// Write opening, content, and closing on same line
+	currentElement->innerXml += indent(elementStack.size()) + "<" + name + ">";
+	currentElement->innerXml += innerXml;
+	currentElement->innerXml += indent(elementStack.size()) + "</" + name + ">" + (lightWeight ? "" : "\n");
+
+	if (elementStack.empty())
+	{
+		if (outputFile.is_open())
+			outputFile << currentElement->innerXml;
+
+		delete currentElement;
+	}
 }
 
 void XmlWriter::endElement()
 {
 	XmlElement * endingElement = elementStack.top();
 	elementStack.pop();
-	XmlElement * nextElement = NULL;
+	XmlElement * currentElement = NULL;
 
 	if (!elementStack.empty())
-		nextElement = elementStack.top();
+		currentElement = elementStack.top();
 	else
-		nextElement = new XmlElement();
+		currentElement = new XmlElement();
 
 	// Print out opening bracket
-	nextElement->innerXml += "<" + endingElement->name;
+	currentElement->innerXml += indent(elementStack.size()) + "<" + endingElement->name;
+
 	if (endingElement->attributes.size() > 0)
 		for (Attribute attr : endingElement->attributes)
-			nextElement->innerXml += " " + attr.name + "=\"" + attr.value + "\"";
-	nextElement->innerXml += ">\n";
+			currentElement->innerXml += " " + attr.name + "=\"" + attr.value + "\"";
+
+	currentElement->innerXml += ">";
+	currentElement->innerXml += (lightWeight ? "" : "\n");
 
 	// Print out inner XML
-	nextElement->innerXml += endingElement->innerXml;
+	currentElement->innerXml += endingElement->innerXml;
 
 	// Print out closing bracket
-	nextElement->innerXml += "</" + endingElement->name + ">\n";
+	currentElement->innerXml += indent(elementStack.size()) + "</" + endingElement->name + ">" + (lightWeight ? "" : "\n");
 
 	// If the closed item was the last in the stack, print to file
 	if (elementStack.empty())
 	{
 		if (outputFile.is_open())
-			outputFile << nextElement->innerXml;
+			outputFile << currentElement->innerXml;
 
-		delete nextElement;
+		delete currentElement;
 	}
 
 	// Clean up memory
 	delete endingElement;
+}
+
+std::string XmlWriter::indent(size_t indentation)
+{
+	if (!lightWeight)
+		return std::string(indentation * 4, ' ');
+	else
+		return "";
 }
