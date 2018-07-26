bd02016ef8beff2e402a6fb708a6925eaecfa3bc
copernicium <logan.traffas@gmail.com>
2018-07-26 09:11:06 -0700

    Bounds check during std::string char[] conversion
    Former-commit-id: 4a2394e854022c018c444a599174a2b39dcec563

---
+++ emulation/hel/scripts/login.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sshpass -p "adskbxd" ssh -p 10022 synthesis@localhost
--- emulation/hel/src/util.cpp
+++ emulation/hel/src/util.cpp
@@ -7,3 +7,9 @@ bool hel::stob(std::string a){
 std::string hel::to_string(bool a){
     return a ? "1" : "0";
 }
+
+void hel::copystr(const std::string& str, char* c_arr){
+    str.copy(c_arr, sizeof(c_arr));
+    std::size_t terminator_index = std::max(std::min((signed int)(str.size()) - 1, (signed int)(sizeof(c_arr)) - 1), 0);
+    c_arr[terminator_index] = '\0';
+}
