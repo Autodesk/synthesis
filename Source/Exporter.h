398e1f8cdad6b6749c7aeb0cc0a2f8033a5d5ec8
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-26 12:12:51 -0700

    Removed unused include statements
    Former-commit-id: 5c08645817cc5304e9958acfca94f388f2883a34

--- Source/Exporter.h
+++ Source/Exporter.h
@@ -1,19 +1,7 @@
 #pragma once
 
-#include <string>
-#include <time.h>
-#include <chrono>
-#include <numeric>
-#include <vector>
-#include <Core/CoreAll.h>
-#include <Fusion/FusionAll.h>
-#include <CAM/CAMAll.h>
-#include <Core/UserInterface/ToolbarControls.h>
-#include <Core/UserInterface/Command.h>
-#include <Core/UserInterface/CommandEvent.h>
-#include <Core/UserInterface/CommandEventHandler.h>
-#include <Core/UserInterface/CommandEventArgs.h>
-#include "AddIn/EUI.h"
+#include <Core/Application/Application.h>
+#include <Core/UserInterface/UserInterface.h>
 #include "Data/BXDA/Mesh.h"
 #include "Data/BXDJ/ConfigData.h"
 #include "Data/BXDJ/RigidNode.h"
@@ -22,12 +10,9 @@
 
 using namespace adsk::core;
 using namespace adsk::fusion;
-using namespace adsk::cam;
 
 namespace Synthesis
 {
-	enum logLevels { info, warn, critikal };
-
 	class Exporter
 	{
 	public:
