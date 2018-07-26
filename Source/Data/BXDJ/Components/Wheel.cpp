57018c3d19b46a46424eeabca1a86a7c907a3080
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-26 16:46:41 -0700

    Added friction levels

--- Source/Data/BXDJ/Components/Wheel.cpp
+++ Source/Data/BXDJ/Components/Wheel.cpp
@@ -9,14 +9,16 @@ Wheel::Wheel(const Wheel & wheelToCopy)
 	width = wheelToCopy.width;
 	center = wheelToCopy.center;
 	type = wheelToCopy.type;
+	frictionLevel = wheelToCopy.frictionLevel;
 }
 
-Wheel::Wheel(Type type)
+Wheel::Wheel(Type type, FrictionLevel frictionLevel)
 {
 	radius = 0;
 	width = 0;
 	center = Vector3<>();
 	this->type = type;
+	this->frictionLevel = frictionLevel;
 }
 
 Wheel::Wheel(const Wheel & wheel, const RotationalJoint & joint) : Wheel(wheel)
@@ -65,14 +67,72 @@ void Wheel::write(XmlWriter & output) const
 	output.write(getCenter());
 	output.endElement();
 
-	output.writeElement("ForwardAsympSlip", std::to_string(0.0f));
-	output.writeElement("ForwardAsympValue", std::to_string(0.0f));
-	output.writeElement("ForwardExtremeSlip", std::to_string(0.0f));
-	output.writeElement("ForwardExtremeValue", std::to_string(0.0f));
-	output.writeElement("SideAsympSlip", std::to_string(0.0f));
-	output.writeElement("SideAsympValue", std::to_string(0.0f));
-	output.writeElement("SideExtremeSlip", std::to_string(0.0f));
-	output.writeElement("SideExtremeValue", std::to_string(0.0f));
+	switch (frictionLevel) // Got to make this smaller, oof
+	{
+	case LOW:
+		output.writeElement("ForwardAsympSlip", std::to_string(1.0f));
+		output.writeElement("ForwardAsympValue", std::to_string(5.0f));
+		output.writeElement("ForwardExtremeSlip", std::to_string(1.5f));
+		output.writeElement("ForwardExtremeValue", std::to_string(3.0f));
+		if (type == OMNI)
+		{
+			output.writeElement("SideAsympSlip", std::to_string(1.0f));
+			output.writeElement("SideAsympValue", std::to_string(0.01f));
+			output.writeElement("SideExtremeSlip", std::to_string(1.5f));
+			output.writeElement("SideExtremeValue", std::to_string(0.005f));
+		}
+		else
+		{
+			output.writeElement("SideAsympSlip", std::to_string(1.0f));
+			output.writeElement("SideAsympValue", std::to_string(5.0f));
+			output.writeElement("SideExtremeSlip", std::to_string(1.5f));
+			output.writeElement("SideExtremeValue", std::to_string(3.0f));
+		}
+		break;
+
+	case MEDIUM:
+		output.writeElement("ForwardAsympSlip", std::to_string(1.0f));
+		output.writeElement("ForwardAsympValue", std::to_string(7.0f));
+		output.writeElement("ForwardExtremeSlip", std::to_string(1.5f));
+		output.writeElement("ForwardExtremeValue", std::to_string(5.0f));
+		if (type == OMNI)
+		{
+			output.writeElement("SideAsympSlip", std::to_string(1.0f));
+			output.writeElement("SideAsympValue", std::to_string(0.01f));
+			output.writeElement("SideExtremeSlip", std::to_string(1.5f));
+			output.writeElement("SideExtremeValue", std::to_string(0.005f));
+		}
+		else
+		{
+			output.writeElement("SideAsympSlip", std::to_string(1.0f));
+			output.writeElement("SideAsympValue", std::to_string(7.0f));
+			output.writeElement("SideExtremeSlip", std::to_string(1.5f));
+			output.writeElement("SideExtremeValue", std::to_string(5.0f));
+		}
+		break;
+
+	case HIGH:
+		output.writeElement("ForwardAsympSlip", std::to_string(1.0f));
+		output.writeElement("ForwardAsympValue", std::to_string(10.0f));
+		output.writeElement("ForwardExtremeSlip", std::to_string(1.5f));
+		output.writeElement("ForwardExtremeValue", std::to_string(8.0f));
+		if (type == OMNI)
+		{
+			output.writeElement("SideAsympSlip", std::to_string(1.0f));
+			output.writeElement("SideAsympValue", std::to_string(0.01f));
+			output.writeElement("SideExtremeSlip", std::to_string(1.5f));
+			output.writeElement("SideExtremeValue", std::to_string(0.005f));
+		}
+		else
+		{
+			output.writeElement("SideAsympSlip", std::to_string(1.0f));
+			output.writeElement("SideAsympValue", std::to_string(10.0f));
+			output.writeElement("SideExtremeSlip", std::to_string(1.5f));
+			output.writeElement("SideExtremeValue", std::to_string(8.0f));
+		}
+		break;
+	}
+
 	output.writeElement("IsDriveWheel", std::to_string(false));
 
 	output.endElement();
