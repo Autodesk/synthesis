fb3c3d3609df774b13ba4dde3bd5ca5b5a4549a2
Mackinnon Buck <mackinnon.buck@gmail.com>
2018-07-27 10:07:02 -0700

    Added percentage indicators for file transferring
    Added a label that shows a percentage representing how far along the resource gathering/distribution processes are.

--- engine/unity5/Assets/Scripts/States/GatheringResourcesState.cs
+++ engine/unity5/Assets/Scripts/States/GatheringResourcesState.cs
@@ -9,22 +9,37 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace Synthesis.States
 {
     public class GatheringResourcesState : SyncState
     {
+        private Text percentageText;
+        private PlayerIdentity[] identities;
+
         /// <summary>
         /// Starts gathering resources from any necessary clients, and continues to the next state when
         /// the process is complete.
         /// </summary>
         public override void Start()
         {
+            percentageText = GameObject.Find("GatheringPercentageText").GetComponent<Text>();
+
             if (!Host)
                 return;
 
             MatchManager.Instance.AwaitChangeState<DistributingResourcesState>(false);
             MatchManager.Instance.GatherResources();
+        }
+
+        /// <summary>
+        /// Updates the percentage label.
+        /// </summary>
+        public override void OnGUI()
+        {
+            identities = UnityEngine.Object.FindObjectsOfType<PlayerIdentity>();
+            percentageText.text = ((int)(identities.Average(p => p.gatheringProgress) * 100f)).ToString() + "%";
         }
     }
 }
