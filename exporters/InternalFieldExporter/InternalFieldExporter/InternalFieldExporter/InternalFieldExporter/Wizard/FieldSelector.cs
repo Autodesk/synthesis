a3f22438daeb48200aca9dbc669f166ca35c0c85
reeblucas <lucasrt62641@gmail.com>
2018-07-27 15:57:06 -0700

    updated nugets, added several windows forms
    Updated OpenTk and Fody nugets
    added windows forms for choosing field assembly, setting property sets, and edited the startup form to allow access through inventor tempoarily

---
+++ exporters/InternalFieldExporter/InternalFieldExporter/InternalFieldExporter/InternalFieldExporter/Wizard/FieldSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InternalFieldExporter.Wizard
+{
+    public partial class FieldSelector : Form
+    {
+        public FieldSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cancel_Button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
