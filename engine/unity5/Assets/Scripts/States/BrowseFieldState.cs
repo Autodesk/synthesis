4779ec7e1d2566b3c03e19f8c53caea00c7a81b1
kirac2093 <kirac2093@gmail.com>
2018-07-30 13:35:59 -0700

    Corrected directory paths
    Former-commit-id: 7523e84be2dbbf20926306f43a65f289dea17eb3

--- engine/unity5/Assets/Scripts/States/BrowseFieldState.cs
+++ engine/unity5/Assets/Scripts/States/BrowseFieldState.cs
@@ -8,7 +8,7 @@ namespace Synthesis.States
         /// Initializes a new <see cref="BrowseFieldState"/> instance.
         /// </summary>
         public BrowseFieldState() : base("FieldDirectory",
-        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "//synthesis//Fields")
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\synthesis\\Fields")
         {
 
         }
