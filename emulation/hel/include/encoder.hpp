9b5e6fd143e20427cd51d966a406c821ad0c8ad1
copernicium <logan.traffas@gmail.com>
2018-07-29 21:50:54 -0700

    Comment breakdown of encoder-related data
    Former-commit-id: a72867a6a960501a2340a22a1f3786edfe08c547

--- emulation/hel/include/encoder.hpp
+++ emulation/hel/include/encoder.hpp
@@ -18,7 +18,9 @@ namespace hel{
 
         /**
          * \var tEnoder::tOutput output
-         * \brief
+         * \brief The encoder's raw count
+         * Direction is the direction of motion
+         * Value is the encoder tick count
          */
 
         nFPGA::nRoboRIO_FPGANamespace::tEncoder::tOutput output;
@@ -26,20 +28,32 @@ namespace hel{
         /**
          * \var tEnoder::tConfig config
          * \brief Configuration for count
+         * IndexSource specifies which input will reset the encoder count
+         * Reverse reversed the encoder's direction
+         * ASource and BSource indicate which port the encoder should pull from
+         *     Module indicates whether it's on the MXP
+         *     Channel is the remapped hardware channel itself (For analog triggers, the first two bits indicate the analog trigger type, the others indicate the actual channel)
+         *     AnalogTrigger indicates that it's an analog input rather than DIO (will be triggered )
          */
 
         nFPGA::nRoboRIO_FPGANamespace::tEncoder::tConfig config;
 
         /**
          * \var tEnoder::tTimerOutput timer_output
-         * \brief Time-based count
+         * \brief The period of the most recent pulse
+         * Stalled represents the encoder stalled state (i.e. it it's stopped turning)
+         * Period is the time taken to count \bCount encoder ticks in units of system ticks (40 microseconds per system tick). It increments by 2's but uses the first bit, so it must be left bit-shifted one before interpretation.
+         * Count is the encoder count during the polling period
          */
 
         nFPGA::nRoboRIO_FPGANamespace::tEncoder::tTimerOutput timer_output;
 
         /**
          * \var tEnoder::tTimerConfig timer_config
-         * \brief Configuration for time-based count
+         * \brief Configuration for encoder period measurement
+         * StallPeriod is the time that must pass until the encoder is continued stalled (in system ticks)
+         * AverageSize sets the number of samples the timer uses when calculating the period (expects 1 to 127)
+         * UpdateWhenEmpty is like the UpdateWhenEmpty of tCounter's TimerConfig, but it is unused
          */
 
         nFPGA::nRoboRIO_FPGANamespace::tEncoder::tTimerConfig timer_config;
