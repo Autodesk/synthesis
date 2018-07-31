771e5c448661d02f202bfca43f581dc292d7e6a4
Nathan Walker <nathanwalker1208@gmail.com>
2018-07-31 13:16:33 -0700

    Improved joint support
    Former-commit-id: e133bc42ba3e6e5b1e3ac37ecbaf82d93590d9aa

---
+++ Source/Data/BXDJ/Joints/BallJoint.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Fusion/Components/BallJointMotion.h>
+#include "../Joint.h"
+
+using namespace adsk;
+
+namespace BXDJ
+{
+	class BallJoint : public Joint
+	{
+	public:
+		BallJoint(const BallJoint &);
+		BallJoint(RigidNode *, core::Ptr<fusion::Joint>, core::Ptr<fusion::Occurrence>);
+
+		void applyConfig(const ConfigData &);
+		void write(XmlWriter &) const;
+
+	private:
+		core::Ptr<fusion::BallJointMotion> fusionJointMotion;
+
+	};
+}
