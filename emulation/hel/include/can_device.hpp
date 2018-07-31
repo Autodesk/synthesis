f7f2a4d2d6795cfc464f33c0d706f9c77574d8f8
copernicium <logan.traffas@gmail.com>
2018-07-31 09:32:36 -0700

    Parse CAN send data to determine how to handle it
    Former-commit-id: e1cb05923b83658ebd4bc11d0ae6a8a12fa17833

--- emulation/hel/include/can_device.hpp
+++ emulation/hel/include/can_device.hpp
@@ -11,7 +11,7 @@ namespace hel{
      * Holds data for generic CAN devices and CAN motor controllers
      */
 
-    struct CANDevice{
+    struct CANDevice{//TODO rename to CANMotorController
 
         enum class Type{VICTOR_SPX,TALON_SRX,UNKNOWN};
 
@@ -23,63 +23,23 @@ namespace hel{
 
         static constexpr uint8_t MAX_CAN_BUS_ADDRESS = 62;
 
-        static constexpr uint8_t MAX_MESSAGE_DATA_SIZE = 8;
+        enum MessageData{
+            COMMAND_BYTE = 7,
+            SIZE = 8
+        };
 
-        #if 0
-        /**
-         * \var static constexpr int32_t CAN_SEND_PERIOD_NO_REPEAT
-         * \brief a send period communicating the message should not be repeated
-         */
-
-        static constexpr const int32_t CAN_SEND_PERIOD_NO_REPEAT = 0;
-
-        /**
-         * \var static constexpr int32_t CAN_SEND_PERIOD_STOP_REPEATING
-         * \brief a send period communicating the message with the associated ID should stop repeating
-         */
-
-        static constexpr int32_t CAN_SEND_PERIOD_STOP_REPEATING = -1;
-
-        /**
-         * \var static constexpr uint32_t CAN_IS_FRAME_REMOTE
-         * \brief used to identify a message ID as that of a remote frame
-         * Remote CAN frames are requests for data from a different source.
-         */
-
-        static constexpr uint32_t CAN_IS_FRAME_REMOTE = 0x80000000;
-
-        /**
-         * \var static constexpr uint32_t CAN_IS_FRAME_11BIT
-         * \brief used to identify a message ID as using 11-bit, base formatting
-         */
-
-        static constexpr uint32_t CAN_IS_FRAME_11BIT = 0x40000000;
-
-        /**
-         * \var static constexpr uint32_t CAN_29BIT_MESSAGE_ID_MASK
-         * \brief used as a message ID mask to communicate the message ID is in 29-bit, extended formatting
-         */
-
-        static constexpr uint32_t CAN_29BIT_MESSAGE_ID_MASK = 0x1FFFFFFF;
-
-        /**
-         * \var static constexpr uint32_t CAN_11BIT_MESSAGE_ID_MASK
-         * \brief used as a message ID mask to communicate the message ID is in 11-bit, base formatting
-         */
-
-        static constexpr uint32_t CAN_11BIT_MESSAGE_ID_MASK = 0x000007FF;
-        #endif
+        enum CommandByteMask: uint8_t{
+            SET_POWER_PERCENT = 5,
+            SET_INVERTED = 6
+        };
 
     private:
-        static constexpr uint32_t ID_MASK_29BIT = 0b01111111;
-
-        static constexpr uint32_t ID_COMPARISON_MASK = 0b11000001000000000001111111;
-
-        static constexpr uint32_t TYPE_COMPARISON_MASK = 0b11000001000000000000000000;
-
-        static constexpr uint32_t BASE_TALON_SRX_ID = 0x02040000; // 33816704
-
-        static constexpr uint32_t BASE_VICTOR_SPX_ID = 0x01040000; // 17039488
+        enum IDMask: uint32_t{
+            DEVICE_ID = 0b01111111,
+            DEVICE_TYPE = 0b11000001000000000000000000,
+            TALON_SRX_TYPE = 0x02040000,
+            VICTOR_SPX_TYPE = 0x01040000
+        };
 
         Type type;
 
@@ -87,8 +47,11 @@ namespace hel{
 
         double speed;
 
+        bool inverted;
+
     public:
         static uint8_t pullDeviceID(uint32_t);
+        static Type pullDeviceType(uint32_t);
 
         std::string toString()const;
 
@@ -96,9 +59,11 @@ namespace hel{
 
         uint8_t getID()const;
 
-        void setSpeed(BoundsCheckedArray<uint8_t,MAX_MESSAGE_DATA_SIZE>);
+        void setSpeed(BoundsCheckedArray<uint8_t,MessageData::SIZE>);
 
         double getSpeed()const;
+
+        void setInverted(bool);
 
         /**
          * \fn std::string serialize()const
