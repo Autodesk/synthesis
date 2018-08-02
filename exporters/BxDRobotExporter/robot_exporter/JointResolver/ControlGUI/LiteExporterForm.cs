19fc88f1dc8ab9e760eaee8dedbefbc072a97a00
Lauren Choquer <choquerlauren@gmail.com>
2018-08-02 09:47:56 -0700

    Fixed error with loading animation

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/ControlGUI/LiteExporterForm.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/ControlGUI/LiteExporterForm.cs
@@ -34,7 +34,7 @@ public partial class LiteExporterForm : Form
     public LiteExporterForm()
     {
         InitializeComponent();
-        LoadingAnimation.Image = JointResolver.Properties.Resources.SyntheMation350x350;
+        LoadingAnimation.Image = JointResolver.Properties.Resources.LoadAnimationNewLogo;
         Instance = this;
         LoadingAnimation.WaitOnLoad = true;
         ExporterWorker.WorkerReportsProgress = true;
