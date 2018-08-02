b4b1a406699bb043e56bd24c71ece38ccbab0efb
Lauren Choquer <choquerlauren@gmail.com>
2018-08-02 09:36:07 -0700

    Added new animation to LiteExporter form
    Former-commit-id: a7e8a30f082161902af7d1bd6958195879702596

--- exporters/BxDRobotExporter/robot_exporter/JointResolver/Properties/Resources.Designer.cs
+++ exporters/BxDRobotExporter/robot_exporter/JointResolver/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace JointResolver.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -86,6 +86,16 @@ namespace JointResolver.Properties {
         internal static System.Drawing.Bitmap LoadAnimation {
             get {
                 object obj = ResourceManager.GetObject("LoadAnimation", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LoadAnimationNewLogo {
+            get {
+                object obj = ResourceManager.GetObject("LoadAnimationNewLogo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
