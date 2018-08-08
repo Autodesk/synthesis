f7305de9a2436f1b7c5f15cef712686ac3d82664
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-08 14:58:46 -0700

    Improved colors
    Former-commit-id: af5b6ac921f33ed42c2872e09435f3bb5455770a

--- exporters/FusionExporter/Source/Data/BXDA/Surface.cpp
+++ exporters/FusionExporter/Source/Data/BXDA/Surface.cpp
@@ -81,14 +81,26 @@ void BXDA::Surface::setColor(unsigned char r, unsigned char g, unsigned char b, 
 	color = r + g * 0x100 + b * 0x10000 + a * 0x1000000;
 }
 
-void Surface::setColor(core::Ptr<core::Appearance> appearance)
+void Surface::setColor(core::Ptr<core::Material> material, core::Ptr<core::Appearance> appearance)
 {
-	if (appearance == nullptr)
+	if (material == nullptr || appearance == nullptr)
 		return;
 
-	core::Ptr<core::ColorProperty> colorProp = appearance->appearanceProperties()->itemByName("Color");
-	if (colorProp != nullptr)
-		setColor(colorProp->value()->red(), colorProp->value()->green(), colorProp->value()->blue(), colorProp->value()->opacity());
+	core::Ptr<core::ColorProperty> colorPropM = material->appearance()->appearanceProperties()->itemById("metal_f0");
+	if (colorPropM == nullptr)
+		colorPropM = material->appearance()->appearanceProperties()->itemByName("Color");
+
+	core::Ptr<core::ColorProperty> colorPropA = appearance->appearanceProperties()->itemById("metal_f0");
+	if (colorPropA == nullptr)
+		colorPropA = appearance->appearanceProperties()->itemByName("Color");
+		
+	if (colorPropM != nullptr && colorPropM->value() != nullptr &&
+		colorPropA != nullptr && colorPropA->value() != nullptr)
+
+		setColor(colorPropM->value()->red() * (colorPropA->value()->red() / 255.0),
+				 colorPropM->value()->green() * (colorPropA->value()->green() / 255.0),
+				 colorPropM->value()->blue() * (colorPropA->value()->blue() / 255.0),
+				 colorPropM->value()->opacity() * (colorPropA->value()->opacity() / 255.0));
 }
 
 void BXDA::Surface::removeColor()
