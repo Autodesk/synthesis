ae1c114a2054c35da8e9e77f95144b18dbcd0ba8
kirac2093 <kirac2093@gmail.com>
2018-08-09 14:26:52 -0700

    Added help button backend for emulation toolbar
    Former-commit-id: dd783faafabce40f85ceb51bfd5bb8e9288cb15d

--- engine/unity5/Assets/Scripts/GUI/EmulationToolbarState.cs
+++ engine/unity5/Assets/Scripts/GUI/EmulationToolbarState.cs
@@ -17,9 +17,23 @@ namespace Assets.Scripts.GUI
     {
         EmulationDriverStation emulationDriverStation;
 
+        GameObject canvas;
+        GameObject tabs;
+        GameObject emulationToolbar;
+
+        GameObject helpMenu;
+        GameObject overlay;
+
         public override void Start()
         {
             emulationDriverStation = StateMachine.SceneGlobal.GetComponent<EmulationDriverStation>();
+
+            canvas = GameObject.Find("Canvas");
+            tabs = Auxiliary.FindObject(canvas, "Tabs");
+            emulationToolbar = Auxiliary.FindObject(canvas, "EmulationToolbar");
+
+            helpMenu = Auxiliary.FindObject(canvas, "Help");
+            overlay = Auxiliary.FindObject(canvas, "Overlay");
         }
 
         public void OnSelectRobotCodeButtonPressed()
@@ -35,6 +49,31 @@ namespace Assets.Scripts.GUI
         public void OnStartRobotCodeButtonPressed()
         {
             emulationDriverStation.ToggleRobotCodeButton();
+        }
+
+        public void OnHelpButtonPressed()
+        {
+            helpMenu.SetActive(true);
+            Auxiliary.FindObject(helpMenu, "Type").GetComponent<Text>().text = "EmulationToolbar";
+            overlay.SetActive(true);
+            tabs.transform.Translate(new Vector3(200, 0, 0));
+            foreach (Transform t in emulationToolbar.transform)
+            {
+                if (t.gameObject.name != "HelpButton") t.Translate(new Vector3(200, 0, 0));
+                else t.gameObject.SetActive(false);
+            }
+        }
+
+        private void CloseHelpMenu()
+        {
+            helpMenu.SetActive(false);
+            overlay.SetActive(false);
+            tabs.transform.Translate(new Vector3(-200, 0, 0));
+            foreach (Transform t in emulationToolbar.transform)
+            {
+                if (t.gameObject.name != "HelpButton") t.Translate(new Vector3(-200, 0, 0));
+                else t.gameObject.SetActive(true);
+            }
         }
     }
 }
