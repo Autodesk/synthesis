a32c57d8335b929467ac33197e942032743876e6
reeblucas <lucasrt62641@gmail.com>
2018-08-09 16:23:59 -0700

    Added QuaternionFromMatrix class
    Added QuaternionFromMatrix class

--- exporters/InternalFieldExporter/InternalFieldExporter/InternalFieldExporter/InternalFieldExporter/Utilities.cs
+++ exporters/InternalFieldExporter/InternalFieldExporter/InternalFieldExporter/InternalFieldExporter/Utilities.cs
@@ -121,5 +121,58 @@ namespace InternalFieldExporter
                 GeneralUseFancyColors = Properties.Settings.Default.FancyColors
             };
         }
+
+        /// <summary>
+        /// Determines the position of a node using BXDVectors
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static BXDVector3 ToBXDVector(dynamic p)
+        {
+            return new BXDVector3(p.X, p.Y, p.Z);
+        }
+
+        public static BXDQuaternion QuaternionFromMatrix(Matrix m)
+        {
+            BXDQuaternion q = new BXDQuaternion();
+
+            double tr = m.Cell[1, 1] + m.Cell[2, 2] + m.Cell[3, 3];
+            double s;
+
+            if (tr > 0)
+            {
+                s= Math.Sqrt(tr + 1.0) *2;
+                q.W = (float)(0.25 * s);
+                q.X = (float)((m.Cell[3, 2]) - (m.Cell[2, 3]) / s);
+                q.Y = (float)((m.Cell[1, 3]) - (m.Cell[3, 1]) / s);
+                q.Z = (float)((m.Cell[2, 1]) - (m.Cell[1, 2]) / s);
+            }
+            else if ((m.Cell[1, 1] > m.Cell[2, 2]) && (m.Cell[1, 1] > m.Cell[3, 3]))
+            {
+                s = Math.Sqrt(1.0 + m.Cell[1, 1] - m.Cell[2, 2] - m.Cell[3, 3]) * 2;
+                q.W = (float)((m.Cell[3, 2] - m.Cell[2, 3]) / s);
+                q.X = (float)(0.25 * s);
+                q.Y = (float)((m.Cell[1, 2] + m.Cell[2, 1]) / s);
+                q.Z = (float)((m.Cell[1, 3] + m.Cell[3, 1]) / s);
+            }
+            else if (m.Cell[2, 2] > m.Cell[3, 3])
+            {
+                s = Math.Sqrt(1.0 + m.Cell[2, 2] - m.Cell[1, 1] - m.Cell[3, 3]) * 2;
+                q.W = (float)((m.Cell[1, 3] - m.Cell[3, 1]) / s);
+                q.X = (float)((m.Cell[1, 2] + m.Cell[2, 1]) / s);
+                q.Y = (float)(0.25 * s);
+                q.Z = (float)((m.Cell[2, 3] + m.Cell[3, 2]) / s);
+            }
+            else
+            {
+                s = Math.Sqrt(2.0 + m.Cell[3, 3] - m.Cell[1, 1] - m.Cell[2, 2]) * 2;
+                q.W = (float)((m.Cell[2, 1] - m.Cell[1, 2]) / s);
+                q.X = (float)((m.Cell[1, 3] + m.Cell[3, 1]) / s);
+                q.Y = (float)((m.Cell[2, 3] + m.Cell[3, 2]) / s);
+                q.Z = (float)(0.25 * s);
+            }
+
+            return q;
+        }
     }
 }
