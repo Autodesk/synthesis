3386c8391e3ed278f40b0b9561554c048ec77e2b
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-10 09:06:46 -0700

    Documented Pneumatic and Wheel
    Former-commit-id: c37de4c4a0616c0298a7c4c201a0bb4be8b01141

--- exporters/FusionExporter/Source/Data/BXDJ/Components/Wheel.h
+++ exporters/FusionExporter/Source/Data/BXDJ/Components/Wheel.h
@@ -8,34 +8,57 @@ namespace BXDJ
 {
 	class RotationalJoint;
 
+	///
+	/// Stores wheel information about a Driver.
+	///
 	class Wheel : public XmlWritable, public CustomJSONObject
 	{
 	public:
+		///
+		/// Various types of wheels.
+		///
 		enum Type
 		{
-			NORMAL = 1,
-			OMNI = 2,
-			MECANUM = 3
+			NORMAL = 1, ///< Typical rubber wheel.
+			OMNI = 2, ///< Omni wheel.
+			MECANUM = 3 ///< Mecanum wheel.
 		};
-		Type type;
+		Type type; ///< Type of the wheel.
 
+		///
+		/// Predetermined degrees of wheel friction.
+		///
 		enum FrictionLevel
 		{
-			LOW = 1,
-			MEDIUM = 2,
-			HIGH = 3
+			LOW = 1, ///< Low friction.
+			MEDIUM = 2, ///< Medium friction.
+			HIGH = 3 ///< High friction.
 		};
-		FrictionLevel frictionLevel;
+		FrictionLevel frictionLevel; ///< The friction level of the wheel.
 
-		bool isDriveWheel;
+		bool isDriveWheel; ///< True if the wheel is used in the robot's drivetrain.
 
+		/// Copy constructor.
 		Wheel(const Wheel &);
-		Wheel(Type = NORMAL, FrictionLevel = MEDIUM, bool isDriveWheel = false);
+
+		///
+		/// Creates a wheel.
+		/// \param type The type of wheel.
+		/// \param frictionLvel The friction level of the wheel.
+		/// \param isDriveWheel Whether or not the wheel is used in the drivetrain.
+		///
+		Wheel(Type = NORMAL, FrictionLevel = MEDIUM, bool = false);
+
+		///
+		/// Copies an existing wheel configuration while recalculating wheel radius and width based on the child of a joint.
+		/// \param wheel Wheel to copy.
+		/// \param joint Joint to calculate wheel dimensions from.
+		///
 		Wheel(const Wheel &, const RotationalJoint &);
 
-		double getRadius() const;
-		double getWidth() const;
-		Vector3<> getCenter() const;
+		double getRadius() const; ///< \return The radius of the wheel. Calculated based on the mesh of the wheel RigidNode.
+		double getWidth() const; ///< \return The width of the wheel. Calculated based on the mesh of the wheel RigidNode.
+		Vector3<> getCenter() const; ///< \return The center point of the wheel. Calculated based on the mesh of the wheel RigidNode.
 
 		float getForwardAsympSlip() const;
 		float getForwardAsympValue() const;
@@ -50,11 +73,14 @@ namespace BXDJ
 		void loadJSONObject(const rapidjson::Value&);
 
 	private:
-		double radius;
-		double width;
-		Vector3<> center;
+		double radius; ///< Radius of the wheel.
+		double width; ///< Width of the wheel.
+		Vector3<> center; ///< Center point of the wheel in Fusion space.
 
 		void write(XmlWriter &) const;
+
+		/// \param type A type of wheel.
+		/// \return The name of the type of wheel.
 		static std::string toString(Type type);
 
 	};
