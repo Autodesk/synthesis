36d2acd09b9eaedc8064335dd36a44f1fb6bf0fd
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-13 16:34:17 -0700

    Added field meta form

---
+++ exporters/field_exporter/FieldExporter/Components/FieldMetaForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FieldExporter.Components
+{
+    partial class FieldMetaForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.spawnPointLabel = new System.Windows.Forms.Label();
+            this.selectCoordinatesButton = new System.Windows.Forms.Button();
+            this.mainLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainLayoutPanel
+            // 
+            this.mainLayoutPanel.ColumnCount = 2;
+            this.mainLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.mainLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainLayoutPanel.Controls.Add(this.spawnPointLabel, 0, 0);
+            this.mainLayoutPanel.Controls.Add(this.selectCoordinatesButton, 1, 0);
+            this.mainLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainLayoutPanel.Name = "mainLayoutPanel";
+            this.mainLayoutPanel.RowCount = 2;
+            this.mainLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainLayoutPanel.Size = new System.Drawing.Size(451, 241);
+            this.mainLayoutPanel.TabIndex = 0;
+            // 
+            // spawnPointLabel
+            // 
+            this.spawnPointLabel.AutoSize = true;
+            this.spawnPointLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.spawnPointLabel.Location = new System.Drawing.Point(3, 3);
+            this.spawnPointLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.spawnPointLabel.Name = "spawnPointLabel";
+            this.spawnPointLabel.Size = new System.Drawing.Size(107, 23);
+            this.spawnPointLabel.TabIndex = 0;
+            this.spawnPointLabel.Text = "Robot Spawn Points:";
+            this.spawnPointLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // selectCoordinatesButton
+            // 
+            this.selectCoordinatesButton.AutoSize = true;
+            this.selectCoordinatesButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.selectCoordinatesButton.Location = new System.Drawing.Point(116, 3);
+            this.selectCoordinatesButton.Name = "selectCoordinatesButton";
+            this.selectCoordinatesButton.Size = new System.Drawing.Size(332, 23);
+            this.selectCoordinatesButton.TabIndex = 1;
+            this.selectCoordinatesButton.Text = "Select Points";
+            this.selectCoordinatesButton.UseVisualStyleBackColor = true;
+            this.selectCoordinatesButton.Click += new System.EventHandler(this.selectCoordinatesButton_Click);
+            // 
+            // FieldMetaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mainLayoutPanel);
+            this.Name = "FieldMetaForm";
+            this.Size = new System.Drawing.Size(451, 241);
+            this.mainLayoutPanel.ResumeLayout(false);
+            this.mainLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainLayoutPanel;
+        private System.Windows.Forms.Label spawnPointLabel;
+        private System.Windows.Forms.Button selectCoordinatesButton;
+    }
+}
---
+++ exporters/field_exporter/FieldExporter/Components/FieldMetaForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Inventor;
+
+namespace FieldExporter.Components
+{
+    public partial class FieldMetaForm : UserControl
+    {
+        bool selectingPoints = false;
+        InteractionEvents interactionEvents;
+        SelectEvents selectEvents;
+        List<dynamic> occurrences = new List<dynamic>();
+
+        public FieldMetaForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Enables interaction events with Inventor.
+        /// </summary>
+        public void EnableInteractionEvents()
+        {
+            if (Program.INVENTOR_APPLICATION.ActiveDocument == Program.ASSEMBLY_DOCUMENT)
+            {
+                try
+                {
+                    interactionEvents = Program.INVENTOR_APPLICATION.CommandManager.CreateInteractionEvents();
+                    interactionEvents.OnActivate += interactionEvents_OnActivate;
+                    interactionEvents.Start();
+
+                    selectCoordinatesButton.Text = "Done";
+
+                    selectingPoints = true;
+                }
+                catch
+                {
+                    MessageBox.Show("Cannot enter select mode.", "Document not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Can only enter select mode for " + Program.ASSEMBLY_DOCUMENT.DisplayName);
+            }
+        }
+
+        /// <summary>
+        /// Disables interaction events with Inventor.
+        /// </summary>
+        public void DisableInteractionEvents()
+        {
+            interactionEvents.Stop();
+            Program.ASSEMBLY_DOCUMENT.SelectSet.Clear();
+
+            selectCoordinatesButton.Text = "Select Points";
+            selectingPoints = false;
+        }
+
+        /// <summary>
+        /// Enables select events when interaction events are activated.
+        /// </summary>
+        private void interactionEvents_OnActivate()
+        {
+            selectEvents = interactionEvents.SelectEvents;
+            selectEvents.AddSelectionFilter(SelectionFilterEnum.kAllPointEntities);
+            selectEvents.OnSelect += selectEvents_OnSelect;
+        }
+        
+        private void selectEvents_OnSelect(ObjectsEnumerator JustSelectedEntities, SelectionDeviceEnum SelectionDevice, Inventor.Point ModelPosition, Point2d ViewPosition, Inventor.View View)
+        {}
+
+        private void getPointSelections()
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Program.LockInventor();
+            selectCoordinatesButton.Enabled = false;
+
+            occurrences.Clear();
+            foreach (dynamic selection in selectEvents.SelectedEntities)
+                occurrences.Add(selection);
+
+            selectCoordinatesButton.Enabled = true;
+            Program.UnlockInventor();
+            Cursor.Current = Cursors.Default;
+            DisableInteractionEvents();
+        }
+
+        private void selectCoordinatesButton_Click(object sender, EventArgs e)
+        {
+            if (selectingPoints)
+            {
+                getPointSelections();
+            }
+            else
+            {
+                EnableInteractionEvents();
+            }
+        }
+    }
+}
