25ae273a7e66c7104e9825e4704eb215ea91c2e5
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-14 10:28:10 -0700

    Fixed scale of spawnpoints

--- exporters/field_exporter/FieldExporter/Forms/ExportForm.cs
+++ exporters/field_exporter/FieldExporter/Forms/ExportForm.cs
@@ -71,7 +71,7 @@ namespace FieldExporter.Components
 
         private void ExportFieldData(string folder)
         {
-            Exporter.FieldProperties fieldProps = new Exporter.FieldProperties(FieldMetaForm.getSpawnpoints());
+            Exporter.FieldProperties fieldProps = new Exporter.FieldProperties(FieldMetaForm.getSpawnpoints(), new Exporter.Gamepiece[0]);
 
             fieldProps.Write(folder + "\\field_data.xml");
         }
