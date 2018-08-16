b5c352869b9ebb782421e8836dd5cbe5ac12e55d
Nicholas Burrell <zero_frost@protonmail.com>
2018-08-16 08:49:20 -0700

    Add scripts for VM building and readme to bash script converter
    Former-commit-id: cc3fd05f017d5a1b9867e66cd2825b1a32742782

--- emulation/hel/scripts/frc_program_chooser.sh
+++ emulation/hel/scripts/frc_program_chooser.sh
@@ -5,7 +5,7 @@
 PREFIX="frc_program_chooser.sh:"
 
 function run_user_program(){ #Run user program infinitely until interrupted by user
-	until $1; do
+	until LD_LIBRARY_PATH=/home/lvuser $1; do
 		if [ $? -eq 134 ]; then #Check exit code for user interrupt to allow users to stop it manually (required to stop java user program)
 			break;
 		fi
@@ -13,17 +13,20 @@ function run_user_program(){ #Run user program infinitely until interrupted by u
 		sleep 1
 	done
 }
-
-if [ -f FRCUserProgram.jar ]; then #Run java user program
+while true; do
+if [ -f /home/lvuser/FRCUserProgram.jar ]; then #Run java user program
 	echo "$PREFIX Executing Java FRCUserProgram"
-	run_user_program "/usr/lib/jvm/java-8-openjdk/bin/java -jar FRCUserProgram.jar"
+	LD_LIBRARY_PATH=/home/lvuser /usr/lib/jvm/java-8-openjdk/bin/java -jar /home/lvuser/FRCUserProgram.jar
 	exit 0;
 fi
-if [ -f FRCUserProgram ]; then #Run cpp user program
+if [ -f /home/lvuser/FRCUserProgram ]; then #Run cpp user program
 	echo "$PREFIX Executing C++ FRCUserProgram"
-	run_user_program "./FRCUserProgram"
+	LD_LIBRARY_PATH=/home/lvuser /home/lvuser/FRCUserProgram
 	exit 0;
 fi
 
+echo "No FRCUserProgram found. Sleeping for 5 seconds and then retrying."
+sleep 5;
+done
 echo "No FRCUserProgram found. Exiting."
 exit 1;
