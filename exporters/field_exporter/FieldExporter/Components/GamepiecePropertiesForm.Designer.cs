d0f5c7e7c819a73acb03c40462baf3fb9d10bf0d
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-16 09:57:52 -0700

    Setup form for gamepiece properties

---
+++ exporters/field_exporter/FieldExporter/Components/GamepiecePropertiesForm.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace FieldExporter.Components
+{
+    partial class GamepiecePropertiesForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TEST";
+            // 
+            // GamepiecePropertiesForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.label1);
+            this.Name = "GamepiecePropertiesForm";
+            this.Size = new System.Drawing.Size(326, 165);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
