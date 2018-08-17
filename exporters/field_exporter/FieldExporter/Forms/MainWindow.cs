1d23801b3f69adafc50ac7209f63f7a44f4bc3a6
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-17 11:38:22 -0700

    Revert "Revert "Added save manager class. Currently only field properties are saved.""
    This reverts commit b59f7683af8ba4d005f634b54deb2b0c0037ecd7.

--- exporters/field_exporter/FieldExporter/Forms/MainWindow.cs
+++ exporters/field_exporter/FieldExporter/Forms/MainWindow.cs
@@ -47,6 +47,17 @@ namespace FieldExporter
         }
 
         /// <summary>
+        /// Saves the current configuration to the field assembly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Program.ASSEMBLY_DOCUMENT != null)
+                Exporter.SaveManager.Save(Program.ASSEMBLY_DOCUMENT, null, null);
+        }
+
+        /// <summary>
         /// Resets the size of the window.
         /// </summary>
         /// <param name="sender"></param>
@@ -94,6 +105,5 @@ namespace FieldExporter
             if (exportForm.IsExporting)
                 e.Cancel = true;
         }
-
     }
 }
