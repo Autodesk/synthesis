f0b6d977a8cccd00ab4393c6c8110450f5c410ac
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-20 10:31:53 -0700

    Added please wait form

---
+++ exporters/field_exporter/FieldExporter/Forms/PleaseWaitForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FieldExporter.Forms
+{
+    public partial class PleaseWaitForm : Form
+    {
+        public PleaseWaitForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
