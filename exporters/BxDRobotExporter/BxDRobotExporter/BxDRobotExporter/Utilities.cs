741088e7da6010ed8c3481ffa6184f7dbdd16084
Lauren Choquer <choquerlauren@gmail.com>
2018-08-22 09:42:59 -0700

    Changed general save location for exported robots
    Because the save location for fields and robots has been changed in the installer, it is now being changed here so that robots are saved in the correct general location, and fields can be found in the correct location.
    
    
    Former-commit-id: d9c8d3bf7298ad4b52b7c30adf20237ecc345be1

--- exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Utilities.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Utilities.cs
@@ -112,7 +112,7 @@ namespace BxDRobotExporter
         {
             // Old configurations get overriden (version numbers below 1)
             if (Properties.Settings.Default.SaveLocation == "" || Properties.Settings.Default.SaveLocation == "firstRun" || Properties.Settings.Default.ConfigVersion < 1 )
-                Properties.Settings.Default.SaveLocation = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + @"\Synthesis\Robots";
+                Properties.Settings.Default.SaveLocation = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + @"\Autodesk\Synthesis\Robots";
 
             SynthesisGUI.PluginSettings = EditorsLibrary.PluginSettingsForm.Values = new EditorsLibrary.PluginSettingsForm.PluginSettingsValues
             {
