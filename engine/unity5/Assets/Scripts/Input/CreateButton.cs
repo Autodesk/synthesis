4a01e06153145fe03a701978a4d7ee7679af7712
Ajay Pai <rasberrypai@gmail.com>
2018-09-15 13:53:07 -0700

    engine-code-clean branch additions
    Former-commit-id: 566f398d6498320a7d6ea21d1fdda3c6610ea4b0

--- engine/unity5/Assets/Scripts/Input/CreateButton.cs
+++ engine/unity5/Assets/Scripts/Input/CreateButton.cs
@@ -725,19 +725,19 @@ namespace Synthesis.Input
                 case 0:  //tank drive slider is OFF
                     InputControl.mPlayerList[InputControl.activePlayerIndex].SetArcadeDrive();
                     Controls.TankDriveEnabled = false;
-                    Controls.Init();
+                    Controls.Init(); //initailize controls to switch to arcade drive
                     UpdateActiveButtons();
                     break;
                 case 1:  //tank drive slider is ON
                     InputControl.mPlayerList[InputControl.activePlayerIndex].SetTankDrive();
                     Controls.TankDriveEnabled = true;
-                    Controls.Init();
+                    Controls.Init(); //initailize controls to switch to tank drive
                     UpdateActiveButtons();
                     break;
                 default: //defaults to arcade drive
                     InputControl.mPlayerList[InputControl.activePlayerIndex].SetArcadeDrive();
                     Controls.TankDriveEnabled = false;
-                    Controls.Init();
+                    Controls.Init(); //initailize controls to switch to arcade drive
                     UpdateActiveButtons();
                     break;
             }
