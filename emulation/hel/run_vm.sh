6dc96cd694269c30d8c2f0941971a43545a52f9f
Logan <logan.traffas@gmail.com>
2019-01-03 02:12:22 -0800

    Update VM image source (ssh into VM broken)
    Former-commit-id: 3b8ac0adc8ebc30be479dc745c80efcb81d28775

--- emulation/hel/run_vm.sh
+++ emulation/hel/run_vm.sh
@@ -11,7 +11,7 @@ if ! mkdir ./vm_lock &> /dev/null ; then
 	expect -c "spawn $SCP_COMMAND;"
 else
 	#(qemu-system-arm -M vexpress-a9 -dtb vm-package/zynq-zed.dtb -kernel vm-package/zImage -append "root=/dev/mmcblk0 rw roottype=ext4 console=ttyAMA0" --nographic -serial null -drive if=sd,driver=raw,cache=writeback,file=vm-package/rootfs.ext4 -redir tcp:10022::22 < /dev/null &> /dev/null; rm -rf ./vm_lock )&
-	(qemu-system-arm -machine xilinx-zynq-a9 -cpu cortex-a9 -m 2048 -kernel vm-package/zImage -dtb vm-package/zynq-zed.dtb -display none -serial null -serial mon:stdio -localtime -append "console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0" -redir tcp:10022::22  -redir tcp:11000::11000 -redir tcp:11001::11001 -redir tcp:2354::2354 -sd vm-package/rootfs.ext4 < /dev/null &> /dev/null; rm -rf ./vm_lock )&
+	(qemu-system-arm -machine xilinx-zynq-a9 -cpu cortex-a9 -m 2048 -kernel vm-package/zImage -dtb vm-package/zynq-zed.dtb -display none -serial null -serial mon:stdio -append "console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0" -sd vm-package/rootfs.ext4 < /dev/null &> /dev/null; rm -rf ./vm_lock )&
 	printf "VM Successfully started. Please wait while it initializes\n"
 	sleep 10 && expect -c "spawn $SSH_COMMAND;"
 	expect -c "spawn $SSH_COMMAND;"
