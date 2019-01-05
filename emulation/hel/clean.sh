5a26e11a44def03edbd8749c3d47af9e478753dd
copernicium <logan.traffas@gmail.com>
2019-01-04 21:16:14 -0800

    Add other vm-package zip format to clean script

--- emulation/hel/clean.sh
+++ emulation/hel/clean.sh
@@ -3,7 +3,7 @@
 case "$1" in
     "emulator")
         killall qemu-system-arm &> /dev/null;
-        rm -rf vm-package vm-package.tar.gz vm_lock;
+        rm -rf vm-package vm-package.zip vm-package.tar.gz vm_lock;
         ;;
     "cmake")
         rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake;
@@ -34,7 +34,7 @@ case "$1" in
         ;;
     "all")
         killall qemu-system-arm;
-        rm -rf vm-package vm-package.tar.gz vm_lock CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake GoogleTest-prefix bin/benchmarks/* bin/tests/* lib/lib lib/lib64 lib/google_test lib/GoogleBench lib/include lib/wpilib lib/ni-libraries lib/ctre lib/ASIO build/* lib/libhel.so docs/html/* docs/latex/*
+        rm -rf vm-package vm-package.zip vm-package.tar.gz vm_lock CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake GoogleTest-prefix bin/benchmarks/* bin/tests/* lib/lib lib/lib64 lib/google_test lib/GoogleBench lib/include lib/wpilib lib/ni-libraries lib/ctre lib/ASIO build/* lib/libhel.so docs/html/* docs/latex/*
         ;;
     *)
         printf "Skipping clean. No target specified.\n"
