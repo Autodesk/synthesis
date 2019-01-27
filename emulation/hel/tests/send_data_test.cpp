6f46175198e3f257fdb589ea32b1efb3310200c8
copernicium <logan.traffas@gmail.com>
2019-01-26 18:19:17 -0800

    Add PCM serialization/deserialization functions

--- emulation/hel/tests/send_data_test.cpp
+++ emulation/hel/tests/send_data_test.cpp
@@ -5,9 +5,14 @@ using namespace nFPGA;
 using namespace nRoboRIO_FPGANamespace;
 
 TEST(SendDataTest, Serialize){
-    hel::SendData a = {};
-    std::cout<<"Serializaton: "<<a.serializeDeep()<<"\n";
+    auto roborio_instance = hel::RoboRIOManager::getInstance();
+    roborio_instance.first->robot_mode.setEnabled(true);
+    auto send_data_instance = hel::SendDataManager::getInstance();
+    hel::hal_is_initialized.store(true);
+    std::cout<<"Serializaton: "<<send_data_instance.first->serializeDeep()<<"\n";
 
+    roborio_instance.second.unlock();
+    send_data_instance.second.unlock();
     EXPECT_EQ(true, true); //TODO
 }
 
@@ -16,15 +21,17 @@ TEST(SendDataTest, Update){
     dio->writeOutputEnable_Headers(1u << 3, nullptr);
     dio->writeDO_Headers(1u << 3, nullptr);
 
-    hel::SendData a = {};
+    auto roborio_instance = hel::RoboRIOManager::getInstance();
+    roborio_instance.first->robot_mode.setEnabled(true);
+    auto send_data_instance = hel::SendDataManager::getInstance();
     hel::hal_is_initialized.store(true);
-    a.updateDeep();
-    std::cout<<"Serializaton: "<<a.serializeDeep()<<"\n";
-    std::cout<<"SendData::toString: "<<a.toString()<<"\n";
+    send_data_instance.first->updateDeep();
+    std::cout<<"Serializaton: "<<send_data_instance.first->serializeDeep()<<"\n";
+    std::cout<<"SendData::toString: "<<send_data_instance.first->toString()<<"\n";
 
-    auto instance = hel::RoboRIOManager::getInstance();
-    std::cout<<"RoboRIO digital_hrds[3]="<<hel::checkBitHigh(instance.first->digital_system.getOutputs().Headers,3)<<"\n";
-    instance.second.unlock();
+    std::cout<<"RoboRIO digital_hrds[3]="<<hel::checkBitHigh(roborio_instance.first->digital_system.getOutputs().Headers,3)<<"\n";
+    roborio_instance.second.unlock();
+    send_data_instance.second.unlock();
 
     EXPECT_EQ(true, true); //TODO
 }
