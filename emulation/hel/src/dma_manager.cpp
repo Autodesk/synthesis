9d9a2a782c9fe1ac96ec1e3b562a2092e9f52298
copernicium <logan.traffas@gmail.com>
2019-06-25 13:44:45 -0700

    Update setup.sh for building Athena for x64 linux
    Former-commit-id: db74305b9d752134cd5340193ea164f6f94179ec

--- emulation/hel/src/dma_manager.cpp
+++ emulation/hel/src/dma_manager.cpp
@@ -1,7 +1,7 @@
 #include "FRC_FPGA_ChipObject/tDMAManager.h"
 
 namespace nFPGA{
-	tDMAManager::tDMAManager(uint32_t dmaChannel, uint32_t hostBufferSize, tRioStatusCode* status): tSystem(status){} // TODO
+	tDMAManager::tDMAManager(uint32_t /*dmaChannel*/, uint32_t /*hostBufferSize*/, tRioStatusCode* status): tSystem(status){} // TODO
 
 	tDMAManager::~tDMAManager(){} // TODO
 
@@ -9,11 +9,11 @@ namespace nFPGA{
 
 	void tDMAManager::stop(tRioStatusCode* /*status*/){} // TODO
 
-	void tDMAManager::read(uint32_t* buf, size_t num, uint32_t timeout, size_t* remaining, tRioStatusCode* /*status*/){} // TODO
+	void tDMAManager::read(uint32_t* /*buf*/, size_t /*num*/, uint32_t /*timeout*/, size_t* /*remaining*/, tRioStatusCode* /*status*/){} // TODO
 
-	void tDMAManager::write(uint32_t* buf, size_t num, uint32_t timeout, size_t* remaining, tRioStatusCode* /*status*/){} // TODO
+	void tDMAManager::write(uint32_t* /*buf*/, size_t /*num*/, uint32_t /*timeout*/, size_t* /*remaining*/, tRioStatusCode* /*status*/){} // TODO
 
-	void tDMAManagerread(uint8_t* buf, size_t num, uint32_t timeout, size_t* remaining, tRioStatusCode* /*status*/){} // TODO
+	void tDMAManagerread(uint8_t* /*buf*/, size_t /*num*/, uint32_t /*timeout*/, size_t* /*remaining*/, tRioStatusCode* /*status*/){} // TODO
 
-	void tDMAManager::write(uint8_t* buf, size_t num, uint32_t timeout, size_t* remaining, tRioStatusCode* /*status*/){} // TODO
+	void tDMAManager::write(uint8_t* /*buf*/, size_t /*num*/, uint32_t /*timeout*/, size_t* /*remaining*/, tRioStatusCode* /*status*/){} // TODO
 }
