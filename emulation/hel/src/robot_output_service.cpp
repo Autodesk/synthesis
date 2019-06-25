51fa2740284a4f15af9884c418d18546b4dc2186
Nicholas Burrell <zero_frost@protonmail.com>
2019-06-25 08:37:16 -0700

    Finish RobotOutputService initial code
    Former-commit-id: 135f07cd6cc4c5b22a9111452ebc074e2e00dd10

--- emulation/hel/src/robot_output_service.cpp
+++ emulation/hel/src/robot_output_service.cpp
@@ -1,6 +1,7 @@
 #include "robot_output_service.hpp"
 #include "send_data.hpp"
 
+#include<unistd.h>
 #include<utility>
 
 using namespace grpc;
@@ -8,10 +9,25 @@ using namespace EmulationService;
 
 Status RobotOutputService::RobotOutputs(
                                       ServerContext* context,
-                                      RobotOutputRequest* request,
-                                      ServerReader<RobotOutputResponse>* stream) {
+                                      const RobotOutputsRequest* request,
+                                      ServerWriter<RobotOutputsResponse>* stream) {
+
     for (;;) {
+        auto instance = hel::SendDataManager::getInstance();
         
+        if(instance.first->hasNewData()){
+            auto data = instance.first->syncShallow();
+            instance.second.unlock();
+            auto res = RobotOutputsResponse{};
+            res.set_api("v1");
+            *res.mutable_output_data() = data;
+            res.set_success(true);
+            stream->Write(res);
+        } else {
+            instance.second.unlock();
+        }
+        usleep(30000);
     }
+    return Status::OK;
 }
 
