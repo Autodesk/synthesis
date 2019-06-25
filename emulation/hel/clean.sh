25159e3f9f94ed6d0b1503cd66f46b07b5787289
copernicium <logan.traffas@gmail.com>
2019-06-25 13:50:51 -0700

    Add grpc files to clean.sh all
    Former-commit-id: 2f15f599ce278105cdfaa62b854fd90c6bc7a5e8

--- emulation/hel/clean.sh
+++ emulation/hel/clean.sh
@@ -40,7 +40,7 @@ case "$1" in
         ;;
     "all")
         killall qemu-system-arm;
-        rm -rf vm-package vm-package.zip vm-package.tar.gz vm_lock CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake GoogleTest-prefix bin/benchmarks/* bin/tests/* lib/lib lib/lib64 lib/google_test lib/GoogleBench lib/include lib/wpilib lib/ni-libraries lib/ctre lib/ASIO build/* lib/libhel.so docs/html/* docs/latex/*
+        rm -rf vm-package vm-package.zip vm-package.tar.gz vm_lock CMakeFiles CMakeCache.txt cmake_install.cmake Makefile CMakeDoxyfile.in CMakeDoxygenDefaults.cmake GoogleTest-prefix bin/benchmarks/* bin/tests/* lib/lib lib/lib64 lib/google_test lib/GoogleBench lib/include lib/wpilib lib/ni-libraries lib/ctre lib/grpc lib/gen lib/ASIO build/* lib/libhel.so docs/html/* docs/latex/*
         ;;
     *)
         printf "Skipping clean. No target specified.\n"
