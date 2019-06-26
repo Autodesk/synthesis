097c5becc39e932d3321579e3c244acd846c2823
copernicium <logan.traffas@gmail.com>
2019-06-26 08:33:03 -0700

    Add gdbserver port to run_vm.sh

--- emulation/hel/run_vm.sh
+++ emulation/hel/run_vm.sh
@@ -20,6 +20,7 @@ else
 	-append "console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0" \
 	-gdb tcp::5789 \
     -net user,hostfwd=tcp::10022-:22,hostfwd=tcp::11000-:11000,hostfwd=tcp::11001-:11001 \
+	-net user,hostfwd=tcp::10022-:22,hostfwd=tcp::11000-:11000,hostfwd=tcp::11001-:11001,hostfwd=tcp::5789-:5789 \
 	-net nic \
 	-sd vm-package/rootfs.ext4 < /dev/null &> /dev/null; rm -rf ./vm_lock )&
 	printf "VM successfully started. Please wait while it initializes.\n"
