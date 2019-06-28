beeaf2b2a59279278aae8ba992c894677ecac46f
Kira Corbett <corbetki@oregonstate.edu>
2019-06-28 10:36:37 -0700

    disabled multiplayer references
    Former-commit-id: dfefc8d5aebd995357d958c6869bc0bbed9fcace

--- engine/unity5/Assets/Scripts/MultiplayerStart.cs
+++ engine/unity5/Assets/Scripts/MultiplayerStart.cs
@@ -1,19 +1,19 @@
-﻿using Synthesis.FSM;
-using Synthesis.States;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿//using Synthesis.FSM;
+//using Synthesis.States;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UnityEngine;
 
-namespace Assets.Scripts
-{
-    public class MultiplayerStart : MonoBehaviour
-    {
-        private void Start()
-        {
-            StateMachine.SceneGlobal.ChangeState(new MultiplayerState());
-        }
-    }
-}
+//namespace Assets.Scripts
+//{
+//    public class MultiplayerStart : MonoBehaviour
+//    {
+//        private void Start()
+//        {
+//            StateMachine.SceneGlobal.ChangeState(new MultiplayerState());
+//        }
+//    }
+//}
