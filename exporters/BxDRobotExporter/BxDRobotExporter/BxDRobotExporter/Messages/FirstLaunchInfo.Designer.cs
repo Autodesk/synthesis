cb2eaf969346f12a6b39d258b45054821043482d
nicolasespinoza <nico@espinozafamily.me>
2019-07-02 14:30:54 -0700

    Enlarged first launch notification font size to 9
    Former-commit-id: 893bfc45e0885e6829335a3e4578d08699410721

--- exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Messages/FirstLaunchInfo.Designer.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/BxDRobotExporter/Messages/FirstLaunchInfo.Designer.cs
@@ -40,10 +40,11 @@ namespace BxDRobotExporter.Messages
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
-            this.label2.Location = new System.Drawing.Point(13, 31);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(364, 42);
+            this.label2.Size = new System.Drawing.Size(318, 39);
             this.label2.TabIndex = 1;
             this.label2.Text = "To begin the export process, select the \"Robot Export\" button under the \"Environm" +
     "ents\" tab.";
@@ -51,10 +52,11 @@ namespace BxDRobotExporter.Messages
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(10, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(346, 17);
+            this.label1.Size = new System.Drawing.Size(295, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "The Synthesis robot exporter add-in has been added.";
             // 
@@ -63,19 +65,21 @@ namespace BxDRobotExporter.Messages
             this.checkBox1.AutoSize = true;
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
-            this.checkBox1.Location = new System.Drawing.Point(16, 76);
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.checkBox1.Location = new System.Drawing.Point(14, 72);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(229, 21);
+            this.checkBox1.Size = new System.Drawing.Size(200, 19);
             this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "Don\'t show this message again.";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(141, 103);
+            this.button1.Location = new System.Drawing.Point(124, 97);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 28);
+            this.button1.Size = new System.Drawing.Size(91, 27);
             this.button1.TabIndex = 3;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
@@ -83,18 +87,20 @@ namespace BxDRobotExporter.Messages
             // 
             // FirstLaunchInfo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 140);
+            this.ClientSize = new System.Drawing.Size(332, 138);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(396, 187);
+            this.MaximumSize = new System.Drawing.Size(348, 177);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(396, 187);
+            this.MinimumSize = new System.Drawing.Size(348, 177);
             this.Name = "FirstLaunchInfo";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
