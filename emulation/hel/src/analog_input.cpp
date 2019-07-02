025713a7ec5ad5d934be28a531ef7fd544819001
copernicium <logan.traffas@gmail.com>
2019-07-02 14:20:08 -0700

    Add PWM unit test file and PWM zero latch logic
    Former-commit-id: 87c2c9c4154a371256aaa1cc8cb5f34c6444a7f7

--- emulation/hel/src/analog_input.cpp
+++ emulation/hel/src/analog_input.cpp
@@ -1,4 +1,3 @@
-#include <thread>
 #include <mutex>
 #include <cmath>
 
--- emulation/hel/src/send_data.cpp
+++ emulation/hel/src/send_data.cpp
@@ -23,7 +23,7 @@ namespace hel{
         RoboRIO roborio = RoboRIOManager::getCopy();
 
         for(unsigned i = 0; i < pwm_hdrs.size(); i++){
-            pwm_hdrs[i] = PWMSystem::getPercentOutput(roborio.pwm_system.getHdrPulseWidth(i));
+            pwm_hdrs[i] = roborio.pwm_system.getHdrZeroLatch(i) ? 0.0 : PWMSystem::getPercentOutput(roborio.pwm_system.getHdrPulseWidth(i));
         }
 
         for(unsigned i = 0; i < digital_mxp.size(); i++){
@@ -39,7 +39,7 @@ namespace hel{
                 if(remapped_i >= 4){ //digital ports 0-3 line up with mxp pwm ports 0-3, the rest are offset by 4
                     remapped_i -= 4;
                 }
-                digital_mxp[i].value = PWMSystem::getPercentOutput(roborio.pwm_system.getMXPPulseWidth(remapped_i));
+                digital_mxp[i].value = roborio.pwm_system.getMXPZeroLatch(i) ? 0.0 : PWMSystem::getPercentOutput(roborio.pwm_system.getMXPPulseWidth(remapped_i));
                 break;
             }
             case MXPData::Config::SPI:
