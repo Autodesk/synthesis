427f43376828c2c003e745e54e4e80c7f283f565
KyroVibe <hunter.barclay18@gmail.com>
2019-07-02 12:39:39 -0700

    Added a little spice

---
+++ engine/unity5/Assets/GIFPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class GIFPlayer : MonoBehaviour
+{
+    public float frameRate;
+    public Sprite[] imgs;
+
+    private float lastNewFrame = 0;
+    private int currentFrame = 0;
+    private Image img;
+
+    public void Start()
+    {
+        img = GetComponent<Image>();
+        img.sprite = imgs[0];
+    }
+
+    public void Update()
+    {
+        if (lastNewFrame + (1 / frameRate) <= Time.unscaledTime)
+        {
+            currentFrame++;
+            if (currentFrame >= imgs.Length)
+            {
+                currentFrame = 0;
+            }
+            img.sprite = imgs[currentFrame];
+            lastNewFrame = Time.unscaledTime;
+        }
+    }
+}
