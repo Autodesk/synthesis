32e459ff20f8414e2d103bec69efb6aeff65328a
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-07-02 10:01:32 -0700

    Add new buttons to fusion environment

--- exporters/FusionExporter/Source/AddIn/Identifiers.h
+++ exporters/FusionExporter/Source/AddIn/Identifiers.h
@@ -5,9 +5,17 @@
 namespace SynthesisAddIn
 {
 	const std::string K_WORKSPACE = "SynthesisExporterWorkspace";
-	const std::string K_PANEL = "SynthesisExporterPanel";
+	const std::string K_FINISH_PANEL = "SynthesisFinishPanel";
+	const std::string K_DRIVE_PANEL = "SynthesisDrivePanel";
+	const std::string K_JOINT_PANEL = "SynthesisJointPanel";
+	const std::string K_PRECHECK_PANEL = "SynthesisPrecheckPanel";
 	const std::string K_EXPORT_PALETTE = "ExporterFormPalette";
 	const std::string K_SENSORS_PALETTE = "SensorsFormPalette";
 	const std::string K_PROGRESS_PALETTE = "ProcessFormPalette";
+	const std::string K_DT_TYPE = "DriveTrainTypeButton";
+	const std::string K_DT_WEIGHT = "DriveTrainWeightButton";
+	const std::string K_EDIT_JOINTS = "EditJointsButton";
+	const std::string K_DOF = "DOFButton";
+	const std::string K_GUIDE = "GuideButton";
 	const std::string K_EXPORT_BUTTON = "ExportRobotButton";
 }
