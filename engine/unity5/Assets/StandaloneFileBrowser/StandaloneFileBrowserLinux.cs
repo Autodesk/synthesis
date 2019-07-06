6959206a1393d8534c1c1cfccf38d8db51490e93
matthew17754 <matmoradi17@gmail.com>
2019-07-06 16:51:06 -0700

    Removed Useless Standalone DllImport Attributes
    Former-commit-id: cab1cb59caf4034fdc1153c3250f74b68f8c4010

--- engine/unity5/Assets/StandaloneFileBrowser/StandaloneFileBrowserLinux.cs
+++ engine/unity5/Assets/StandaloneFileBrowser/StandaloneFileBrowserLinux.cs
@@ -21,15 +21,9 @@ namespace SFB {
         [DllImport("StandaloneFileBrowser")]
         private static extern IntPtr DialogOpenFilePanel(string title, string directory, string extension, bool multiselect);
         [DllImport("StandaloneFileBrowser")]
-        private static extern void DialogOpenFilePanelAsync(string title, string directory, string extension, bool multiselect, AsyncCallback callback);
-        [DllImport("StandaloneFileBrowser")]
         private static extern IntPtr DialogOpenFolderPanel(string title, string directory, bool multiselect);
         [DllImport("StandaloneFileBrowser")]
-        private static extern void DialogOpenFolderPanelAsync(string title, string directory, bool multiselect, AsyncCallback callback);
-        [DllImport("StandaloneFileBrowser")]
         private static extern IntPtr DialogSaveFilePanel(string title, string directory, string defaultName, string extension);
-        [DllImport("StandaloneFileBrowser")]
-        private static extern void DialogSaveFilePanelAsync(string title, string directory, string defaultName, string extension, AsyncCallback callback);
 
         public StandaloneFileBrowserLinux()
         {
