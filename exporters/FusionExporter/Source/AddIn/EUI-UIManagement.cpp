469c49fa9d46d7a4d9e41774115f7695836ca4a4
nicolasespinoza <nico@espinozafamily.me>
2019-07-22 10:35:47 -0700

    Reworked key to use HTML instead of images and added description
    Former-commit-id: 3a1121fc131cdd12512b2cc2661ae91042eb1e47

--- exporters/FusionExporter/Source/AddIn/EUI-UIManagement.cpp
+++ exporters/FusionExporter/Source/AddIn/EUI-UIManagement.cpp
@@ -326,7 +326,7 @@ bool EUI::createKeyPalette()
 	if (!keyPalette)
 	{
 		// Create palette
-		keyPalette = palettes->add(PALETTE_KEY, "Degrees of Freedom Key", "Palette/dofkey.html", false, true, false, 220, 150);
+		keyPalette = palettes->add(PALETTE_KEY, "Degrees of Freedom Key", "Palette/dofkey.html", false, true, false, 220, 165);
 		if (!keyPalette)
 			return false;
 
