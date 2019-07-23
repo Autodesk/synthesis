dec74bb4c728f2aa21790e9a18b30a519149bbd7
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-07-23 15:29:07 -0700

    Rename StandardAddInServer to RobotExporterAddInServer
    Former-commit-id: 06dd3412f5c1893fb9b17d216dc84c4c18b26c70

--- exporters/BxDRobotExporter/BxDRobotExporter/GUI/Editors/JointEditor/JointCard.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/GUI/Editors/JointEditor/JointCard.cs
@@ -54,13 +54,13 @@ namespace BxDRobotExporter.GUI.Editors.JointEditor
             jointForm.ResetAllHighlight();
             isHighlighted = true;
 
-            InventorUtils.FocusAndHighlightNode(node, StandardAddInServer.Instance.MainApplication.ActiveView.Camera, 0.8);
+            InventorUtils.FocusAndHighlightNode(node, RobotExporterAddInServer.Instance.MainApplication.ActiveView.Camera, 0.8);
         }
 
         public void LoadPreviewIcon()
         {
             var iconCamera = InventorManager.Instance.TransientObjects.CreateCamera();
-            iconCamera.SceneObject = StandardAddInServer.Instance.AsmDocument.ComponentDefinition;
+            iconCamera.SceneObject = RobotExporterAddInServer.Instance.AsmDocument.ComponentDefinition;
 
             const double zoom = 0.6; // Zoom, where a zoom of 1 makes the camera the size of the whole robot
             
@@ -75,8 +75,8 @@ namespace BxDRobotExporter.GUI.Editors.JointEditor
 
             pictureBox1.Image = AxHostConverter.PictureDispToImage(
                 iconCamera.CreateImage(pictureBox1.Height * widthConst, pictureBox1.Height,
-                    StandardAddInServer.Instance.MainApplication.TransientObjects.CreateColor(210, 222, 239),
-                    StandardAddInServer.Instance.MainApplication.TransientObjects.CreateColor(175, 189, 209)));
+                    RobotExporterAddInServer.Instance.MainApplication.TransientObjects.CreateColor(210, 222, 239),
+                    RobotExporterAddInServer.Instance.MainApplication.TransientObjects.CreateColor(175, 189, 209)));
         }
 
         public void ResetHighlight()
--- exporters/BxDRobotExporter/BxDRobotExporter/GUI/Editors/JointEditor/JointForm.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/GUI/Editors/JointEditor/JointForm.cs
@@ -36,7 +36,7 @@ namespace BxDRobotExporter.GUI.Editors.JointEditor
 
             Closing += (sender, e) => // Every close
             {
-                InventorUtils.FocusAndHighlightNodes(null, StandardAddInServer.Instance.MainApplication.ActiveView.Camera, 1);
+                InventorUtils.FocusAndHighlightNodes(null, RobotExporterAddInServer.Instance.MainApplication.ActiveView.Camera, 1);
             };
 
             FormClosing += (sender, e) =>
