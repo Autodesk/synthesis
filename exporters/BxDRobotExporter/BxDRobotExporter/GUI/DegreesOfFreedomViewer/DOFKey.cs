0d1143b94d16fb04828b2d2ace51a02985a8a430
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-07-24 10:52:47 -0700

    Add DOFKey

---
+++ exporters/BxDRobotExporter/BxDRobotExporter/GUI/DegreesOfFreedomViewer/DOFKey.cs
@@ -0,0 +1,40 @@
+using System;
+using Inventor;
+
+namespace BxDRobotExporter.GUI.DegreesOfFreedomViewer
+{
+    public class DOFKey
+    {
+        private DockableWindow embeddedDofKeyPane;
+
+        public bool Enabled
+        {
+            get => embeddedDofKeyPane.Visible;
+            set => embeddedDofKeyPane.Visible = value;
+        }
+
+        public void CreateDockableWindow(UserInterfaceManager uiMan)
+        {
+            DestroyDockableWindow();
+            embeddedDofKeyPane = uiMan.DockableWindows.Add(Guid.NewGuid().ToString(), "BxD:RobotExporter:KeyPane", "Degrees of Freedom Key");
+            embeddedDofKeyPane.DockingState = DockingStateEnum.kFloat;
+            embeddedDofKeyPane.Width = 220;
+            embeddedDofKeyPane.Height = 130;
+            embeddedDofKeyPane.SetMinimumSize(120, 220);
+            embeddedDofKeyPane.ShowVisibilityCheckBox = false;
+            embeddedDofKeyPane.ShowTitleBar = true;
+            var keyPanel = new DofKeyPane();
+            embeddedDofKeyPane.AddChild(keyPanel.Handle);
+            embeddedDofKeyPane.Visible = false;
+        }
+
+        public void DestroyDockableWindow()
+        {
+            if (embeddedDofKeyPane != null)
+            {
+                embeddedDofKeyPane.Visible = false;
+                embeddedDofKeyPane.Delete();
+            }
+        }
+    }
+}
