b11e65e803ae15c010fdc6cf9080dc23aaae887a
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-07-24 08:46:20 -0700

    Use automatic type detection and move static methods into util class

--- exporters/BxDRobotExporter/BxDRobotExporter/Utilities/InventorUtils.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/Utilities/InventorUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using BxDRobotExporter.ControlGUI;
 using BxDRobotExporter.ExportGuide;
@@ -11,6 +12,7 @@ using BxDRobotExporter.Properties;
 using Inventor;
 using Application = Inventor.Application;
 using Environment = System.Environment;
+using IPictureDisp = stdole.IPictureDisp;
 
 namespace BxDRobotExporter
 {
@@ -241,7 +243,7 @@ namespace BxDRobotExporter
             
             RobotExporterAddInServer.Instance.ChildHighlight.Clear();
             // Highlighting must occur after the camera is moved, as inventor clears highlight objects when the camera is moved
-            FocusCameraOnOccurrences(occurrences, 15, camera, zoom, RobotExporterAddInServer.ViewDirection.Y);
+            FocusCameraOnOccurrences(occurrences, 15, camera, zoom, InventorUtils.ViewDirection.Y);
             HighlightOccurrences(occurrences);
         }
 
@@ -265,7 +267,7 @@ namespace BxDRobotExporter
             
             RobotExporterAddInServer.Instance.ChildHighlight.Clear();
             // Highlighting must occur after the camera is moved, as inventor clears highlight objects when the camera is moved
-            FocusCameraOnOccurrences(occurrences, 15, camera, RobotExporterAddInServer.ViewDirection.Y);
+            FocusCameraOnOccurrences(occurrences, 15, camera, InventorUtils.ViewDirection.Y);
             HighlightOccurrences(occurrences);
         }
 
@@ -361,7 +363,7 @@ namespace BxDRobotExporter
         /// <param name="zoom"></param>
         /// <param name="viewDirection">Direction to view the point from.</param>
         /// <param name="animate">True to animate movement of camera.</param>
-        public static void SetCameraView(Vector focus, double viewDistance, Camera camera, double zoom, RobotExporterAddInServer.ViewDirection viewDirection = RobotExporterAddInServer.ViewDirection.Y, bool animate = true)
+        public static void SetCameraView(Vector focus, double viewDistance, Camera camera, double zoom, InventorUtils.ViewDirection viewDirection = InventorUtils.ViewDirection.Y, bool animate = true)
         {
             camera.Fit(); // TODO: Determine model size properly
             camera.GetExtents(out var width, out var height);
@@ -381,7 +383,7 @@ namespace BxDRobotExporter
         public static void SetCameraView(Vector focus, Box boundingBox, double viewDistance, Camera camera, bool animate = true)
         {
             var boxVector = boundingBox.MinPoint.VectorTo(boundingBox.MaxPoint);
-            SetCameraView(focus, viewDistance, boxVector.X, boxVector.Z, camera, RobotExporterAddInServer.ViewDirection.Y, animate);
+            SetCameraView(focus, viewDistance, boxVector.X, boxVector.Z, camera, InventorUtils.ViewDirection.Y, animate);
         }
 
         /// <summary>
@@ -393,7 +395,7 @@ namespace BxDRobotExporter
         /// <param name="zoom"></param>
         /// <param name="viewDirection">Direction to view the point from.</param>
         /// <param name="animate">True to animate movement of camera.</param>
-        public static void SetCameraView(Vector focus, double viewDistance, double width, double height, Camera camera, RobotExporterAddInServer.ViewDirection viewDirection = RobotExporterAddInServer.ViewDirection.Y, bool animate = true)
+        public static void SetCameraView(Vector focus, double viewDistance, double width, double height, Camera camera, InventorUtils.ViewDirection viewDirection = InventorUtils.ViewDirection.Y, bool animate = true)
         {
             Point focusPoint = RobotExporterAddInServer.Instance.MainApplication.TransientGeometry.CreatePoint(focus.X, focus.Y, focus.Z);
 
@@ -402,25 +404,25 @@ namespace BxDRobotExporter
             camera.Target = focusPoint;
 
             // Flip view for negative direction
-            if ((viewDirection & RobotExporterAddInServer.ViewDirection.Negative) == RobotExporterAddInServer.ViewDirection.Negative)
+            if ((viewDirection & InventorUtils.ViewDirection.Negative) == InventorUtils.ViewDirection.Negative)
                 viewDistance = -viewDistance;
 
             UnitVector up = null;
 
             // Find camera position and upwards direction
-            if ((viewDirection & RobotExporterAddInServer.ViewDirection.X) == RobotExporterAddInServer.ViewDirection.X)
+            if ((viewDirection & InventorUtils.ViewDirection.X) == InventorUtils.ViewDirection.X)
             {
                 focus.X += viewDistance;
                 up = RobotExporterAddInServer.Instance.MainApplication.TransientGeometry.CreateUnitVector(0, 1, 0);
             }
 
-            if ((viewDirection & RobotExporterAddInServer.ViewDirection.Y) == RobotExporterAddInServer.ViewDirection.Y)
+            if ((viewDirection & InventorUtils.ViewDirection.Y) == InventorUtils.ViewDirection.Y)
             {
                 focus.Y += viewDistance;
                 up = RobotExporterAddInServer.Instance.MainApplication.TransientGeometry.CreateUnitVector(0, 0, 1);
             }
 
-            if ((viewDirection & RobotExporterAddInServer.ViewDirection.Z) == RobotExporterAddInServer.ViewDirection.Z)
+            if ((viewDirection & InventorUtils.ViewDirection.Z) == InventorUtils.ViewDirection.Z)
             {
                 focus.Z += viewDistance;
                 up = RobotExporterAddInServer.Instance.MainApplication.TransientGeometry.CreateUnitVector(0, 1, 0);
@@ -446,7 +448,7 @@ namespace BxDRobotExporter
         /// <param name="viewDirection">The direction of the camera</param>
         /// <param name="animate">True if you want to animate the camera moving to the new position</param>
         public static void FocusCameraOnOccurrences(List<ComponentOccurrence> occurrences, double viewDistance, Camera camera, double zoom,
-            RobotExporterAddInServer.ViewDirection viewDirection = RobotExporterAddInServer.ViewDirection.Y, bool animate = false)
+            InventorUtils.ViewDirection viewDirection = InventorUtils.ViewDirection.Y, bool animate = false)
         {
             if (occurrences.Count < 1)
                 return;
@@ -466,7 +468,7 @@ namespace BxDRobotExporter
         /// <param name="viewDirection">The direction of the camera</param>
         /// <param name="animate">True if you want to animate the camera moving to the new position</param>
         public static void FocusCameraOnOccurrences(List<ComponentOccurrence> occurrences, double viewDistance, Camera camera,
-            RobotExporterAddInServer.ViewDirection viewDirection = RobotExporterAddInServer.ViewDirection.Y, bool animate = false)
+            InventorUtils.ViewDirection viewDirection = InventorUtils.ViewDirection.Y, bool animate = false)
         {
             if (occurrences.Count < 1)
                 return;
@@ -504,6 +506,225 @@ namespace BxDRobotExporter
                 resultBox.Extend(occurrence.RangeBox.MinPoint);
             }
             return resultBox;
+        }
+
+        public static void CreateHighlightSet(List<RigidNode_Base> nodes, HighlightSet highlightSet)
+        {
+            highlightSet.Clear();
+            foreach (var componentOccurrence in InventorUtils.GetComponentOccurrencesFromNodes(nodes))
+            {
+                highlightSet.AddItem(componentOccurrence);
+            }
+        }
+
+        /// <summary>
+        /// Causes the exporter to close.
+        /// </summary>
+        /// <param name="suppressClosingEvent">Whether or not the exporter closing handler should be suppressed from being called.</param>
+        public static async void ForceQuitExporter(AssemblyDocument document)
+        {
+            await Task.Delay(1); // Delay is needed so that environment is closed after it has finished opening
+            document.EnvironmentManager.SetCurrentEnvironment(document.EnvironmentManager.EditObjectEnvironment);
+        }
+
+        /// <summary>
+        /// Causes the exporter to close.
+        /// </summary>
+        /// <param name="suppressClosingEvent">Whether or not the exporter closing handler should be suppressed from being called.</param>
+        public static async void ForceQuitExporter(DrawingDocument document)
+        {
+            await Task.Delay(1); // Delay is needed so that environment is closed after it has finished opening
+            document.EnvironmentManager.SetCurrentEnvironment(document.EnvironmentManager.EditObjectEnvironment);
+        }
+
+        /// <summary>
+        /// Causes the exporter to close.
+        /// </summary>
+        /// <param name="suppressClosingEvent">Whether or not the exporter closing handler should be suppressed from being called.</param>
+        public static async void ForceQuitExporter(PartDocument document)
+        {
+            await Task.Delay(1); // Delay is needed so that environment is closed after it has finished opening
+            document.EnvironmentManager.SetCurrentEnvironment(document.EnvironmentManager.EditObjectEnvironment);
+        }
+
+        /// <summary>
+        /// Causes the exporter to close.
+        /// </summary>
+        /// <param name="suppressClosingEvent">Whether or not the exporter closing handler should be suppressed from being called.</param>
+        public static async void ForceQuitExporter(PresentationDocument document)
+        {
+            await Task.Delay(1); // Delay is needed so that environment is closed after it has finished opening
+            document.EnvironmentManager.SetCurrentEnvironment(document.EnvironmentManager.EditObjectEnvironment);
+        }
+
+        /// <summary>
+        /// Disables all components in a document that are not connected to another component by a joint.
+        /// </summary>
+        /// <param name="asmDocument">Document to traverse.</param>
+        /// <returns>List of disabled components.</returns>
+        public static List<ComponentOccurrence> DisableUnconnectedComponents(AssemblyDocument asmDocument)
+        {
+            // Find all components in the assembly that are connected to a joint
+            var jointedAssemblyOccurences = new List<ComponentOccurrence>();
+            foreach (AssemblyJoint joint in asmDocument.ComponentDefinition.Joints)
+            {
+                if (!joint.Definition.JointType.Equals(AssemblyJointTypeEnum.kRigidJointType))
+                {
+                    jointedAssemblyOccurences.Add(joint.AffectedOccurrenceOne);
+                    jointedAssemblyOccurences.Add(joint.AffectedOccurrenceTwo);
+                }
+            }
+
+            // Hide any components not associated with a joint
+            var disabledAssemblyOccurences = new List<ComponentOccurrence>();
+            foreach (ComponentOccurrence c in asmDocument.ComponentDefinition.Occurrences)
+            {
+                if (!jointedAssemblyOccurences.Contains(c) || c.Grounded)
+                {
+                    try
+                    {
+                        //accounts for components that can't be disabled
+                        disabledAssemblyOccurences.Add(c);
+                        c.Enabled = false;
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+
+            return disabledAssemblyOccurences;
+        }
+
+        /// <summary>
+        /// Enables all components in a list.
+        /// </summary>
+        /// <param name="components">Components to enable.</param>
+        public static void EnableComponents(List<ComponentOccurrence> components)
+        {
+            foreach (var c in components)
+            {
+                try
+                {
+                    //accounts for components that can't be disabled
+                    c.Enabled = true;
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if a baseNode matches up with the assembly. Passed as a <see cref="ValidationAction"/> to
+        /// </summary>
+        /// <param name="baseNode"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static bool ValidateAssembly(RigidNode_Base baseNode, out string message)
+        {
+            var validationCount = 0;
+            var failedCount = 0;
+            var nodes = baseNode.ListAllNodes();
+            foreach (var node in nodes)
+            {
+                var failedValidation = false;
+                foreach (var componentName in node.ModelFullID.Split(new string[] {"-_-"},
+                    StringSplitOptions.RemoveEmptyEntries))
+                {
+                    if (!CheckForOccurrence(componentName))
+                    {
+                        failedCount++;
+                        failedValidation = true;
+                    }
+                }
+
+                if (!failedValidation)
+                {
+                    validationCount++;
+                }
+            }
+
+            if (validationCount == nodes.Count)
+            {
+                message = String.Format("The assembly validated successfully. {0} / {1} nodes checked out.",
+                    validationCount, nodes.Count);
+                return true;
+            }
+            else
+            {
+                message = String.Format(
+                    "The assembly failed to validate. {0} / {1} nodes checked out. {2} parts/assemblies were not found.",
+                    validationCount, nodes.Count, failedCount);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if a <see cref="ComponentOccurrence"/> of the specified name exists
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private static bool CheckForOccurrence(string name)
+        {
+            foreach (ComponentOccurrence component in RobotExporterAddInServer.Instance.AsmDocument.ComponentDefinition.Occurrences)
+            {
+                if (component.Name == name)
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Sets the tooltip of a <see cref="ButtonDefinition"/>
+        /// </summary>
+        /// <param name="button">The <see cref="ButtonDefinition"/> the tool tip is being applied to</param>
+        /// <param name="description">The description of the command which the <paramref name="button"/> executes</param>
+        /// <param name="expandedDescription">The expanded description of the command which appears after hovering the cursor over the button for a few seconds</param>
+        /// <param name="picture">The image that appears along side the <paramref name="expandedDescription"/></param>
+        /// <param name="title">The bolded title appearing at the top of the tooltip</param>
+        public static void ToolTip(ButtonDefinition button, string title, string description,
+            string expandedDescription = null, IPictureDisp picture = null)
+        {
+            if (description != null)
+                button.ProgressiveToolTip.Description = description;
+            if (expandedDescription != null)
+            {
+                button.ProgressiveToolTip.ExpandedDescription = expandedDescription;
+                button.ProgressiveToolTip.IsProgressive = true;
+            }
+
+            if (picture != null)
+            {
+                button.ProgressiveToolTip.Image = picture;
+                button.ProgressiveToolTip.IsProgressive = true;
+            }
+
+            button.ProgressiveToolTip.Title = title;
+        }
+
+        /// <summary>
+        /// <see cref="X"/>, <see cref="Y"/>, <see cref="Z"/> position the camera right of, in front of, and above the target relative to the front of the robot.
+        /// Bitwise OR <see cref="X"/>, <see cref="Y"/>, or <see cref="Z"/> with <see cref="Negative"/> to invert these (make them left of, behind or above the robot).
+        /// </summary>
+        public enum ViewDirection : byte
+        {
+            /// <summary>
+            /// Positions the camera to the right of the robot.
+            /// </summary>
+            X = 0b00000001,
+
+            /// <summary>
+            /// Positions the camera above the robot.
+            /// </summary>
+            Y = 0b00000010,
+
+            /// <summary>
+            /// Positions the robot in front of the robot.
+            /// </summary>
+            Z = 0b00000100,
+            Negative = 0b00001000
         }
     }
 }
