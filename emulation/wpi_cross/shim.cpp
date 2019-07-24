0d49a5b1eb61bc2e4ca09a7f2c0c902345ed8e6e
Nicholas Burrell <zero_frost@protonmail.com>
2019-07-24 10:51:34 -0700

    Update shim.cpp

--- emulation/wpi_cross/shim.cpp
+++ emulation/wpi_cross/shim.cpp
@@ -1,3 +1,4 @@
+#warning "Including shim.cpp"
 namespace nFPGA {
     namespace nRoboRIO_FPGANamespace {
         unsigned int g_currentTargetClass; //Ni FPGA declares this as extern, so define it here
