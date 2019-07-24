9913f2166530bc9b2a963b8b507d31954531d478
Nicholas Burrell <zero_frost@protonmail.com>
2019-07-24 11:57:34 -0700

    Update shim.cpp

--- emulation/wpi_cross/shim.cpp
+++ emulation/wpi_cross/shim.cpp
@@ -1,6 +1,15 @@
-#warning "Including shim.cpp"
+// DO NOT DELETE THIS EMPTY LINE
+// Don't compile this file. It will never work. It is a shim that is injected into WPILib so it compiles for x86
+
 namespace nFPGA {
     namespace nRoboRIO_FPGANamespace {
         unsigned int g_currentTargetClass; //Ni FPGA declares this as extern, so define it here
     }
+}
+namespace sim{
+	jint SimOnLoad(JavaVM* vm, void* reserved){
+		return JNI_VERSION_1_6;
+	}
+
+	void SimOnUnload(JavaVM* vm, void* reserved){}
 }
