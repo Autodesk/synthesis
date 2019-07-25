3a0fb275eef45afdb1488ecf70845ed483f769ab
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-07-25 15:33:03 -0700

    Refactor environment button setup
    Former-commit-id: 741d76efbf0e26fce2b0d81e78c6d61d12a73d68

--- exporters/BxDRobotExporter/BxDRobotExporter/RobotExporterAddInServer.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/RobotExporterAddInServer.cs
@@ -12,6 +12,7 @@ using BxDRobotExporter.Properties;
 using BxDRobotExporter.Utilities;
 using BxDRobotExporter.Utilities.Synthesis;
 using Inventor;
+using static BxDRobotExporter.PictureDispConverter;
 
 namespace BxDRobotExporter
 {
@@ -21,24 +22,22 @@ namespace BxDRobotExporter
     [Guid("0c9a07ad-2768-4a62-950a-b5e33b88e4a3")]
     public class RobotExporterAddInServer : SingleEnvironmentAddInServer
     {
-        // ------ Add-In ------
+        // Add-In
         public static RobotExporterAddInServer Instance { get; private set; }
         public readonly AddInSettings AddInSettings = new AddInSettings();
 
-        // ------ Robot Document ------
+        // Robot/Document
         public RobotData RobotData { get; private set; }
         private List<ComponentOccurrence> disabledAssemblyOccurrences;
 
-        // ------ UI ------
         // Environment
         private Environment exporterEnv;
 
         //Ribbon Pannels
         private RibbonPanel driveTrainPanel;
         private RibbonPanel jointPanel;
-        private RibbonPanel checklistPanel;
-        private RibbonPanel pluginPanel;
-        private RibbonPanel exitPanel;
+        private RibbonPanel precheckPanel;
+        private RibbonPanel addInSettingsPanel;
 
         //Standalone Buttons
         private ButtonDefinition driveTrainTypeButton;
@@ -49,13 +48,14 @@ namespace BxDRobotExporter
 
         private ButtonDefinition guideButton;
         private ButtonDefinition dofButton;
+
         private ButtonDefinition settingsButton;
 
         // Dockable window managers
         private readonly AdvancedJointEditor advancedJointEditor = new AdvancedJointEditor();
         private readonly DOFKey dofKey = new DOFKey();
         private readonly Guide guide = new Guide(true);
-        
+
         // Other managers
         public readonly HighlightManager HighlightManager = new HighlightManager();
 
@@ -68,64 +68,54 @@ namespace BxDRobotExporter
             AnalyticsUtils.SetUser(Application.UserName);
             AnalyticsUtils.LogPage("Inventor");
 
-            var editJointIconSmall = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.JointEditor32)); //these are still here at request of QA
-            var editJointIconLarge = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.JointEditor32));
+            InitEnvironment(clientId);
 
-            var drivetrainTypeIconSmall = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.DrivetrainType32)); //these are still here at request of QA
-            var drivetrainTypeIconLarge = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.DrivetrainType32));
+            Instance = this;
+            return exporterEnv;
+        }
 
-            var guideIconSmall = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.Guide32)); //these are still here at request of QA
-            var guideIconLarge = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.Guide32));
-
-            var drivetrainWeightIconSmall = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.RobotWeight32));
-            var drivetrainWeightIconLarge = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.RobotWeight32));
-
-            var synthesisLogoSmall = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.SynthesisLogo16));
-            var synthesisLogoLarge = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.SynthesisLogo32));
-
-            var gearLogoSmall = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.Gears16));
-            var gearLogoLarge = PictureDispConverter.ToIPictureDisp(new Bitmap(Resources.Gears32));
-
-            exporterEnv = Application.UserInterfaceManager.Environments.Add("Robot Export", "BxD:RobotExporter:Environment", null, synthesisLogoSmall, synthesisLogoLarge);
+        private void InitEnvironment(string clientId)
+        {
+            exporterEnv = Application.UserInterfaceManager.Environments.Add("Robot Export", "BxD:RobotExporter:Environment", null, ToIPictureDisp(new Bitmap(Resources.SynthesisLogo16)), ToIPictureDisp(new Bitmap(Resources.SynthesisLogo32)));
 
             var exporterTab = Application.UserInterfaceManager.Ribbons["Assembly"].RibbonTabs.Add("Robot Export", "BxD:RobotExporter:RobotExporterTab", clientId, "", false, true);
-
             var controlDefs = Application.CommandManager.ControlDefinitions;
 
+            InitEnvironmentPanels(exporterTab, clientId, controlDefs);
+
+            exporterEnv.DefaultRibbonTab = "BxD:RobotExporter:RobotExporterTab";
+            exporterEnv.DisabledCommandList.Add(Application.CommandManager.ControlDefinitions["BxD:RobotExporter:Environment"]); // TODO: Can this be removed?
+        }
+
+        private void InitEnvironmentPanels(RibbonTab exporterTab, string clientId, ControlDefinitions controlDefs)
+        {
+            // DRIVETRAIN PANEL
             driveTrainPanel = exporterTab.RibbonPanels.Add("Drive Train Setup", "BxD:RobotExporter:DriveTrainPanel", clientId);
-            jointPanel = exporterTab.RibbonPanels.Add("Joint Setup", "BxD:RobotExporter:JointPanel", clientId);
-            checklistPanel = exporterTab.RibbonPanels.Add("Robot Setup Checklist", "BxD:RobotExporter:ChecklistPanel", clientId);
-            pluginPanel = exporterTab.RibbonPanels.Add("Plugin", "BxD:RobotExporter:PluginSettings", clientId);
-            exitPanel = exporterTab.RibbonPanels.Add("Finish", "BxD:RobotExporter:ExitPanel", clientId);
 
-            // Reset positioning of panels
-            jointPanel.Reposition("BxD:RobotExporter:DriveTrainPanel", false);
-            checklistPanel.Reposition("BxD:RobotExporter:JointPanel", false);
-            exitPanel.Reposition("BxD:RobotExporter:ChecklistPanel", false);
-
-            //Drive Train panel buttons
             driveTrainTypeButton = controlDefs.AddButtonDefinition("Drive Train\nType",
                 "BxD:RobotExporter:SetDriveTrainType", CommandTypesEnum.kNonShapeEditCmdType, clientId, null,
-                "Select the drivetrain type (tank, H-drive, or mecanum).", drivetrainTypeIconSmall, drivetrainTypeIconLarge);
+                "Select the drivetrain type (tank, H-drive, or mecanum).", ToIPictureDisp(new Bitmap(Resources.DrivetrainType32)), ToIPictureDisp(new Bitmap(Resources.DrivetrainType32)));
             driveTrainTypeButton.OnExecute += context => new DrivetrainTypeForm(RobotData).ShowDialog();
             driveTrainPanel.CommandControls.AddButton(driveTrainTypeButton, true);
 
             drivetrainWeightButton = controlDefs.AddButtonDefinition("Drive Train\nWeight",
                 "BxD:RobotExporter:SetDriveTrainWeight", CommandTypesEnum.kNonShapeEditCmdType, clientId, null,
-                "Assign the weight of the drivetrain.", drivetrainWeightIconSmall, drivetrainWeightIconLarge);
+                "Assign the weight of the drivetrain.", ToIPictureDisp(new Bitmap(Resources.RobotWeight32)), ToIPictureDisp(new Bitmap(Resources.RobotWeight32)));
             drivetrainWeightButton.OnExecute += context => AnalyticsUtils.LogEvent("Toolbar", "Button Clicked", "Set Weight");
             drivetrainWeightButton.OnExecute += context => RobotData.PromptRobotWeight();
             driveTrainPanel.CommandControls.AddButton(drivetrainWeightButton, true);
 
-            // Joint panel buttons
+            // JOINT PANEL
+            jointPanel = exporterTab.RibbonPanels.Add("Joint Setup", "BxD:RobotExporter:JointPanel", clientId);
+
             advancedEditJointButton = controlDefs.AddButtonDefinition("Advanced Editor", "BxD:RobotExporter:AdvancedEditJoint",
-                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "Joint editor for advanced users.", editJointIconSmall, editJointIconLarge);
+                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "Joint editor for advanced users.", ToIPictureDisp(new Bitmap(Resources.JointEditor32)), ToIPictureDisp(new Bitmap(Resources.JointEditor32)));
             advancedEditJointButton.OnExecute += context => AnalyticsUtils.LogEvent("Toolbar", "Button Clicked", "Advanced Edit Joint");
             advancedEditJointButton.OnExecute += context => advancedJointEditor.Visible = !advancedJointEditor.Visible;
             jointPanel.SlideoutControls.AddButton(advancedEditJointButton);
 
             editJointButton = controlDefs.AddButtonDefinition("Edit Joints", "BxD:RobotExporter:EditJoint",
-                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "Edit existing joints.", editJointIconSmall, editJointIconLarge);
+                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "Edit existing joints.", ToIPictureDisp(new Bitmap(Resources.JointEditor32)), ToIPictureDisp(new Bitmap(Resources.JointEditor32)));
             editJointButton.OnExecute += context =>
             {
                 AnalyticsUtils.LogEvent("Toolbar", "Button Clicked", "Edit Joint");
@@ -134,34 +124,32 @@ namespace BxDRobotExporter
             };
             jointPanel.CommandControls.AddButton(editJointButton, true);
 
-            // ChecklistPanel buttons
+            // PRECHECK PANEL
+            precheckPanel = exporterTab.RibbonPanels.Add("Robot Setup Checklist", "BxD:RobotExporter:ChecklistPanel", clientId);
+
             guideButton = controlDefs.AddButtonDefinition("Toggle Robot\nExport Guide", "BxD:RobotExporter:Guide",
                 CommandTypesEnum.kNonShapeEditCmdType, clientId, null,
-                "View a checklist of all tasks necessary prior to export.", guideIconSmall, guideIconLarge);
+                "View a checklist of all tasks necessary prior to export.", ToIPictureDisp(new Bitmap(Resources.Guide32)), ToIPictureDisp(new Bitmap(Resources.Guide32)));
             guideButton.OnExecute += context => guide.Visible = !guide.Visible;
-            checklistPanel.CommandControls.AddButton(guideButton, true);
+            precheckPanel.CommandControls.AddButton(guideButton, true);
 
             dofButton = controlDefs.AddButtonDefinition("Toggle Degrees\nof Freedom View", "BxD:RobotExporter:DOF",
-                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "View degrees of freedom.", guideIconSmall, guideIconLarge);
+                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "View degrees of freedom.", ToIPictureDisp(new Bitmap(Resources.Guide32)), ToIPictureDisp(new Bitmap(Resources.Guide32)));
             dofButton.OnExecute += context =>
             {
                 AnalyticsUtils.LogEvent("Toolbar", "Button Clicked", "DOF");
                 dofKey.Visible = !dofKey.Visible;
                 HighlightManager.DisplayDof = dofKey.Visible;
             };
-            checklistPanel.CommandControls.AddButton(dofButton, true);
+            precheckPanel.CommandControls.AddButton(dofButton, true);
+
+            // ADD-IN SETTINGS PANEL
+            addInSettingsPanel = exporterTab.RibbonPanels.Add("Plugin", "BxD:RobotExporter:PluginSettings", clientId);
 
             settingsButton = controlDefs.AddButtonDefinition("Plugin Settings", "BxD:RobotExporter:Settings",
-                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "View degrees of freedom.", gearLogoSmall, gearLogoLarge);
+                CommandTypesEnum.kNonShapeEditCmdType, clientId, null, "View degrees of freedom.", ToIPictureDisp(new Bitmap(Resources.Gears16)), ToIPictureDisp(new Bitmap(Resources.Gears32)));
             settingsButton.OnExecute += context => new ExporterSettingsForm().ShowDialog();
-            pluginPanel.CommandControls.AddButton(settingsButton, true);
-
-            exporterEnv.DefaultRibbonTab = "BxD:RobotExporter:RobotExporterTab";
-            exporterEnv.DisabledCommandList.Add(Application.CommandManager.ControlDefinitions["BxD:RobotExporter:Environment"]); // TODO: Can this be removed?
-
-            Instance = this;
-
-            return exporterEnv;
+            addInSettingsPanel.CommandControls.AddButton(settingsButton, true);
         }
 
         protected override void DestroyEnvironment()
@@ -189,7 +177,7 @@ namespace BxDRobotExporter
             }
 
             RobotData.LoadRobotData(OpenDocument);
-            
+
             // Create dockable window UI
             var uiMan = Application.UserInterfaceManager;
             advancedJointEditor.CreateDockableWindow(uiMan);
@@ -234,7 +222,7 @@ namespace BxDRobotExporter
             dofKey.TemporaryHide();
             guide.TemporaryHide();
         }
-        
+
         protected override void OnEnvironmentShow()
         {
             // Restore visible state of dockable windows
@@ -244,7 +232,7 @@ namespace BxDRobotExporter
             // And DOF highlight in case that gets cleared
             HighlightManager.DisplayDof = dofKey.Visible;
         }
-        
+
         protected override bool IsDocumentSupported(Document document)
         {
             return document is AssemblyDocument; // The robot exporter is only compatible with assembly documents
