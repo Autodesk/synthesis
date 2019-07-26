b6778389bc92c5a0a9fe859070f52480887816f2
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-07-26 09:53:50 -0700

    Add SkeletonProgressUpdate class

--- exporters/BxDRobotExporter/BxDRobotExporter/Skeleton/SkeletonBuilder.cs
+++ exporters/BxDRobotExporter/BxDRobotExporter/Skeleton/SkeletonBuilder.cs
@@ -24,7 +24,21 @@ namespace BxDRobotExporter.Skeleton
             public NoGroundException() : base("Assembly has no ground.") { }
         }
 
-        public static RigidNode_Base ExporterWorker_DoWork(IProgress<int> progress)
+        public class SkeletonProgressUpdate
+        {
+            public readonly string Message;
+            public readonly int CurrentProgress;
+            public readonly int MaxProgress;
+
+            public SkeletonProgressUpdate(string message, int currentProgress, int maxProgress)
+            {
+                Message = message;
+                CurrentProgress = currentProgress;
+                MaxProgress = maxProgress;
+            }
+        }
+
+        public static RigidNode_Base ExporterWorker_DoWork(IProgress<SkeletonProgressUpdate> progress)
         {
             if (RobotExporterAddInServer.Instance.OpenDocument == null || !(RobotExporterAddInServer.Instance.OpenDocument is AssemblyDocument))
             {
@@ -39,13 +53,12 @@ namespace BxDRobotExporter.Skeleton
                 return null;
             }
 
-            RobotExporterAddInServer.Instance.Application.UserInterfaceManager.UserInteractionDisabled = true;
 
-            RigidNode_Base skeleton = null;
+            RigidNode_Base skeletonBase = null;
 
             try
             {
-                skeleton = ExportSkeleton(progress);
+                skeletonBase = ExportSkeleton(progress);
             }
             catch (EmptyAssemblyException)
             {
@@ -60,53 +73,36 @@ namespace BxDRobotExporter.Skeleton
                 MessageBox.Show("Please ground a part in your assembly to export your robot.", "No Ground", MessageBoxButtons.OK);
             }
 
-            return skeleton;
+            return skeletonBase;
         }
 
-        /// <summary>
-        /// The lightweight equivalent of the 'Add From Inventor' button in the <see cref="ExporterForm"/>. Used in <see cref="ExportMeshesLite(RigidNode_Base)"/>
-        /// </summary>
-        /// <param name="occurrences"></param>
-        /// <returns></returns>
-        private static RigidNode_Base ExportSkeleton(IProgress<int> progress)
+        private static RigidNode_Base ExportSkeleton(IProgress<SkeletonProgressUpdate> progress)
         {
             //Getting Rigid Body Info...
-
-            progress.Report(1);
-//            SetProgress("Getting physics info...", occurrences.Count, occurrences.Count + 3);
+            progress.Report(new SkeletonProgressUpdate("Getting physics info...", 1, 4));
             var rigidGetOptions = RobotExporterAddInServer.Instance.Application.TransientObjects.CreateNameValueMap();
-
             rigidGetOptions.Add("DoubleBearing", false);
             var asmDocument = RobotExporterAddInServer.Instance.OpenDocument as AssemblyDocument;
             var rawRigidResults = asmDocument.ComponentDefinition.RigidBodyAnalysis(rigidGetOptions);
-
-            //Getting Rigid Body Info...Done
             var rigidResults = new CustomRigidResults(rawRigidResults);
 
-
             //Building Model...
-            progress.Report(4);
-//            SetProgress("Building model...", occurrences.Count + 1, occurrences.Count + 3);
+            progress.Report(new SkeletonProgressUpdate("Building model...", 2, 4));
             RigidBodyCleaner.CleanGroundedBodies(rigidResults);
-            var baseNode = RigidBodyCleaner.BuildAndCleanDijkstra(rigidResults);
-
-            //Building Model...Done
+            var skeletonBase = RigidBodyCleaner.BuildAndCleanDijkstra(rigidResults);
 
             //Cleaning Up...
-            progress.Report(40);
-//            SetProgress("Cleaning up...", occurrences.Count + 2, occurrences.Count + 3);
+            progress.Report(new SkeletonProgressUpdate("Cleaning up...", 3, 4));
             var nodes = new List<RigidNode_Base>();
-            baseNode.ListAllNodes(nodes);
-
+            skeletonBase.ListAllNodes(nodes);
             foreach (var node in nodes)
             {
-                node.ModelFileName = ((RigidNode)node).@group.ToString();
+                node.ModelFileName = ((RigidNode)node).group.ToString();
                 node.ModelFullID = node.GetModelID();
             }
-            //Cleaning Up...Done
-            progress.Report(100);
-//            SetProgress("Done", occurrences.Count + 3, occurrences.Count + 3);
-            return baseNode;
+            
+            progress.Report(new SkeletonProgressUpdate("Done", 4, 4));
+            return skeletonBase;
         }
     }
 }
