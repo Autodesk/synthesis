83291d91187a406646b8bf7ea8f4a4b852a29c0d
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-07-30 10:19:06 -0700

    Add SimpleEditor mockups

---
+++ exporters/InventorRobotExporter/InventorRobotExporter/GUI/Editors/SimpleJointEditor/SimpleEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventorRobotExporter.GUI.Editors.SimpleJointEditor
+{
+    public partial class SimpleEditor : Form
+    {
+        public SimpleEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void SimpleEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
