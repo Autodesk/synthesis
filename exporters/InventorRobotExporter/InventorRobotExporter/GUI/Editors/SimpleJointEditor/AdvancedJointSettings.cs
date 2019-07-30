9220d4d8388339aaaf08b621dbbc928615ddb7c2
nicolasespinoza <nico@espinozafamily.me>
2019-07-30 10:49:04 -0700

    Added Advanced Joint Settings window with stock options

---
+++ exporters/InventorRobotExporter/InventorRobotExporter/GUI/Editors/SimpleJointEditor/AdvancedJointSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventorRobotExporter.GUI.Editors.SimpleJointEditor
+{
+    public partial class AdvancedJointSettings : Form
+    {
+        public AdvancedJointSettings()
+        {
+            InitializeComponent();
+
+            this.MaximizeBox = false;
+
+            FillInfo();
+        }
+
+        private void FillInfo()
+        {
+            // To be removed later, just for concept
+            ListViewItem item = new ListViewItem("Encoder");
+            item.SubItems.Add("3");
+            item.SubItems.Add("4");
+
+            sensorsTable.Items.Add(item);
+
+            gearRatioInput.Text = "7.300";
+
+            portInput.Text = "5";
+            portTypeInput.SelectedIndex = 0;
+        }
+
+    }
+}
---
+++ exporters/InventorRobotExporter/InventorRobotExporter/GUI/Editors/SimpleJointEditor/AdvancedJointSettings.designer.cs
@@ -0,0 +1,213 @@
+﻿namespace InventorRobotExporter.GUI.Editors.SimpleJointEditor
+{
+    partial class AdvancedJointSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdvancedJointSettings));
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.gearRatioInput = new System.Windows.Forms.NumericUpDown();
+            this.portInput = new System.Windows.Forms.TextBox();
+            this.sensorsTable = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.portTypeInput = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gearRatioInput)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(236, 262);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(317, 262);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // gearRatioInput
+            // 
+            this.gearRatioInput.Location = new System.Drawing.Point(6, 20);
+            this.gearRatioInput.Name = "gearRatioInput";
+            this.gearRatioInput.Size = new System.Drawing.Size(120, 20);
+            this.gearRatioInput.TabIndex = 4;
+            // 
+            // portInput
+            // 
+            this.portInput.Location = new System.Drawing.Point(6, 19);
+            this.portInput.Name = "portInput";
+            this.portInput.Size = new System.Drawing.Size(46, 20);
+            this.portInput.TabIndex = 6;
+            // 
+            // sensorsTable
+            // 
+            this.sensorsTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.sensorsTable.Location = new System.Drawing.Point(6, 19);
+            this.sensorsTable.Name = "sensorsTable";
+            this.sensorsTable.Size = new System.Drawing.Size(366, 127);
+            this.sensorsTable.TabIndex = 7;
+            this.sensorsTable.UseCompatibleStateImageBehavior = false;
+            this.sensorsTable.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Sensor Type";
+            this.columnHeader1.Width = 136;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Port A";
+            this.columnHeader2.Width = 113;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Port B";
+            // 
+            // portTypeInput
+            // 
+            this.portTypeInput.FormattingEnabled = true;
+            this.portTypeInput.Items.AddRange(new object[] {
+            "CAN",
+            "PWM"});
+            this.portTypeInput.Location = new System.Drawing.Point(58, 18);
+            this.portTypeInput.Name = "portTypeInput";
+            this.portTypeInput.Size = new System.Drawing.Size(52, 21);
+            this.portTypeInput.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sensorsTable);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 183);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sensors";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.gearRatioInput);
+            this.groupBox2.Location = new System.Drawing.Point(12, 206);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(134, 50);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gear Ratio";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.portTypeInput);
+            this.groupBox3.Controls.Add(this.portInput);
+            this.groupBox3.Location = new System.Drawing.Point(168, 206);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(117, 50);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Port";
+            // 
+            // AdvancedJointSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 297);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Advanced Joint Settings";
+            this.Text = "Advanced Joint Settings";
+            ((System.ComponentModel.ISupportInitialize)(this.gearRatioInput)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.NumericUpDown gearRatioInput;
+        private System.Windows.Forms.TextBox portInput;
+        private System.Windows.Forms.ListView sensorsTable;
+        private System.Windows.Forms.ComboBox portTypeInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
