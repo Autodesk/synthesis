11dd9d5d6ef14c6db7b7e29d2b33a9ceb4817bd0
matthew17754 <matmoradi17@gmail.com>
2019-08-05 11:55:54 -0700

    Added MaM rsync to Linux Install

--- installer/Linux/LinuxInstaller.sh
+++ installer/Linux/LinuxInstaller.sh
@@ -10,28 +10,31 @@ setup_scroll_area
 draw_progress_bar 0
 echo "Creating Directories..."
 sudo mkdir -p $HOME/.config/Autodesk/Synthesis
-draw_progress_bar 10
+draw_progress_bar 5
 sudo mkdir -p $ROOT/usr/share/Autodesk
-draw_progress_bar 20
+draw_progress_bar 10
 echo "Initializing Directory Permissions..."
 sudo chown -R $USER: $HOME/.config/Autodesk
-draw_progress_bar 30
+draw_progress_bar 15
 sudo chown -R $USER: $HOME/.config/Autodesk/Synthesis
-draw_progress_bar 40
+draw_progress_bar 20
 echo "Copying Robot Files..."
 sudo rsync -a --info=progress2 Robots $HOME/.config/Autodesk/Synthesis
-draw_progress_bar 50
+draw_progress_bar 30
+echo "Copying MixAndMatch Files..."
+sudo rsync -a --info=progress2 MixAndMatch $HOME/.config/Autodesk/Synthesis
+draw_progress_bar 40
 echo "Copying Field Files..."
 sudo rsync -a --info=progress2 Fields $HOME/.config/Autodesk/Synthesis
-draw_progress_bar 60
+draw_progress_bar 50
 echo "Copying Program Files..."
 sudo rsync -a --info=progress2 Synthesis $ROOT/usr/share/Autodesk
-draw_progress_bar 70
+draw_progress_bar 60
 sudo rsync -a --info=progress2 synthesis.png $ROOT/usr/share/pixmaps
-draw_progress_bar 80
+draw_progress_bar 70
 sudo rsync -a --info=progress2 Synthesis.desktop $ROOT/usr/share/applications
-draw_progress_bar 90
+draw_progress_bar 80
 sudo chmod +x $ROOT/usr/share/Autodesk/Synthesis/Synthesis.x86_64
-block_progress_bar 100
+block_progress_bar 85
 destroy_scroll_area
 echo "Autodesk Synthesis $VERSION For Linux Installation Complete"
