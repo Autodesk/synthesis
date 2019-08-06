046f419ae624e85f79efde87db38fd4f951f136c
matthew17754 <matmoradi17@gmail.com>
2019-08-06 14:06:06 -0700

    PlayerPref Removal For Linux Uninstaller
    Former-commit-id: 5a8157c8184b5e1173b98876b1dd3b68da44a588

--- installer/Linux/SynthesisUninstaller.sh
+++ installer/Linux/SynthesisUninstaller.sh
@@ -2,7 +2,9 @@
 #sed -i -e 's/\r$//' SynthesisUninstaller.sh
 
 sudo rm -rf $HOME/.config/Autodesk/Synthesis
+sudo rm -rf $HOME/.config/unity3d/Autodesk
 sudo rm -rf $ROOT/usr/share/Autodesk/Synthesis
+
 sudo rm $ROOT/usr/share/pixmaps/synthesis.png
 sudo rm $ROOT/usr/share/applications/Synthesis.desktop
 
