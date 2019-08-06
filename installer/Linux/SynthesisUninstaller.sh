7533eb65f9b8ff059c18198806b38ecfa498dde6
matthew17754 <matmoradi17@gmail.com>
2019-08-06 13:05:09 -0700

    Implemented Linux Uninstaller
    No one will probably ever use this...
    
    
    Former-commit-id: 701e3aa881e014edc053f43eab9fbe41a4ccf6ff

---
+++ installer/Linux/SynthesisUninstaller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#sed -i -e 's/\r$//' SynthesisUninstaller.sh
+
+sudo rm -rf $HOME/.config/Autodesk/Synthesis
+sudo rm -rf $ROOT/usr/share/Autodesk/Synthesis
+sudo rm $ROOT/usr/share/pixmaps/synthesis.png
+sudo rm $ROOT/usr/share/applications/Synthesis.desktop
+
+echo "Autodesk Synthesis Has Been Uninstalled"
