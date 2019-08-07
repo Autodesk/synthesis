ee48e8239066f07ee5376dfc29545189f635f7bc
nicolasespinoza <31714622+nicolasespinoza@users.noreply.github.com>
2019-08-07 15:51:56 -0700

    Created 'Unsupported Components' form

---
+++ exporters/InventorRobotExporter/InventorRobotExporter/GUI/Messages/UnsupportedComponents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InventorRobotExporter.Utilities;
+
+namespace InventorRobotExporter.GUI.Messages
+{
+
+    public partial class UnsupportedComponents : Form
+    {
+        public UnsupportedComponents()
+        {
+            InitializeComponent();
+        }
+
+        public void AddUnsupportedComponent(string component, string category, string type)
+        {
+            ListViewItem item = new ListViewItem(component);
+            item.SubItems.Add(category);
+            item.SubItems.Add(type);
+            componentListView.Items.Add(item);
+        }
+
+        private void ContinueButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ComponentListView_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
+        {
+            e.NewWidth = componentListView.Columns[e.ColumnIndex].Width;
+            e.Cancel = true;
+        }
+    }
+
+}
