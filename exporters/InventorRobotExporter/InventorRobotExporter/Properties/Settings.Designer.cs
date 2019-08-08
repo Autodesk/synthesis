167e4f0d67bb1dabe28e48b38dde918051bcc577
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-08-08 10:00:13 -0700

    Make "use analytics" true by default

--- exporters/InventorRobotExporter/InventorRobotExporter/Properties/Settings.Designer.cs
+++ exporters/InventorRobotExporter/InventorRobotExporter/Properties/Settings.Designer.cs
@@ -133,7 +133,7 @@ namespace InventorRobotExporter.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool UseAnalytics {
             get {
                 return ((bool)(this["UseAnalytics"]));
