0e6b7137c58ffb798db70d608020bffd0872ddc0
nicolasespinoza <31714622+nicolasespinoza@users.noreply.github.com>
2019-08-13 11:39:21 -0700

    Latest

--- exporters/FusionRobotExporter/palette/js/utility.js
+++ exporters/FusionRobotExporter/palette/js/utility.js
@@ -1,12 +1,3 @@
-// Used for completely hiding elements in the page
-function toggleElement(id) {
-    if (document.getElementById(id).style.display === "none") {
-        document.getElementById(id).style.display = "block";
-    } else {
-        document.getElementById(id).style.display = "none";
-    }
-}
-
 // Used for hiding/showing elements in the following function
 function setVisible(element, visible)
 {
