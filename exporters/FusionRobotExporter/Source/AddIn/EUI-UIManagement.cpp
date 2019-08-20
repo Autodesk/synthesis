704f7584828fec0fd999bd9cd145926ae812ec2a
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-08-20 09:58:40 -0700

    Fix guide closing automatically when switching assemblies
    Former-commit-id: 4b9e951dd0b9f5b812d8a048fe8e075e9d434abd

--- exporters/FusionRobotExporter/Source/AddIn/EUI-UIManagement.cpp
+++ exporters/FusionRobotExporter/Source/AddIn/EUI-UIManagement.cpp
@@ -91,7 +91,7 @@ void EUI::closeAllPalettes()
 	closeDriveWeightPalette();
 	closeJointEditorPalette();
 	closeSensorsPalette();
-	closeGuidePalette();
+	closeGuidePalette(false);
 	closeSettingsPalette("");
 	cancelExportRobot();
 }
@@ -349,11 +349,12 @@ void EUI::openGuidePalette()
 	Analytics::guideEnabled = true;
 }
 
-void EUI::closeGuidePalette()
+void EUI::closeGuidePalette(bool manualClose)
 {
 	robotExportGuideButton->controlDefinition()->isEnabled(true);
 	guidePalette->isVisible(false);
-	Analytics::guideEnabled = false;
+
+	if (manualClose) Analytics::guideEnabled = false;
 
 	static std::thread* uiThread = nullptr;
 	if (uiThread != nullptr) { uiThread->join(); delete uiThread; }
@@ -785,7 +786,7 @@ void EUI::closeSettingsPalette(std::string guideEnabled) {
 		openGuidePalette();
 	} else if (guideEnabled == "false")
 	{
-		closeGuidePalette();
+		closeGuidePalette(true);
 	}
 	Analytics::SaveSettings();
 }
