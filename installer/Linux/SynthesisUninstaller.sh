8be5a41e07acdfad85f4f36d925285310d3b44bb
matthew17754 <matmoradi17@gmail.com>
2020-03-28 15:16:31 -0700

    Removed Emulator Shell Installation
    Removes both Emulator and Mix & Match extraction from Linux installation procedure

--- installer/Linux/SynthesisUninstaller.sh
+++ installer/Linux/SynthesisUninstaller.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 #sed -i -e 's/\r$//' SynthesisUninstaller.sh
 
-read -r -p "Would you like to uninstall QEMU as well? [ Y / N ] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-	sudo apt-get remove --auto-remove qemu
-	sudo apt-get purge --auto-remove qemu
-fi
-
 sudo rm -rf $HOME/.config/Autodesk/Synthesis
 sudo rm -rf $HOME/.config/unity3d/Autodesk
 sudo rm -rf $ROOT/usr/share/Autodesk/Synthesis
