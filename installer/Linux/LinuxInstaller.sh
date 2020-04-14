f0272343c91bde7ca76d2596d3bc4c9219ed51f6
KyroVibe <hunter.barclay18@gmail.com>
2020-04-14 04:44:54 -0700

    Merged master into synthesis-2020

--- installer/Linux/LinuxInstaller.sh
+++ installer/Linux/LinuxInstaller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sed -i -e 's/\r$//' LinuxInstaller.sh
 
-VERSION="4.3.1"
+VERSION="4.3.3"
 source ./ProgressBar.sh
 sudo echo "Installing Autodesk Synthesis $VERSION For Linux"
 echo "This software is licensed under the terms and conditions of the Apache 2.0 License."
@@ -16,14 +16,11 @@ sudo mkdir -p $ROOT/usr/share/Autodesk
 draw_progress_bar 10
 echo "Initializing Directory Permissions..."
 sudo chown -R $USER: $HOME/.config/Autodesk
-draw_progress_bar 15
-sudo chown -R $USER: $HOME/.config/Autodesk/Synthesis
 draw_progress_bar 20
+sudo chown -R $USER: $HOME/.config/Autodesk/Synthesis
+draw_progress_bar 30
 echo "Copying Robot Files..."
 sudo rsync -a --info=progress2 Robots $HOME/.config/Autodesk/Synthesis
-draw_progress_bar 30
-echo "Copying MixAndMatch Files..."
-sudo rsync -a --info=progress2 MixAndMatch $HOME/.config/Autodesk/Synthesis
 draw_progress_bar 40
 echo "Copying Field Files..."
 sudo rsync -a --info=progress2 Fields $HOME/.config/Autodesk/Synthesis
@@ -32,46 +29,13 @@ echo "Copying Program Files..."
 sudo rsync -a --info=progress2 Synthesis $ROOT/usr/share/Autodesk
 draw_progress_bar 60
 sudo rsync -a --info=progress2 synthesis.png $ROOT/usr/share/pixmaps
-draw_progress_bar 65
-sudo rsync -a --info=progress2 Synthesis.desktop $ROOT/usr/share/applications
 draw_progress_bar 70
+sudo rsync -a --info=progress2 Synthesis.desktop $ROOT/usr/share/applications
+draw_progress_bar 80
 sudo rsync -a --info=progress2 SynthesisUninstaller.sh $HOME/.config/Autodesk/Synthesis
 chmod +x $HOME/.config/Autodesk/Synthesis/SynthesisUninstaller.sh
-draw_progress_bar 75
+draw_progress_bar 90
 sudo chmod +x $ROOT/usr/share/Autodesk/Synthesis/Synthesis.x86_64
-block_progress_bar 80
-
-read -r -p "Would you like to install the Code Emulator as well? [ Y / N ] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-	echo "Copying Emulator Files..."
-	rsync --info=progress2 Emulator $HOME/.config/Autodesk/Synthesis
-	draw_progress_bar 90
-
-	echo "Installing QEMU..."
-
-	APT_GET_CMD=$(which apt-get)
-	YUM_CMD=$(which yum)
-	PACMAN_CMD=$(which pacman)
-	ZYPPER_CMD=$(which zypper)
-	DNF_CMD=$(which dnf)
-
-	if [[ ! -z $APT_GET_CMD ]]; then
-		sudo apt-get install qemu
-	elif [[ ! -z $YUM_CMD ]]; then
-		sudo yum install qemu-kvm
-	elif [[ ! -z $PACMAN_CMD ]]; then
-		sudo pacman -S qemu
-	elif [[ ! -z $ZYPPER_CMD ]]; then
-		sudo zypper install qemu
-	elif [[ ! -z $DNF_CMD ]]; then
-		sudo dnf install @virtualization
-	else
-    echo "Failed to install QEMU. Install it manually from: https://www.qemu.org/download/"
-    exit 1;
-	fi
-fi
-
 draw_progress_bar 100
 destroy_scroll_area
 echo "Autodesk Synthesis $VERSION For Linux Installation Complete"
--- installer/Linux/README.md
+++ installer/Linux/README.md
@@ -6,20 +6,28 @@ For installation on Linux operating systems, we use our own custom written shell
 In order to acheive the proper package structure for proper extraction, you must first compile a Unity build as `Synthesis.x86_64` which must reside in the `Synthesis` folder. 
 
 Prior to compiling the package, you must first ensure that all scripts have the execute bit set properly using:   
-`$ chmod +x LinuxInstaller.sh`. 
+`$ chmod +x LinuxInstaller.sh`    
 
 Then, download [makeself](https://makeself.io) and compile the `.run` executable using:  
-`$ ./makeself.sh LinuxInstaller/ SynthesisLinux*.run "Synthesis For Linux 4.X.X" ./LinuxInstaller.sh`
+`$ ./makeself.sh LinuxInstaller/ SynthesisLinux4.X.X.run "Synthesis For Linux 4.X.X" ./LinuxInstaller.sh`   
+(Replace **4.X.X** with correct product version number)
 
-### Running The Package: ###
+### Installing The Package (FOR USERS): ###
 Installing the package should be as simple as `$ ./SynthesisLinux*.run`.  
 
 An alternate method of execution using BA$H would be `$ sh SynthesisLinux*.run`.
+
+### Uninstalling The Package (FOR USERS): ###
+The uninstallation script can be found at `\home\.config\Autodesk\Synthesis\SynthesisUninstaller.sh`
+
+Uninstalling the package should be as simple as `$ ./SynthesisUninstaller.sh`.  
+
+An alternate method of execution using BA$H would be `$ sh SynthesisUninstaller.sh`.
 
 ### Troubleshooting: ###
 If you're having trouble executing the `.run` package, try ensuring that the execute bit is properly set by running  
 `chmod +x SynthesisLinux*.run`
 
-If still run into issues or have problems with the installation itself, please feel free to submit an [issue](https://github.com/Autodesk/synthesis/issues).
+If still run into issues or have problems with the **installation itself**, please feel free to submit an [issue](https://github.com/Autodesk/synthesis/issues).
 
 For questions reguarding compilation, feel free to contact matthew.moradi@autodesk.com
