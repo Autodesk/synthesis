94b8afe6ea8709592c1325d665413b83fadee834
KyroVibe <hunter.barclay18@gmail.com>
2020-06-22 12:30:14 -0700

    Input Manager moved. Waiting on event bus for imple. and testing

---
+++ controller/Api/InputManager/Behaviors/InputGlobalBehavior.cs
@@ -0,0 +1,22 @@
+﻿using SynthesisAPI.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthesisAPI.InputManager.Behaviors
+{
+    public class InputGlobalBehavior : GlobalBehavior<InputBehavior>
+    {
+        private static InputGlobalBehavior instance;
+        public static InputGlobalBehavior Instance {
+            get {
+                if (instance == null) instance = new InputGlobalBehavior();
+                return instance;
+            }
+        }
+
+        private InputGlobalBehavior(): base() { }
+    }
+}
