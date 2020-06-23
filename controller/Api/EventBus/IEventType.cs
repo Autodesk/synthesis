308aa4c8d6680008ae5965a8ad8022e2a8797cc8
Anna Nixon <anna.nixon@autodesk.com>
2020-06-23 16:11:18 -0700

    Added IEventType

---
+++ controller/Api/EventBus/IEventType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace SynthesisAPI.EventBus
+{
+    public interface IEvent 
+    {
+        public string Type { get; internal set; }
+
+        public byte[] getData();
+    }
+
+}
