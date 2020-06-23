00246f59664b733bce971ef0ff0ca9ff16fe2950
Liam Wang <5054950+wangl5@users.noreply.github.com>
2020-06-23 16:50:11 -0700

    Add hierarchy debug utils

---
+++ exporter/SynthesisFusionExporter/utils/DebugHierarchy.py
@@ -0,0 +1,27 @@
+import adsk, adsk.core, adsk.fusion, traceback
+
+def printTabs(num):
+    for i in range(num):
+        print("    ", end ="")
+
+def printOccurrence(occur, depth):
+    printTabs(depth)
+    print("[O] "+occur.name)
+    printComponent(occur.component, depth)
+
+def printComponent(comp, depth):
+    printTabs(depth)
+    print("[C] "+comp.name)
+    if comp.joints.count > 0:
+        printTabs(depth)
+        print("Joints")
+        for joint in comp.joints:
+            printTabs(depth)
+            print("| "+joint.name)
+    for occur in comp.occurrences:
+        printOccurrence(occur, depth+1)
+
+def printHierarchy(root_comp):
+    print("------Full Hierarchy-------")
+    printComponent(root_comp, 0)
+    print("---------------------------")
