380edb5bf996dd7a93f4b508cc10048fffacb0d1
Anna Nixon <anna.nixon@autodesk.com>
2020-06-25 11:53:47 -0700

    EventBus framework

--- controller/Api/EventBus/EventBus.cs
+++ controller/Api/EventBus/EventBus.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Channels;
+using System.Collections;
+using SynthesisAPI.EventBus;
 
 namespace SynthesisAPI.EventBus
 {
-    public class EventBus
+    public static class EventBus
     {
+        public static Dictionary<string, Channel<IEvent>> topics = new Dictionary<string, Channel<IEvent>>();
+
+        static EventBus()
+        {
+
+        }
+
+        private static Channel<IEvent> CreateChannel(string tag)
+        {
+            Channel<IEvent> c = Channel.CreateUnbounded<IEvent>();
+            topics.Add(tag, c);
+            return c;
+        }
+        public static void Push(IEvent event, String [] tags)
+        {
+            foreach (string tag in tags)
+            {
+                Channel<IEvent> ch;
+                if (topics.TryGetValue(tag, out ch)) 
+                {
+                    await ch.Writer.WriteAsync(event);
+                    ch.Writer.Complete();
+                } 
+                else 
+                {
+                    ch = CreateChannel(tag);
+                    await ch.Writer.WriteAsync(event);
+                    ch.Writer.Complete();
+                }
+            }
+        }
+
+        public static ChannelReader<IEvent> Subscribe (string tag)
+        {
+            Channel<IEvent> ch;
+            if (topics.TryGetValue(tag, out ch))
+            {
+                return ch.Reader;
+            }
+            else
+            {
+                ch = CreateChannel(tag);
+                return ch.Reader;
+            }
+        }
+
     }
 }
