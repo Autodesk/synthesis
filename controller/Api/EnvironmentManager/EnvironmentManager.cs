1bf6f8e5c373e1acf81d9a9544628267a02d9791
ajaypai <ajaypai@cs.washington.edu>
2020-06-24 17:42:39 -0700

    made environment manager static

--- controller/Api/EnvironmentManager/EnvironmentManager.cs
+++ controller/Api/EnvironmentManager/EnvironmentManager.cs
@@ -4,30 +4,27 @@ using System.Collections.Generic;
 //Entity is a 32 bit generational index
 // 1st 16 bits represent index
 // 2nd 16 bits represent generation
-using Entity = System.UInt32; 
+using Entity = System.UInt32;
 
 namespace SynthesisAPI.EnvironmentManager
 {
     /// <summary>
     /// ECS System
     /// </summary>
-    public class EnvironmentManager
+    public static class EnvironmentManager
     {
-        AnyMap components; //dynamic mapping of components to Entity by index
-        List<Entity> entities; //Entities that are in environment 
-        Stack<Entity> removed; //deallocated Entities that still exist in entities null Entities
+        static AnyMap components = new AnyMap(); //dynamic mapping of components to Entity by index
+        static List<Entity> entities = new List<Entity>(); //Entities that are in environment 
+        static Stack<Entity> removed = new Stack<Entity>(); //deallocated Entities that still exist in entities null Entities
 
-        const Entity NULL_ENTITY = 0; 
+        const Entity NULL_ENTITY = 0;
         const ushort BASE_GEN = 1; //no entity should have a generation of 0
 
-        public EnvironmentManager()
-        {
-            entities = new List<Entity>();
-            removed = new Stack<Entity>();
-            components = new AnyMap();
-        }
-
-        public Entity AddEntity()
+        /// <summary>
+        /// Create and allocate new entity
+        /// </summary>
+        /// <returns>new Entity</returns>
+        public static Entity AddEntity()
         {
             Entity newEntity;
             if (removed.Count > 0) //allocates empty/deallocated spaces in entities
@@ -35,7 +32,7 @@ namespace SynthesisAPI.EnvironmentManager
                 Entity oldEntity = removed.Pop();
                 ushort oldEntityIndex = EntityManager.GetIndex(oldEntity);
                 ushort oldEntityGen = EntityManager.GetGen(oldEntity);
-                newEntity = EntityManager.SetGen(oldEntity, (ushort)(oldEntityGen+1)) ;
+                newEntity = EntityManager.SetGen(oldEntity, (ushort)(oldEntityGen + 1));
                 entities[oldEntityIndex] = newEntity;
             }
             else //adds entity by increasing entity list size
@@ -46,7 +43,12 @@ namespace SynthesisAPI.EnvironmentManager
             return newEntity;
         }
 
-        public bool RemoveEntity(Entity entity)
+        /// <summary>
+        /// Remove and deallocate entity
+        /// </summary>
+        /// <param name="entity">entity to be removed</param>
+        /// <returns>if entity was removed - will only return false if entity does not exist/is not allocated</returns>
+        public static bool RemoveEntity(Entity entity)
         {
             if (!EntityExists(entity))
                 return false;
@@ -55,35 +57,59 @@ namespace SynthesisAPI.EnvironmentManager
             return true;
         }
 
-        public T GetComponent<T>(Entity entity)
+        /// <summary>
+        /// Get component of type, T, that is associated with the given entity
+        /// </summary>
+        /// <typeparam name="T">Component Type</typeparam>
+        /// <param name="entity">given entity</param>
+        /// <returns>Component instance</returns>
+        public static T GetComponent<T>(Entity entity)
         {
             if (EntityExists(entity))
                 return components.Get<T>(entity);
             return default;
         }
 
-        public void SetComponent<T>(Entity entity, T component)
+        /// <summary>
+        /// Set component of type, T, to the given entity
+        /// </summary>
+        /// <typeparam name="T">Component Type</typeparam>
+        /// <param name="entity">given entity</param>
+        /// <param name="component">instance of component to be set</param>
+        public static void SetComponent<T>(Entity entity, T component)
         {
             if (EntityExists(entity)) components.Set<T>(entity, component);
         }
 
-        public void RemoveComponent<T>(Entity entity)
+        /// <summary>
+        /// Remove component (sets component to null) of type, T, from given entity
+        /// </summary>
+        /// <typeparam name="T">Component Type</typeparam>
+        /// <param name="entity">given entity</param>
+        public static void RemoveComponent<T>(Entity entity)
         {
-            if(EntityExists(entity)) components.Remove<T>(entity);
+            if (EntityExists(entity)) components.Remove<T>(entity);
         }
 
-        public bool EntityExists(Entity entity)
+        /// <summary>
+        /// Check if entity exists in current context
+        /// </summary>
+        /// <param name="entity">entity to be checked</param>
+        /// <returns>if entity equals the entity in its given index</returns>
+        public static bool EntityExists(Entity entity)
         {
             ushort entityIndex = EntityManager.GetIndex(entity);
             return entityIndex < entities.Count && entities[entityIndex] == entity;
         }
+
+        //
 
         /// <summary>
         /// Dynamic mapping of any reference type to its corresponding GenIndexArray
         /// </summary>
         class AnyMap
         {
-            public Dictionary<Type, object> componentDict;
+            public Dictionary<Type, object> componentDict; //Dictionary<T,GenIndexArray<T>>
             public AnyMap()
             {
                 componentDict = new Dictionary<Type, object>();
@@ -149,7 +175,7 @@ namespace SynthesisAPI.EnvironmentManager
                         for (int i = entries.Count; i < entityIndex; i++)
                             entries.Add(new Entry(default, 0)); //no Entity has gen of 0 so these entries are essentially null
                         entries.Add(new Entry(val, entityGen));
-                    }   
+                    }
                 }
 
                 public T Get(Entity entity)
@@ -159,14 +185,14 @@ namespace SynthesisAPI.EnvironmentManager
                     if (entityIndex >= entries.Count) return default; //prevents IndexOutOfBoundsException
                     Entry entry = entries[entityIndex];
                     //only get component if generations match - avoids having reallocated Entities point to the components of deallocated Entities
-                    if (entry.Gen == entityGen) return entry.Val; 
+                    if (entry.Gen == entityGen) return entry.Val;
                     return default;
                 }
             }
         }
 
         /// <summary>
-        /// Encapsulation of static methods to read and write to Entities
+        /// Reads and writes bits of an Entity
         /// </summary>
         class EntityManager
         {
@@ -188,7 +214,7 @@ namespace SynthesisAPI.EnvironmentManager
             {
                 return ((uint)index << 16) + (entity & 65535);
             }
-            public static Entity SetGen(Entity entity,ushort gen)
+            public static Entity SetGen(Entity entity, ushort gen)
             {
                 return (entity & 4294901760) + gen;
             }
