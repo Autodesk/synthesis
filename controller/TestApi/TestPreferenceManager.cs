e339bbb483f51986f04ddcdd8c0b423db71b27fa
Hunter <hunter.barclay18@gmail.com>
2020-06-30 13:26:57 -0700

    Added unit test

--- controller/TestApi/TestPreferenceManager.cs
+++ controller/TestApi/TestPreferenceManager.cs
@@ -5,6 +5,7 @@ using SynthesisAPI.VirtualFileSystem;
 using System;
 using System.Text;
 using NUnit.Framework;
+using System.Threading.Tasks;
 
 namespace TestApi
 {
@@ -23,8 +24,29 @@ namespace TestApi
         {
             PreferenceManager.SetPreference(Program.TestGuid, "name", "Nikola Tesla");
             PreferenceManager.SetPreference(Program.TestGuid, "age", 163);
+
             if (!PreferenceManager.Save()) throw new Exception();
             if (!PreferenceManager.Load()) throw new Exception();
+            
+            string name = PreferenceManager.GetPreference<string>(Program.TestGuid, "name");
+            int age = PreferenceManager.GetPreference<int>(Program.TestGuid, "age");
+
+            Assert.AreEqual(name, "Nikola Tesla");
+            Assert.AreEqual(age, 163);
+        }
+
+        [Test]
+        public static void TestAsyncSaveLoad()
+        {
+            PreferenceManager.SetPreference(Program.TestGuid, "name", "Nikola Tesla");
+            PreferenceManager.SetPreference(Program.TestGuid, "age", 163);
+
+            Task<bool> saveTask = PreferenceManager.SaveAsync();
+            if (!saveTask.Wait(1000)) throw new Exception();
+
+            Task<bool> loadTask = PreferenceManager.LoadAsync();
+            if (!loadTask.Wait(1000)) throw new Exception();
+
             string name = PreferenceManager.GetPreference<string>(Program.TestGuid, "name");
             int age = PreferenceManager.GetPreference<int>(Program.TestGuid, "age");
 
@@ -37,8 +59,10 @@ namespace TestApi
         {
             CustomStruct customOriginal = new CustomStruct() { Name = "Thomas Edison" };
             PreferenceManager.SetPreference(Program.TestGuid, "custom_type", customOriginal);
+
             if (!PreferenceManager.Save()) throw new Exception();
             if (!PreferenceManager.Load()) throw new Exception();
+
             CustomStruct customCopy = PreferenceManager.GetPreference<CustomStruct>(Program.TestGuid, "custom_type", useJsonDeserialization: true);
 
             Assert.AreEqual(customOriginal, customCopy);
