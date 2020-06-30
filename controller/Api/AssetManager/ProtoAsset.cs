bc86928956dc8af1d34ac8269a37444371f1ae23
Kira Corbett <kirac2093@gmail.com>
2020-06-29 17:35:46 -0700

    ProtoAsset init

---
+++ controller/Api/AssetManager/ProtoAsset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthesisAPI.AssetManager
+{
+	public class ProtoAsset : Asset
+    {
+		public ProtoAsset(string name, Guid owner, Permissions permissions, string path) : 
+			base(name, owner, permissions, path) { }
+    }
+}
