5f8a15fdf542b70dca8d5cbfe8f6b6d916744219
Kira Corbett <kirac2093@gmail.com>
2020-07-01 09:45:04 -0700

    transferred protoutils to Api

--- controller/Api/AssetManager/ProtoAsset.cs
+++ controller/Api/AssetManager/ProtoAsset.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Dynamic;
+using System.IO;
 using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SynthesisAPI.AssetManager
 {
+	// path is the source to the protobuf file
+	// ProtoAsset reads the data
 	public class ProtoAsset : Asset
     {
 		public ProtoAsset(string name, Guid owner, Permissions permissions, string path) : 
 			base(name, owner, permissions, path) { }
+
+		public GetProtoFile(string path)
+        {
+			FileStream fs = new FileStream(path, FileMode.Open);
+			ProtoItem protoObject = ProtoItem.Parser.ParseFrom(fs);
+			fs.Close();
+			return protoObject;
+        }
     }
 }
