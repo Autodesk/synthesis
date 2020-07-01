f4058fdbb2346737a2f0f8a3c468f690fb152790
Kira Corbett <kirac2093@gmail.com>
2020-07-01 10:55:26 -0700

    temp commit

--- controller/Api/AssetManager/ProtoAsset.cs
+++ controller/Api/AssetManager/ProtoAsset.cs
@@ -4,22 +4,38 @@ using System.IO;
 using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using Synthesis.Importbuf;
+using SynthesisAPI.VirtualFileSystem;
 
 namespace SynthesisAPI.AssetManager
 {
-	// path is the source to the protobuf file
-	// ProtoAsset reads the data
-	public class ProtoAsset : Asset
+    // path is the source to the protobuf file
+    // ProtoAsset reads the data
+    public class ProtoAsset<T> : Asset where T : Google.Protobuf.IMessage<T>
     {
-		public ProtoAsset(string name, Guid owner, Permissions permissions, string path) : 
-			base(name, owner, permissions, path) { }
 
-		public GetProtoFile(string path)
+        public ProtoAsset(string name, Guid owner, Permissions permissions, string path)
         {
-			FileStream fs = new FileStream(path, FileMode.Open);
-			ProtoItem protoObject = ProtoItem.Parser.ParseFrom(fs);
-			fs.Close();
-			return protoObject;
+            Init(name, owner, permissions, path);
+        }
+
+        //public static Document GetProtoFile(string path)
+        //{
+        //    FileStream fs = new FileStream(path, FileMode.Open);
+        //    Document protoObject = Document.Parser.ParseFrom(fs);
+        //    Console.Write(fs);
+        //    fs.Close();
+        //    return protoObject;
+        //}
+
+        public override IEntry Load(byte[] data)
+        {
+            var stream = new MemoryStream();
+            stream.Write(data, 0, data.Length);
+            stream.Position = 0;
+            Document protoObj = Document.Parser.ParseFrom(stream);
         }
     }
 }
