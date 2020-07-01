3c17bc786e097cc866827cef1c275753864fab5f
Kira Corbett <kirac2093@gmail.com>
2020-07-01 13:16:43 -0700

    init GltfAsset instead of ProtoAsset

---
+++ controller/Api/AssetManager/GltfAsset.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using SynthesisAPI.VirtualFileSystem;
+
+namespace SynthesisAPI.AssetManager
+{
+    public class GltfAsset<T> : Asset
+    {
+
+        public GltfAsset(string name, Guid owner, Permissions permissions, string path)
+        {
+            Init(name, owner, permissions, path);
+        }
+
+        public override IEntry Load(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
