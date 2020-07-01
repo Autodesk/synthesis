8457ae8f05c49d9f576baf63bf5fb1dee202269e
Liam Wang <5054950+wangl5@users.noreply.github.com>
2020-06-30 18:02:37 -0700

    Add joint serialization

--- exporter/SynthesisFusionGltfExporter/gltfutils/GLTFDesignExporter.py
+++ exporter/SynthesisFusionGltfExporter/gltfutils/GLTFDesignExporter.py
@@ -19,6 +19,10 @@ from pyutils.timers import SegmentedStopwatch
 
 from gltfutils.GLTFConstants import ComponentType, DataType
 
+from ..proto.synthesis_importbuf_pb2 import Joint
+
+from google.protobuf.json_format import MessageToDict
+
 
 class GLTFDesignExporter(object):  # todo: can this exporter be made generic (non fusion specific?)
     """Class for exporting fusion designs into the glTF binary file format, aka glB.
@@ -240,7 +244,7 @@ class GLTFDesignExporter(object):  # todo: can this exporter be made generic (no
                     self.GLB_CHUNK_SIZE + len(jsonBytes) + \
                     self.GLB_CHUNK_SIZE + len(primaryBufferData)
 
-        with open(filepath, 'xb') as stream:
+        with open(filepath, 'wb') as stream:
             # header
             stream.write(b'glTF')  # magic
             stream.write(struct.pack('<I', self.GLTF_VERSION))  # version
@@ -298,6 +302,8 @@ class GLTFDesignExporter(object):  # todo: can this exporter be made generic (no
         self.progressBar.progressValue+=1
         scene.nodes.append(self.exportRootNode(self.ao.root_comp))
         self.perfWatch.stop()
+
+        scene.extras['joints'] = self.exportJoints(self.ao.design.rootComponent.allJoints)
 
         self.gltf.scenes.append(scene)
         return len(self.gltf.scenes) - 1
@@ -750,3 +756,147 @@ class GLTFDesignExporter(object):  # todo: can this exporter be made generic (no
             return None
         aluminum = fusionMatLib.appearances.itemById("PrismMaterial-002_physmat_aspects:Prism-028") # Aluminum - Satin
         return aluminum
+
+    def exportJoints(self, fusionJoints):
+        joints = []
+        for fusionJoint in fusionJoints:
+            if self.isJointInvalid(fusionJoint):
+                continue
+            joints.append(MessageToDict(self.fillJoint(fusionJoint)))
+        return joints
+
+    def isJointInvalid(self, fusionJoint):
+        if fusionJoint.occurrenceOne is None and fusionJoint.occurrenceTwo is None:
+            print("WARNING: Ignoring joint with unknown occurrences!")  # todo: Show these messages to the user
+            return True
+        if fusionJoint.jointMotion.jointType not in range(6):
+            print("WARNING: Ignoring joint with unknown type!")
+            return True
+        return False
+
+    def fillJoint(self, fusionJoint):
+        protoJoint = Joint()
+        # protoJoint.header.uuid = item_id(fusionJoint, ATTR_GROUP_NAME)
+        protoJoint.header.name = fusionJoint.name
+        self.fillVector3D(self.getJointOrigin(fusionJoint), protoJoint.origin)
+        protoJoint.isLocked = fusionJoint.isLocked
+        protoJoint.isSuppressed = fusionJoint.isSuppressed
+
+        # If occurrenceOne or occurrenceTwo is null, the joint is jointed to the root component
+        protoJoint.occurrenceOneUUID = self.getJointedOccurrenceUUID(fusionJoint, fusionJoint.occurrenceOne)
+        protoJoint.occurrenceTwoUUID = self.getJointedOccurrenceUUID(fusionJoint, fusionJoint.occurrenceTwo)
+
+        fillJointMotionFuncSwitcher = {
+            0: self.fillRigidJointMotion,
+            1: self.fillRevoluteJointMotion,
+            2: self.fillSliderJointMotion,
+            3: self.fillCylindricalJointMotion,
+            4: self.fillPinSlotJointMotion,
+            5: self.fillPlanarJointMotion,
+            6: self.fillBallJointMotion,
+        }
+
+        fillJointMotionFunc = fillJointMotionFuncSwitcher.get(fusionJoint.jointMotion.jointType, lambda: None)
+        fillJointMotionFunc(fusionJoint.jointMotion, protoJoint)
+        return protoJoint
+
+    def getJointOrigin(self, fusionJoint):
+        geometryOrOrigin = fusionJoint.geometryOrOriginOne if fusionJoint.geometryOrOriginOne.objectType == 'adsk::fusion::JointGeometry' else fusionJoint.geometryOrOriginTwo
+        if geometryOrOrigin.objectType == 'adsk::fusion::JointGeometry':
+            return geometryOrOrigin.origin
+        else:  # adsk::fusion::JointOrigin
+            origin = geometryOrOrigin.geometry.origin
+            return adsk.core.Point3D.create(  # todo: Is this the correct way to calculate a joint origin's true location? Why isn't this exposed in the API?
+                origin.x + geometryOrOrigin.offsetX.value,
+                origin.y + geometryOrOrigin.offsetY.value,
+                origin.z + geometryOrOrigin.offsetZ.value)
+
+    def getJointedOccurrenceUUID(self, fusionJoint, fusionOccur):
+        # if fusionOccur is None:
+        #     return item_id(fusionJoint.parentComponent, ATTR_GROUP_NAME)  # If the occurrence of a joint is null, the joint is jointed to the parent component (which should always be the root object)
+        # return item_id(fusionOccur, ATTR_GROUP_NAME)
+        if fusionOccur is None:
+            return ""  # If the occurrence of a joint is null, the joint is jointed to the parent component (which should always be the root object)
+        return fusionOccur.fullPathName
+
+    def fillRigidJointMotion(self, fusionJointMotion, protoJoint):
+        protoJoint.rigidJointMotion.SetInParent()
+
+    def fillRevoluteJointMotion(self, fusionJointMotion, protoJoint):
+        protoJointMotion = protoJoint.revoluteJointMotion
+
+        self.fillVector3D(fusionJointMotion.rotationAxisVector, protoJointMotion.rotationAxisVector)
+        protoJointMotion.rotationValue = fusionJointMotion.rotationValue
+        self.fillJointLimits(fusionJointMotion.rotationLimits, protoJointMotion.rotationLimits)
+
+    def fillSliderJointMotion(self, fusionJointMotion, protoJoint):
+        protoJointMotion = protoJoint.sliderJointMotion
+
+        self.fillVector3D(fusionJointMotion.slideDirectionVector, protoJointMotion.slideDirectionVector)
+        protoJointMotion.slideValue = fusionJointMotion.slideValue
+        self.fillJointLimits(fusionJointMotion.slideLimits, protoJointMotion.slideLimits)
+
+    def fillCylindricalJointMotion(self, fusionJointMotion, protoJoint):
+        protoJointMotion = protoJoint.cylindricalJointMotion
+
+        self.fillVector3D(fusionJointMotion.rotationAxisVector, protoJointMotion.rotationAxisVector)
+        protoJointMotion.rotationValue = fusionJointMotion.rotationValue
+        self.fillJointLimits(fusionJointMotion.rotationLimits, protoJointMotion.rotationLimits)
+
+        protoJointMotion.slideValue = fusionJointMotion.slideValue
+        self.fillJointLimits(fusionJointMotion.slideLimits, protoJointMotion.slideLimits)
+
+    def fillPinSlotJointMotion(self, fusionJointMotion, protoJoint):
+        protoJointMotion = protoJoint.pinSlotJointMotion
+
+        self.fillVector3D(fusionJointMotion.rotationAxisVector, protoJointMotion.rotationAxisVector)
+        protoJointMotion.rotationValue = fusionJointMotion.rotationValue
+        self.fillJointLimits(fusionJointMotion.rotationLimits, protoJointMotion.rotationLimits)
+
+        self.fillVector3D(fusionJointMotion.slideDirectionVector, protoJointMotion.slideDirectionVector)
+        protoJointMotion.slideValue = fusionJointMotion.slideValue
+        self.fillJointLimits(fusionJointMotion.slideLimits, protoJointMotion.slideLimits)
+
+    def fillPlanarJointMotion(self, fusionJointMotion, protoJoint):
+        protoJointMotion = protoJoint.planarJointMotion
+
+        self.fillVector3D(fusionJointMotion.normalDirectionVector, protoJointMotion.normalDirectionVector)
+
+        self.fillVector3D(fusionJointMotion.primarySlideDirectionVector, protoJointMotion.primarySlideDirectionVector)
+        protoJointMotion.primarySlideValue = fusionJointMotion.primarySlideValue
+        self.fillJointLimits(fusionJointMotion.primarySlideLimits, protoJointMotion.primarySlideLimits)
+
+        self.fillVector3D(fusionJointMotion.secondarySlideDirectionVector, protoJointMotion.secondarySlideDirectionVector)
+        protoJointMotion.secondarySlideValue = fusionJointMotion.secondarySlideValue
+        self.fillJointLimits(fusionJointMotion.secondarySlideLimits, protoJointMotion.secondarySlideLimits)
+
+        protoJointMotion.rotationValue = fusionJointMotion.rotationValue
+        self.fillJointLimits(fusionJointMotion.rotationLimits, protoJointMotion.rotationLimits)
+
+    def fillBallJointMotion(self, fusionJointMotion, protoJoint):
+        protoJointMotion = protoJoint.ballJointMotion
+
+        self.fillVector3D(fusionJointMotion.rollDirectionVector, protoJointMotion.rollDirectionVector)
+        protoJointMotion.rollValue = fusionJointMotion.rollValue
+        self.fillJointLimits(fusionJointMotion.rollLimits, protoJointMotion.rollLimits)
+
+        self.fillVector3D(fusionJointMotion.pitchDirectionVector, protoJointMotion.pitchDirectionVector)
+        protoJointMotion.pitchValue = fusionJointMotion.pitchValue
+        self.fillJointLimits(fusionJointMotion.pitchLimits, protoJointMotion.pitchLimits)
+
+        self.fillVector3D(fusionJointMotion.yawDirectionVector, protoJointMotion.yawDirectionVector)
+        protoJointMotion.yawValue = fusionJointMotion.yawValue
+        self.fillJointLimits(fusionJointMotion.yawLimits, protoJointMotion.yawLimits)
+
+    def fillJointLimits(self, fusionJointLimits, protoJointLimits):
+        protoJointLimits.isMaximumValueEnabled = fusionJointLimits.isMaximumValueEnabled
+        protoJointLimits.isMinimumValueEnabled = fusionJointLimits.isMinimumValueEnabled
+        protoJointLimits.isRestValueEnabled = fusionJointLimits.isRestValueEnabled
+        protoJointLimits.maximumValue = fusionJointLimits.maximumValue
+        protoJointLimits.minimumValue = fusionJointLimits.minimumValue
+        protoJointLimits.restValue = fusionJointLimits.restValue
+
+    def fillVector3D(self, fusionVector3D, protoVector3D):
+        protoVector3D.x = fusionVector3D.x
+        protoVector3D.y = fusionVector3D.y
+        protoVector3D.z = fusionVector3D.z
