1346f03339d82092b234a217311386d2ccf0d33f
Logan Traffas <logan.traffas@gmail.com>
2020-07-08 13:29:50 -0700

    Setup SystemBases defined in Api and CoreEngine as entities on start
    Co-authored-by: Nicholas Burrell <burrelni@oregonstate.edu>

--- engine/CoreEngine/Assets/Scripts/Engine/ModuleLoader/SystemMonoBehavior.cs
+++ engine/CoreEngine/Assets/Scripts/Engine/ModuleLoader/SystemMonoBehavior.cs
@@ -6,12 +6,15 @@ namespace Engine.ModuleLoader
 {
 	public class SystemMonoBehavior : MonoBehaviour, IApiAdapter<SystemBase>
 	{
+		public string Name;
+
 		public void Update() => _system.OnUpdate();
 
 		private SystemBase _system;
 		public void SetInstance(SystemBase instance)
 		{
 			_system = instance;
+			Name = _system.GetType().FullName;
 		}
 	}
 }
--- engine/CoreEngine/Assets/Scripts/Testing/TestInput2.cs
+++ engine/CoreEngine/Assets/Scripts/Testing/TestInput2.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using SynthesisAPI.InputManager;
 using SynthesisAPI.InputManager.Digital;
+using SynthesisAPI.EventBus;
+using SynthesisAPI.InputManager.Events;
 
 public class TestInput2 : MonoBehaviour
 {
     private KeyDigital key;
 
+    public void DigitalStateEventCallback(IEvent e)
+    {
+        Debug.Log($"DigitalStateEventCallback {e.GetArguments()[0]} {e.GetArguments()[1]}");
+    }
+
     // Start is called before the first frame update
     public void Start()
     {
         key = KeyCode.Space;
+
+        InputManager.AssignDigital("test", key, DigitalStateEventCallback);
     }
 
     // Update is called once per frame
@@ -18,7 +27,7 @@ public class TestInput2 : MonoBehaviour
         InputManager.DigitalState state = key.GetState();
         if (state != InputManager.DigitalState.None)
         {
-            Debug.Log(state.ToString());
+            // Debug.Log(state.ToString());
         }
     }
 }
