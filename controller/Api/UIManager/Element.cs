13f81fa370b69b87ee7da4a04ab18bf21c94724c
nicolasespinoza <31714622+nicolasespinoza@users.noreply.github.com>
2020-07-09 14:12:58 -0700

    Added base implementation of UIManager into API

---
+++ controller/Api/UIManager/Element.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UIElements;
+
+namespace SynthesisAPI.UIManager
+{
+    public class Element
+    {
+        public string pathToElement { get; }
+        public string treeName { get; }
+        public string uniqueKey { get; }
+        public VisualTreeAsset visualTreeAsset { get; }
+        public VisualElement visualElement { get; }
+
+        public Element(string pathToElement, string treeName, string uniqueKey)
+        {
+            this.pathToElement = pathToElement;
+            this.treeName = treeName;
+            this.uniqueKey = uniqueKey;
+
+            this.visualTreeAsset = /*AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(pathToElement);*/ null;
+            this.visualElement = visualTreeAsset.CloneTree();
+        }
+    }
+}
--- controller/Api/UIManager/UIManager.cs
+++ controller/Api/UIManager/UIManager.cs
@@ -8,5 +8,47 @@ namespace SynthesisAPI.UIManager
 {
     public class UIManager
     {
+        private Dictionary<string, Element> elements = new Dictionary<string, Element>();
+        
+        public void RegisterElement()
+        {
+            
+        }
+
+        public void UnregisterElement(string key)
+        {
+            
+        }
+
+        public void ToggleElement()
+        {
+            
+        }
+
+        public void AddTab(string tabName)
+        {
+            
+        }
+
+        public void RemoveTab(string tabName)
+        {
+            
+        }
+
+        private void HighlightTab(String tabName)
+        {
+            
+        }
+        
+        private void SetChildrenVisibilty()
+        {
+            
+        }
+
+        public void CenterElementHorizontally(string key, float width)
+        {
+            
+        }
+
     }
 }
