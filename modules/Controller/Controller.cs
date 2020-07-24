c2af10e8775b2cc39b9d4974ddb6fc194bfb545d
Logan Traffas <logan.traffas@gmail.com>
2020-07-24 15:13:43 -0700

    Prep controller module project for development

--- modules/Controller/Controller.cs
+++ modules/Controller/Controller.cs
@@ -1,16 +1,10 @@
 ﻿using SynthesisAPI.Modules.Attributes;
-using SynthesisAPI.Runtime;
 
 namespace Controller
 {
     [ModuleExport]
 	public class Controller
 	{
-		[Callback]
-		public void SampleListenCallback(SampleEvent s)
-		{
-			ApiProvider.Log(s.A);
-			ApiProvider.Log(s.B);
-		}
+
 	}
 }
