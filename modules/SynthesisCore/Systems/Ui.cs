5ae13a10bd14ab488ad38b07373a44c74e790acb
nicolasespinoza <31714622+nicolasespinoza@users.noreply.github.com>
2020-07-27 16:48:07 -0700

    Began initial transfer of basic UI files

---
+++ modules/SynthesisCore/Systems/Ui.cs
@@ -0,0 +1,22 @@
+﻿using SynthesisAPI.AssetManager;
+using SynthesisAPI.EnvironmentManager;
+using SynthesisAPI.UIManager;
+using SynthesisAPI.UIManager.UIComponents;
+
+namespace SynthesisCore.Systems
+{
+    public class Ui : SystemBase
+    {
+        public override void Setup()
+        {
+            var tabAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/SynthesisCore/UI/uxml/Tab.uxml");
+            Tab engineTab = new Tab("Engine", tabAsset, null);
+            
+            UIManager.AddTab(engineTab);
+        }
+
+        public override void OnPhysicsUpdate() { }
+
+        public override void OnUpdate() { }
+    }
+}
