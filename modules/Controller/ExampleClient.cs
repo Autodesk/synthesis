def9bf8126f89210bce61dede1d41ad0abd58b66
Logan Traffas <logan.traffas@gmail.com>
2020-07-27 16:34:43 -0700

    Add example HTTP client/server to controller module

---
+++ modules/Controller/ExampleClient.cs
@@ -0,0 +1,38 @@
+﻿using SynthesisAPI.EnvironmentManager;
+using SynthesisAPI.Modules.Attributes;
+using SynthesisAPI.Runtime;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Controller
+{
+    [ModuleExport]
+    public class ExampleClient : SystemBase
+    {
+        private HttpClient client = new HttpClient();
+        public override void Setup()
+        {
+            client.BaseAddress = new Uri("http://localhost:5000/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Test();
+        }
+        public override void OnUpdate() { }
+        public override void OnPhysicsUpdate() { }
+
+        private async void Test()
+        {
+            ApiProvider.Log("Client: sending get request");
+            HttpResponseMessage response = await client.GetAsync("");
+            if (response.IsSuccessStatusCode)
+            {
+                ApiProvider.Log(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                ApiProvider.Log("Client: bad response");
+            }
+        }
+    }
+}
---
+++ modules/Controller/Server.cs
@@ -0,0 +1,34 @@
+﻿using SynthesisAPI.EnvironmentManager;
+using SynthesisAPI.Modules.Attributes;
+using SynthesisAPI.Runtime;
+using System.Net;
+
+namespace Controller
+{
+    [ModuleExport]
+    public class Server : SystemBase
+    {
+        HttpListener listener = new HttpListener();
+        public override void Setup()
+        {
+            listener.Prefixes.Add("http://localhost:5000/"); // Must add prefixes
+            listener.Start();
+            Test();
+        }
+        public override void OnUpdate() { }
+        public override void OnPhysicsUpdate() { }
+
+        private async void Test()
+        {
+            ApiProvider.Log("Server: Waiting for connection");
+            HttpListenerContext context = await listener.GetContextAsync();
+            // HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes("Hello world!");
+            response.ContentLength64 = buffer.Length;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close(); // Must close output stream
+            listener.Stop();
+        }
+    }
+}
