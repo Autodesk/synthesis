bd4b955b7bcc46837a6fb2671ed635d5a9f007fd
Logan Traffas <logan.traffas@gmail.com>
2020-07-31 12:13:17 -0700

    Create a very inconsistently buggy Java Controller interface project

---
+++ modules/Controller/lib/cpp/controller_sys/src/controller_sys_jni.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <jni.h>
+
+#include "controller_sys.hpp"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT jint JNICALL Java_ControllerSys_Test(JNIEnv *env, jclass, jint value, jobject obj){
+	int error_code = 0;
+	const char* error_message = NULL;
+	const char* error_data = NULL;
+
+	int ret = Test(value, &error_code, &error_message, &error_data);
+
+	jclass myclass = env->FindClass("ControllerError");
+
+	jfieldID  fid = env->GetFieldID(myclass,"error_code","I");
+	env->SetIntField(obj ,fid, error_code);
+
+	if(error_message != NULL){
+		fid = env->GetFieldID(myclass,"error_message","Ljava/lang/String;");
+		env->SetObjectField(obj ,fid, env->NewStringUTF(error_message));
+	}
+	if(error_data != NULL){
+		fid = env->GetFieldID(myclass,"error_data","Ljava/lang/String;");
+		env->SetObjectField(obj ,fid, env->NewStringUTF(error_data));
+	}
+
+	return ret;
+}
+
+#ifdef __cplusplus
+}
+#endif
