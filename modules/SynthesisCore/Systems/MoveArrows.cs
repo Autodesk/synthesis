62f51d686e1dfdad04214d221d09101899ea621e
Logan Traffas <logan.traffas@gmail.com>
2020-08-13 11:52:20 -0700

    Move custom shader so it is available in Unity builds

--- modules/SynthesisCore/Systems/MoveArrows.cs
+++ modules/SynthesisCore/Systems/MoveArrows.cs
@@ -8,6 +8,9 @@ using SynthesisAPI.Utilities;
 
 namespace SynthesisCore.Systems
 {
+    /// <summary>
+    /// System that controls arrows to move entities around
+    /// </summary>
     [ModuleExport]
     public class MoveArrows : SystemBase
     {
@@ -16,10 +19,13 @@ namespace SynthesisCore.Systems
         private static Entity? targetEntity = null;
         private static Transform targetTransform = null;
 
-        private static Arrow[] arrows = new Arrow[3];
+        private static readonly Arrow[] arrows = new Arrow[3];
         private static UnitVector3D? selectedArrowDirection = null;
-        private static bool isMovingEntity => targetEntity != null;
+        private static bool IsMovingEntity => targetEntity != null;
 
+        /// <summary>
+        /// Represents an arrow for movement along an axis
+        /// </summary>
         public class Arrow
         {
             public Entity ArrowEntity { get; private set; }
@@ -27,12 +33,10 @@ namespace SynthesisCore.Systems
             public UnitVector3D Direction { get; private set; }
 
             private Entity arrowSpriteEntity;
-            private MeshCollider2D collider;
+            private readonly MeshCollider2D collider;
             private bool hasSetSpritePivot = false;
-            private Sprite sprite;
+            private readonly Sprite sprite;
 
-            private const double sizeEpsilon = 0.001;
-            private double lastSize = 0;
             private bool hasUpdatedScalingOnce = false;
 
             public Arrow(UnitVector3D direction)
@@ -86,6 +90,7 @@ namespace SynthesisCore.Systems
 
             public void UpdateSpritePivot()
             {
+                // Move sprite pivot point from center of image to base of arrow
                 sprite.Visible = hasSetSpritePivot && hasUpdatedScalingOnce;
                 if (!hasSetSpritePivot && collider.Bounds.Extents.Y != 0)
                 {
@@ -97,14 +102,11 @@ namespace SynthesisCore.Systems
 
             public void UpdateScaling()
             {
+                // Update size of arrows so they always look the same size as they move
                 var vectorToCamera = CameraController.Instance.cameraTransform.Position - targetTransform.Position;
 
                 var size = vectorToCamera.Length * 0.01;
-                if (System.Math.Abs(size - lastSize) > sizeEpsilon)
-                {
-                    arrowsTransform.Scale = new Vector3D(size, size, size);
-                    lastSize = size;
-                }
+                arrowsTransform.Scale = new Vector3D(size, size, size);
 
                 hasUpdatedScalingOnce = true;
             }
@@ -114,10 +116,11 @@ namespace SynthesisCore.Systems
 
         public override void OnUpdate()
         {
-            if (isMovingEntity)
+            if (IsMovingEntity)
             {
                 foreach (var arrow in arrows)
                 {
+                    // Make arrow face camera
                     var forward = CameraController.Instance.cameraTransform.Position - targetTransform.Position;
                     forward -= forward.ProjectOn(arrow.Direction);
                     arrow.Transform.Rotation = MathUtil.LookAt(forward.Normalize(), arrow.Direction);
@@ -133,6 +136,10 @@ namespace SynthesisCore.Systems
 
         public override void Teardown() { }
 
+        /// <summary>
+        /// Add move arrows to an entity
+        /// </summary>
+        /// <param name="entity"></param>
         public static void MoveEntity(Entity entity)
         {
             if (entity.GetComponent<Transform>() == null)
@@ -140,7 +147,7 @@ namespace SynthesisCore.Systems
                 Logger.Log("Move arrows cannot move an entity that has no transform", LogLevel.Error);
                 return;
             }
-            if (!isMovingEntity)
+            if (!IsMovingEntity)
             {
                 // Create arrows
                 arrowsEntity = EnvironmentManager.AddEntity();
@@ -155,15 +162,21 @@ namespace SynthesisCore.Systems
             targetTransform = targetEntity?.GetComponent<Transform>();
         }
 
+        /// <summary>
+        /// Remove move arrows from an entity
+        /// </summary>
         public static void StopMovingEntity()
         {
-            if (isMovingEntity)
+            if (IsMovingEntity)
             {
                 EnvironmentManager.RemoveEntity(arrowsEntity);
                 targetEntity = null;
             }
         }
 
+        /// <summary>
+        /// Use mouse input to move the entity
+        /// </summary>
         private void MoveEntityTransform()
         {
             if (selectedArrowDirection.HasValue)
@@ -173,7 +186,6 @@ namespace SynthesisCore.Systems
 
                 if (xMod != 0 || yMod != 0)
                 {
-
                     var magnitude = (System.Math.Abs(xMod) + System.Math.Abs(yMod)) * 0.2;
 
                     var horizontalDir = UnitVector3D.YAxis.CrossProduct(CameraController.Instance.cameraTransform.Forward); // Side to side direction of mouse movement
