25b1922434cfd266aaf3f9744cc85660c8f0129a
Logan Traffas <logan.traffas@gmail.com>
2020-08-14 10:18:15 -0700

    Remove duplicate Toolbar files from module
    They had naming conflicts with those in engine, and they weren't being used

--- api/Api/AssetManager/UssAsset.cs
+++ api/Api/AssetManager/UssAsset.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using SynthesisAPI.UIManager;
+using SynthesisAPI.Utilities;
 using SynthesisAPI.VirtualFileSystem;
 
 namespace SynthesisAPI.AssetManager
@@ -19,7 +21,14 @@ namespace SynthesisAPI.AssetManager
         public override IEntry Load(byte[] data)
         {
             string[] contents = Encoding.UTF8.GetString(data).Split('\n');
-            StyleSheet = new StyleSheet(contents);
+            try
+            {
+                StyleSheet = new StyleSheet(contents);
+            }
+            catch(Exception e)
+            {
+                Logger.Log($"Failed to load stylesheet {Name}\n{e}", LogLevel.Error);
+            }
             return this;
         }
     }
