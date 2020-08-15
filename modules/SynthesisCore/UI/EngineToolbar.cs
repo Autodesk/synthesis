640b3f9c434c2e44dd8149b6f13ff8cc020f6f28
nicolasespinoza <31714622+nicolasespinoza@users.noreply.github.com>
2020-08-14 18:43:30 -0700

    Add base implementation for adding to Environments & Entities lists

--- modules/SynthesisCore/UI/EngineToolbar.cs
+++ modules/SynthesisCore/UI/EngineToolbar.cs
@@ -15,23 +15,91 @@ namespace SynthesisCore.UI
             if (toolbarCreated)
                 return;
 
+            var environmentAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Environment.uxml");
+            var environmentsAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Environments.uxml");
+            var entityAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Entity.uxml");
+            var entitiesAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Entities.uxml");
+
+            Panel entitiesWindow = new Panel("Entities", entitiesAsset,
+                element =>
+                {
+                    Utilities.RegisterOKCloseButtons(element, "Entities");
+                    AddEntityToList(entityAsset.GetElement("entity"), element, "Test-Entity", "10d");
+                });
+            
+            UIManager.AddPanel(entitiesWindow);
+            
+            Panel environmentsWindow = new Panel("Environments", environmentsAsset,
+                element =>
+                {
+                    Utilities.RegisterOKCloseButtons(element, "Environments");
+                    AddEnvironmentToList(environmentAsset.GetElement("environment"), element, "Test-Environment", "3d");
+                });
+            
+            UIManager.AddPanel(environmentsWindow);
+            
             var engineTab = new Tab("Engine", Ui.ToolbarAsset, toolbarElement => {
                 var designCategory = ToolbarTools.AddButtonCategory(toolbarElement, "ENTITIES");
                 ToolbarTools.AddButton(designCategory, "add-entity-button", "/modules/synthesis_core/UI/images/add-icon.png",
-                    _ => Logger.Log("TODO"));
+                    _ => UIManager.TogglePanel("Entities"));
                 ToolbarTools.AddButton(designCategory, "change-environment-button", "/modules/synthesis_core/UI/images/environments-icon.png",
                     _ => UIManager.TogglePanel("Environments"));
             });
+            
             UIManager.AddTab(engineTab);
             UIManager.SetDefaultTab(engineTab.Name);
-
-            var environmentsAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Environments.uxml");
-            Panel environmentsWindow = new Panel("Environments", environmentsAsset,
-                element => Utilities.RegisterOKCloseButtons(element, "Environments"));
-
-            UIManager.AddPanel(environmentsWindow);
-
+            
             toolbarCreated = true;
         }
+        
+        private void LoadEntitiesWindowContent(VisualElement entitiesWindow)
+        {
+            var entityAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Entity.uxml");
+
+            // foreach (/* loop through files stored for entities, waiting on File Browser */)
+            // {
+            //     VisualElement entityElement = entityAsset.GetElement("entity");
+            //
+            //     Label nameLabel = (Label) entityElement.Get("name");
+            //     Label lastModifiedLabel = (Label) entityElement.Get("last-modified-date");
+            //
+            //     nameLabel.Text = nameLabel.Text.Replace("%name%", /* Get file name */);
+            //     lastModifiedLabel.Text = lastModifiedLabel.Text.Replace("%time%", /* Get last modified */);
+            //
+            //     ListView entityList = (ListView) entitiesWindow.Get("entity-list");
+            //     entityList.Add(entityList);
+            // }
+        }
+        
+        private void LoadEnvironmentsWindowContent(VisualElement environmentsWindow)
+        {
+            
+        }
+
+        private static void AddEntityToList(VisualElement entityElement, VisualElement entitiesWindow, string name, string lastModified)
+        {
+            Label nameLabel = (Label) entityElement?.Get("name");
+            Label lastModifiedLabel = (Label) entityElement?.Get("last-modified-date");
+            
+            nameLabel.Text = nameLabel.Text.Replace("%name%", name);
+            lastModifiedLabel.Text = lastModifiedLabel.Text.Replace("%time%", lastModified);
+            
+            ListView entityList = (ListView) entitiesWindow.Get("entity-list");
+            entityList.Add(entityElement);
+        }
+
+        private static void AddEnvironmentToList(VisualElement environmentElement, VisualElement environmentsWindow, string name, string lastModified)
+        {
+            Label nameLabel = (Label) environmentElement.Get("name");
+            Label lastModifiedLabel = (Label) environmentElement.Get("last-modified-date");
+            
+            nameLabel.Text = nameLabel.Text.Replace("%name%", name);
+            lastModifiedLabel.Text = lastModifiedLabel.Text.Replace("%time%", lastModified);
+            
+            ListView environmentList = (ListView) environmentsWindow.Get("environment-list");
+            environmentList.Add(environmentElement);
+        }
+        
     }
+
 }
