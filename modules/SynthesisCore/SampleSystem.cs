9af092fa65733b5ea52eb84e0a82b209cacc2373
Kira Corbett <kirac2093@gmail.com>
2020-08-19 14:52:41 -0700

    Module Dict for joints

--- modules/SynthesisCore/SampleSystem.cs
+++ modules/SynthesisCore/SampleSystem.cs
@@ -15,8 +15,6 @@ namespace SynthesisCore
 {
     public class SampleSystem : SystemBase
     {
-        private Entity testBody;
-
         private MotorController frontLeft, frontRight, backLeft, backRight;
         private MotorController arm;
 
@@ -26,10 +24,12 @@ namespace SynthesisCore
 
         public override void Setup()
         {
-            testBody = EnvironmentManager.AddEntity();
+            Entity testBody = EnvironmentManager.AddEntity();
             GltfAsset g = AssetManager.GetAsset<GltfAsset>("/modules/synthesis_core/Test.glb");
             Bundle o = g.Parse();
             testBody.AddBundle(o);
+            SynthesisCoreData.ModelsDict.Add("TestBody", testBody);
+            var body = testBody.GetComponent<Joints>().AllJoints;
 
             (float r, float g, float b, float a)[] colors = {
                 (1, 0, 0, 1),
@@ -82,6 +82,18 @@ namespace SynthesisCore
 
             InputManager.AssignAxis("vert", new Analog("Vertical"));
             InputManager.AssignAxis("hori", new Analog("Horizontal"));
+
+            Entity jointTest = EnvironmentManager.AddEntity();
+            Bundle b = new Bundle();
+            Selectable selectable = new Selectable();
+            b.Components.Add(selectable);
+            Transform t = new Transform();
+            t.Position = new Vector3D(10, 10, 10); //joint anchor position
+            b.Components.Add(t);
+            b.Components.Add(sphere()); //replace the cube function with your mesh creation
+            jointTest.AddBundle(b);
+            //when done
+            jointTest.RemoveEntity();
         }
 
         public override void OnUpdate() {
@@ -103,6 +115,40 @@ namespace SynthesisCore
         }
 
         private Mesh cube()
+        {
+            if (m == null)
+                m = new Mesh();
+
+            m.Vertices = new List<Vector3D>()
+            {
+                new Vector3D(-0.5,-0.5,-0.5),
+                new Vector3D(0.5,-0.5,-0.5),
+                new Vector3D(0.5,0.5,-0.5),
+                new Vector3D(-0.5,0.5,-0.5),
+                new Vector3D(-0.5,0.5,0.5),
+                new Vector3D(0.5,0.5,0.5),
+                new Vector3D(0.5,-0.5,0.5),
+                new Vector3D(-0.5,-0.5,0.5)
+            };
+            m.Triangles = new List<int>()
+            {
+                0, 2, 1, //face front
+			    0, 3, 2,
+                2, 3, 4, //face top
+			    2, 4, 5,
+                1, 2, 5, //face right
+			    1, 5, 6,
+                0, 7, 4, //face left
+			    0, 4, 3,
+                5, 4, 7, //face back
+			    5, 7, 6,
+                0, 6, 7, //face bottom
+			    0, 1, 6
+            };
+            return m;
+        }
+
+        private Mesh sphere()
         {
             if (m == null)
                 m = new Mesh();
--- modules/SynthesisCore/UI/EngineToolbar.cs
+++ modules/SynthesisCore/UI/EngineToolbar.cs
@@ -1,4 +1,5 @@
-﻿using SynthesisAPI.AssetManager;
+﻿using MathNet.Spatial.Euclidean;
+using SynthesisAPI.AssetManager;
 using SynthesisAPI.EnvironmentManager;
 using SynthesisAPI.EnvironmentManager.Components;
 using SynthesisAPI.UIManager;
@@ -6,6 +7,7 @@ using SynthesisAPI.UIManager.UIComponents;
 using SynthesisAPI.UIManager.VisualElements;
 using SynthesisAPI.Utilities;
 using SynthesisCore.Systems;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices.ComTypes;
 
@@ -61,12 +63,20 @@ namespace SynthesisCore.UI
         {
             var jointAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Joint.uxml");
 
-            foreach (var j in Joints.GlobalAllJoints)
+            foreach (var entity in SynthesisCoreData.ModelsDict.Values)
             {
-                VisualElement jointElement = jointAsset?.GetElement("joint");
-                ListView jointList = (ListView)visualElement.Get("joint-list");
-                jointList.Add(jointElement);
+                if (entity.GetComponent<Joints>() != null)
+                {
+                    foreach (var joint in entity.GetComponent<Joints>().AllJoints)
+                    {
+                        VisualElement jointElement = jointAsset?.GetElement("joint");
+                        ListView jointList = (ListView)visualElement.Get("joint-list");
 
+                        HighlightJoint();
+
+                        jointList.Add(jointElement);
+                    }
+                }
             }
             //    //if (j.GetType() == typeof(HingeJoint))
             //    //{
@@ -97,6 +107,58 @@ namespace SynthesisCore.UI
             //        ListView jointList = (ListView)visualElement.Get("joint-list");
             //        jointList.Add(jointElement);
             //    //}
+        }
+
+        private void HighlightJoint()
+        {
+            Entity jointTest = EnvironmentManager.AddEntity();
+            Bundle b = new Bundle();
+
+            Selectable selectable = new Selectable();
+            b.Components.Add(selectable);
+
+            Transform t = new Transform();
+            t.Position = new Vector3D(10, 10, 10); //joint anchor position
+            b.Components.Add(t);
+            b.Components.Add(cube()); //replace the cube function with your mesh creation
+            jointTest.AddBundle(b);
+            //when done
+            jointTest.RemoveEntity();
+        }
+
+        private Mesh cube()
+        {
+            Mesh m = new Mesh();
+            if (m == null)
+                m = new Mesh();
+
+            m.Vertices = new List<Vector3D>()
+            {
+                new Vector3D(-0.5,-0.5,-0.5),
+                new Vector3D(0.5,-0.5,-0.5),
+                new Vector3D(0.5,0.5,-0.5),
+                new Vector3D(-0.5,0.5,-0.5),
+                new Vector3D(-0.5,0.5,0.5),
+                new Vector3D(0.5,0.5,0.5),
+                new Vector3D(0.5,-0.5,0.5),
+                new Vector3D(-0.5,-0.5,0.5)
+            };
+            m.Triangles = new List<int>()
+            {
+                0, 2, 1, //face front
+			    0, 3, 2,
+                2, 3, 4, //face top
+			    2, 4, 5,
+                1, 2, 5, //face right
+			    1, 5, 6,
+                0, 7, 4, //face left
+			    0, 4, 3,
+                5, 4, 7, //face back
+			    5, 7, 6,
+                0, 6, 7, //face bottom
+			    0, 1, 6
+            };
+            return m;
         }
     }
 }
