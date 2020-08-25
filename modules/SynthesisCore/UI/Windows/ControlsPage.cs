409cb5fce006570954d498ad4a21a7aa2068918a
nicolasespinoza <31714622+nicolasespinoza@users.noreply.github.com>
2020-08-25 13:39:35 -0700

    Updated Controls page

--- modules/SynthesisCore/UI/Windows/ControlsPage.cs
+++ modules/SynthesisCore/UI/Windows/ControlsPage.cs
@@ -1,4 +1,6 @@
-﻿using SynthesisAPI.AssetManager;
+﻿using System.Collections.Generic;
+using SynthesisAPI.AssetManager;
+using SynthesisAPI.PreferenceManager;
 using SynthesisAPI.UIManager.VisualElements;
 
 namespace SynthesisCore.UI
@@ -17,27 +19,40 @@ namespace SynthesisCore.UI
             ControlAsset = AssetManager.GetAsset<VisualElementAsset>("/modules/synthesis_core/UI/uxml/Control.uxml");
             ControlList = (ListView) ControlsElement.Get("controls");
 
+            LoadPreferences();
             LoadPageContent();
         }
 
         private void LoadPageContent()
         {
-            // load from preferencemanager (?)
-            AddControlToList("Camera Forward", "W");
-            AddControlToList("Camera Backward", "S");
-            AddControlToList("Camera Left", "A");
-            AddControlToList("Camera Right", "D");
-            AddControlToList("Camera Up", "Space");
-            AddControlToList("Camera Down", "Left Shift");
-            AddControlToList("Entity Forward", "Up Arrow");
-            AddControlToList("Entity Backward", "Down Arrow");
-            AddControlToList("Entity Left", "Left Arrow");
-            AddControlToList("Entity Right", "Right Arrow");
+            AddControlToList("Camera Forward", GetPreference("Camera Forward"));
+            AddControlToList("Camera Backward", GetPreference("Camera Backward"));
+            AddControlToList("Camera Left", GetPreference("Camera Left"));
+            AddControlToList("Camera Right", GetPreference("Camera Right"));
+            AddControlToList("Camera Up", GetPreference("Camera Up"));
+            AddControlToList("Camera Down", GetPreference("Camera Down"));
+            AddControlToList("Entity Forward", GetPreference("Entity Forward"));
+            AddControlToList("Entity Backward", GetPreference("Entity Backward"));
+            AddControlToList("Entity Left", GetPreference("Entity Left"));
+            AddControlToList("Entity Right", GetPreference("Entity Right"));
         }
 
         private void LoadPreferences()
         {
-            
+            Dictionary<string, string> defaultControls = new Dictionary<string, string>
+            {
+                {"Camera Forward", "W"},
+                {"Camera Backward", "S"},
+                {"Camera Left", "A"},
+                {"Camera Up", "Space"},
+                {"Camera Down", "Left Shift"},
+                {"Entity Forward", "Up Arrow"},
+                {"Entity Backward", "Down Arrow"},
+                {"Entity Left", "Left Arrow"},
+                {"Entity Right", "Right Arrow"}
+            };
+
+            LoadOrSetDefaultPreference(defaultControls);
         }
 
         private void AddControlToList(string name, string key)
@@ -45,5 +60,26 @@ namespace SynthesisCore.UI
             ControlList.Add(new ControlItem(ControlAsset, new ControlInfo(name, key)).ControlElement);
         }
 
+        private void LoadOrSetDefaultPreference(Dictionary<string, string> defaultControls)
+        {
+            foreach (string controlName in defaultControls.Keys)
+            {
+                if (PreferenceManager.GetPreference("SynthesisCore", controlName) == null)
+                {
+                    PreferenceManager.SetPreference("SynthesisCore", controlName, defaultControls[controlName]);
+                    PreferenceManager.Save();
+                }
+            }
+        }
+
+        private string GetPreference(string controlName)
+        {
+            var controlKey = PreferenceManager.GetPreference("SynthesisCore", controlName);
+            if (controlKey is string)
+            {
+                return (string) controlKey;
+            }
+            return "Unassigned";
+        }
     }
 }
