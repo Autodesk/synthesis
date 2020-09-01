1296ce4d24bf59ed561443b1fec5d21fcf03fad9
ajaypai <ajaypai@cs.washington.edu>
2020-09-01 14:46:09 -0700

    highlight toggle

--- modules/SynthesisCore/UI/Windows/JointItem.cs
+++ modules/SynthesisCore/UI/Windows/JointItem.cs
@@ -120,41 +120,51 @@ namespace SynthesisCore.UI
 
             highlightButton.Subscribe(x =>
             {
-                UnHighlightAllButtons();
-                HighlightButton(true);
-
-                if (JointsWindow.jointHighlightEntity == null)
+                //toggle highlight
+                if (!IsHighlighted)
                 {
-                    JointsWindow.jointHighlightEntity = EnvironmentManager.AddEntity();
-                    JointsWindow.jointHighlightEntity?.AddComponent<Transform>();
-                    JointsWindow.jointHighlightEntity?.AddComponent<Mesh>();
-                    
-                    // Create mesh
-                    Mesh m = JointsWindow.jointHighlightEntity?.GetComponent<Mesh>();
-                    m.Color = (1f, 1f, 0f, 0.5f);
-                    Cylinder.Make(m, 16, 0.01, 0.25);
+                    UnHighlightAllButtons();
+                    HighlightButton(true);
+
+                    if (JointsWindow.jointHighlightEntity == null)
+                    {
+                        JointsWindow.jointHighlightEntity = EnvironmentManager.AddEntity();
+                        JointsWindow.jointHighlightEntity?.AddComponent<Transform>();
+                        JointsWindow.jointHighlightEntity?.AddComponent<Mesh>();
+
+                        // Create mesh
+                        Mesh m = JointsWindow.jointHighlightEntity?.GetComponent<Mesh>();
+                        m.Color = (1f, 1f, 0f, 0.5f);
+                        Cylinder.Make(m, 16, 0.01, 0.25);
+                    }
+
+                    // Set highlight entity parent
+                    var parentComponent = JointsWindow.jointHighlightEntity?.GetComponent<Parent>();
+                    parentComponent.ParentEntity = jointEntity;
+
+                    // Set highlight entity transform
+                    var jointHighlightTransform = JointsWindow.jointHighlightEntity?.GetComponent<Transform>();
+                    jointHighlightTransform.Position = j.Anchor;
+
+                    var axis = j.Axis.Normalize();
+                    jointHighlightTransform.Rotation = MathUtil.LookAt(
+                        axis.IsParallelTo(UnitVector3D.XAxis) ? axis.CrossProduct(UnitVector3D.YAxis) : axis.CrossProduct(UnitVector3D.XAxis),
+                        axis);
+
+                    // Move camera to look at the joint
+                    var parentPosition = jointEntity.GetComponent<Parent>()?.ParentEntity.GetComponent<Transform>()?.GlobalPosition ?? new Vector3D();
+                    var cameraOffset = (jointHighlightTransform.GlobalPosition - parentPosition).Normalize().ScaleBy(3);
+                    CameraController.Instance.cameraTransform.GlobalPosition = jointHighlightTransform.GlobalPosition + cameraOffset;
+
+                    CameraController.Instance.cameraTransform.LookAt(jointHighlightTransform.GlobalPosition);
+                    CameraController.Instance.SetNewFocus(jointHighlightTransform.GlobalPosition, false);
                 }
-
-                // Set highlight entity parent
-                var parentComponent = JointsWindow.jointHighlightEntity?.GetComponent<Parent>();
-                parentComponent.ParentEntity = jointEntity;
-
-                // Set highlight entity transform
-                var jointHighlightTransform = JointsWindow.jointHighlightEntity?.GetComponent<Transform>();
-                jointHighlightTransform.Position = j.Anchor;
-                
-                var axis = j.Axis.Normalize();
-                jointHighlightTransform.Rotation = MathUtil.LookAt(
-                    axis.IsParallelTo(UnitVector3D.XAxis) ? axis.CrossProduct(UnitVector3D.YAxis) : axis.CrossProduct(UnitVector3D.XAxis), 
-                    axis);
-
-                // Move camera to look at the joint
-                var parentPosition = jointEntity.GetComponent<Parent>()?.ParentEntity.GetComponent<Transform>()?.GlobalPosition ?? new Vector3D();
-                var cameraOffset = (jointHighlightTransform.GlobalPosition - parentPosition).Normalize().ScaleBy(3);
-                CameraController.Instance.cameraTransform.GlobalPosition = jointHighlightTransform.GlobalPosition + cameraOffset;
-                
-                CameraController.Instance.cameraTransform.LookAt(jointHighlightTransform.GlobalPosition);
-                CameraController.Instance.SetNewFocus(jointHighlightTransform.GlobalPosition, false);
+                else
+                {
+                    UnHighlightButton();
+                    if (JointsWindow.jointHighlightEntity?.RemoveEntity() ?? false)
+                        JointsWindow.jointHighlightEntity = null;
+                }
             });
         }
 
