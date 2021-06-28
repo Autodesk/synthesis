9dfd5419e559c439b557fe8fedb09228aa968a43
Aaron Boyd <aaron.boyd.org@gmail.com>
2021-06-28 10:51:20 -0700

    Added Tcp server to deserialize json objects

---
+++ api/Api/Utilities/APIPacket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SynthesisAPI.Utilities
+{
+    public class APIPacket
+    {
+        public string Id { get; set; }
+        public List<DigitalOutput> Data { get; set; }
+
+    }
+
+}
---
+++ api/Api/Utilities/DigitalOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SynthesisAPI.Utilities
+{
+    public class DigitalOutput
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Value { get; set; }
+    }
+}
