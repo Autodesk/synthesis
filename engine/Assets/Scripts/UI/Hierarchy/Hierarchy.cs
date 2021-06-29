76c43543557fbefbb33b59b17382fc4f245c74d1
khuntetad <63430428+khuntetad@users.noreply.github.com>
2021-06-29 16:03:23 -0700

    Updated hierarchy view with correct displays (#719)
    * I added back your test script but you'll have to add all your scene changes back
    
    * hierarchy complete with correct text and folder representations
    
    * resolved hierarchy UI issues
    
    * implemented hierarchy with functional context menu and scroll view
    
    * Fixed things unity keeps breaking
    
    Co-authored-by: Hunter <hunter.barclay18@gmail.com>

--- engine/Assets/Scripts/UI/Hierarchy/Hierarchy.cs
+++ engine/Assets/Scripts/UI/Hierarchy/Hierarchy.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using Synthesis.UI.Hierarchy.HierarchyItems;
 using UnityEngine;
 using Synthesis.UI.ContextMenus;
-using Synthesis.Util;
+using Synthesis.Util; 
 
 using ContextMenu = Synthesis.UI.ContextMenus.ContextMenu;
 
@@ -26,58 +26,29 @@ namespace Synthesis.UI.Hierarchy
         public float TabSize = 20f;
         public float Padding = 2.5f;
 
-        private void Awake() {
+        private void Awake()
+        {
             HierarchyInstance = this;
         }
 
-        public void Start() {
+        public void Start()
+        {
 
             // ContextMenu.Show()
             // ContextMenu.Show(new Vector2(500, -500), "Test Menu", new string[]{"Hello", "There"});
 
             rootFolder.Init("Scene", null);
 
-            var robots = rootFolder.CreateFolder("Robotssssssss");
+            var robots = rootFolder.CreateFolder("Robots");
             var fields = rootFolder.CreateFolder("Fields");
 
-            robots.CreateItem("997 Spartan Robotics");
-            robots.CreateItem("1425 Error Code");
-
             fields.CreateItem("2020 Infinite Recharge");
-
-            /*
-            RootFolder
-                Folder A
-                    Item AA
-                    Folder AA
-                        Item AAA
-                    Item AB
-                Item A
-                Folder B
-                    Item BA
-            */
-
-            // Big Test
-            /*var folderA = RootFolder.CreateFolder("Folder A");
-            var itemA = RootFolder.CreateItem("Item A");
-            var folderB = RootFolder.CreateFolder("Folder B");
-
-            var itemAA = folderA.CreateItem("Item AA");
-            var folderAA = folderA.CreateFolder("Folder AA");
-            var itemAB = folderA.CreateFolder("Item AB");
-
-            var itemAAA = folderAA.CreateItem("Item AAA");
-
-            var itemBA = folderB.CreateItem("Item BA");*/
-
-            // folderB.Remove();
-            // folderAA.Add(folderB);
-
-            // RootFolder.DebugPrint();
         }
 
-        public void Update() {
-            if (Changes) {
+        public void Update()
+        {
+            if (Changes)
+            {
                 Canvas.ForceUpdateCanvases();
                 Changes = false;
 
@@ -87,8 +58,10 @@ namespace Synthesis.UI.Hierarchy
                 t.offsetMax = new Vector2((horizontalPadding * 2) + childWidth, t.offsetMax.y);
 
                 float heightAccum = t.rect.height + Padding;
-                for (int i = 0; i < RootFolder.Items.Count; i++) {
-                    if (RootFolder.Items[i].item.Visible) {
+                for (int i = 0; i < RootFolder.Items.Count; i++)
+                {
+                    if (RootFolder.Items[i].item.Visible)
+                    {
                         float tabSize = RootFolder.Items[i].item.Depth * TabSize;
                         t = RootFolder.Items[i].item.GetComponent<RectTransform>();
 
@@ -96,8 +69,8 @@ namespace Synthesis.UI.Hierarchy
                         // t.localPosition = new Vector3(t.localPosition.x, -heightAccum, t.localPosition.z);
 
                         // t.offsetMin = new Vector2(tabSize, t.offsetMin.y);
-                        
-                        
+
+
                         // childWidth = t.transform.GetChild(0).GetComponent<RectTransform>().offsetMax.x;
                         childWidth = t.transform.GetChild(0).GetComponent<TMPro.TMP_Text>().bounds.max.x - t.transform.GetChild(0).GetComponent<TMPro.TMP_Text>().bounds.min.x;
                         horizontalPadding = t.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition.x; // TODO
@@ -107,6 +80,8 @@ namespace Synthesis.UI.Hierarchy
                         heightAccum += t.rect.height + Padding;
                     }
                 }
+                var contentRect = contentContainer.GetComponent<RectTransform>();
+                contentRect.sizeDelta = new Vector2(contentRect.sizeDelta.x, heightAccum);
             }
         }
     }
