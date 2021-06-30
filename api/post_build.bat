635e4a0f43646d477039b35b683af94d004cd7d4
Hunter <hunter.barclay18@gmail.com>
2021-06-30 14:36:27 -0700

    Refactored Aardvark from .NETFramework 3.5 to .NETStandard 2.0. Fixed post_build script, and removed unity newtonsoft package and replaced it with a nuget newtonsoft package

--- api/post_build.bat
+++ api/post_build.bat
@@ -1,5 +1,5 @@
 @echo off
 copy Api\\bin\\Debug\\netstandard2.0\\Api.dll ..\\engine\\Assets\\Packages\\Api.dll
 del /f ..\\engine\\Assets\\Packages\\Api.dll.meta
-copy Api\\bin\\Debug\\netstandard2.0\\SimulatorAPI.dll ..\\engine\\Assets\\Packages\\SimulatorAPI.dll
-del /f ..\\engine\\Assets\\Packages\\SimulatorAPI.dll.meta
+copy Api\\bin\\Debug\\netstandard2.0\\Aardvark.dll ..\\engine\\Assets\\Packages\\Aardvark.dll
+del /f ..\\engine\\Assets\\Packages\\Aardvark.dll.meta
