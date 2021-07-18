bc818eb700809d389f9a34f2faa138823bc3aa11
Aaron Boyd <aaron.boyd.org@gmail.com>
2021-07-17 20:32:31 -0700

    Added Controllable State

---
+++ api/Api/Utilities/ControllableState.cs
@@ -0,0 +1,90 @@
+﻿using SynthesisAPI.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Utilities
+{
+    class ControllableState
+    {
+        
+        public Layout? currentLayout { get; set; }
+        public Dictionary<string, DigitalOutput> DOs { get; set; }
+        public Dictionary<string, DigitalInput> DIs { get; set; }
+        public Dictionary<string, AnalogOutput> AOs { get; set; }
+        public Dictionary<string, AnalogInput> AIs { get; set; }
+
+        public void SetLayout(Layout layout)
+        {
+            currentLayout = layout;
+            DOs.Clear();
+            DIs.Clear();
+            AOs.Clear();
+            AIs.Clear();
+            foreach (var entry in layout.Fields.DOEntries)
+            {
+                DOs[entry.Key] = new DigitalOutput()
+                {
+                    Name = entry.Value.Name,
+                    Type = entry.Value.Type
+                };
+            }
+            foreach (var entry in layout.Fields.DIEntries)
+            {
+                DIs[entry.Key] = new DigitalInput()
+                {
+                    Name = entry.Value.Name,
+                    Type = entry.Value.Type
+                };
+            }
+            foreach (var entry in layout.Fields.AOEntries)
+            {
+                AOs[entry.Key] = new AnalogOutput()
+                {
+                    Name = entry.Value.Name,
+                    Type = entry.Value.Type
+                };
+            }
+            foreach (var entry in layout.Fields.AIEntries)
+            {
+                AIs[entry.Key] = new AnalogInput()
+                {
+                    Name = entry.Value.Name,
+                    Type = entry.Value.Type
+                };
+            }
+        }
+
+        public class DigitalOutput
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public double Value { get; set; }
+        }
+
+        public class DigitalInput
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public double Value { get; set; }
+        }
+
+        
+        public class AnalogOutput
+        {
+            
+            public string Name { get; set; }
+            
+            public string Type { get; set; }
+            
+            public double Value { get; set; }
+        }
+
+        public class AnalogInput
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public double Value { get; set; }
+        }
+    }
+}
---
+++ api/Api/Utilities/Layout.cs
@@ -0,0 +1,68 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SynthesisAPI.Utilities.Messages
+{
+    [ProtoContract]
+    public class Layout
+    {
+        [ProtoMember(1)]
+        public string Id { get; set; }
+
+        [ProtoMember(2)]
+        public ModifiedFields Fields { get; set; }
+
+        [ProtoContract]
+        public class ModifiedFields
+        {
+            [ProtoMember(1)]
+            public Dictionary<string, DigitalOutput> DOEntries { get; set; }
+            [ProtoMember(2)]
+            public Dictionary<string, DigitalInput> DIEntries { get; set; }
+            [ProtoMember(3)]
+            public Dictionary<string, AnalogOutput> AOEntries { get; set; }
+            [ProtoMember(4)]
+            public Dictionary<string, AnalogInput> AIEntries { get; set; }
+        }
+    }
+
+    [ProtoContract]
+    public class DigitalOutputEntry
+    {
+        [ProtoMember(1)]
+        public string Name { get; set; }
+        [ProtoMember(2)]
+        public string Type { get; set; }
+    }
+
+    [ProtoContract]
+    public class DigitalInputEntry
+    {
+        [ProtoMember(1)]
+        public string Name { get; set; }
+        [ProtoMember(2)]
+        public string Type { get; set; }
+    }
+
+    [ProtoContract]
+    public class AnalogOutputEntry
+    {
+        [ProtoMember(1)]
+        public string Name { get; set; }
+        [ProtoMember(2)]
+        public string Type { get; set; }
+    }
+
+    [ProtoContract]
+    public class AnalogInputEntry
+    {
+        [ProtoMember(1)]
+        public string Name { get; set; }
+        [ProtoMember(2)]
+        public string Type { get; set; }
+    }
+
+}
