295da0f08db015bd02893edd5383b3f4dd194b2f
Aaron Boyd <aaron.boyd.org@gmail.com>
2021-07-28 12:22:39 -0700

    Generated Python file

---
+++ engine/Assets/Scripts/SendData.py
@@ -0,0 +1,10 @@
+﻿import socket
+import sys
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 13000)
+
+sock.connect(server_address)
+
+sock.sendall(message)
