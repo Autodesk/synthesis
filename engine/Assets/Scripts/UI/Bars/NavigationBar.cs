aa68c07cbc1f9eb5065dc9c233a71e82a14dce43
victorxheng <victorchengthesecond@gmail.com>
2021-07-28 16:43:14 -0700

    The new UI look completed

---
+++ engine/Assets/Scripts/UI/Bars/NavigationBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationBar : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    //tab switching and instantiation
+    //have instantiation process be inside of the panel manager, which needs to be renamed
+
+    //panel manager calls
+
+
+}
