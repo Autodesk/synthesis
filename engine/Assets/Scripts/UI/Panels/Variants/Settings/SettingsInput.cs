ce474e7ba74dee0290ac06a38481bd33b820796f
victorxheng <victorchengthesecond@gmail.com>
2021-08-02 17:07:37 -0700

    Integrated Preference Manager to Load and Save setting states

--- engine/Assets/Scripts/UI/Panels/Variants/Settings/SettingsInput.cs
+++ engine/Assets/Scripts/UI/Panels/Variants/Settings/SettingsInput.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class SettingsInput : MonoBehaviour
 {
-    SettingsPanel.inputType type;//type
+    SettingsPanel.InputType type;//type
 
 
     public TextMeshProUGUI _name;//shared
@@ -21,26 +21,29 @@ public class SettingsInput : MonoBehaviour
     //dropdown list
     public TMP_Dropdown _dropdown;
 
+    public Slider _slider;
+    public TextMeshProUGUI _sliderValue;
+
 
     public void Init(string name, bool state)//TOGGLE
     {
-        type = SettingsPanel.inputType.toggle;
+        type = SettingsPanel.InputType.TOGGLE;
         _name.text = name;
         _toggle.isOn = state;
     }
-    public SettingsPanel.inputType getType()
+    public SettingsPanel.InputType getType()
     {
         return type;
     }
     public void Init(string name, string control)//KEYBIND
     {
-        type = SettingsPanel.inputType.keybind;
+        type = SettingsPanel.InputType.KEYBIND;
         _name.text = name;
         _key.text = control;
     }
     public void Init(string name, List<string> dropdownList, int value)//DROPDOWN LIST
     {
-        type = SettingsPanel.inputType.dropdown;
+        type = SettingsPanel.InputType.DROPDOWN;
         _name.text = name;
         foreach (string c in dropdownList)
         {
@@ -48,13 +51,25 @@ public class SettingsInput : MonoBehaviour
         }
         _dropdown.value = value;//CURRENT STATE
     }
+    public void Init(string name, float lowValue, float highValue, float value)//SLIDER
+    {
+        type = SettingsPanel.InputType.SLIDER;
+        _name.text = name;
+        _slider.minValue = lowValue;
+        _slider.maxValue = highValue;
+        _slider.value = value;        //Make sure it is formatted correctly
+        sliderValueChanged();
+    }
+    public void sliderValueChanged(){
+        _sliderValue.text = _slider.value.ToString();
+    }
     public void setKeybind() //CALLED BY KEYBIND
     {
         bind = true;
     }
     private void OnGUI()
     {
-        if (type== SettingsPanel.inputType.keybind&&bind) //SET KEYBIND
+        if (type== SettingsPanel.InputType.KEYBIND&&bind) //SET KEYBIND
         {
             Event e = Event.current;
             if (e.isKey)
@@ -64,5 +79,7 @@ public class SettingsInput : MonoBehaviour
             }
         }
     }   
+
+    //ON CHANGE FUNCTION
     
 }
--- engine/Assets/Scripts/UI/Panels/Variants/Settings/SettingsPanel.cs
+++ engine/Assets/Scripts/UI/Panels/Variants/Settings/SettingsPanel.cs
@@ -1,7 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TMPro;
+using Synthesis.PreferenceManager;
+public static class Preference{
+    public const string RESOLUTION = "Resolution";//Dropdown: Different resolution settings
+    public static List<string> ResolutionList = new List<string>{"1920x1080"};    
+    public const string SCREEN_MODE = "Screen Mode";//Dropdown: Fullscreen or Windowed
+    public static List<string> ScreenModeList = new List<string>{"Fullscreen","Windowed"}; //OLD SYNTHESIS SUCKS AT THIS   
+    public const string QUALITY_SETTINGS = " Quality Settings";//Dropdown: Low Medium High
+    public static List<string> QualitySettingsList = new List<string>{"High","Medium","Low"};    
+    public const string ALLOW_DATA_GATHERING = "Allow Data Gathering";//Toggle
+    public const string MEASUREMENTS = "Measurements";//Dropdown: Metric or Imperial
+    public static List<string> MeasurementsList = new List<string>{"Imperial","Metric"};   
+    public const string ZOOM_SENSITIVITY = "Zoom Sensitivity";
+
+
+
+
+    //sliders for camera controller
+
+    //sliders
+
+
+    public static void LoadSettings(){
+            PreferenceManager.Load();
+            if(PreferenceManager.GetPreference(Preference.RESOLUTION)==null){//checks if preferences are initialized with default values
+                setDefaultPreferences();
+            }
+
+            //implement settings by going through each, loading, and setting
+    }
+    public static void setDefaultPreferences(){
+        PreferenceManager.SetPreference(Preference.RESOLUTION,(int)0);
+        PreferenceManager.SetPreference(Preference.SCREEN_MODE,(int)0);
+        PreferenceManager.SetPreference(Preference.QUALITY_SETTINGS,(int)0);
+        PreferenceManager.SetPreference(Preference.ALLOW_DATA_GATHERING,(bool)true);
+        PreferenceManager.SetPreference(Preference.MEASUREMENTS,(int)0);
+        PreferenceManager.SetPreference(Preference.ZOOM_SENSITIVITY,(float)0.52f);
+        PreferenceManager.Save();        
+    }
+}
 
 public class SettingsPanel : MonoBehaviour
 {
@@ -16,66 +56,76 @@ public class SettingsPanel : MonoBehaviour
     public GameObject toggleInput;
     [SerializeField]
     public GameObject titleText;
+    [SerializeField]
+    public GameObject sliderInput;
 
     List<GameObject> settingsList = new List<GameObject>();
 
-    public enum inputType
+    public enum InputType
     {
-        toggle,
-        dropdown,
-        keybind
+        TOGGLE,
+        DROPDOWN,
+        KEYBIND,
+        SLIDER
     }
 
-    // Start is called before the first frame update
+    /*
+    At the start: Load Preference Manager
+    Script that converts preference manager into the proper implemented settings
+
+    When Panel is loaded:
+    Show Settings goes through each preference and gets them and their state, creates the parts
+
+    When save button is pressed: save changes, then call the load function to apply them
+
+    When close is pressed with unsaved changes: issue warning saying you have unsaved changes    
+
+
+    */
+
+
     void Start()
     {
+        Preference.LoadSettings();
         showSettings();
     }
 
     private void showSettings()
-    {
-        //TEST SCRIPT: ADDS EXAMPLE SETTINGS. Read from preference manager
-        createTitle("Keybinds");
-        createKeybind("Move Forward", "W");
-        createKeybind("Move Backwards", "S");
-        createTitle("Graphics");
-        createToggle("Shaders", true);
-        List<string> resolutionList = new List<string>();
-        resolutionList.Add("Low");
-        resolutionList.Add("Medium");
-        resolutionList.Add("High");
-        createDropdown("Resolution", resolutionList, 0);
+    {        
+        createDropdown(Preference.RESOLUTION, Preference.ResolutionList, Convert.ToInt32(PreferenceManager.GetPreference(Preference.RESOLUTION)));
+        createDropdown(Preference.SCREEN_MODE,Preference.ScreenModeList,Convert.ToInt32(PreferenceManager.GetPreference(Preference.SCREEN_MODE)));
+        createDropdown(Preference.QUALITY_SETTINGS,Preference.QualitySettingsList,Convert.ToInt32(PreferenceManager.GetPreference(Preference.QUALITY_SETTINGS)));
+        createToggle(Preference.ALLOW_DATA_GATHERING,(bool)PreferenceManager.GetPreference(Preference.ALLOW_DATA_GATHERING));
+        createDropdown(Preference.MEASUREMENTS, Preference.MeasurementsList, Convert.ToInt32(PreferenceManager.GetPreference(Preference.MEASUREMENTS)));
+        createSlider(Preference.ZOOM_SENSITIVITY,0.1f,5,Convert.ToSingle(PreferenceManager.GetPreference(Preference.ZOOM_SENSITIVITY)));
     }
+
+    //Depricate it: Have settings saved within each object when changed. Then just call preference manager save on save.
     public void saveSettings()//writes settings back into preference manager when save button is clicked
     {
         foreach(GameObject g in settingsList)
         {
             //MODIFY THIS: Each Input field has different outputs
             SettingsInput si = g.GetComponent<SettingsInput>();
-            string name = si._name.text; //shared
-            bool toggleState;//toggle
-            string key; //for keybind
-            int value; //for dropdown (value gives position in list of the selected setting)
+            string name = si._name.text; //key for preference manager
             
             switch (si.getType())//sets each value based on type. Needs to be changed when writing to preference manager
             {
-                case inputType.toggle:
-                    toggleState = si._toggle.isOn;
+                case InputType.TOGGLE:
+                    PreferenceManager.SetPreference(name,si._toggle.isOn);
                     break;
-                case inputType.dropdown:
-                    value = si._dropdown.value;
+                case InputType.DROPDOWN:
+                    PreferenceManager.SetPreference(name,si._dropdown.value);
                     break;
-                case inputType.keybind:
-                    key = si._key.text;
+                case InputType.KEYBIND:
+                    PreferenceManager.SetPreference(name,si._key.text);
                     break;
-                default:
-                    Debug.Log("Object in settingsList type not set");
+                case InputType.SLIDER:
+                    PreferenceManager.SetPreference(name,si._slider.value);
                     break;
             }
-
-            //ADD:
-            //write to preference manager
         }
+        PreferenceManager.Save();
     }
 
     private void createDropdown(string title, List<string> dropdownList, int value)
@@ -103,6 +153,12 @@ public class SettingsPanel : MonoBehaviour
        GameObject g = Instantiate(titleText, list.transform);
        g.GetComponentInChildren<TextMeshProUGUI>().text=title;
        settingsList.Add(g);        
+    }
+    private void createSlider(string title, float lowVal, float highVal, float value)
+    {
+        GameObject g = Instantiate(sliderInput, list.transform);
+        g.GetComponent<SettingsInput>().Init(title, lowVal,highVal,value);
+        settingsList.Add(g);
     }
 }
 
