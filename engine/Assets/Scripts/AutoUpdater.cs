c0f57d774247bd37897d43b0382e0f350879c574
khuntetad <63430428+khuntetad@users.noreply.github.com>
2021-08-05 11:46:40 -0700

    Auto Updater GUI (#741)
    * Implemented Auto Updater Class
    
    * created a panel containing the update popup, bug fixes necessary on implementation
    
    * created a panel containing the update popup, bug fixes necessary on implementation
    
    * updated the panel to a hig compliant style, bug fixed button
    
    * fixed panel issues and made the panel white
    
    * changed the script to only spawn panel when update is required
    
    * implemented rounded UI buttons and panel
    
    * cleaned up the UI with dividing line
    
    * final UI cleanup, corrected AutoUpdater to spawn panel only when the version is not updated
    
    * finalized UI for the panel, implemented the version number
    
    * Moved the update prompt to be a panel, changed the look of it, changed the location of the version number
    
    * Update Local Version Number
    
    Co-authored-by: matthew17754 <matmoradi17@gmail.com>
    Co-authored-by: Hunter <hunter.barclay18@gmail.com>

---
+++ engine/Assets/Scripts/AutoUpdater.cs
@@ -0,0 +1,92 @@
+using Assets.Scripts;
+using Newtonsoft.Json;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Net.Security;
+using System.Diagnostics;
+using Synthesis.UI.Panels;
+using Debug = UnityEngine.Debug;
+
+public class AutoUpdater : MonoBehaviour
+{
+    // public static string updater;
+    public const string LocalVersion = "5.0.0.0";
+    public GameObject updaterPanelPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // var versionText = GameObject.Find("VersionNumber").GetComponent<Text>();
+        // versionText.text = "Version " + LocalVersion;
+        Debug.Log($"Version {LocalVersion}");
+        // game = GameObject.Find("UpdatePrompt");
+
+        if (CheckConnection())
+        {
+            WebClient client = new WebClient();
+            ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
+            var json = new WebClient().DownloadString("https://raw.githubusercontent.com/Autodesk/synthesis/master/VersionManager.json");
+            VersionManager update = JsonConvert.DeserializeObject<VersionManager>(json);
+            var updater = update.URL;
+
+            var localVersion = new Version(LocalVersion);
+            var globalVersion = new Version(update.Version);
+
+            var check = localVersion.CompareTo(globalVersion);
+
+            if (check < 0) { // if outdated, set update prompt to true
+                Debug.Log($"Version {globalVersion.ToString()} available");
+                var result = LayoutManager.OpenPanel(updaterPanelPrefab, true);
+                if (result.success) { // This really shouldn't ever fail but just in case
+                    (result.panel as UpdatePromptPanel).UpdaterLink = updater;
+                }
+            }
+        }
+    }
+
+    public bool CheckConnection()
+    {
+        try
+        {
+            WebClient client = new WebClient();
+            ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
+
+            using (client.OpenRead("https://raw.githubusercontent.com/Autodesk/synthesis/master/VersionManager.json"))
+            {
+                return true;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+    {
+        bool isOk = true;
+        // If there are errors in the certificate chain, look at each error to determine the cause.
+        if (sslPolicyErrors != SslPolicyErrors.None)
+        {
+            for (int i = 0; i < chain.ChainStatus.Length; i++)
+            {
+                if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                {
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                    }
+                }
+            }
+        }
+        return isOk;
+    }
+}
---
+++ engine/Assets/Scripts/VersionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    class VersionManager
+    {
+        public string Version { get; set; }
+        public string URL { get; set; }
+    }
+}
