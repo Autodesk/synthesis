f27118813345abe65aef85d7409b48e0baeaad89
hices <shawnhice@gmail.com>
2022-05-01 16:45:21 -0700

    updated mirabuf to 1.2.0 with back compatibility

--- exporter/SynthesisFusionAddin/proto/build.bat
+++ exporter/SynthesisFusionAddin/proto/build.bat
@@ -1,3 +1,4 @@
 mkdir .\proto_out
+@RD /S /Q "./proto_out/__pycache__"
 git submodule update --init --recursive
 protoc -I=../../../protocols/mirabuf --python_out=./proto_out ../../../protocols/mirabuf/*.proto
