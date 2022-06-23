e2b6fa9d087d9bf4557ecb7582b646b852bf2040
Aaron7317 <aaron.boyd.org@gmail.com>
2022-06-23 12:49:32 -0700

    Loading a JSON configuration works

--- server/Program.cs
+++ server/Program.cs
@@ -16,7 +16,8 @@ namespace SynthesisServer
         
         public static async Task Main(string[] args)
         {
-
+            string filePath = "appsettings.json";
+            bool createConfig = false;
             var builder = new HostBuilder();
 
             builder.ConfigureAppConfiguration((hostingContext, config) =>
@@ -29,16 +30,27 @@ namespace SynthesisServer
                 }
 
                 //configure build to use local json file
-                //config.AddJsonFile("");
+                try
+                {
+                    config.AddJsonFile(filePath);
+                } catch (System.IO.FileNotFoundException e)
+                {
+                    createConfig = true;
+                }
+                
             });
 
             builder.ConfigureServices((hostContext, services) =>
             {
                 services.AddOptions();
 
+                foreach (var x in hostContext.Configuration.AsEnumerable())
+                {
+                    Console.WriteLine(x);
+                }
                 //Configure commands and config file loading here
                 //services.Configure<DaemonConfig>(hostContext.Configuration.GetSection("Daemon"));
-                services.Configure<DaemonConfig>(hostContext.Configuration);
+                services.Configure<DaemonConfig>(hostContext.Configuration.GetSection("Settings"));
                 services.AddSingleton<IHostedService, SynthesisService>();
             });
 
@@ -50,6 +62,10 @@ namespace SynthesisServer
 
             var host = builder.Build();
             
+            if (createConfig)
+            {
+                //create json config using current host configuration
+            }
             
             await host.RunAsync();
             //await builder.RunConsoleAsync();
