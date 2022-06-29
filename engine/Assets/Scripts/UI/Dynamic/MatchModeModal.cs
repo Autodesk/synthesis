1d2271692a82394bbbbb41ca3de73fe198cb9eeb
PepperLola <julian.orion.wright@gmail.com>
2022-06-29 14:06:38 -0700

    merged master and resolved MainScene issues

--- engine/Assets/Scripts/UI/Dynamic/MatchModeModal.cs
+++ engine/Assets/Scripts/UI/Dynamic/MatchModeModal.cs
@@ -11,26 +11,53 @@ public class MatchModeModal : ModalDynamic
         Description.SetText("Configure Match Mode");
 
         MainContent.CreateLabel().ApplyTemplate(Label.BigLabelTemplate).SetText("Configure Match");
-        var allianceSelectionLabel = MainContent.CreateLabel().ApplyTemplate(Label.VerticalLayoutTemplate)
-            .SetText("Select Alliance Color");
-        var allianceSelection = MainContent.CreateDropdown().ApplyTemplate(Dropdown.VerticalLayoutTemplate)
-            .SetOptions(new string[] { "Red", "Blue" })
-            .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d.Label.Text} -> [{i}] {o.text}"));
-        var robotDropdownLabel = MainContent.CreateLabel().ApplyTemplate(Label.VerticalLayoutTemplate)
-            .SetText("Select Robot");
-        var robotDropdown = MainContent.CreateDropdown().ApplyTemplate(Dropdown.VerticalLayoutTemplate)
-            .SetOptions(new string[] { "2018 Mean Machine", "Spartabots", "Dozer"})
-            .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d.Label.Text} -> [{i}] {o.text}"));
-        var fieldDropdownLabel = MainContent.CreateLabel().ApplyTemplate(Label.VerticalLayoutTemplate)
-            .SetText("Select Game");
-        var fieldDropdown = MainContent.CreateDropdown().ApplyTemplate(Dropdown.VerticalLayoutTemplate)
-            .SetOptions(new string[] { "2018 First Power Up", "2019 Destination Deep Space" })
-            .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d.Label.Text} -> [{i}] {o.text}"));
-        var spawnPositionLabel = MainContent.CreateLabel().ApplyTemplate(Label.VerticalLayoutTemplate)
-            .SetText("Select Spawn Position");
-        var spawnPosition = MainContent.CreateDropdown().ApplyTemplate(Dropdown.VerticalLayoutTemplate)
-            .SetOptions(new string[] { "Left", "Middle", "Right" })
-            .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d.} -> [{i}] {o.text}"));
+        var allianceSelection = MainContent.CreateLabeledDropdown()
+            .StepIntoLabel(l =>
+            {
+                l.SetText("Select Alliance")
+                    .ApplyTemplate(Label.VerticalLayoutTemplate);
+            })
+            .StepIntoDropdown(d =>
+            {
+                d.SetOptions(new string[] { "Red", "Blue" })
+                    .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d} -> [{i}] {o.text}"));
+            });
+        var robotDropdown = MainContent.CreateLabeledDropdown()
+            .StepIntoLabel(l =>
+            {
+                l.SetText("Select Robot")
+                    .ApplyTemplate(Label.VerticalLayoutTemplate);
+            })
+            .StepIntoDropdown(d =>
+            {
+                d.ApplyTemplate(Dropdown.VerticalLayoutTemplate)
+                    .SetOptions(new string[] { "2018 Mean Machine", "Spartabots", "Dozer" })
+                    .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d} -> [{i}] {o.text}"));
+            });
+        var fieldDropdown = MainContent.CreateLabeledDropdown()
+            .StepIntoLabel(l =>
+            {
+                l.SetText("Select Game")
+                    .ApplyTemplate(Label.VerticalLayoutTemplate);
+            })
+            .StepIntoDropdown(d =>
+            {
+                d.ApplyTemplate(Dropdown.VerticalLayoutTemplate)
+                    .SetOptions(new string[] { "2018 First Power Up", "2019 Destination Deep Space" })
+                    .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d} -> [{i}] {o.text}"));
+            });
+        var spawnPosition = MainContent.CreateLabeledDropdown()
+            .StepIntoLabel(l =>
+            {
+                l.SetText("Select Spawn Position")
+                    .ApplyTemplate(Label.VerticalLayoutTemplate);
+            })
+            .StepIntoDropdown(d =>
+            {
+                d.ApplyTemplate(Dropdown.VerticalLayoutTemplate)
+                    .SetOptions(new string[] { "Left", "Middle", "Right" })
+                    .AddOnValueChangedEvent((d, i, o) => Debug.Log($"{d} -> [{i}] {o.text}"));
+            });
     }
     
     public override void Update() {}
