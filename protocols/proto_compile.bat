1a4005bad3e151d3a051dc86e0f39859be5f0b48
Aaron7317 <aaron.boyd.org@gmail.com>
2022-06-30 23:41:21 -0700

    Updated Protobuf

--- protocols/proto_compile.bat
+++ protocols/proto_compile.bat
@@ -1,7 +1,9 @@
-@echo off
+@echo on
 md ..\api\Api\Gen\Proto\
 md ..\api\Api\Gen\Mirabuf\
+md ..\server\Gen\Proto\
 protoc --csharp_out=../api/Api/Gen/Proto/ v1/*.proto
+protoc --csharp_out=../server/Gen/Proto/ v1/ServerConnection.proto
 protoc --proto_path=../mirabuf --csharp_out=../api/Api/Gen/Mirabuf ../mirabuf/*.proto
 cd ..
 
