b052cf22af1c9b7cffb0ab9b1e59cb405cace44d
Aaron7317 <aaron.boyd.org@gmail.com>
2022-06-30 23:41:21 -0700

    Updated Protobuf

--- protocols/proto_compile.bat
+++ protocols/proto_compile.bat
@@ -1,2 +1,9 @@
+@echo on
+md ..\api\Api\Gen\Proto\
+md ..\api\Api\Gen\Mirabuf\
+md ..\server\Gen\Proto\
 protoc --csharp_out=../api/Api/Gen/Proto/ v1/*.proto
-protoc --proto_path=../mirabuf --csharp_out=../api/Api/Gen/Mirabuf/ ../mirabuf/*.proto
+protoc --csharp_out=../server/Gen/Proto/ v1/ServerConnection.proto
+protoc --proto_path=../mirabuf --csharp_out=../api/Api/Gen/Mirabuf ../mirabuf/*.proto
+cd ..
+
--- protocols/proto_compile.sh
+++ protocols/proto_compile.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 mkdir -p ../api/Api/Gen/Proto/
 mkdir -p ../api/Api/Gen/Mirabuf/
+mkdir -p ../server/Gen/Proto/
 protoc --csharp_out=../api/Api/Gen/Proto/ v1/*.proto
+protoc --csharp_out=../server/Gen/Proto/ v1/ServerConnection.proto
 protoc --proto_path=../mirabuf --csharp_out=../api/Api/Gen/Mirabuf/ ../mirabuf/*.proto
 
