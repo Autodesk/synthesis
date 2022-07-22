80a5c3277c94b49efcdf93a6671b32c865a63959
Aaron7317 <aaron.boyd.org@gmail.com>
2022-07-22 00:41:37 -0700

    More work on protobuf and tests

--- protocols/proto_compile.bat
+++ protocols/proto_compile.bat
@@ -2,8 +2,11 @@
 md ..\api\Api\Gen\Proto\
 md ..\api\Api\Gen\Mirabuf\
 md ..\server\Gen\Proto\
+md ..\server\Gen\Mirabuf\
 protoc --csharp_out=../api/Api/Gen/Proto/ v1/*.proto
 protoc --csharp_out=../server/Gen/Proto/ v1/ServerConnection.proto
+protoc --csharp_out=../server/Gen/Proto/ v1/ClientConnection.proto
 protoc --proto_path=../mirabuf --csharp_out=../api/Api/Gen/Mirabuf ../mirabuf/*.proto
+protoc --proto_path=../mirabuf --csharp_out=../Server/Gen/Mirabuf ../mirabuf/*.proto
 cd ..
 pause
