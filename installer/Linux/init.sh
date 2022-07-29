57d1f25e5da2b19aaeec08d6c9dfd3fa286c2df0
Aaron7317 <aaron.boyd.org@gmail.com>
2022-07-28 20:47:51 -0700

    Finished creating linux packaging. Needs testing and updated docs

--- installer/Linux/init.sh
+++ installer/Linux/init.sh
@@ -7,12 +7,16 @@ show_help() {
 	echo "-h	display help message"
 	echo "-f	specify the input directory for fields"
 	echo "-r	specify the input directory for robots"
+	echo "-b	specify the build directory of synthesis"
 }
-
 
 OPTIND=1
 
-while getopts "h?f:r:" opt; do
+APP_NAME="Synthesis"
+INIT_DIR="$(dirname "$(readlink -f "${0}")")"
+APP_DIR="$INIT_DIR/$APP_NAME.AppDir"
+
+while getopts "h?f:r:b:" opt; do
 	case "$opt" in
 		h|\?)
 			show_help
@@ -24,16 +28,30 @@ while getopts "h?f:r:" opt; do
 		r)
 			robots="$OPTARG"
 			;;
+		b)
+			build="$OPTARG"
+			;;
 	esac
 done
 
 shift $((OPTIND-1))
 
-
-if [ ! -n "$fields" ] || [ ! -n "$robots" ] ; then
-	echo "Make sure you specify an input directory both fields and robots"
+if [ ! -n "$fields" ] ; then
+	echo "Specify input directory for fields using \"-f\""
 	exit 1
 fi
 
-echo $fields
-echo $robots
+
+if [ ! -n "$robots" ] ; then
+	echo "Specify input directory for robots using \"-r\""
+	exit 1
+fi
+
+if [ ! -n "$build" ] ; then
+	echo "Specify synthesis build directory using \"-b\""
+	exit 1
+fi
+
+cp "$fields/"*.mira "$APP_DIR/fields/" 
+cp "$robots/"*.mira "$APP_DIR/robots/"
+cp -R "$build/"* "$APP_DIR/usr/bin" 
