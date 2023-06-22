80e855943f43141613447449ac4022ec004b6244
Brandon <pacewicbrandon@gmail.com>
2023-06-22 10:18:42 -0700

    Streamline updates for the format validation script

--- tools/format_validation.py
+++ tools/format_validation.py
@@ -6,25 +6,26 @@ FILE_TARGETS = [".cs"]
 FORMAT_COMMAND = "clang-format -i -style=file"
 
 def main():
-    clang_format_version = os.system("clang-format --version")
-    print(clang_format_version)
-
     files = []
-    for root, _, file_names in os.walk("engine/Assets/Scripts"):
-        for file_name in file_names:
-            if file_name.endswith(".cs"):
-                files.append(os.path.join(root, file_name))
+    for dir in FILES_DIRS:
+        for root, _, filenames in os.walk(dir):
+            for filename in filenames:
+                if os.path.splitext(filename)[1] in FILE_TARGETS:
+                    files.append(os.path.join(root, filename))
 
     for file in files:
-        previous_file_state = open(file, "r").read()
-        os.system(f"clang-format -i {file}")
-        new_file_state = open(file, "r").read()
+        with open(file, "r") as f:
+            previous_file_state = f.read()
+
+        os.system(f"{FORMAT_COMMAND} {file}")
+        with open(file, "r") as f:
+            new_file_state = f.read()
 
         if previous_file_state != new_file_state:
             print(f"File {file} is not formatted correctly!")
             sys.exit(1)
 
-    print("Done!")
+    print("Success!")
 
 if __name__ == '__main__':
     main()
