c28a5b6c6677bd5e29dbe0098f5e27c4e47653c1
hices <shawnhice@gmail.com>
2023-06-22 11:27:49 -0700

    Made OSX match up with expected output for windows - removed Aardvark

--- api/Api/Mirabuf/Vector3.cs
+++ api/Api/Mirabuf/Vector3.cs
@@ -15,10 +15,6 @@ namespace Mirabuf {
             => new Vector3() { X = v.x * -100, Y = v.y * 100, Z = v.z * 100 };
         public static implicit operator UnityEngine.Vector3(Vector3 v)
             => new UnityEngine.Vector3(v.X * -0.01f, v.Y * 0.01f, v.Z * 0.01f);
-        public static implicit operator Vector3(BXDVector3 v)
-            => new Vector3() { X = v.x, Y = v.y, Z = v.z };
-        public static implicit operator BXDVector3(Vector3 v)
-            => new BXDVector3(v.X, v.Y, v.Z);
 
         public static Vector3 operator +(Vector3 a, Vector3 b) => new Vector3() { X = a.X + b.X, Y = a.Y + b.Y, Z = a.Z + b.Z };
         public static Vector3 operator -(Vector3 a, Vector3 b) => new Vector3() { X = a.X - b.X, Y = a.Y - b.Y, Z = a.Z - b.Z };
