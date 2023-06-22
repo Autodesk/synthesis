b546060be7794c36dbf6d040876a08513144f69f
Brandon <pacewicbrandon@gmail.com>
2023-06-22 09:29:19 -0700

    Another test

--- tools/format_validation.py
+++ tools/format_validation.py
@@ -1,7 +1,14 @@
 import os
 import sys
 
+FILES_DIRS = ["engine/Assets/Scripts"]
+FILE_TARGETS = [".cs"]
+FORMAT_COMMAND = "clang-format -i -style=file"
+
 def main():
+    clang_format_version = os.system("clang-format --version")
+    print(clang_format_version)
+
     files = []
     for root, _, file_names in os.walk("engine/Assets/Scripts"):
         for file_name in file_names:
@@ -15,10 +22,9 @@ def main():
 
         if previous_file_state != new_file_state:
             print(f"File {file} is not formatted correctly!")
-            # sys.exit(1)
+            sys.exit(1)
 
     print("Done!")
-    sys.exit(0)
 
 if __name__ == '__main__':
     main()
