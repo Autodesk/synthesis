02a1702d2a2b56130d1e08a1b2c4c707b26f3799
Brandon <pacewicbrandon@gmail.com>
2023-06-22 21:26:34 -0700

    Revision of .net install step

--- scripts/win/install_net.bat
+++ scripts/win/install_net.bat
@@ -1,18 +1,21 @@
 @echo off
 
-REM Check if .NET SDK is already installed
-dotnet --version >nul 2>&1
-if %errorlevel% equ 0 (
-    echo ".NET SDK is already installed."
-) else (
-    echo "Installing .NET SDK..."
-    PowerShell -Command "Invoke-WebRequest -Uri 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'; .\dotnet-install.ps1 -Channel LTS -InstallDir 'C:\Program Files\dotnet' -NoPath"
+for /f "usebackq delims=" %%G in (`powershell -Command "Get-ExecutionPolicy"`) do set "executionPolicy=%%G"
 
-    echo "Adding .NET SDK to PATH..."
-    setx /m PATH "%PATH%;C:\Program Files\dotnet"
-
-    echo "Cleaning up..."
-    del dotnet-install.ps1
+if /i "%executionPolicy%" neq "RemoteSigned" (
+    echo "Current Execution Policy is not RemoteSigned. This is required for the .NET installation."
+    echo "Setting Execution Policy to RemoteSigned..."
+    powershell -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned"
+    echo "Execution Policy set to RemoteSigned."
 )
+
+echo "Installing .NET SDK..."
+PowerShell -Command "Invoke-WebRequest -Uri 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'; .\dotnet-install.ps1 -Channel LTS -InstallDir 'C:\Program Files\dotnet' -NoPath"
+
+echo "Adding .NET SDK to PATH..."
+setx /m PATH "%PATH%;C:\Program Files\dotnet"
+
+echo "Cleaning up..."
+del dotnet-install.ps1
 
 echo ".NET SDK installation complete."
