6e3a1e8e9cafc6c9b41577b955e2c0d4def70280
Brandon <pacewicbrandon@gmail.com>
2023-06-23 12:26:25 -0700

    Small bug fix

--- scripts/format/format_validation.py
+++ scripts/format/format_validation.py
@@ -10,7 +10,7 @@ As of 6/23/2023, clang-format-16 is not available on Unbuntu through apt-get.
 Trying to validate these files on clang-format-15 will cause a segfault. This is the current workaround.
 """
 IGNORED_FILES = set([
-    "engine/Assets/Scripts/UI/Dynamic/DynamicUIManager.cs"
+    "engine/Assets/Scripts/UI/Dynamic/DynamicUIManager.cs",
     "engine/Assets/Scripts/WebSockets/WebSocketManager.cs"
 ])
 
