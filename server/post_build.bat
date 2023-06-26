00e8b831ee9f6f0fcde44437f18ed7b79c082707
Brandon <pacewicbrandon@gmail.com>
2023-06-26 08:28:18 -0700

    Merge dev

--- server/post_build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy ServerApi\\bin\\Debug\\netstandard2.0\\ServerApi.dll ..\\engine\\Assets\\Packages\\ServerApi.dll
-del /f ..\\engine\\Assets\\Packages\\ServerApi.dll.meta
--- server/run.bat
+++
@@ -1,1 +0,0 @@
-dotnet run --project SynthesisServer\SynthesisServer.csproj
