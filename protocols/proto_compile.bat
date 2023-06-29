06fc4422e273e6ea6e840b4f871f054ab0a74787
Hunter Barclay <hunter.barclay18@gmail.com>
2023-06-29 11:59:42 -0700

    Fixed pathing issues for protoc

--- protocols/proto_compile.bat
+++ protocols/proto_compile.bat
@@ -5,5 +5,5 @@ md ..\api\Api\Gen\Proto\Server\
 md ..\api\Api\Gen\Mirabuf\
 @echo on
 protoc --csharp_out=../api/Api/Gen/Proto/ v1/*.proto
-protoc --csharp_out=../api/Api/Gen/Proto/Server/ v1/server/*.proto -I v1/server/
+protoc --csharp_out=../api/Api/Gen/Proto/Server/ v1/server/*.proto
 protoc --proto_path=../mirabuf --csharp_out=../api/Api/Gen/Mirabuf/ ../mirabuf/*.proto
--- protocols/proto_compile.sh
+++ protocols/proto_compile.sh
@@ -17,5 +17,5 @@ then
 fi
 
 protoc --csharp_out=../api/Api/Gen/Proto/ v1/*.proto
-protoc --csharp_out=../api/Api/Gen/Proto/Server/ v1/server/*.proto -I v1/server/
+protoc --csharp_out=../api/Api/Gen/Proto/Server/ v1/server/*.proto
 protoc --proto_path=../mirabuf --csharp_out=../api/Api/Gen/Mirabuf/ ../mirabuf/*.proto
