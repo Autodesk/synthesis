c786397c1fd9df561bc62a15086a0bb05183ed74
BrandonPacewic <pacewicbrandon@gmail.com>
2023-07-13 14:02:18 -0700

    `using` lobby server cleanups

--- api/Api/Aether/Lobby/LobbyServer.cs
+++ api/Api/Aether/Lobby/LobbyServer.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using SynthesisAPI.Controller;
 using SynthesisAPI.Utilities;
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
 using System.Security.Cryptography;
+using System.Threading;
 
 #nullable enable
 
