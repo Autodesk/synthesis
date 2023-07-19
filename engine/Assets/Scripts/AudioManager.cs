b15b66cc0e905ea69e83964b99d701591630b979
Markytt <theinteruptingchicken@gmail.com>
2023-07-19 15:49:53 -0700

    Removed events on destroy

--- engine/Assets/Scripts/AudioManager.cs
+++ engine/Assets/Scripts/AudioManager.cs
@@ -5,32 +5,51 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
     private AudioSource source;
+    private EventBus.EventCallback startCallback;
+    private EventBus.EventCallback endCallback;
 
     public void Start() {
         source = gameObject.GetComponent<AudioSource>();
 
-        EventBus.NewTypeListener<MatchStateMachine.OnStateStarted>(e => {
-            MatchStateMachine.OnStateStarted onStateStarted = (MatchStateMachine.OnStateStarted) e;
-            Type stateType                                  = onStateStarted.state.GetType();
-            if (stateType == typeof(MatchStateMachine.Auto)) {
+        startCallback = e =>
+        {
+            MatchStateMachine.OnStateStarted onStateStarted = (MatchStateMachine.OnStateStarted)e;
+            Type stateType = onStateStarted.state.GetType();
+            if (stateType == typeof(MatchStateMachine.Auto))
+            {
                 source.clip = SynthesisAssetCollection.GetAudioClip("Start_Auto");
                 source.Play();
-            } else if (stateType == typeof(MatchStateMachine.Teleop)) {
+            }
+            else if (stateType == typeof(MatchStateMachine.Teleop))
+            {
                 source.clip = SynthesisAssetCollection.GetAudioClip("Start_Teleop");
                 source.Play();
-            } else if (stateType == typeof(MatchStateMachine.Endgame)) {
+            }
+            else if (stateType == typeof(MatchStateMachine.Endgame))
+            {
                 source.clip = SynthesisAssetCollection.GetAudioClip("Start_of_End_Game");
                 source.Play();
             }
-        });
+        };
 
-        EventBus.NewTypeListener<MatchStateMachine.OnStateEnded>(e => {
-            MatchStateMachine.OnStateEnded onStateEnded = (MatchStateMachine.OnStateEnded) e;
-            Type stateType                              = onStateEnded.state.GetType();
-            if (stateType == typeof(MatchStateMachine.Endgame)) {
+        endCallback = e =>
+        {
+            MatchStateMachine.OnStateEnded onStateEnded = (MatchStateMachine.OnStateEnded)e;
+            Type stateType = onStateEnded.state.GetType();
+            if (stateType == typeof(MatchStateMachine.Endgame))
+            {
                 source.clip = SynthesisAssetCollection.GetAudioClip("Match_End");
                 source.Play();
             }
-        });
+        };
+
+        EventBus.NewTypeListener<MatchStateMachine.OnStateStarted>(startCallback);
+        EventBus.NewTypeListener<MatchStateMachine.OnStateEnded>(endCallback);
+    }
+
+    public void OnDestroy()
+    {
+        EventBus.RemoveTypeListener<MatchStateMachine.OnStateStarted>(startCallback);
+        EventBus.RemoveTypeListener<MatchStateMachine.OnStateEnded>(endCallback);
     }
 }
