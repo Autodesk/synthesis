fa0725a070952a07cc0b79fa79d3cedc670358ec
BrandonPacewic <pacewicbrandon@gmail.com>
2023-07-20 14:07:21 -0700

    Updated windows main

--- cef/cef/src/main.cpp
+++ cef/cef/src/main.cpp
@@ -1,7 +1,12 @@
 #include "shared_main.h"
 
 #if defined(OS_WIN)
-// TODO
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+    return synthesis::shared::wWinMain(hInstance);
+}
 #else
 int main(int argc, char* argv[]) {
     return synthesis::shared::main(argc, argv);
