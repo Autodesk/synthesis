0257a39af03bad7e976b0153137bc4e8ba0740b6
a-crowell <ayla.crowell@gmail.com>
2023-07-27 10:10:50 -0700

    Fun aspect ratio fixes

--- engine/Assets/Scripts/UI/MainMenu/MenuGraphic.cs
+++ engine/Assets/Scripts/UI/MainMenu/MenuGraphic.cs
@@ -5,7 +5,7 @@ public class MenuGraphic : MonoBehaviour {
 
     public void Start() {
         float rat = (float) Screen.width / (float) Screen.height;
-        this.gameObject.SetActive(rat > 1.61);
+        this.gameObject.SetActive(rat > 1.7);
     }
 
 }
--- engine/Assets/Scripts/UI/MainMenu/MenuSlideTween.cs
+++ engine/Assets/Scripts/UI/MainMenu/MenuSlideTween.cs
@@ -6,17 +6,24 @@ using UnityEngine.EventSystems;
 
 public class MenuSlideTween: MonoBehaviour, IPointerClickHandler {
     private string KEY = "slide";
+    private float imageAnchorDiff;
+    private float textAnchorDiff;
     private Transform screenTransform;
 
     public void OnPointerClick(PointerEventData eventData) {
-        screenTransform = GameObject.Find("Canvas").transform.Find("MenuPanel").transform.Find("ScreenSpace").transform;
-        SynthesisTween.MakeTween(KEY, GetRectTransform("ScreenOne").anchoredPosition.x, -4000f, 1f,
+        screenTransform = GameObject.Find("Canvas").transform.Find("MenuPanel").transform.Find("ScreenSpace");
+        imageAnchorDiff = GetRectTransform("MakeAnythingImage").anchoredPosition.x;
+        textAnchorDiff = GetRectTransform("ScreenOneText").anchoredPosition.x;
+        
+        SynthesisTween.MakeTween(KEY, GetRectTransform("Anchor").anchoredPosition.x, -4000f, 1f,
                 (t, a, b) => SynthesisTweenInterpolationFunctions.FloatInterp(t, (float) a, (float) b),
                 SynthesisTweenScaleFunctions.EaseInOutQuint, TweenProgress);
     }
 
     public void TweenProgress(SynthesisTween.SynthesisTweenStatus status) {
-        MoveObject(GetRectTransform("ScreenOne"), status.CurrentValue<float>());
+        MoveObject(GetRectTransform("Anchor"), status.CurrentValue<float>());
+        MoveObject(GetRectTransform("MakeAnythingImage"), status.CurrentValue<float>() + imageAnchorDiff);
+        MoveObject(GetRectTransform("ScreenOneText"), status.CurrentValue<float>() + textAnchorDiff);
         MoveObject(GetRectTransform("ScreenTwo"), status.CurrentValue<float>() + 4000f);
     }
 
@@ -25,7 +32,7 @@ public class MenuSlideTween: MonoBehaviour, IPointerClickHandler {
     }
 
     private RectTransform GetRectTransform(string name) {
-        return screenTransform.Find(name).gameObject.GetComponent<RectTransform>();
+        return screenTransform.transform.Find(name).gameObject.GetComponent<RectTransform>();
     }
 
 
