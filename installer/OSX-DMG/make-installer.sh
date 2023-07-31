2e163a5967fbf38d3f03e114afcc9738404c36cc
Hunter Barclay <hunter.barclay18@gmail.com>
2023-07-31 16:43:49 -0600

    Added EULA

--- installer/OSX-DMG/make-installer.sh
+++ installer/OSX-DMG/make-installer.sh
@@ -3,14 +3,13 @@ test -f Synthesis-Installer.dmg && rm Synthesis-Installer.dmg
 ./create-dmg/create-dmg \
   --volname "Synthesis Installer" \
   --window-pos 200 120 \
-  --window-size 500 300 \
+  --window-size 500 180 \
   --text-size 12 \
   --icon-size 80 \
   --icon "Synthesis.app" 50 50 \
   --hide-extension "Synthesis.app" \
-  --icon "license.html" 210 160 \
-  --hide-extension "license.html" \
   --app-drop-link 360 50 \
+  --eula "license.txt" \
   "Synthesis-Installer.dmg" \
   "source_folder/"
 
