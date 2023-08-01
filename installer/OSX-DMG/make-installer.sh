8e16b6a58a9c23433918229f0277de6d2722f6de
Hunter Barclay <hunter.barclay18@gmail.com>
2023-07-31 21:39:25 -0600

    Added background

--- installer/OSX-DMG/make-installer.sh
+++ installer/OSX-DMG/make-installer.sh
@@ -2,13 +2,14 @@
 test -f Synthesis-Installer.dmg && rm Synthesis-Installer.dmg
 ./create-dmg/create-dmg \
   --volname "Synthesis Installer" \
+  --background "Synthesis-Background.png" \
   --window-pos 200 120 \
-  --window-size 500 180 \
+  --window-size 500 270 \
   --text-size 12 \
   --icon-size 80 \
-  --icon "Synthesis.app" 50 50 \
+  --icon "Synthesis.app" 120 115 \
   --hide-extension "Synthesis.app" \
-  --app-drop-link 360 50 \
+  --app-drop-link 380 115 \
   --eula "license.txt" \
   "Synthesis-Installer.dmg" \
   "source_folder/"
