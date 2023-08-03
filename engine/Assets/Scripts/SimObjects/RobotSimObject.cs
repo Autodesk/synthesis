7428f89752618ff15ae5cee55944b1635ee6ee23
a-crowell <ayla.crowell@gmail.com>
2023-08-03 11:18:44 -0700

    Added catches in deserialization for different types

--- engine/Assets/Scripts/SimObjects/RobotSimObject.cs
+++ engine/Assets/Scripts/SimObjects/RobotSimObject.cs
@@ -266,7 +266,7 @@ public class RobotSimObject : SimObject, IPhysicsOverridable, IGizmo {
 
     private static Analog TryGetSavedInput(string key, Analog defaultInput) {
         if (PreferenceManager.ContainsPreference(key)) {
-            var input            = PreferenceManager.GetPreference<Digital>(key);
+            var input  = PreferenceManager.GetPreference<Digital>(key) ?? (Digital) PreferenceManager.GetPreference<InputData[]>(key)[0].GetInput();
             input.ContextBitmask = defaultInput.ContextBitmask;
             return input;
         }
