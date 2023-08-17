5c254f8a8d730061806c08bd488aa4e8630d8447
Hunter <hunter.barclay18@gmail.com>
2023-08-17 11:27:45 -0600

    Removed unused using statements

--- engine/Assets/Scripts/Gizmos/MoveArrow.cs
+++ engine/Assets/Scripts/Gizmos/MoveArrow.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Synthesis.Gizmo;
 using Synthesis.Physics;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Synthesis.Configuration {
     public class MoveArrow : MonoBehaviour {
