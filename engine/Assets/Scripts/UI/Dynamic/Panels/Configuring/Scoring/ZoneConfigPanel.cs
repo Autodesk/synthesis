bf122673ed61c1e63879371e438a088c4aafdb37
LucaHaverty <lucahaverty@gmail.com>
2023-08-23 13:44:51 -0700

    Move tool and scoring zones null ref exception fixed

--- engine/Assets/Scripts/UI/Dynamic/Panels/Configuring/Scoring/ZoneConfigPanel.cs
+++ engine/Assets/Scripts/UI/Dynamic/Panels/Configuring/Scoring/ZoneConfigPanel.cs
@@ -60,7 +60,16 @@ public class ZoneConfigPanel : PanelDynamic {
             _isNewZone            = false;
             _initialData.Name     = zone.Name;
             _initialData.Alliance = zone.Alliance;
-            var parent = zone.GameObject.transform.parent.name == "grounded" ? null : zone.GameObject.transform.parent;
+
+            /*Transform parent;
+            if (zone.GameObject.transform.parent == null)
+                parent = null;
+            else parent = zone.GameObject.transform.parent.name == "grounded" ? null : zone.GameObject.transform.parent;*/
+            
+            var parent = (zone.GameObject.transform.parent == null || zone.GameObject.transform.parent.name == "grounded") 
+                ? null 
+                : zone.GameObject.transform.parent;
+            
             if (parent is not null) {
                 _initialParent               = parent.name;
                 _initialData.Parent          = parent.name;
