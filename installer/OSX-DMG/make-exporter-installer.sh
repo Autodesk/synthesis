8e1d247c3def3e766e9ff23f79be80b6bb7ea0a8
Hunter Barclay <hunter.barclay18@gmail.com>
2023-11-09 23:44:45 -0700

    Merging the two installers into one. For some reason a bigger background image actually showed up smaller?

--- installer/OSX-DMG/make-exporter-installer.sh
+++ installer/OSX-DMG/make-exporter-installer.sh
@@ -7,9 +7,10 @@ test -f Synthesis-Exporter-Installer.dmg && rm Synthesis-Exporter-Installer.dmg
   --window-size 500 270 \
   --text-size 12 \
   --icon-size 80 \
-  --icon "applink/" 120 115 \
+  --icon "AddIns" 120 115 \
   --hide-extension "Synthesis.app" \
   --eula "license.txt" \
+  --add-file "SynthesisFusionAddin" ../../exporter/SynthesisFusionAddin 200 115 \
   "Synthesis-Exporter-Installer.dmg" \
   "exporter_source_folder/"
 
--- installer/OSX-DMG/make-installer.sh
+++ installer/OSX-DMG/make-installer.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+if ! test -f addins-folder-link; then
+  ln -s ~/Library/Application\ Support/Autodesk/Autodesk\ Fusion\ 360/API/AddIns addins-folder-link
+fi
+
 test -f Synthesis-Installer.dmg && rm Synthesis-Installer.dmg
 ./create-dmg/create-dmg \
   --volname "Synthesis Installer" \
-  --background "Synthesis-Background.png" \
+  --background "SynthesisMacInstallerBackground.png" \
   --window-pos 200 120 \
-  --window-size 500 270 \
+  --window-size 500 600 \
   --text-size 12 \
   --icon-size 80 \
   --icon "Synthesis.app" 120 115 \
+  --add-file AddIns addins-folder-link 380 0 \
+  --add-file SynthesisFusionAddin ../../exporter/SynthesisFusionAddin 120 0 \
   --hide-extension "Synthesis.app" \
   --app-drop-link 380 115 \
   --eula "license.txt" \
@@ -16,3 +22,4 @@ test -f Synthesis-Installer.dmg && rm Synthesis-Installer.dmg
 
 # --volicon "synthesis-icon.icns" \
 # --background "installer_background.png" \
+# --background "Synthesis-Background.png" \
