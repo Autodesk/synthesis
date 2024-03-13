117f8ef5bf12b355fdfb7e341158623502c7a543
KyroVibe <hunter.barclay18@gmail.com>
2024-03-12 21:06:18 -0600

    Preliminary rigid node generation done. Need either AB testing for full integration to test further for bugs

--- fission/src/mirabuf/MirabufParser.ts
+++ fission/src/mirabuf/MirabufParser.ts
@@ -7,7 +7,16 @@ export enum ParseErrorSeverity {
     JustAWarning = 4
 }
 
+const GROUNDED_JOINT_ID = 'grounded';
+
+/**
+ * TODO:
+ * 1. Account for special versions
+ * 2. Gamepieces added to their own RigidNodes
+ */
 class MirabufParser {
+
+    private _nodeNameCounter: number = 0;
 
     private _assembly: mirabuf.Assembly;
     private _errors: Array<[severity: ParseErrorSeverity, message: string]>;
@@ -15,11 +24,147 @@ class MirabufParser {
     protected _partTreeValues: Map<string, number> = new Map();
     private _designHierarchyRoot: mirabuf.INode = new mirabuf.Node();
 
+    protected _partToNodeMap: Map<string, RigidNode> = new Map();
+    protected _rigidNodes: Array<RigidNode> = [];
+
     public constructor(assembly: mirabuf.Assembly) {
         this._assembly = assembly;
         this._errors = new Array<[ParseErrorSeverity, string]>();
 
         this.GenerateTreeValues();
+
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        const that = this;
+
+        function traverseJointParts(nodes: mirabuf.INode[], rn: RigidNode) {
+            nodes.forEach(x => {
+                if (x.children) {
+                    traverseJointParts(x.children, rn);
+                }
+                that.MovePartToRigidNode(x.value!, rn);
+            });
+        }
+
+        // 1: Initial Rigidgroups from ancestorial breaks in joints
+        (Object.keys(assembly.data!.joints!.jointInstances!) as (string)[]).forEach(key => {
+            if (key != GROUNDED_JOINT_ID) {
+                const jInst = assembly.data!.joints!.jointInstances![key];
+                const [ancestorA, ancestorB] = this.FindAncestorialBreak(jInst.parentPart!, jInst.childPart!);
+                const parentRN = this.NewRigidNode();
+                this.MovePartToRigidNode(ancestorA, parentRN);
+                this.MovePartToRigidNode(ancestorB, this.NewRigidNode());
+                if (jInst.parts && jInst.parts.nodes)
+                    traverseJointParts(jInst.parts.nodes, parentRN);
+            }
+        });
+
+        // 2: Grounded joint
+        const gInst = assembly.data!.joints!.jointInstances![GROUNDED_JOINT_ID];
+        const gNode = this.NewRigidNode();
+        
+        traverseJointParts(gInst.parts!.nodes!, gNode);
+        
+        // 3: Traverse and round up
+        function traverseNodeRoundup(node: mirabuf.INode, parentNode: RigidNode) {
+            const currentNode = that._partToNodeMap.get(node.value!);
+            if (!currentNode) {
+                that.MovePartToRigidNode(node.value!, parentNode);
+            }
+
+            if (node.children) {
+                node.children!.forEach(x => traverseNodeRoundup(x, currentNode ? currentNode : parentNode));
+            }
+        }
+        this._designHierarchyRoot.children?.forEach(x => traverseNodeRoundup(x, gNode));
+
+        // 4: Bandage via rigidgroups
+        assembly.data!.joints!.rigidGroups!.forEach(rg => {
+            const rn = this.NewRigidNode(rg.name!);
+            rg.occurrences!.forEach(y => this.MovePartToRigidNode(y, rn));
+        });
+
+        // 5. Remove Empty RNs
+        this._rigidNodes = this._rigidNodes.filter(x => x.parts.size > 0);
+    }
+
+    private NewRigidNode(suffix?: string): RigidNode {
+        const node = new RigidNode((this._nodeNameCounter++).toString() + (suffix ? `_${suffix}` : ''));
+        this._rigidNodes.push(node);
+        return node;
+    }
+
+    private MovePartToRigidNode(part: string, node: RigidNode) {
+        if (part.length < 1)
+            return;
+        
+        const original = this._partToNodeMap.get(part);
+        if (original) {
+            if (original === node)
+                return;
+
+            original.parts.delete(part);
+            this._partToNodeMap.delete(part);
+        }
+
+        node.parts.add(part);
+        this._partToNodeMap.set(part, node);
+    }
+
+    private FindAncestorialBreak(partA: string, partB: string): [string, string] {
+        if (!this._partTreeValues.has(partA) || !this._partTreeValues.has(partB)) {
+            this._errors.push([ParseErrorSeverity.LikelyIssues, 'Part not found in tree.']);
+            return [partA, partB];
+        } else if (partA == partB) {
+            this._errors.push([ParseErrorSeverity.LikelyIssues, 'Part A and B are the same.']);
+        }
+
+        const ptv = this._partTreeValues;
+
+        function binarySearch(target: number, children: mirabuf.INode[]): number {
+            let l = 0;
+            let h = children.length;
+
+            while (h - l > 1) {
+                const i = Math.floor((h + l) / 2.0);
+                const iVal = ptv.get(children[i].value!)!;
+                if (iVal > target) {
+                    h = i;
+                } else if (iVal < target) {
+                    l = i + 1;
+                } else {
+                    return i;
+                }
+            }
+
+            return Math.floor((h + l) / 2.0);
+        }
+
+        let pathA = this._designHierarchyRoot;
+        let pathB = this._designHierarchyRoot;
+        const valueA = ptv.get(partA)!;
+        const valueB = ptv.get(partB)!;
+
+        while (pathA.value! == pathB.value! && pathA.value! != partA && pathB.value! != partB) {
+            const ancestorIndexA = binarySearch(valueA, pathA.children!);
+            const ancestorValueA = ptv.get(pathA.children![ancestorIndexA].value!)!;
+            pathA = pathA.children![ancestorIndexA + (ancestorValueA < valueA ? 1 : 0)];
+
+            const ancestorIndexB = binarySearch(valueB, pathB.children!);
+            const ancestorValueB = ptv.get(pathB.children![ancestorIndexB].value!)!;
+            pathB = pathB.children![ancestorIndexB + (ancestorValueB < valueB ? 1 : 0)];
+        }
+
+        if (pathA.value! == partA && pathA.value! == pathB.value!) {
+            const ancestorIndexB = binarySearch(valueB, pathB.children!);
+            const ancestorValueB = ptv.get(pathB.children![ancestorIndexB].value!)!;
+            pathB = pathB.children![ancestorIndexB + (ancestorValueB < valueB ? 1 : 0)];
+        } else if (pathB.value! == partB && pathA.value! == pathB.value!) {
+            const ancestorIndexA = binarySearch(valueA, pathA.children!);
+            const ancestorValueA = ptv.get(pathA.children![ancestorIndexA].value!)!;
+            pathA = pathA.children![ancestorIndexA + (ancestorValueA < valueA ? 1 : 0)];
+        }
+
+        return [pathA.value!, pathB.value!];
     }
 
     private GenerateTreeValues() {
@@ -39,6 +184,18 @@ class MirabufParser {
 
         recursive(this._designHierarchyRoot);
         this._partTreeValues = partTreeValues;
+    }
+}
+
+/**
+ * Collection of mirabuf parts that are bound together
+ */
+export class RigidNode {
+    public name: string;
+    public parts: Set<string> = new Set();
+
+    public constructor(name: string) {
+        this.name = name;
     }
 }
 
--- fission/src/test/mirabuf/MirabufParser.test.ts
+++ fission/src/test/mirabuf/MirabufParser.test.ts
@@ -1,26 +1,63 @@
 import { describe, test, expect } from "vitest";
 import * as fs from 'fs';
 import { mirabuf } from "../../proto/mirabuf";
-import MirabufParser from "../../mirabuf/MirabufParser";
+import MirabufParser, { RigidNode } from "../../mirabuf/MirabufParser";
 
 describe('Mirabuf Parser Tests', () => {
     test('Test Load TestCube_v1.mira', () => {
-        const testCubeMira = mirabuf.Assembly.decode(new Uint8Array(fs.readFileSync('./public/TestCube_v1.mira')));
+        const testCubeMira = mirabuf.Assembly.decode(new Uint8Array(fs.readFileSync('./public/test_mira/TestCube_v1.mira')));
         
         expect(testCubeMira).toBeDefined();
         expect(testCubeMira.info!.name!).toBe('TestCube v1');
     });
 
-    test('Generate Design Hierarchy Node Values (TestCube_v1.mira)', () => {
-        const testCubeMira = mirabuf.Assembly.decode(new Uint8Array(fs.readFileSync('./public/TestCube_v1.mira')));
+    test('Generate Rigid Nodes (TestCube_v1.mira)', () => {
+        const testCubeMira = mirabuf.Assembly.decode(new Uint8Array(fs.readFileSync('./public/test_mira/TestCube_v1.mira')));
         
         class T extends MirabufParser {
             public get partTreeValues() { return this._partTreeValues; }
+            public get rigidNodes() { return this._rigidNodes; }
         }
 
         const t = new T(testCubeMira);
         const vals = t.partTreeValues;
+        const rn = t.rigidNodes;
 
-        vals.forEach((v, k) => console.debug(`${k} -> ${v}`));
+        expect(rn.length).toBe(1);
+        printRigidNodeParts(rn, testCubeMira);
+    });
+
+    // test('Generate JSON (PhysicsSpikeTest_v1.mira)', () => {
+    //     const testCubeMira = mirabuf.Assembly.decode(new Uint8Array(fs.readFileSync('./public/test_mira/PhysicsSpikeTest_v1.mira')));
+        
+    //     console.log('=== START JSON ===');
+    //     console.log(JSON.stringify(testCubeMira));
+    //     console.log('\n=== END JSON ===');
+
+    //     expect(true).toBe(true);
+    // });
+
+    test('Generate Rigid Nodes (PhysicsSpikeTest_v1.mira)', () => {
+        const spikeMira = mirabuf.Assembly.decode(new Uint8Array(fs.readFileSync('./public/test_mira/PhysicsSpikeTest_v1.mira')));
+        
+        class T extends MirabufParser {
+            public get partTreeValues() { return this._partTreeValues; }
+            public get rigidNodes() { return this._rigidNodes; }
+        }
+
+        const t = new T(spikeMira);
+        const vals = t.partTreeValues;
+        const rn = t.rigidNodes;
+
+        expect(rn.length).toBe(4);
+        printRigidNodeParts(rn, spikeMira);
     });
 });
+
+function printRigidNodeParts(nodes: RigidNode[], mira: mirabuf.Assembly) {
+    nodes.forEach(x => {
+        console.log(`[ ${x.name} ]:`);
+        x.parts.forEach(y => console.log(`-> '${mira.data!.parts!.partInstances![y]!.info!.name!}'`));
+        console.log('');
+    });
+}
