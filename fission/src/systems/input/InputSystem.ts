28c77adf22056d5bb7a58faff61f4f4960f17d0a
LucaHaverty <lucahaverty@gmail.com>
2024-06-21 14:43:09 -0700

    Fixed issue with key modifiers staying when the document loses focus

--- fission/src/systems/input/InputSystem.ts
+++ fission/src/systems/input/InputSystem.ts
@@ -69,7 +69,13 @@ class InputSystem extends WorldSystem {
         }
     }
 
-    public Update(_: number): void {InputSystem
+    public Update(_: number): void {
+        if (!document.hasFocus()) {
+            for (var keyCode in InputSystem._keysPressed) 
+                delete InputSystem._keysPressed[keyCode];
+            return;
+        }
+
         InputSystem._currentModifierState = { ctrl: InputSystem.isKeyPressed("ControlLeft") || InputSystem.isKeyPressed("ControlRight"), alt: InputSystem.isKeyPressed("AltLeft") || InputSystem.isKeyPressed("AltRight"), shift: InputSystem.isKeyPressed("ShiftLeft") || InputSystem.isKeyPressed("ShiftRight"), meta: InputSystem.isKeyPressed("MetaLeft") || InputSystem.isKeyPressed("MetaRight") }
     }
 
