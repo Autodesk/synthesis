9571656de834ba6269c488bbe46f98d64a78a260
BrandonPacewic <pacewicbrandon@gmail.com>
2024-06-21 11:38:57 -0700

    Shorter embedded python

--- scripts/format/isort_format_validation.py
+++ scripts/format/isort_format_validation.py
@@ -2,39 +2,18 @@ import os
 import subprocess
 import sys
 
-FILES_DIRS = ["exporter/SynthesisFusionAddin"]
-FILE_TARGETS = [".py", ".pyi"]
+EXPORTER_DIR = "exporter/SynthesisFusionAddin"
+files = (lambda d: [os.path.abspath(os.path.join(root, filename)) for root, _, filenames in os.walk(d)
+         for filename in filenames if filename.endswith(".py")])(EXPORTER_DIR)
+oldFileStates = [open(file, "r").readlines() for file in files]
+subprocess.call(["isort", EXPORTER_DIR], bufsize=1, shell=False)
+newFileStates = [open(file, "r").readlines() for file in files]
+exitCode = 0
+for i, (oldFileState, newFileState) in enumerate(zip(oldFileStates, newFileStates)):
+  for j, (previousLine, newLine) in enumerate(zip(oldFileState, newFileState)):
+    if previousLine != newLine:
+      print(f"File {files[i]} is not formatted correctly!\nLine: {j + 1}")
+      exitCode = 1
 
-def main():
-    if sys.platform != "linux":
-        print("Warning: This script was designed to be run by github action linux machines")
-
-    files = []
-    for dir in FILES_DIRS:
-        for root, _, filenames in os.walk(dir):
-            for filename in filenames:
-                if os.path.splitext(filename)[1] in FILE_TARGETS:
-                    files.append(os.path.abspath(os.path.join(root, filename)))
-
-    file_states = [open(file, "r").readlines() for file in files]
-    for dir in FILES_DIRS:
-        subprocess.call(["isort", dir], bufsize=1, shell=False,)
-
-    new_file_states = [open(file, "r").readlines() for file in files]
-    exit_code = 0
-    for i, (previous_file_state, new_file_state) in enumerate(zip(file_states, new_file_states)):
-        for j, (previous_line, new_line) in enumerate(zip(previous_file_state, new_file_state)):
-            if previous_line != new_line:
-                print(f"File {files[i]} is not formatted correctly!")
-                print(f"Line: {j + 1}")
-                exit_code = 1
-                break
-
-    if exit_code == 0:
-        print("All files are formatted correctly!")
-
-    sys.exit(exit_code)
-
-
-if __name__ == '__main__':
-    main()
+if not exitCode: print("All files are formatted correctly with isort!")
+sys.exit(exitCode)
