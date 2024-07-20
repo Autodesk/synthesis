117f0d13a011238d21b8faedb641a5697f73c612
KyroVibe <hunter.barclay18@gmail.com>
2024-07-19 23:58:39 -0600

    Recreated orbit camera controls

--- fission/src/systems/scene/CameraControls.ts
+++ fission/src/systems/scene/CameraControls.ts
@@ -67,22 +67,32 @@ class PointerHandler {
     private _wheelMove: (ev: WheelEvent) => void
     private _pointerDown: (ev: PointerEvent) => void
     private _pointerUp: (ev: PointerEvent) => void
+    private _contextMenu: (ev: MouseEvent) => unknown
 
     private _domElement: HTMLElement
 
-    private _isPointerDown: boolean = false
-    public get isPointerDown() { return this._isPointerDown }
-
-    public constructor(domElement: HTMLElement, pointerMove: (ev: PointerEvent) => void, wheelMove: (ev: WheelEvent) => void) {
+    public constructor(
+        domElement: HTMLElement,
+        pointerDown: (ev: PointerEvent) => void,
+        pointerUp: (ev: PointerEvent) => void,
+        pointerMove: (ev: PointerEvent) => void,
+        wheelMove: (ev: WheelEvent) => void,
+        onContextMenu?: (ev: MouseEvent) => unknown
+    ) {
         this._pointerMove = pointerMove
         this._wheelMove = wheelMove
         this._domElement = domElement
 
-        this._pointerDown = (ev) => { this._isPointerDown = true }
-        this._pointerUp = (ev) => { this._isPointerDown = false }
+        this._pointerDown = pointerDown
+        this._pointerUp = pointerUp
+
+        this._contextMenu = onContextMenu ?? ((ev: MouseEvent) => {
+            ev.preventDefault()
+        }) 
         
         this._domElement.addEventListener("pointermove", this._pointerMove)
-        this._domElement.addEventListener("wheel", this._wheelMove)
+        this._domElement.addEventListener("wheel", this._wheelMove, { passive: false })
+        this._domElement.addEventListener("contextmenu", this._contextMenu)
         
         this._domElement.addEventListener("pointerdown", this._pointerDown)
         this._domElement.addEventListener("pointerup", this._pointerUp)
@@ -93,6 +103,7 @@ class PointerHandler {
     public dispose() {
         this._domElement.removeEventListener("pointermove", this._pointerMove)
         this._domElement.removeEventListener("wheel", this._wheelMove)
+        this._domElement.removeEventListener("contextmenu", this._contextMenu)
 
         this._domElement.removeEventListener("pointerdown", this._pointerDown)
         this._domElement.removeEventListener("pointerup", this._pointerUp)
@@ -100,6 +111,25 @@ class PointerHandler {
         this._domElement.removeEventListener("pointerleave", this._pointerUp)
     }
 }
+
+interface SphericalCoords {
+    theta: number
+    phi: number
+    r: number
+}
+
+const CO_MAX_ZOOM = 40.0
+const CO_MIN_ZOOM = 0.1
+const CO_MAX_PHI = Math.PI / 2.1
+const CO_MIN_PHI = -Math.PI / 2.1
+
+const CO_SENSITIVITY_ZOOM = 3.0
+const CO_SENSITIVITY_PHI = 1.0
+const CO_SENSITIVITY_THETA = 1.0
+
+const CO_DEFAULT_ZOOM = 3.5
+const CO_DEFAULT_PHI = -Math.PI / 6.0
+const CO_DEFAULT_THETA = -Math.PI / 4.0
 
 export class CustomOrbitControls extends CameraControls {
     private _enabled = true
@@ -109,7 +139,11 @@ export class CustomOrbitControls extends CameraControls {
 
     private _pointerHandler: PointerHandler
 
-    private _omega: THREE.Vec2
+    private _isPointerDown: boolean
+    private _currentPos: SphericalCoords
+    private _lastPos: SphericalCoords
+    private _coords: SphericalCoords
+    private _focus: THREE.Vector3
 
     public set enabled(val: boolean) {
         this._enabled = val
@@ -124,29 +158,77 @@ export class CustomOrbitControls extends CameraControls {
         this._mainCamera = mainCamera
         this._domElement = domElement
 
-        this._omega = { x: 0, y: 0 }
+        this._currentPos = { theta: CO_DEFAULT_THETA, phi: CO_DEFAULT_PHI, r: CO_DEFAULT_ZOOM }
+        this._lastPos = this._currentPos
+        this._coords = { theta: CO_DEFAULT_THETA, phi: CO_DEFAULT_PHI, r: CO_DEFAULT_ZOOM }
+        this._isPointerDown = false
+
+        this._focus = new THREE.Vector3(0,0,0)
 
         this._pointerHandler = new PointerHandler(domElement,
-            (ev) => {
-                this._omega = { x: ev.movementX, y: ev.movementY }
-            },
-            (ev) => {
-
-            }
+            (ev) => this.pointerDown(ev),
+            (ev) => this.pointerUp(ev),
+            (ev) => this.pointerMove(ev),
+            (ev) => this.wheelMove(ev)
         )
     }
 
+    public wheelMove(ev: WheelEvent) {
+        this._currentPos.r += ev.deltaY * 0.01
+    }
+
+    public pointerUp(ev: PointerEvent) {
+        this._isPointerDown = false
+    }
+
+    public pointerDown(ev: PointerEvent) {
+        this._isPointerDown = ev.button == 0
+    }
+
+    public pointerMove(ev: PointerEvent) {
+        if (!this._isPointerDown) {
+            return
+        }
+
+        this._currentPos.theta -= ev.movementX
+        this._currentPos.phi -= ev.movementY
+
+        console.debug(this._currentPos)
+    }
+
     public update(deltaT: number): void {
-        if (this._pointerHandler.isPointerDown)
-        console.debug(this._omega)
+        const omega: SphericalCoords = this.enabled ? {
+            theta: this._currentPos.theta - this._lastPos.theta,
+            phi: this._currentPos.phi - this._lastPos.phi,
+            r: this._currentPos.r - this._lastPos.r
+        } : { theta: 0, phi: 0, r: 0 }
+
+        this._lastPos = { theta: this._currentPos.theta, phi: this._currentPos.phi, r: this._currentPos.r }
+
+        this._coords.theta += omega.theta * deltaT * CO_SENSITIVITY_THETA
+        this._coords.phi += omega.phi * deltaT * CO_SENSITIVITY_PHI
+        this._coords.r += omega.r * deltaT * CO_SENSITIVITY_ZOOM * Math.pow(this._coords.r, 1.6)
+
+        this._coords.phi = Math.min(CO_MAX_PHI, Math.max(CO_MIN_PHI, this._coords.phi))
+        this._coords.r = Math.min(CO_MAX_ZOOM, Math.max(CO_MIN_ZOOM, this._coords.r))
+
+        const deltaTransform = (new THREE.Matrix4()).makeTranslation(0, 0, this._coords.r)
+            .premultiply((new THREE.Matrix4()).makeRotationFromEuler(new THREE.Euler(this._coords.phi, this._coords.theta, 0, "YXZ")))
+
+        deltaTransform.premultiply((new THREE.Matrix4()).makeTranslation(this._focus))
+
+        this._mainCamera.position.setFromMatrixPosition(deltaTransform)
+        this._mainCamera.rotation.setFromRotationMatrix(deltaTransform)
     }
 
     public setFocus(vec: THREE.Vector3): void {
-    
+        if (this.enabled) {
+            this._focus.copy(vec)
+        }
     }
 
     public dispose(): void {
-        
+        this._pointerHandler.dispose()
     }
 
 }
