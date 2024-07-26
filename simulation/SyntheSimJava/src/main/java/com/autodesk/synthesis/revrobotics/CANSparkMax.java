34a7933a5cd93e49eb0cd83b62353466f7a28262
Azalea Colburn <azalea.colburn@autodesk.com>
2024-07-25 20:04:32 -0700

    Send all data through CANMotor to the websocket

--- simulation/SyntheSimJava/src/main/java/com/autodesk/synthesis/revrobotics/CANSparkMax.java
+++ simulation/SyntheSimJava/src/main/java/com/autodesk/synthesis/revrobotics/CANSparkMax.java
@@ -15,8 +15,9 @@ public class CANSparkMax extends com.revrobotics.CANSparkMax {
     /**
      * Creates a new CANSparkMax, wrapped with simulation support.
      * 
-     * @param deviceId CAN Device ID.
-     * @param motorType Motortype. For Simulation purposes, this is discarded at the moment.
+     * @param deviceId  CAN Device ID.
+     * @param motorType Motortype. For Simulation purposes, this is discarded at the
+     *                  moment.
      */
     public CANSparkMax(int deviceId, MotorType motorType) {
         super(deviceId, motorType);
@@ -29,6 +30,14 @@ public class CANSparkMax extends com.revrobotics.CANSparkMax {
     public void set(double percent) {
         super.set(percent);
         m_motor.setPercentOutput(percent);
+    }
+
+    public void test() {
+        System.out.println("test");
+    }
+
+    public void setNeutralDeadband(double n) {
+        m_motor.setNeutralDeadband(n);
     }
 
     @Override
--- simulation/samples/JavaSample/src/main/java/frc/robot/Robot.java
+++ simulation/samples/JavaSample/src/main/java/frc/robot/Robot.java
@@ -4,9 +4,6 @@
 
 package frc.robot;
 
-// import com.ctre.phoenix6.hardware.TalonFX;
-// import com.revrobotics.CANSparkBase.IdleMode;
-// import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -18,115 +15,138 @@ import com.autodesk.synthesis.revrobotics.CANSparkMax;
 import com.autodesk.synthesis.ctre.TalonFX;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private Spark m_Spark = new Spark(0);
-  private CANSparkMax m_SparkMax = new CANSparkMax(1, MotorType.kBrushless);
-  private TalonFX m_Talon = new TalonFX(2);
+    // private Spark m_Spark = new Spark(0);
+    private CANSparkMax m_SparkMax = new CANSparkMax(1, MotorType.kBrushless);
+    private TalonFX m_Talon = new TalonFX(2);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
-
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-
-    m_Spark.set(0.5);
-    m_SparkMax.set(1.0);
-    m_Talon.set(-1.0);
-
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+        m_chooser.addOption("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
     }
-  }
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items
+     * like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+    }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-    m_Spark.set(0.25);
-    m_SparkMax.set(0.75);
-    m_Talon.set(-0.5);
-  }
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different
+     * autonomous modes using the dashboard. The sendable chooser code works with
+     * the Java
+     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+     * chooser code and
+     * uncomment the getString line to get the auto name from the text box below the
+     * Gyro
+     *
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to the
+     * switch structure
+     * below with additional strings. If using the SendableChooser make sure to add
+     * them to the
+     * chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
+        m_autoSelected = m_chooser.getSelected();
+        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+        System.out.println("Auto selected: " + m_autoSelected);
+    }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {
-    m_Spark.set(0.0);
-    m_SparkMax.set(0.0);
-    m_Talon.set(0.0);
-  }
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+        // m_Spark.set(0.5);
+        m_SparkMax.set(1.0);
+        // m_SparkMax.setNeutralDeadband(0.01); // FIXME: for some reason I can't set
+        // the deadband even after defining the function in the child
+        m_Talon.set(-1.0);
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+        switch (m_autoSelected) {
+            case kCustomAuto:
+                // Put custom auto code here
+                break;
+            case kDefaultAuto:
+            default:
+                // Put default auto code here
+                break;
+        }
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This function is called once when teleop is enabled. */
+    @Override
+    public void teleopInit() {
+    }
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {
+        // m_Spark.set(0.25);
+        m_SparkMax.set(0.75);
+        m_Talon.set(-0.5);
+    }
 
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+    /** This function is called once when the robot is disabled. */
+    @Override
+    public void disabledInit() {
+        // m_Spark.set(0.0);
+        m_SparkMax.set(0.0);
+        m_Talon.set(0.0);
+    }
+
+    /** This function is called periodically when disabled. */
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    /** This function is called once when test mode is enabled. */
+    @Override
+    public void testInit() {
+    }
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {
+    }
+
+    /** This function is called once when the robot is first started up. */
+    @Override
+    public void simulationInit() {
+    }
+
+    /** This function is called periodically whilst in simulation. */
+    @Override
+    public void simulationPeriodic() {
+    }
 }
