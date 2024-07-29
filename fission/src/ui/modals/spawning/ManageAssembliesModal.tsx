9159dda77d0d3856fd902615dbc65c3cd2249423
KyroVibe <hunter.barclay18@gmail.com>
2024-07-29 17:45:03 -0600

    Change delete button for manage assemblies, removed view button from MainHUD, fixed temp scoreboard reset

--- fission/src/ui/modals/spawning/ManageAssembliesModal.tsx
+++ fission/src/ui/modals/spawning/ManageAssembliesModal.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useReducer } from "react"
 import Modal, { ModalPropsImpl } from "@/components/Modal"
 import { FaWrench } from "react-icons/fa6"
-import Button from "@/components/Button"
+import Button, { ButtonProps, ButtonSize } from "@/components/Button"
 import Label, { LabelSize } from "@/components/Label"
 import World from "@/systems/World"
 import MirabufSceneObject from "@/mirabuf/MirabufSceneObject"
@@ -10,10 +10,23 @@ import { setSelectedBrainIndexGlobal } from "@/ui/panels/configuring/ChooseInput
 import SynthesisBrain from "@/systems/simulation/synthesis_brain/SynthesisBrain"
 import { useModalControlContext } from "@/ui/ModalContext"
 import InputSystem from "@/systems/input/InputSystem"
+import { HiDownload } from "react-icons/hi"
+import { IoTrashBin } from "react-icons/io5"
 
 interface AssemblyCardProps {
     mira: MirabufSceneObject
     update: React.DispatchWithoutAction
+}
+
+const ButtonSecondary: React.FC<ButtonProps> = ({ value, onClick }) => {
+    return (
+        <Button
+            sizeOverrideClass="px-6 py-2"
+            value={value}
+            onClick={onClick}
+            colorOverrideClass="bg-cancel-button hover:brightness-90"
+        ></Button>
+    )
 }
 
 const AssemblyCard: React.FC<AssemblyCardProps> = ({ mira, update }) => {
@@ -30,6 +43,7 @@ const AssemblyCard: React.FC<AssemblyCardProps> = ({ mira, update }) => {
             <Label className="text-wrap break-all">{`[${InputSystem.brainIndexSchemeMap.get(brain)?.schemeName ?? "-"}] ${mira.assemblyName}`}</Label>
             <div className="flex flex-row gap-4">
                 <Button
+                    sizeOverrideClass="px-6 py-2"
                     value="Set Scheme"
                     onClick={() => {
                         setSelectedBrainIndexGlobal(brain)
@@ -37,8 +51,8 @@ const AssemblyCard: React.FC<AssemblyCardProps> = ({ mira, update }) => {
                         closeModal()
                     }}
                 />
-                <Button
-                    value="Delete"
+                <ButtonSecondary
+                    value={<IoTrashBin />}
                     onClick={() => {
                         World.SceneRenderer.RemoveSceneObject(mira.id)
                         update()
@@ -50,9 +64,6 @@ const AssemblyCard: React.FC<AssemblyCardProps> = ({ mira, update }) => {
 }
 
 const ManageAssembliesModal: React.FC<ModalPropsImpl> = ({ modalId }) => {
-    // update tooltip based on type of drivetrain, receive message from Synthesis
-    // const { showTooltip } = useTooltipControlContext()
-
     const [_, update] = useReducer(x => !x, false)
 
     const assemblies = [...World.SceneRenderer.sceneObjects.entries()]
@@ -63,12 +74,12 @@ const ManageAssembliesModal: React.FC<ModalPropsImpl> = ({ modalId }) => {
         .map(x => x[1] as MirabufSceneObject)
 
     return (
-        <Modal name={"Manage Assemblies"} icon={<FaWrench />} modalId={modalId}>
+        <Modal name={"Manage Assemblies"} icon={<FaWrench />} modalId={modalId} acceptEnabled={false} cancelName="Back">
             <div className="flex overflow-y-auto flex-col gap-2 min-w-[50vw] max-h-[60vh] bg-background-secondary rounded-md p-2">
                 <Label size={LabelSize.Medium} className="text-center border-b-[1pt] mt-[4pt] mb-[2pt] mx-[5%]">
                     {assemblies ? `${assemblies.length} Assemblies` : "No Assemblies"}
                 </Label>
-                {assemblies.map(x => AssemblyCard({ mira: x, update: update }))}
+                {assemblies.map(x => <AssemblyCard key={x.id} mira={x} update={update} />)}
             </div>
         </Modal>
     )
