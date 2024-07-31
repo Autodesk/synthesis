dce50890903aafb1194093315918c995f9680cad
BrandonPacewic <pacewicbrandon@gmail.com>
2024-07-31 09:29:38 -0700

    Basic installer done

--- installer/OSX/build.sh
+++ installer/OSX/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-FUSION_ADDIN_LOCATION="Support/Autodesk/Autodesk\ Fusion\ 360/API/AddIns/"
-EXPORTER_SOURCE_DIR=../../exporter/
-FUSION_ADDIN_LOCATION=~/Documents/
+FUSION_ADDIN_LOCATION=~/Library/Application\ Support/Autodesk/Autodesk\ Fusion\ 360/API/AddIns/
+EXPORTER_SOURCE_DIR=../../exporter/SynthesisFusionAddin/
 
-pkgbuild --root $EXPORTER_SOURCE_DIR --identifier com.Autodesk.Synthesis --version 1.0 --scripts Scripts --install-location $FUSION_ADDIN_LOCATION MyApp.pkg
+mkdir -p tmp/
+cp -r "$EXPORTER_SOURCE_DIR"/* tmp/
+
+pkgbuild --root tmp/ --identifier com.Autodesk.Synthesis --version 1.0 --install-location "$FUSION_ADDIN_LOCATION" MyApp.pkg
 productbuild --distribution distribution.xml --package-path . MyAppInstaller.pkg
+
+rm -r tmp/
