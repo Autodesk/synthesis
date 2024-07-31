ee6cf471512a36a9425d573e1ee80833c74befd4
PepperLola <PepperLola@users.noreply.github.com>
2024-07-31 14:38:13 -0700

    ran formatter

--- fission/src/ui/modals/APSManagementModal.tsx
+++ fission/src/ui/modals/APSManagementModal.tsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react"
 import Modal, { ModalPropsImpl } from "@/components/Modal"
 import Stack, { StackDirection } from "@/components/Stack"
-import Label, { LabelSize } from "@/components/Label"
 import { HiUser } from "react-icons/hi"
 import APS from "@/aps/APS"
 
 const APSManagementModal: React.FC<ModalPropsImpl> = ({ modalId }) => {
-    const [userInfo, setUserInfo] = useState(APS.userInfo)
+    const [userInfo, _] = useState(APS.userInfo)
     return (
-        <Modal name={userInfo?.name ?? "Not signed in"} icon={
-            userInfo?.picture ?
-                <img src={userInfo?.picture} className="h-10 rounded-full" /> :
-                <HiUser />
-        } modalId={modalId} acceptName="Logout" onAccept={() => {
-            APS.logout()
-        }}>
-            <Stack direction={StackDirection.Vertical} spacing={10}>
-            </Stack>
+        <Modal
+            name={userInfo?.name ?? "Not signed in"}
+            icon={userInfo?.picture ? <img src={userInfo?.picture} className="h-10 rounded-full" /> : <HiUser />}
+            modalId={modalId}
+            acceptName="Logout"
+            onAccept={() => {
+                APS.logout()
+            }}
+        >
+            <Stack direction={StackDirection.Vertical} spacing={10}></Stack>
         </Modal>
     )
 }
