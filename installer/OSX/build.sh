0d51cf7294c6501833234a43be9fe1769a97e47e
BrandonPacewic <pacewicbrandon@gmail.com>
2024-07-31 10:41:55 -0700

    Bump exporter version

--- installer/OSX/build.sh
+++ installer/OSX/build.sh
@@ -6,7 +6,8 @@ EXPORTER_SOURCE_DIR=../../exporter/SynthesisFusionAddin/
 mkdir -p tmp/
 cp -r "$EXPORTER_SOURCE_DIR"/* tmp/
 
-pkgbuild --root tmp/ --identifier com.Autodesk.Synthesis --version 1.0 --install-location "$FUSION_ADDIN_LOCATION" MyApp.pkg
-productbuild --distribution distribution.xml --package-path . MyAppInstaller.pkg
+pkgbuild --root tmp/ --identifier com.Autodesk.Synthesis --version 2.0.0 --install-location "$FUSION_ADDIN_LOCATION" SynthesisExporter.pkg
+productbuild --distribution distribution.xml --package-path . SynthesisExporterInstaller.pkg
 
+rm SynthesisExporter.pkg
 rm -r tmp/
