b7795a84fb6e4fc164e41555eebeabbbba3c9c05
BrandonPacewic <pacewicbrandon@gmail.com>
2024-08-01 14:28:56 -0700

    Formatting and dest updates for the windows installer

--- installer/Windows/build.bat
+++ installer/Windows/build.bat
@@ -7,10 +7,10 @@ set "EXPORTER_SOURCE_DIR=..\..\exporter\SynthesisFusionAddin\"
 
 mkdir tmp\
 xcopy /e /i "%EXPORTER_SOURCE_DIR%" tmp\
-@REM pushd tmp\
-tar -a -c -f resources.zip tmp\*
-@REM popd
+tar -a -c -f SynthesisExporter.zip tmp\*
 
+@REM Find and run pyinstaller, this is a workaround that allows you to call pip packages as scripts without
+@REM them being added to the system PATH.
 for /f "delims=" %%i in ('pip show pyinstaller') do (
     echo %%i | findstr /b /c:"Location:" >nul
     if not errorlevel 1 (
@@ -23,9 +23,9 @@ for %%a in ("%executable%") do set "executable=%%~dpa"
 set "executable=%executable%Scripts\pyinstaller.exe "
 set executable=%executable:~0,-1%
 
-%executable% --onefile --add-data "resources.zip;." installer.py
+%executable% --onefile --add-data "SynthesisExporter.zip;." installer.py
 
 rmdir /s /q tmp
-del resources.zip
+del SynthesisExporter.zip
 
 endlocal
--- installer/Windows/installer.py
+++ installer/Windows/installer.py
@@ -1,57 +1,51 @@
 import os
 import shutil
 import sys
-import zipfile
 import tempfile
+import zipfile
+
 
 def resource_path(relative_path):
-    try:
+    if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
         base_path = sys._MEIPASS
-    except Exception:
+    else:
         base_path = os.path.dirname(__file__)
+
     return os.path.join(base_path, relative_path)
+
 
 def extract_file(resource_name, dest_folder):
     resource_full_path = resource_path(resource_name)
     if not os.path.exists(resource_full_path):
         raise FileNotFoundError(f"Resource '{resource_name}' not found.")
-    
+
     shutil.copy(resource_full_path, os.path.join(dest_folder, resource_name))
+
 
 def move_folder(src_folder, dest_folder):
     if not os.path.exists(src_folder):
         print(f"Source folder '{src_folder}' does not exist.")
         return
-    
+
     if not os.path.exists(dest_folder):
         print(f"Destination folder '{dest_folder}' does not exist. Creating it.")
         os.makedirs(dest_folder)
-    
-    try:
-        shutil.move(src_folder, dest_folder)
-        print(f"Successfully moved '{src_folder}' to '{dest_folder}'.")
-    except Exception as e:
-        print(f"Error: {e}")
+
+    shutil.move(src_folder, dest_folder)
+    print(f"Successfully moved '{src_folder}' to '{dest_folder}'.")
+
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: installer.exe <destination_folder>")
-        sys.exit(1)
-    
-    destination_folder = sys.argv[1]
+    destination_folder = r"%appdata%\Autodesk\Autodesk Fusion\API\AddIns"
 
     with tempfile.TemporaryDirectory() as temp_dir:
-        try:
-            extract_file('resources.zip', temp_dir)
-            
-            with zipfile.ZipFile(os.path.join(temp_dir, 'resources.zip'), 'r') as zip_ref:
-                zip_ref.extractall(temp_dir)
-            
-            src_folder = os.path.join(temp_dir, 'tmp')
-            move_folder(src_folder, destination_folder)
-        
-        except Exception as e:
-            print(f"Error: {e}")
+        extract_file("resources.zip", temp_dir)
+        with zipfile.ZipFile(os.path.join(temp_dir, "resources.zip"), "r") as zip_ref:
+            zip_ref.extractall(temp_dir)
+
+        src_folder = os.path.join(temp_dir, "tmp")
+        move_folder(src_folder, destination_folder)
+
 
 if __name__ == "__main__":
     main()
