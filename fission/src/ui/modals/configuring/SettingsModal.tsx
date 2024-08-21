21f318d1dbca4d7d2e2d37a620087bcdcbee9cc6
LucaHaverty <lucahaverty@gmail.com>
2024-08-21 11:10:11 -0700

    Reverted change to settings modal

--- fission/src/ui/modals/configuring/SettingsModal.tsx
+++ fission/src/ui/modals/configuring/SettingsModal.tsx
@@ -66,7 +66,7 @@ const SettingsModal: React.FC<ModalPropsImpl> = ({ modalId }) => {
                 saveSettings()
             }}
         >
-            <div className="flex overflow-y-auto flex-col gap-2 bg-background-secondary rounded-md p-2 max-h-[60vh]">
+            <div className="flex overflow-y-auto flex-col gap-2 bg-background-secondary rounded-md p-2 min-w-[22vw]">
                 <Label size={LabelSize.Medium}>Screen Settings</Label>
                 <Dropdown
                     label="Quality Settings"
