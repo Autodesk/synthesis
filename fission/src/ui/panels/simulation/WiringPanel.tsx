30bcf0869804654cab823066f4f3cc18ad229705
HunterBarclay <hunter.barclay18@gmail.com>
2024-10-27 13:55:58 -0600

    feat(task): Labels

--- fission/src/ui/panels/simulation/WiringPanel.tsx
+++ fission/src/ui/panels/simulation/WiringPanel.tsx
@@ -2,14 +2,17 @@ import Panel, { PanelPropsImpl } from "@/components/Panel"
 import { SynthesisIcons } from "@/ui/components/StyledComponents"
 import Label, { LabelSize } from "@/ui/components/Label"
 import { colorNameToVar } from "@/ui/ThemeContext"
-import { useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 const EDGE_FLAT = 0.1
-const EDGE_CURVE = 0.5
+const EDGE_CURVE = 0.3
+const NODE_SIZE = 10
+const NODE_LABEL_Y_OFFSET = NODE_SIZE + 5
 
 type NodeProps = {
     x: number,
-    y: number
+    y: number,
+    label?: string
 }
 
 type EdgeProps = {
@@ -30,15 +33,37 @@ function Edge({ nodeA, nodeB }: EdgeProps) {
 
     return (
         <svg>
-            <path stroke="white" fill="none" d={cmds} />
+            <path strokeWidth={"0.125rem"} stroke="white" fill="none" d={cmds} />
         </svg>
     )
 }
 
-function Node({ x, y }: NodeProps) {
+function Node({ x, y, label }: NodeProps) {
+
+    const onClick = useCallback((e: React.MouseEvent<SVGCircleElement>) => {
+        console.debug(`Clicked node: ${label ? label : `${x.toFixed(1)}, ${y.toFixed(1)}`}`)
+    }, [label, x, y])
+
     return (
         <svg>
-            <circle r={20} cx={x} cy={y} fill={colorNameToVar("InteractiveBackground")} strokeWidth={"0.125rem"} stroke={colorNameToVar("InteractiveElementSolid")} />
+            <circle
+                onClick={onClick}
+                r={NODE_SIZE}
+                cx={x} cy={y}
+                fill={colorNameToVar("InteractiveBackground")}
+                strokeWidth={"0.125rem"}
+                stroke={colorNameToVar("InteractiveElementSolid")}
+            />
+            {label ? (<text
+                className="Rrrrr"
+                fontSize="1rem"
+                textAnchor="middle"
+                style={{ transform: "translate(0, 0px)" }}
+                fill="white"
+                x={x} y={y - NODE_LABEL_Y_OFFSET}>
+                    {label}
+                </text>
+            ) : (<></>)}
         </svg>
     )
 }
@@ -85,9 +110,9 @@ function WiringPanel({ panelId }: PanelPropsImpl) {
             <div className="w-[70vw] h-[70vh]">
                 <Label size={LabelSize.Large}>Hunter's Messy Brain</Label>
                 <svg ref={svgRef} className="absolute top-0 left-0 w-full h-full">
-                    <Edge nodeA={nodeA} nodeB={nodeB} />åå
-                    <Node x={nodeA.x} y={nodeA.y} />
-                    <Node x={nodeB.x} y={nodeB.y} />
+                    <Edge nodeA={nodeA} nodeB={nodeB} />
+                    <Node x={nodeA.x} y={nodeA.y} label="Node A" />
+                    <Node x={nodeB.x} y={nodeB.y} label="Node B" />
                 </svg>
             </div>
         </Panel>
