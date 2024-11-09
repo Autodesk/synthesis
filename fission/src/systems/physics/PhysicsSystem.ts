2c5cbcf0408d469ffefc70c779603047367efbf4
HunterBarclay <hunter.barclay18@gmail.com>
2024-11-09 13:16:21 -0700

    fix: One more approach to try

--- fission/src/systems/physics/PhysicsSystem.ts
+++ fission/src/systems/physics/PhysicsSystem.ts
@@ -326,6 +326,7 @@ class PhysicsSystem extends WorldSystem {
      */
     public CreateJointsFromParser(parser: MirabufParser, mechanism: Mechanism) {
         const jointData = parser.assembly.data!.joints!
+        console.debug(jointData)
         for (const [jGuid, jInst] of Object.entries(jointData.jointInstances!) as [
             string,
             mirabuf.joint.JointInstance,
@@ -701,17 +702,19 @@ class PhysicsSystem extends WorldSystem {
         const yawAxis = new JOLT.Vec3(yawDof?.axis?.x ?? 0, yawDof?.axis?.y ?? 0, yawDof?.axis?.z ?? 0)
         const rollAxis = new JOLT.Vec3(rollDof?.axis?.x ?? 0, rollDof?.axis?.y ?? 0, rollDof?.axis?.z ?? 0)
 
+        console.debug(`Anchor Point: ${joltVec3ToString(anchorPoint)}`)
         console.debug(`Pitch Axis: ${joltVec3ToString(pitchAxis)} ${pitchDof?.limits ? `[${pitchDof.limits.lower!.toFixed(3)}, ${pitchDof.limits.upper!.toFixed(3)}]` : ''}`)
         console.debug(`Yaw Axis: ${joltVec3ToString(yawAxis)} ${yawDof?.limits ? `[${yawDof.limits.lower!.toFixed(3)}, ${yawDof.limits.upper!.toFixed(3)}]` : ''}`)
         console.debug(`Roll Axis: ${joltVec3ToString(rollAxis)} ${rollDof?.limits ? `[${rollDof.limits.lower!.toFixed(3)}, ${rollDof.limits.upper!.toFixed(3)}]` : ''}`)
 
-        const constraints: { axis: Jolt.Vec3, value: number, upper?: number, lower?: number }[] = []
+        const constraints: { axis: Jolt.Vec3, friction: number, value: number, upper?: number, lower?: number }[] = []
 
         if (pitchDof?.limits && (pitchDof.limits.upper ?? 0) - (pitchDof.limits.lower ?? 0) < 0.001) {
             console.debug('Pitch Fixed')
         } else {
             constraints.push({
                 axis: pitchAxis,
+                friction: 0.0,
                 value: pitchDof?.value ?? 0,
                 upper: pitchDof?.limits ? pitchDof.limits.upper ?? 0 : undefined,
                 lower: pitchDof?.limits ? pitchDof.limits.lower ?? 0 : undefined
@@ -723,6 +726,7 @@ class PhysicsSystem extends WorldSystem {
         } else {
             constraints.push({
                 axis: yawAxis,
+                friction: 0.0,
                 value: yawDof?.value ?? 0,
                 upper: yawDof?.limits ? yawDof.limits.upper ?? 0 : undefined,
                 lower: yawDof?.limits ? yawDof.limits.lower ?? 0 : undefined
@@ -734,14 +738,15 @@ class PhysicsSystem extends WorldSystem {
         } else {
             constraints.push({
                 axis: rollAxis,
+                friction: 0.0,
                 value: rollDof?.value ?? 0,
                 upper: rollDof?.limits ? rollDof.limits.upper ?? 0 : undefined,
                 lower: rollDof?.limits ? rollDof.limits.lower ?? 0 : undefined
             })
         }
 
-        let bodyStart = bodyA
-        let bodyNext = bodyB
+        let bodyStart = bodyB
+        let bodyNext = bodyA
         if (constraints.length > 1) {
             console.debug('Starting with Ghost Body')
             bodyNext = this.CreateGhostBody(anchorPoint)
@@ -752,7 +757,7 @@ class PhysicsSystem extends WorldSystem {
             console.debug(`Constraint ${i}`)
             const c = constraints[i]
             const hingeSettings = new JOLT.HingeConstraintSettings()
-            hingeSettings.mMaxFrictionTorque = 0.1;
+            hingeSettings.mMaxFrictionTorque = c.friction;
             hingeSettings.mPoint1 = hingeSettings.mPoint2 = anchorPoint
             hingeSettings.mHingeAxis1 = hingeSettings.mHingeAxis2 = c.axis.Normalized()
             hingeSettings.mNormalAxis1 = hingeSettings.mNormalAxis2 = getPerpendicular(
@@ -776,8 +781,8 @@ class PhysicsSystem extends WorldSystem {
             this._constraints.push(hingeConstraint)
             bodyStart = bodyNext
             if (i == constraints.length - 2) {
-                bodyNext = bodyB
-                console.debug('Finishing with Body B')
+                bodyNext = bodyA
+                console.debug('Finishing with Body A')
             } else {
                 console.debug('New Ghost Body')
                 bodyNext = this.CreateGhostBody(anchorPoint)
@@ -786,6 +791,121 @@ class PhysicsSystem extends WorldSystem {
             }
         }
     }
+
+    // private CreateBallBadAgainConstraint(
+    //     jointInstance: mirabuf.joint.JointInstance,
+    //     jointDefinition: mirabuf.joint.Joint,
+    //     bodyA: Jolt.Body,
+    //     bodyB: Jolt.Body,
+    //     mechanism: Mechanism,
+    // ): void {
+
+    //     const jointOrigin = jointDefinition.origin
+    //         ? MirabufVector3_JoltVec3(jointDefinition.origin as mirabuf.Vector3)
+    //         : new JOLT.Vec3(0, 0, 0)
+    //     // TODO: Offset transformation for robot builder.
+    //     const jointOriginOffset = jointInstance.offset
+    //         ? MirabufVector3_JoltVec3(jointInstance.offset as mirabuf.Vector3)
+    //         : new JOLT.Vec3(0, 0, 0)
+
+    //     const anchorPoint = jointOrigin.Add(jointOriginOffset)
+
+    //     const pitchDof = jointDefinition.custom!.dofs!.at(0)
+    //     const yawDof = jointDefinition.custom!.dofs!.at(1)
+    //     const rollDof = jointDefinition.custom!.dofs!.at(2)
+    //     const pitchAxis = new JOLT.Vec3(pitchDof?.axis?.x ?? 0, pitchDof?.axis?.y ?? 0, pitchDof?.axis?.z ?? 0)
+    //     const yawAxis = new JOLT.Vec3(yawDof?.axis?.x ?? 0, yawDof?.axis?.y ?? 0, yawDof?.axis?.z ?? 0)
+    //     const rollAxis = new JOLT.Vec3(rollDof?.axis?.x ?? 0, rollDof?.axis?.y ?? 0, rollDof?.axis?.z ?? 0)
+
+    //     console.debug(`Anchor Point: ${joltVec3ToString(anchorPoint)}`)
+    //     console.debug(`Pitch Axis: ${joltVec3ToString(pitchAxis)} ${pitchDof?.limits ? `[${pitchDof.limits.lower!.toFixed(3)}, ${pitchDof.limits.upper!.toFixed(3)}]` : ''}`)
+    //     console.debug(`Yaw Axis: ${joltVec3ToString(yawAxis)} ${yawDof?.limits ? `[${yawDof.limits.lower!.toFixed(3)}, ${yawDof.limits.upper!.toFixed(3)}]` : ''}`)
+    //     console.debug(`Roll Axis: ${joltVec3ToString(rollAxis)} ${rollDof?.limits ? `[${rollDof.limits.lower!.toFixed(3)}, ${rollDof.limits.upper!.toFixed(3)}]` : ''}`)
+
+    //     const constraints: { axis: Jolt.Vec3, friction: number, value: number, upper?: number, lower?: number }[] = []
+
+    //     if (pitchDof?.limits && (pitchDof.limits.upper ?? 0) - (pitchDof.limits.lower ?? 0) < 0.001) {
+    //         console.debug('Pitch Fixed')
+    //     } else {
+    //         constraints.push({
+    //             axis: pitchAxis,
+    //             friction: 0.0,
+    //             value: pitchDof?.value ?? 0,
+    //             upper: pitchDof?.limits ? pitchDof.limits.upper ?? 0 : undefined,
+    //             lower: pitchDof?.limits ? pitchDof.limits.lower ?? 0 : undefined
+    //         })
+    //     }
+
+    //     if (yawDof?.limits && (yawDof.limits.upper ?? 0) - (yawDof.limits.lower ?? 0) < 0.001) {
+    //         console.debug('Yaw Fixed')
+    //     } else {
+    //         constraints.push({
+    //             axis: yawAxis,
+    //             friction: 0.0,
+    //             value: yawDof?.value ?? 0,
+    //             upper: yawDof?.limits ? yawDof.limits.upper ?? 0 : undefined,
+    //             lower: yawDof?.limits ? yawDof.limits.lower ?? 0 : undefined
+    //         })
+    //     }
+
+    //     if (rollDof?.limits && (rollDof.limits.upper ?? 0) - (rollDof.limits.lower ?? 0) < 0.001) {
+    //         console.debug('Roll Fixed')
+    //     } else {
+    //         constraints.push({
+    //             axis: rollAxis,
+    //             friction: 0.0,
+    //             value: rollDof?.value ?? 0,
+    //             upper: rollDof?.limits ? rollDof.limits.upper ?? 0 : undefined,
+    //             lower: rollDof?.limits ? rollDof.limits.lower ?? 0 : undefined
+    //         })
+    //     }
+
+    //     let bodyStart = bodyB
+    //     let bodyNext = bodyA
+    //     if (constraints.length > 1) {
+    //         console.debug('Starting with Ghost Body')
+    //         bodyNext = this.CreateGhostBody(anchorPoint)
+    //         this._joltBodyInterface.AddBody(bodyNext.GetID(), JOLT.EActivation_Activate)
+    //         mechanism.ghostBodies.push(bodyNext.GetID())
+    //     }
+    //     for (let i = 0; i < constraints.length; ++i) {
+    //         console.debug(`Constraint ${i}`)
+    //         const c = constraints[i]
+    //         const hingeSettings = new JOLT.HingeConstraintSettings()
+    //         hingeSettings.mMaxFrictionTorque = c.friction;
+    //         hingeSettings.mPoint1 = hingeSettings.mPoint2 = anchorPoint
+    //         hingeSettings.mHingeAxis1 = hingeSettings.mHingeAxis2 = c.axis.Normalized()
+    //         hingeSettings.mNormalAxis1 = hingeSettings.mNormalAxis2 = getPerpendicular(
+    //             hingeSettings.mHingeAxis1
+    //         )
+    //         if (c.upper && c.lower) {
+    //             // Some values that are meant to be exactly PI are perceived as being past it, causing unexpected behavior.
+    //             // This safety check caps the values to be within [-PI, PI] wth minimal difference in precision.
+    //             const piSafetyCheck = (v: number) => Math.min(3.14158, Math.max(-3.14158, v))
+
+    //             const currentPos = piSafetyCheck(c.value)
+    //             const upper = piSafetyCheck(c.upper) - currentPos
+    //             const lower = piSafetyCheck(c.lower) - currentPos
+
+    //             hingeSettings.mLimitsMin = -upper
+    //             hingeSettings.mLimitsMax = -lower
+    //         }
+
+    //         const hingeConstraint = hingeSettings.Create(bodyStart, bodyNext)
+    //         this._joltPhysSystem.AddConstraint(hingeConstraint)
+    //         this._constraints.push(hingeConstraint)
+    //         bodyStart = bodyNext
+    //         if (i == constraints.length - 2) {
+    //             bodyNext = bodyA
+    //             console.debug('Finishing with Body A')
+    //         } else {
+    //             console.debug('New Ghost Body')
+    //             bodyNext = this.CreateGhostBody(anchorPoint)
+    //             this._joltBodyInterface.AddBody(bodyNext.GetID(), JOLT.EActivation_Activate)
+    //             mechanism.ghostBodies.push(bodyNext.GetID())
+    //         }
+    //     }
+    // }
 
     private IsWheel(jDef: mirabuf.joint.Joint) {
         return (
@@ -1129,7 +1249,7 @@ class PhysicsSystem extends WorldSystem {
             JOLT.EMotionType_Dynamic,
             LAYER_GHOST
         )
-        creationSettings.mMassPropertiesOverride.mMass = 0.1
+        creationSettings.mMassPropertiesOverride.mMass = 0.01
 
         const body = this._joltBodyInterface.CreateBody(creationSettings)
         JOLT.destroy(rot)
