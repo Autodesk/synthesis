f77e5e6995026908d560dc316d831fe6b582576c
Hunter Barclay <hunter.barclay@autodesk.com>
2025-05-19 15:46:28 -0600

    Lygia version break (#1121)

--- fission/src/test/PhysicsSystem.test.ts
+++ fission/src/test/PhysicsSystem.test.ts
@@ -80,7 +80,7 @@ describe("GodMode", () => {
 
 describe("Mirabuf Physics Loading", () => {
     test("Body Loading (Dozer)", async () => {
-        const assembly = await MirabufCachingService.CacheRemote("/api/mira/Robots/Dozer_v9.mira", MiraType.ROBOT).then(
+        const assembly = await MirabufCachingService.CacheRemote("/api/mira/robots/Dozer_v9.mira", MiraType.ROBOT).then(
             x => MirabufCachingService.Get(x!.id, MiraType.ROBOT)
         )
         const parser = new MirabufParser(assembly!)
@@ -92,7 +92,7 @@ describe("Mirabuf Physics Loading", () => {
 
     test("Body Loading (Team_2471_(2018)_v7.mira)", async () => {
         const assembly = await MirabufCachingService.CacheRemote(
-            "/api/mira/Robots/Team 2471 (2018)_v7.mira",
+            "/api/mira/robots/Team 2471 (2018)_v7.mira",
             MiraType.ROBOT
         ).then(x => {
             return MirabufCachingService.Get(x!.id, MiraType.ROBOT)
