9a915f641e34267642206a9b79c373a9ffc055da
Dhruv Arora <dhruv.arora1@autodesk.com>
2025-06-18 12:06:11 -0700

    Reverting formatting changes Kept the timemout change in testing

--- fission/postcss.config.js
+++ fission/postcss.config.js
@@ -2,6 +2,6 @@ export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
-        ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
     },
 }
