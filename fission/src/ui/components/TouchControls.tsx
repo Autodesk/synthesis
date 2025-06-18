9f7d9eb6e79bc984a33727aae3cb09c19df222d8
Dhruv Arora <dhruv.arora1@autodesk.com>
2025-06-18 15:09:34 -0700

    Fix: Removed the place assembly button Redundant after merge

--- fission/src/ui/components/TouchControls.tsx
+++ fission/src/ui/components/TouchControls.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react"
-import Label from "./Label"
+import { useEffect, useRef, useState } from "react"
 import PreferencesSystem from "@/systems/preferences/PreferencesSystem"
 
 function TouchControls() {
@@ -32,38 +31,9 @@ function TouchControls() {
         }
     }, [isJoystickVisible, isPlaceButtonVisible])
 
-    /** simulates an enter key press and release within a 100 millisecond succession */
-    const PlaceMirabufAssembly = useCallback(() => {
-        if (inputRef.current) {
-            const pressEvent = new KeyboardEvent("keydown", {
-                key: "ConfirmAssemblyButton",
-                code: "ConfirmAssemblyButton",
-                bubbles: true,
-            })
-            inputRef.current.dispatchEvent(pressEvent)
-
-            setTimeout(() => {
-                const releaseEvent = new KeyboardEvent("keyup", {
-                    key: "ConfirmAssemblyButton",
-                    code: "ConfirmAssemblyButton",
-                    bubbles: true,
-                })
-                inputRef.current?.dispatchEvent(releaseEvent)
-            }, 100)
-        }
-    }, [])
-
     return (
         <div className="select-none">
             <input ref={inputRef} className="hidden" />
-            <div
-                id="place-assembly-button"
-                className={`absolute top-5 right-5 w-[8vw] min-w-20 min-h-20 aspect-square touch-none bg-gray-100 bg-opacity-20 rounded-md flex justify-center items-center ${isPlaceButtonVisible ? "" : "hidden"}`}
-                onClick={PlaceMirabufAssembly}
-            >
-                <Label className="px-2 py-2 text-main-text text-center">Place Assembly</Label>
-            </div>
-
             <div
                 id="joystick-base-left"
                 className={`fixed bottom-[5vh] left-[5vw] w-60 h-60 touch-none ${isJoystickVisible ? "" : "hidden"}`}
