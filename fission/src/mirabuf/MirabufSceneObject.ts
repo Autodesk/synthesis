312fa60a8df36f18bdbc18aa46d164652f8dadbb
Zach Rutman <zachary.rutman@autodesk.com>
2025-06-19 16:46:36 -0700

    fix: don't leave ghost boxes

--- fission/src/mirabuf/MirabufSceneObject.ts
+++ fission/src/mirabuf/MirabufSceneObject.ts
@@ -9,7 +9,12 @@ import * as THREE from "three"
 import JOLT from "@/util/loading/JoltSyncLoader"
 import { BodyAssociate, LayerReserve } from "@/systems/physics/PhysicsSystem"
 import Mechanism from "@/systems/physics/Mechanism"
-import { EjectorPreferences, FieldPreferences, IntakePreferences } from "@/systems/preferences/PreferenceTypes"
+import {
+    EjectorPreferences,
+    FieldPreferences,
+    IntakePreferences,
+    ScoringZonePreferences,
+} from "@/systems/preferences/PreferenceTypes"
 import PreferencesSystem from "@/systems/preferences/PreferencesSystem"
 import { MiraType } from "./MirabufLoader"
 import IntakeSensorSceneObject from "./IntakeSensorSceneObject"
@@ -443,7 +448,7 @@ class MirabufSceneObject extends SceneObject implements ContextSupplier {
     }
 
     public UpdateScoringZones(render?: boolean) {
-        this._scoringZones.forEach(zone => World.SceneRenderer.RemoveSceneObject(zone.id))
+        this._scoringZones.filter(zone => zone.id != -1).forEach(zone => World.SceneRenderer.RemoveSceneObject(zone.id))
         this._scoringZones = []
 
         if (this._fieldPreferences && this._fieldPreferences.scoringZones) {
@@ -455,6 +460,17 @@ class MirabufSceneObject extends SceneObject implements ContextSupplier {
                 )
                 this._scoringZones.push(newZone)
                 World.SceneRenderer.RegisterSceneObject(newZone)
+            }
+        }
+    }
+
+    public RemoveScoringZoneObject(zone: ScoringZonePreferences) {
+        const index = this._fieldPreferences?.scoringZones?.indexOf(zone)
+        if (index != null) {
+            const zone = this._scoringZones[index]
+            if (zone != null) {
+                World.SceneRenderer.RemoveSceneObject(zone.id)
+                zone.id = -1
             }
         }
     }
