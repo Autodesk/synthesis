6ed87c84f484d5bfc73c1ec027c26547cee50bab
Ryan Zhang <ryzhangofficial@gmail.com>
2025-06-19 15:52:55 -0700

    refactor theme context

--- fission/src/ui/ThemeContext.tsx
+++ fission/src/ui/ThemeContext.tsx
@@ -1,65 +1,13 @@
 import React, { ReactNode, createContext, useContext, useState } from "react"
-import { RgbaColor } from "react-colorful"
 import { addGlobalFunc } from "@/util/dom"
-
-export const defaultThemeName = "Default"
-export type ColorName =
-    | "InteractiveElementSolid"
-    | "InteractiveElementLeft"
-    | "InteractiveElementRight"
-    | "Background"
-    | "BackgroundSecondary"
-    | "InteractiveBackground"
-    | "BackgroundHUD"
-    | "InteractiveHover"
-    | "InteractiveSelect"
-    | "MainText"
-    | "Scrollbar"
-    | "AcceptButton"
-    | "CancelButton"
-    | "InteractiveElementText"
-    | "Icon"
-    | "MainHUDIcon"
-    | "MainHUDCloseIcon"
-    | "HighlightHover"
-    | "HighlightSelect"
-    | "SkyboxTop"
-    | "SkyboxBottom"
-    | "FloorGrid"
-    | "AcceptCancelButtonText"
-    | "MatchRedAlliance"
-    | "MatchBlueAlliance"
-    | "ToastInfo"
-    | "ToastWarning"
-    | "ToastError"
-
-export const colorNameToTailwind = (colorName: ColorName) => {
-    return (
-        "bg" +
-        colorName
-            .replace(/([A-Z]+)/g, "-$1")
-            .replace(/(?<=[A-Z])([A-Z])(?![A-Z]|$)/g, "-$1")
-            .toLowerCase()
-    )
-}
-export const colorNameToProp = (colorName: ColorName) => {
-    return (
-        "-" +
-        colorName
-            .replace(/([A-Z]+)/g, "-$1")
-            .replace(/(?<=[A-Z])([A-Z])(?![A-Z]|$)/g, "-$1")
-            .toLowerCase()
-    )
-}
-
-export const colorNameToVar = (colorName: ColorName) => {
-    return `var(${colorNameToProp(colorName)})`
-}
-
-export type Theme = {
-    [name in ColorName]: { color: RgbaColor; above: (ColorName | string)[] }
-}
-export type Themes = { [name: string]: Theme }
+import { RgbaColor } from "react-colorful"
+import {
+  ColorName,
+  Themes,
+  Theme,
+  defaultThemeName,
+  colorNameToProp,
+} from "./helpers/useThemeHelpers";
 
 type ThemeContextType = {
     themes: Themes
