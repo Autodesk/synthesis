852238571f397dac651ad3558b0c871876bd0a6f
Alexey Dmitriev <157652245+AlexD717@users.noreply.github.com>
2025-06-19 08:35:59 -0700

    SoundPlayer.ts Updated to Follow Best Practices

--- fission/src/systems/sound/SoundPlayer.ts
+++ fission/src/systems/sound/SoundPlayer.ts
@@ -5,54 +5,44 @@ import buttonPressSound from "@/assets/sound-files/ButtonPress.mp3"
 import checkboxPressSound from "@/assets/sound-files/CheckboxPress.wav"
 import dropdownMenuSound from "@/assets/sound-files/DullClick.wav"
 
-enum SoundType {
-    SFX = 0,
-    Music = 1
-}
-
 class SoundPlayer {
     private audio: HTMLAudioElement
 
-    constructor(filePath: string, soundType: SoundType) {
+    constructor(filePath: string) {
         this.audio = new Audio(filePath)
         
-        if (PreferencesSystem.getGlobalPreference<boolean>("MuteAllSound")) {
-            this.audio.volume = 0;
-        }
-        else if (soundType == SoundType.SFX) {
-            let sfxVolume = PreferencesSystem.getGlobalPreference<number>("SFXVolume") / 100 // Changes value from percent (0 - 100) to decimal (0 - 1)
-            sfxVolume = clamp(sfxVolume, 0, 1)
-            this.audio.volume = sfxVolume;
-        }
+        this.audio.volume = PreferencesSystem.getGlobalPreference<boolean>("MuteAllSound")
+            ? 0
+            : clamp(PreferencesSystem.getGlobalPreference<number>("SFXVolume") / 100, 0, 1)
     }
 
-    Play(): Promise<void> {
+    async play(): Promise<void> {
         return this.audio.play().catch((error) => {
             console.error("Error playing the audio file:", error)
         })
     }
 
-    Pause(): void {
+    pause(): void {
         this.audio.pause()
     }
 
-    Stop(): void {
+    stop(): void {
         this.audio.pause()
         this.audio.currentTime = 0
     }
 }
 
 export function buttonPressSFX() {
-    const buttonPressedSFX = new SoundPlayer(buttonPressSound, SoundType.SFX)
-    buttonPressedSFX.Play()
+    const buttonPressedSFX = new SoundPlayer(buttonPressSound)
+    buttonPressedSFX.play()
 }
 
 export function checkboxPressedSFX() {
-    const checkboxPressedSFX = new SoundPlayer(checkboxPressSound, SoundType.SFX)
-    checkboxPressedSFX.Play()
+    const checkboxPressedSFX = new SoundPlayer(checkboxPressSound)
+    checkboxPressedSFX.play()
 }
 
 export function dropdownMenuSFX() {
-    const dropdownMenuSFX = new SoundPlayer(dropdownMenuSound, SoundType.SFX)
-    dropdownMenuSFX.Play()
+    const dropdownMenuSFX = new SoundPlayer(dropdownMenuSound)
+    dropdownMenuSFX.play()
 }
