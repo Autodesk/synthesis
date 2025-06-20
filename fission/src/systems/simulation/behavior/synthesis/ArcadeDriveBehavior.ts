ec9fef663c80f8843498c655737f0ae4576f66e9
Azalea Colburn <azalea.colburn@autodesk.com>
2025-06-20 10:35:08 -0700

    wip: testing for fixing lighter / smaller / something robot turning

--- fission/src/systems/simulation/behavior/synthesis/ArcadeDriveBehavior.ts
+++ fission/src/systems/simulation/behavior/synthesis/ArcadeDriveBehavior.ts
@@ -21,15 +21,41 @@ class ArcadeDriveBehavior extends Behavior {
     ) {
         super(leftWheels.concat(rightWheels), leftStimuli.concat(rightStimuli))
 
-        this.leftWheels = leftWheels
+        // this.leftWheels = leftWheels.map(wheel => {
+        //     wheel.maxForce = wheel.maxForce == 50 ? 500 : wheel.maxForce
+        //     wheel.maxVelocity = wheel.maxVelocity < 15 ? 150 : wheel.maxVelocity
+        //     return wheel
+        // })
+        // this.rightWheels = rightWheels.map(wheel => {
+        //     wheel.maxForce = wheel.maxForce == 50 ? 500 : wheel.maxForce
+        //     wheel.maxVelocity = wheel.maxVelocity < 15 ? 150 : wheel.maxVelocity
+        //     return wheel
+        // })
         this.rightWheels = rightWheels
+        this.leftWheels = leftWheels
+
         this._brainIndex = brainIndex
+
+        // console.log("left")
+        // this.leftWheels.forEach(wheel => {
+        //     console.log(`force: ${wheel.maxForce}`)
+        // })
+        // console.log("right")
+        // this.rightWheels.forEach(wheel => {
+        //     console.log(`force: ${wheel.maxForce}`)
+        // })
     }
 
     // Sets the drivetrains target linear and rotational velocity
     private DriveSpeeds(driveInput: number, turnInput: number) {
-        const leftDirection = Math.min(1, Math.max(-1, driveInput + turnInput))
-        const rightDirection = Math.min(1, Math.max(-1, driveInput - turnInput))
+        // if (driveInput != 0 || turnInput != 0) {
+        //     console.log(`driveInput: ${driveInput} turnInput: ${turnInput}`)
+        // }
+        let leftDirection = Math.min(1, Math.max(-1, driveInput + turnInput))
+        let rightDirection = Math.min(1, Math.max(-1, driveInput - turnInput))
+        // if (leftDirection != 0 || rightDirection != 0) {
+        //     console.log(`left: ${leftDirection} right: ${rightDirection}`)
+        // }
 
         this.leftWheels.forEach(wheel => (wheel.accelerationDirection = leftDirection))
         this.rightWheels.forEach(wheel => (wheel.accelerationDirection = rightDirection))
