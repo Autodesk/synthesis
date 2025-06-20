cddd410f6669ea0444ca03b801bf587f5fe0909c
Ryan Zhang <ryzhangofficial@gmail.com>
2025-06-20 15:40:36 -0700

    refactor use hooks

--- fission/src/ui/ModalContext.tsx
+++ fission/src/ui/ModalContext.tsx
@@ -1,18 +1,4 @@
-import { createContext, useContext, ReactNode } from "react"
-
-type ModalControlContextType = {
-    openModal: (modalId: string, onOpen?: () => void, onClose?: () => void) => void
-    closeModal: () => void
-    children?: ReactNode
-}
-
-const ModalControlContext = createContext<ModalControlContextType | null>(null)
-
-export const useModalControlContext = () => {
-    const context = useContext(ModalControlContext)
-    if (!context) throw new Error("useModalControlContext must be used within a ModalControlProvider")
-    return context
-}
+import { ModalControlContext, ModalControlContextType } from "./helpers/UseModalManager"
 
 export const ModalControlProvider: React.FC<ModalControlContextType> = ({ children, ...methods }) => {
     return <ModalControlContext.Provider value={methods}>{children}</ModalControlContext.Provider>
--- fission/src/ui/PanelContext.tsx
+++ fission/src/ui/PanelContext.tsx
@@ -1,19 +1,4 @@
-import React, { createContext, useContext, ReactNode } from "react"
-
-type PanelControlContextType = {
-    openPanel: (panelId: string) => void
-    closePanel: (panelId: string) => void
-    closeAllPanels: () => void
-    children?: ReactNode
-}
-
-const PanelControlContext = createContext<PanelControlContextType | null>(null)
-
-export const usePanelControlContext = () => {
-    const context = useContext(PanelControlContext)
-    if (!context) throw new Error("usePanelControlContext must be used within a PanelControlProvider")
-    return context
-}
+import { PanelControlContext, PanelControlContextType } from "./helpers/UsePanelManager"
 
 export const PanelControlProvider: React.FC<PanelControlContextType> = ({ children, ...methods }) => {
     return <PanelControlContext.Provider value={methods}>{children}</PanelControlContext.Provider>
