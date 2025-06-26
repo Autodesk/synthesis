8a7d37ae34993b44824f3457b21dd2d546d4ac6d
Alexey Dmitriev <157652245+AlexD717@users.noreply.github.com>
2025-06-26 14:27:01 -0700

    Undo vite.config.ts timout increase

--- fission/vite.config.ts
+++ fission/vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
         ],
     },
     test: {
-        testTimeout: 30000,
+        testTimeout: 10000,
         globals: true,
         environment: "jsdom",
         browser: {
