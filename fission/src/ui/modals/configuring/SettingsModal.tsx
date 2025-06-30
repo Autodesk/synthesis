8bc8e085d283612a869a6e57f3c76d63dd27eef4
Ryan <ryzhangofficial@gmail.com>
2025-06-30 15:54:36 -0700

    Update fission/src/ui/modals/configuring/SettingsModal.tsx
    Co-authored-by: Azalea Colburn <62953415+azaleacolburn@users.noreply.github.com>

--- fission/src/ui/modals/configuring/SettingsModal.tsx
+++ fission/src/ui/modals/configuring/SettingsModal.tsx
@@ -123,7 +123,7 @@ const SettingsModal: React.FC<ModalPropsImpl> = ({ modalId }) => {
             <div className="flex overflow-y-auto flex-col gap-2 bg-background-secondary rounded-md p-2 max-h-[60vh] min-w-[20vw]">
                 <Tabs
                     value={activeTab}
-                    onChange={(_, v) => setActiveTab(v as any)}
+                    onChange={(_, v) => setActiveTab(v as React.SetStateAction<"general" | "graphics">)}
                     textColor="inherit"
                     indicatorColor="primary"
                 >
