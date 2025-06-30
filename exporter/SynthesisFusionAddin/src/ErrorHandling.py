6c69af3ef62caa286cdb2a94ec33ccfa6e0442a0
Azalea Colburn <azalea.colburn@autodesk.com>
2025-06-30 11:23:46 -0700

    doc: result and it's variants

--- exporter/SynthesisFusionAddin/src/ErrorHandling.py
+++ exporter/SynthesisFusionAddin/src/ErrorHandling.py
@@ -11,6 +11,7 @@ logger = getLogger()
 # If an error is non-fatal to the function that generated it, it should be declared but not return, which prints it to the screen
 class ErrorSeverity(Enum):
     Fatal = 50  # Critical Error
+    Error = 40  # Non-critical Error
     Warning = 30  # Warning
 
 
@@ -18,24 +19,56 @@ T = TypeVar("T")
 
 
 class Result(Generic[T]):
+    """
+    Result class for error handling, similar to the Result enum in Rust. The `Err` and `Ok` variants are child types, rather than enum variants though. Another difference is that the error variant is necessarily packaged with a message and a severity, rather than being arbitrary.
+    Since python3 has no match statements, use the `is_ok()` or `is_err()` function to check the variant, then `unwrap()` or `unwrap_err()` to get the value or error message and severity.
+
+    ## Example
+    ```py
+    foo_result = foo()
+    if foo_result.is_err() and foo_result.unwrap_err()[1] == ErrorSeverity.Fatal:
+        return foo_result
+    ```
+
+    Please see the `Ok` and `Err` child class documentation for instructions on instantiating errors and ok-values respectively
+    """
+
     def is_ok(self) -> bool:
+        """
+        Returns if the Result is the Ok variant
+        """
         return isinstance(self, Ok)
 
     def is_err(self) -> bool:
+        """
+        Returns if the Result is the Err variant
+        """
+
         return isinstance(self, Err)
 
     def unwrap(self) -> T:
+        """
+        Returns the value contained in the Ok variant of the result, or raises an exception if unwrapping an Err variant. Be sure to check first.
+        """
         if self.is_ok():
             return self.value  # type: ignore
         raise Exception(f"Called unwrap on Err: {self.message}")  # type: ignore
 
     def unwrap_err(self) -> tuple[str, ErrorSeverity]:
+        """
+        Returns the error message and severity contained in the Err variant of the result, or raises an exception if unwrapping an Ok variant. Be sure to check first.
+        """
+
         if self.is_err():
             return (self.message, self.severity)  # type: ignore
         raise Exception(f"Called unwrap_err on Ok: {self.value}")  # type: ignore
 
 
 class Ok(Result[T]):
+    """
+    The non-error variant of the Result class. Contains the value of the happy path of the function. Return when the function has executed successfully.
+    """
+
     value: T
 
     def __init__(self, value: T):
@@ -46,6 +79,26 @@ class Ok(Result[T]):
 
 
 class Err(Result[T]):
+    """
+    The error variant of the Result class. Contains an error message and severity. Severity is the `ErrorSeverity` enum and is either Fatal, Error, or Warning, each corresponding to a logger severity level, Critical Error (50) and Warning (30) respectively. When an `Err` is instantiated, it is automatically logged in the current synthesis logfile.
+
+    If an error is fatal to the entire program (or the parent function), it should be returned and marked as Fatal:
+    ```python
+    return Err("Foo not found", ErrorSeverity.Fatal)
+    ```
+
+    If an error is fatal to the current function, it should be returned and marked as Error, as the parent could recover it:
+    ```python
+    return Err("Bar not found", ErrorSeverity.Error)
+    ```
+
+    If an error is not fatal to the current function, but ought to be logged, it should be marked as Warning and instantiated but not returned, as to not break control flow:
+    ```python
+    _: Err[T] = Err("Baz not found", ErrorSeverity.Warning)
+    ```
+    Note that the lattermost example will raise a warning if not explicitely typed
+    """
+
     message: str
     severity: ErrorSeverity
 
@@ -59,5 +112,4 @@ class Err(Result[T]):
         return f"Err({self.message})"
 
     def write_error(self) -> None:
-        # Figure out how to integrate severity with the logger
         logger.log(self.severity.value, self.message)
--- exporter/SynthesisFusionAddin/src/Parser/SynthesisParser/Materials.py
+++ exporter/SynthesisFusionAddin/src/Parser/SynthesisParser/Materials.py
@@ -142,7 +142,7 @@ def getPhysicalMaterialData(
 
     missingProperties: list[str] = [k for k, v in vars(mechanicalProperties).items() if v is None]  # ignore: type
     if missingProperties.__len__() > 0:
-        _ = Err(f"Missing Mechanical Properties {missingProperties}", ErrorSeverity.Warning)
+        _: Err[None] = Err(f"Missing Mechanical Properties {missingProperties}", ErrorSeverity.Warning)
 
     """
     Strength Properties
@@ -152,7 +152,7 @@ def getPhysicalMaterialData(
 
     missingStrengthProperties: list[str] = [k for k, v in vars(strengthProperties).items() if v is None]  # ignore: type
     if missingStrengthProperties.__len__() > 0:
-        _ = Err(f"Missing Strength Properties {missingProperties}", ErrorSeverity.Warning)
+        __: Err[None] = Err(f"Missing Strength Properties {missingProperties}", ErrorSeverity.Warning)
 
     """
     strengthProperties.thermal_treatment = materialProperties.itemById(
