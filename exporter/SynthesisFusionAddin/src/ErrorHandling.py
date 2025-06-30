b552dcbee54aaf8ec4a40ecfedbb157d312fc2a4
Azalea Colburn <azalea.colburn@autodesk.com>
2025-06-30 08:26:02 -0700

    chore: format files

--- exporter/SynthesisFusionAddin/src/ErrorHandling.py
+++ exporter/SynthesisFusionAddin/src/ErrorHandling.py
@@ -4,14 +4,17 @@ from typing import Generic, TypeVar
 
 logger = getLogger()
 
+
 # NOTE
 # Severity refers to to the error's affect on the parser as a whole, rather than on the function itself
 # If an error is non-fatal to the function that generated it, it should be declared but not return, which prints it to the screen
 class ErrorSeverity(Enum):
-    Fatal = 50 # Critical Error
-    Warning = 30 # Warning
+    Fatal = 50  # Critical Error
+    Warning = 30  # Warning
 
-T = TypeVar('T')
+
+T = TypeVar("T")
+
 
 class Result(Generic[T]):
     def is_ok(self) -> bool:
@@ -22,25 +25,29 @@ class Result(Generic[T]):
 
     def unwrap(self) -> T:
         if self.is_ok():
-            return self.value # type: ignore
-        raise Exception(f"Called unwrap on Err: {self.message}") # type: ignore
+            return self.value  # type: ignore
+        raise Exception(f"Called unwrap on Err: {self.message}")  # type: ignore
 
     def unwrap_err(self) -> tuple[str, ErrorSeverity]:
         if self.is_err():
-            return (self.message, self.severity) # type: ignore
-        raise Exception(f"Called unwrap_err on Ok: {self.value}") # type: ignore
+            return (self.message, self.severity)  # type: ignore
+        raise Exception(f"Called unwrap_err on Ok: {self.value}")  # type: ignore
+
 
 class Ok(Result[T]):
     value: T
+
     def __init__(self, value: T):
         self.value = value
 
     def __repr__(self):
         return f"Ok({self.value})"
 
+
 class Err(Result[T]):
     message: str
     severity: ErrorSeverity
+
     def __init__(self, message: str, severity: ErrorSeverity):
         self.message = message
         self.severity = severity
