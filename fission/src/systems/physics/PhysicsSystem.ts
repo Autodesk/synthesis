dfcbe0246f46f7042950464462b599bb74f363f9
BrandonPacewic <92102436+BrandonPacewic@users.noreply.github.com>
2025-07-03 10:45:17 -0700

    docs(physics): add github pr and jira issue link comment

--- fission/src/systems/physics/PhysicsSystem.ts
+++ fission/src/systems/physics/PhysicsSystem.ts
@@ -72,6 +72,11 @@ export function GetLastDeltaT(): number {
 // Friction constants
 const FLOOR_FRICTION = 0.7
 const DEFAULT_FRICTION = 0.7
+
+// Transition GH-1152, AARD-1885:
+// Temporary workaround to reduce visible levitation of robots by minimizing suspension.
+// Setting these values to 0 causes physics issues (e.g., ground collisionn problems).
+// Some robots still float slightly, assuming this is due to different export conditions.
 const SUSPENSION_MIN_FACTOR = 0.0001
 const SUSPENSION_MAX_FACTOR = 0.0001
 
