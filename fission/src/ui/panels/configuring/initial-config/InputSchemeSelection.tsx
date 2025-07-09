021102202c9572b059fff3e8639911dee3128340
Dhruv Arora <dhruv.arora1@autodesk.com>
2025-07-09 16:21:32 -0700

    fix: eslint error

--- fission/src/ui/panels/configuring/initial-config/InputSchemeSelection.tsx
+++ fission/src/ui/panels/configuring/initial-config/InputSchemeSelection.tsx
@@ -33,9 +33,10 @@ const InputSchemeSelection: React.FC<InputSchemeSelectionProps> = ({ brainIndex,
 
                 {/** Creates list items with buttons */}
                 {InputSchemeManager.availableInputSchemes.map(scheme => {
-                    if (scheme.usesTouchControls && !matchMedia("(hover: none)").matches) { // Skip schemes that use touch controls if the device does not support touch
+                    if (scheme.usesTouchControls && !matchMedia("(hover: none)").matches) {
+                        // Skip schemes that use touch controls if the device does not support touch
                         return null
-                    } 
+                    }
                     return (
                         <Box
                             component={"div"}
