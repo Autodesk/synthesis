066e38cd0aa6706a0763ccf67f9a1308df891fb8
Dhruv Arora <dhruv.arora1@autodesk.com>
2025-07-14 13:02:37 -0700

    feat: adding height and perimeter rule checks

--- exporter/SynthesisFusionAddin/src/UI/DesignCheckTab.py
+++ exporter/SynthesisFusionAddin/src/UI/DesignCheckTab.py
@@ -1,9 +1,11 @@
 import adsk.core
 import adsk.fusion
+from adsk.fusion import Design
 
+from src import gm
 from src.Logging import getLogger, logFailure
 
-logger = getLogger # TODO: Remove
+logger = getLogger() # TODO: Remove
 
 
 class DesignCheckTab:
@@ -13,6 +15,12 @@ class DesignCheckTab:
     def __init__(self, args: adsk.core.CommandCreatedEventArgs) -> None:
         self.designCheckTab = args.command.commandInputs.addTabCommandInput("designCheckTab", "Design Check")
         designCheckTabInputs = self.designCheckTab.children
+
+        # add a height thing (maximum 106 cm)
+        logger.info(f"{self.fusion_design_height}")
+
+        # get the robot perimeter
+        logger.info(f"{self.fusion_design_perimeter}")
 
     @property
     def isVisible(self) -> bool:
@@ -31,3 +39,19 @@ class DesignCheckTab:
         self, args: adsk.core.InputChangedEventArgs, globalCommandInputs: adsk.core.CommandInputs
     ) -> None:
         commandInput = args.input
+
+    @property
+    def fusion_design_height(self) -> float:
+        design = Design.cast(gm.app.activeProduct)
+        if design:
+            overall_bounding_box = design.rootComponent.orientedMinimumBoundingBox
+            return overall_bounding_box.height
+        return 0.0
+
+    @property
+    def fusion_design_perimeter(self) -> float:
+        design = Design.cast(gm.app.activeProduct)
+        if design:
+            overall_bounding_box = design.rootComponent.orientedMinimumBoundingBox
+            return 2 * (overall_bounding_box.width + overall_bounding_box.length)
+        return 0.0
