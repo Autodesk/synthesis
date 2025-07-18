b0764c61881c12f13ea7303cc121dcd0118defa7
Zach Rutman <zachary.rutman@autodesk.com>
2025-07-18 13:09:20 -0700

    fix: format and vite config

--- fission/src/test/TestSetup.server.ts
+++ fission/src/test/TestSetup.server.ts
@@ -1,24 +1,24 @@
-import sirv from "sirv";
-import http from "node:http";
+import sirv from "sirv"
+import http from "node:http"
 import path from "node:path"
 
-let server: http.Server | undefined;
+let server: http.Server | undefined
 const PORT = 3001
 const serveDirectory = path.join(process.cwd(), "public")
 export async function setup() {
     if (server) {
-        return;
+        return
     }
 
-    console.log("Starting static file server...");
+    console.log("Starting static file server...")
 
-    const assets = sirv(serveDirectory);
+    const assets = sirv(serveDirectory)
 
     server = http.createServer((req, res) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET");
-        assets(req, res);
-    });
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.setHeader("Access-Control-Allow-Methods", "GET")
+        assets(req, res)
+    })
 
     await new Promise<void>((resolve, reject) => {
         if (!server) {
@@ -26,34 +26,32 @@ export async function setup() {
             return
         }
         server.listen(PORT, "127.0.0.1", () => {
-            console.log(
-                `Serving files from ${serveDirectory} on port ${PORT} `,
-            );
-            resolve();
-        });
+            console.log(`Serving files from ${serveDirectory} on port ${PORT} `)
+            resolve()
+        })
 
-        server.once("error", (err) => {
-            console.error("Failed to start static file server:", err);
-            server = undefined;
-            reject(err);
-        });
-    });
+        server.once("error", err => {
+            console.error("Failed to start static file server:", err)
+            server = undefined
+            reject(err)
+        })
+    })
 }
 
 export async function teardown() {
     if (server) {
         await new Promise<void>((resolve, reject) => {
-            server!.close((err) => {
+            server!.close(err => {
                 if (err) {
-                    console.error("Error stopping static file server:", err);
-                    reject(err);
-                    return;
+                    console.error("Error stopping static file server:", err)
+                    reject(err)
+                    return
                 }
 
-                console.log("Static file server stopped.");
-                server = undefined;
-                resolve();
-            });
-        });
+                console.log("Static file server stopped.")
+                server = undefined
+                resolve()
+            })
+        })
     }
 }
