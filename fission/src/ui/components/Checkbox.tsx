73ba00db92a71554dab30cd5206c693d89406331
Zach Rutman <zachary.rutman@autodesk.com>
2025-07-22 09:54:03 -0700

    feat: allow using own state with checkboxes, make controller and touch modes mutually exclusive

--- fission/src/ui/components/Checkbox.tsx
+++ fission/src/ui/components/Checkbox.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react"
-import Label, { LabelSize } from "./Label"
-import { Switch } from "@mui/base/Switch"
-import { Box } from "@mui/material"
-import { LabelWithTooltip } from "./StyledComponents"
-import { SoundPlayer } from "@/systems/sound/SoundPlayer"
+import StatefulCheckbox from "@/components/StatefulCheckbox.tsx"
 
 type CheckboxProps = {
     label: string
     className?: string
     defaultState: boolean
-    stateOverride?: boolean
     hideLabel?: boolean
     onClick?: (checked: boolean) => void
     tooltipText?: string
@@ -27,57 +22,29 @@ type CheckboxProps = {
  * @param {function} props.onClick - Callback function to handle state changes of the checkbox.
  * @param {string} props.tooltipText - Text shows as a tooltip next to the label.
  *
- * @returns {JSX.Element} The rendered Dropdown component.
+ * @returns {React.ReactElement} The rendered Dropdown component.
  */
 const Checkbox: React.FC<CheckboxProps> = ({
     label,
     className,
     defaultState,
-    stateOverride,
     hideLabel,
     onClick,
     tooltipText,
-}: CheckboxProps): JSX.Element => {
-    const [state] = useState(defaultState)
+}: CheckboxProps): React.ReactElement => {
+    const [state, setState] = useState(defaultState)
     return (
-        <Box
-            display="flex"
-            flexDirection={"row"}
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            textAlign={"center"}
-        >
-            {hideLabel ? null : tooltipText ? (
-                LabelWithTooltip(label, tooltipText)
-            ) : (
-                <Label size={LabelSize.SMALL} className={`mr-12 ${className} whitespace-nowrap`}>
-                    {label}
-                </Label>
-            )}
-            <Switch
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => onClick && onClick(e.target.checked)}
-                {...SoundPlayer.checkboxSoundEffects()}
-                slotProps={{
-                    root: {
-                        className: `group relative inline-block w-[24px] h-[24px] m-2.5 cursor-pointer transform transition-transform hover:scale-[1.03] active:scale-[1.06]`,
-                    },
-                    input: {
-                        className: `cursor-inherit absolute w-full h-full top-0 left-0 opacity-0 z-10 border-none`,
-                    },
-                    track: ownerState => {
-                        return {
-                            className: `absolute block w-full h-full transition rounded-full border border-solid outline-none border-interactive-element-right dark:border-interactive-element-right group-[.base--focusVisible]:shadow-outline-switch ${ownerState.checked ? "bg-gradient-to-br from-interactive-element-left to-interactive-element-right" : "bg-background-secondary"} transform transition-transform group-hover:scale-[1.03] group-active:scale-[1.06]`,
-                        }
-                    },
-                    thumb: {
-                        className: `display-none`,
-                    },
-                }}
-                defaultChecked={stateOverride != null ? undefined : state}
-                // checked={state}
-                id="checkbox-switch"
-            />
-        </Box>
+        <StatefulCheckbox
+            label={label}
+            checked={state}
+            className={className}
+            hideLabel={hideLabel}
+            tooltipText={tooltipText}
+            onClick={v => {
+                setState(v)
+                onClick?.(v)
+            }}
+        />
     )
 }
 
