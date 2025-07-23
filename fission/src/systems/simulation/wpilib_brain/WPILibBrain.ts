33820b6cb151fb54a485b3325048b9d91030b3d0
Ryan Zhang <ryzhangofficial@gmail.com>
2025-07-23 14:29:40 -0700

    Update accelerometer implementation

--- fission/src/systems/simulation/wpilib_brain/WPILibBrain.ts
+++ fission/src/systems/simulation/wpilib_brain/WPILibBrain.ts
@@ -577,7 +577,7 @@ class WPILibBrain extends Brain {
         }
 
         this.addSimInput(new SimGyroInput("Test Gyro[1]", this._mechanism))
-        this.addSimInput(new SimAccelInput("ADXL362[4]", this._mechanism))
+        this.addSimInput(new SimAccelInput("Test Accel[4]", this._mechanism))
         this.addSimInput(new SimDigitalInput("SYN DI[0]", () => random() > 0.5))
         this.addSimOutput(new SimDigitalOutput("SYN DO[1]"))
         this.addSimInput(new SimAnalogInput("SYN AI[0]", () => random() * 12))
