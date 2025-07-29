c322278bc950bc01aa516ae5e83b19b0dae8e219
Alexey Dmitriev <157652245+AlexD717@users.noreply.github.com>
2025-07-29 09:21:19 -0700

    Formatting & Window size depends on enviornment

--- fission/electron/vite.renderer.config.ts
+++ fission/electron/vite.renderer.config.ts
@@ -1,9 +1,5 @@
-import { defineConfig, mergeConfig } from 'vite';
-import baseConfig from '../vite.config';
+import { defineConfig, mergeConfig } from "vite"
+import baseConfig from "../vite.config"
 
 // https://vitejs.dev/config
-export default defineConfig((env) =>
-  mergeConfig(baseConfig(env), {
-    // Any renderer-specific config overrides can go here
-  })
-); 
+export default defineConfig((env) => mergeConfig(baseConfig(env), {}))
--- fission/forge.config.ts
+++ fission/forge.config.ts
@@ -1,46 +1,44 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { VitePlugin } from '@electron-forge/plugin-vite';
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import type { ForgeConfig } from "@electron-forge/shared-types"
+import { MakerSquirrel } from "@electron-forge/maker-squirrel"
+import { MakerZIP } from "@electron-forge/maker-zip"
+import { MakerDeb } from "@electron-forge/maker-deb"
+import { MakerRpm } from "@electron-forge/maker-rpm"
+import { VitePlugin } from "@electron-forge/plugin-vite"
+import { FusesPlugin } from "@electron-forge/plugin-fuses"
+import { FuseV1Options, FuseVersion } from "@electron/fuses"
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    name: 'Synthesis Fission',
-    executableName: 'synthesis-fission',
+    name: "Synthesis Fission",
+    executableName: "synthesis-fission",
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
+    new MakerZIP({}, ["darwin"]),
     new MakerRpm({}),
-    new MakerDeb({})
+    new MakerDeb({}),
   ],
   plugins: [
     new VitePlugin({
-      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-      // If you are familiar with Vite configuration, it will look really familiar.
       build: [
         {
           // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-          entry: 'src/main.ts',
-          config: 'electron/vite.main.config.ts',
-          target: 'main',
+          entry: "src/main.ts",
+          config: "electron/vite.main.config.ts",
+          target: "main",
         },
         {
-          entry: 'src/preload.ts',
-          config: 'electron/vite.preload.config.ts',
-          target: 'preload',
+          entry: "src/preload.ts",
+          config: "electron/vite.preload.config.ts",
+          target: "preload",
         },
       ],
       renderer: [
         {
-          name: 'main_window',
-          config: 'electron/vite.renderer.config.ts',
+          name: "main_window",
+          config: "electron/vite.renderer.config.ts",
         },
       ],
     }),
@@ -56,6 +54,6 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
-};
+}
 
-export default config; 
+export default config
