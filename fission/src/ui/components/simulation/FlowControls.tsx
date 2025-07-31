5d8929120964dd45d0cee5573c0d08c958b52ca8
PepperLola <PepperLola@users.noreply.github.com>
2025-07-30 21:39:26 -0700

    fix: eliminate redundant custom flow button component

--- fission/src/ui/components/simulation/FlowControls.tsx
+++ fission/src/ui/components/simulation/FlowControls.tsx
@@ -1,32 +1,26 @@
 import { Panel as FlowPanel, useReactFlow } from "@xyflow/react"
-import type { PropsWithChildren } from "react"
 import type { FlowControlsProps } from "@/systems/simulation/SimConfigShared"
 import { MdFitScreen, MdZoomInMap, MdZoomOutMap } from "react-icons/md"
 import { FaPlus } from "react-icons/fa6"
 import { Button } from "@mui/material"
-
-// This is required for the onClick types to work nicely with the react flow functions
-function FlowControlButton({ onClick, children }: PropsWithChildren<{ onClick?: () => void }>) {
-    return <Button onClick={() => onClick?.()}>{children}</Button>
-}
 
 function FlowControls({ onCreateJunction }: FlowControlsProps) {
     const { zoomIn, zoomOut, fitView } = useReactFlow()
 
     return (
         <FlowPanel position="bottom-left" className="flex flex-col-reverse gap-1">
-            <FlowControlButton onClick={fitView}>
+            <Button onClick={() => fitView()}>
                 <MdFitScreen className="w-full h-full" />
-            </FlowControlButton>
-            <FlowControlButton onClick={zoomOut}>
+            </Button>
+            <Button onClick={() => zoomOut()}>
                 <MdZoomOutMap className="w-full h-full" />
-            </FlowControlButton>
-            <FlowControlButton onClick={zoomIn}>
+            </Button>
+            <Button onClick={() => zoomIn()}>
                 <MdZoomInMap className="w-full h-full" />
-            </FlowControlButton>
-            <FlowControlButton onClick={onCreateJunction}>
+            </Button>
+            <Button onClick={() => onCreateJunction?.()}>
                 <FaPlus className="w-full h-full" />
-            </FlowControlButton>
+            </Button>
         </FlowPanel>
     )
 }
