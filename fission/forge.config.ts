d0a70070da3c78affa56029ef55158320384c47b
Alexey Dmitriev <157652245+AlexD717@users.noreply.github.com>
2025-08-04 09:07:41 -0700

    Linux build fix

--- fission/forge.config.ts
+++ fission/forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from "@electron-forge/shared-types"
 import { MakerSquirrel } from "@electron-forge/maker-squirrel"
 import { MakerZIP } from "@electron-forge/maker-zip"
-import { MakerDeb } from "@electron-forge/maker-deb"
-import { MakerRpm } from "@electron-forge/maker-rpm"
 import { VitePlugin } from "@electron-forge/plugin-vite"
 import { FusesPlugin } from "@electron-forge/plugin-fuses"
 import { FuseV1Options, FuseVersion } from "@electron/fuses"
@@ -20,9 +18,7 @@ const config: ForgeConfig = {
     new MakerSquirrel({
       setupIcon: path.resolve(__dirname, "src/assets/icons/synthesis-logo.ico"),
     }),
-    new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({}),
+    new MakerZIP({}, ["darwin", "linux"]),
   ],
   plugins: [
     new VitePlugin({
