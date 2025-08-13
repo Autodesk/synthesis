893c7e0721f4f3c84c5375e5f4d6e8778a7d25a0
Ryan Zhang <ryzhangofficial@gmail.com>
2025-08-13 10:21:30 -0700

    Update StatefulSlider to include UI value

--- fission/src/ui/components/StatefulSlider.tsx
+++ fission/src/ui/components/StatefulSlider.tsx
@@ -1,4 +1,4 @@
-import { FormControlLabel, Slider, Tooltip } from "@mui/material"
+import { Box, FormControlLabel, Slider, Stack, Tooltip, Typography } from "@mui/material"
 import { useState } from "react"
 
 const StatefulSlider: React.FC<
@@ -7,25 +7,48 @@ const StatefulSlider: React.FC<
         defaultValue: number
         onChange: (val: number) => void
         tooltip?: string
+        format?: Intl.NumberFormatOptions
+        unit?: string
     }
 > = props => {
     const [value, setValue] = useState(props.defaultValue)
+    
+    const formatValue = (val: number) => {
+        const formattedNumber = props.format 
+            ? new Intl.NumberFormat('en-US', props.format).format(val)
+            : val.toString()
+        return props.unit ? `${formattedNumber} ${props.unit}` : formattedNumber
+    }
+    
     return (
         <Tooltip title={props.tooltip ?? ""}>
-            <FormControlLabel
-                label={props.label}
-                labelPlacement="top"
-                control={
-                    <Slider
-                        {...props}
-                        value={value}
-                        onChange={(_, value) => {
-                            setValue(value as number)
-                            props.onChange?.(value as number)
+            <Box>
+                <Stack direction="row" justifyContent="space-between" alignItems="center" mb={1}>
+                    <Typography variant="body2" component="label">
+                        {props.label}
+                    </Typography>
+                    <Typography 
+                        variant="body2" 
+                        component="span"
+                        sx={{ 
+                            fontWeight: 'medium',
+                            color: 'primary.main',
+                            minWidth: 'fit-content',
+                            textAlign: 'right'
                         }}
-                    ></Slider>
-                }
-            />
+                    >
+                        {formatValue(value)}
+                    </Typography>
+                </Stack>
+                <Slider
+                    {...props}
+                    value={value}
+                    onChange={(_, value) => {
+                        setValue(value as number)
+                        props.onChange?.(value as number)
+                    }}
+                />
+            </Box>
         </Tooltip>
     )
 }
