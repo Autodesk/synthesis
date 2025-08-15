0aaa9f0cdec4bb8d318ca953d2456db9a2111c88
Alexey Dmitriev <157652245+AlexD717@users.noreply.github.com>
2025-08-15 08:27:23 -0700

    Revert "Hotfix: Chrome Rendering Issue `[AARD-2058]`"

--- fission/src/mirabuf/MirabufSceneObject.ts
+++ fission/src/mirabuf/MirabufSceneObject.ts
@@ -515,13 +515,7 @@ class MirabufSceneObject extends SceneObject implements ContextSupplier {
                 .clone()
                 .premultiply(transform)
             const meshes = this._mirabufInstance.meshes.get(part) ?? []
-            meshes.forEach(([mesh, index]) => {
-                mesh.setMatrixAt(index, partTransform)
-                // Only update instanceMatrix for InstancedMesh
-                if ("instanceMatrix" in mesh) {
-                    mesh.instanceMatrix.needsUpdate = true
-                }
-            })
+            meshes.forEach(([batch, id]) => batch.setMatrixAt(id, partTransform))
         })
     }
 
