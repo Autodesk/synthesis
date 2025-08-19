b33eac8a0b5d68b1fe8a44848ef95ec1a648cfee
Azalea Colburn <azaleacolburn@gmail.com>
2025-08-18 18:10:21 -0700

    feat: flake files for nix dev shell

---
+++ fission/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A Nix-flake-based Bun development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            pkgs = [ ];
+            nativeBuildInputs = with pkgs; [
+              playwright-driver.browsers
+            ];
+
+            shellHook = ''
+              export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+              export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+            '';
+
+          };
+        }
+      );
+    };
+}
