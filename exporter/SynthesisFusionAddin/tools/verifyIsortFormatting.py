c040c2489d5b81d612635fbff77e6fd2873319b8
BrandonPacewic <92102436+BrandonPacewic@users.noreply.github.com>
2026-04-03 12:30:47 -0700

    fix: stabilize Black/Isort CI format validation

--- exporter/SynthesisFusionAddin/tools/verifyIsortFormatting.py
+++ exporter/SynthesisFusionAddin/tools/verifyIsortFormatting.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 import sys
 
@@ -6,33 +5,13 @@ ROOT_EXPORTER_DIR = "exporter/SynthesisFusionAddin"
 
 
 def main() -> None:
-    files = [
-        os.path.abspath(os.path.join(root, filename))
-        for root, _, filenames in os.walk(ROOT_EXPORTER_DIR)
-        for filename in filenames
-        if filename.endswith(".py")
-    ]
-    oldFileStates = [open(file, "r").readlines() for file in files]
-    subprocess.call(["isort", ROOT_EXPORTER_DIR], bufsize=1, shell=False)
-    newFileStates = [open(file, "r").readlines() for file in files]
-    exitCode = 0
-    for i, (oldFileState, newFileState) in enumerate(zip(oldFileStates, newFileStates)):
-        for j, (previousLine, newLine) in enumerate(zip(oldFileState, newFileState)):
-            if previousLine == newLine:
-                continue
-
-            print(f"File {files[i]} is not formatted correctly!\nLine: {j + 1}")
-            oldFileStateRange = range(max(0, j - 10), min(len(oldFileState), j + 11))
-            print("\nOld file state:\n" + "\n".join(oldFileState[k].strip() for k in oldFileStateRange))
-            newFileStateRange = range(max(0, j - 10), min(len(newFileState), j + 11))
-            print("\nNew file state:\n" + "\n".join(newFileState[k].strip() for k in newFileStateRange))
-            exitCode = 1
-            break
-
-    if not exitCode:
+    result = subprocess.call(
+        ["isort", "--check-only", "--diff", "--settings-path", ROOT_EXPORTER_DIR, ROOT_EXPORTER_DIR],
+        shell=False,
+    )
+    if not result:
         print("All files are formatted correctly with isort!")
-
-    sys.exit(exitCode)
+    sys.exit(result)
 
 
 if __name__ == "__main__":
