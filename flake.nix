c47e3fc299a663df1d13cf07f9da109783bf4c5b
0xda157 <da157@voidq.com>
2026-06-16 10:27:46 -0700

    chore(flake): clean up system access

--- flake.nix
+++ flake.nix
@@ -15,42 +15,52 @@
         "aarch64-darwin"
       ];
       forEachSupportedSystem =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
     in
     {
-      devShells = forEachSupportedSystem (pkgs: {
-        default = self.devShells.${pkgs.stdenv.hostPlatform.system}.fission;
-        fission = pkgs.mkShell {
-          packages = with pkgs; [
-            nodejs
-            bun
-            git-lfs
-            playwright-driver.browsers
-          ];
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = self.devShells.${system}.fission;
+          fission = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs
+              bun
+              git-lfs
+              playwright-driver.browsers
+            ];
 
-          env = {
-            PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
-            PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+            env = {
+              PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
+              PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+            };
           };
-        };
-        exporter = pkgs.mkShell {
-          packages = with pkgs; [
-            python3
-            black
-            isort
-            bun
-          ];
-        };
-        multiplayer = pkgs.mkShell {
-          packages = with pkgs; [
-            bun
-          ];
-        };
-      });
+          exporter = pkgs.mkShell {
+            packages = with pkgs; [
+              python3
+              black
+              isort
+              bun
+            ];
+          };
+          multiplayer = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+            ];
+          };
+        }
+      );
 
-      formatter = forEachSupportedSystem (pkgs: pkgs.nixfmt-tree);
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt-tree);
 
       # Build all devShells, instead of just verifying they are derivations
-      checks = forEachSupportedSystem (pkgs: self.devShells.${pkgs.stdenv.hostPlatform.system});
+      checks = forEachSupportedSystem ({ system, ... }: self.devShells.${system});
     };
 }
