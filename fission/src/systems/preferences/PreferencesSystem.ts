f432a16274555cded2fa0dfad2601c017f818829
Roushil Satta <94149218+RoushilS@users.noreply.github.com>
2026-06-17 10:37:25 -0700

    fix: disable alert if graphics are default

--- fission/src/systems/preferences/PreferencesSystem.ts
+++ fission/src/systems/preferences/PreferencesSystem.ts
@@ -222,6 +222,19 @@ class PreferencesSystem {
         this.savePreferences()
     }
 
+    public static graphicsPreferencesAreDefault(): boolean {
+        const current = this._preferences[GRAPHICS_PREFERENCE_KEY]
+        const defaults = defaultGraphicsPreferences()
+        return (
+            current!.lightIntensity === defaults.lightIntensity &&
+            current!.fancyShadows === defaults.fancyShadows &&
+            current!.maxFar === defaults.maxFar &&
+            current!.cascades === defaults.cascades &&
+            current!.shadowMapSize === defaults.shadowMapSize &&
+            current!.antiAliasing === defaults.antiAliasing
+        )
+    }
+
     /** Loads all preferences from local storage. */
     public static loadPreferences() {
         const loadedPrefs = window.localStorage.getItem(this._localStorageKey)
