7d711cc35dec6a221fd169b26d54e2578ddeb050
0xda157 <da157@voidq.com>
2026-06-22 13:19:01 -0700

    chore: remove unused electron code [SYNTH-150]

--- fission/src/vite-env.d.ts
+++ fission/src/vite-env.d.ts
@@ -1,21 +1,4 @@
 /// <reference types="vite/client" />
-
-// Electron Forge Vite plugin environment variables
-declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string | undefined
-declare const MAIN_WINDOW_VITE_NAME: string
-
-// Type declaration for electron-squirrel-startup
-declare module "electron-squirrel-startup" {
-    const started: boolean
-    export = started
-}
-
-interface Window {
-    electronAPI?: {
-        platform: string
-    }
-}
-
 interface ImportMetaEnv {
     // biome-ignore lint/style/useNamingConvention: environment variable
     readonly VITE_MULTIPLAYER_PORT: string
