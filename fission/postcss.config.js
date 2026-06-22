8f2661cc8e3fb4362bfee469855483da3cd2aebe
0xda157 <da157@voidq.com>
2026-06-22 15:18:46 -0700

    chore: apply more consistant formatting

--- fission/postcss.config.js
+++ fission/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
     plugins: {
-        '@tailwindcss/postcss': {},
+        "@tailwindcss/postcss": {},
         ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
     },
 }
