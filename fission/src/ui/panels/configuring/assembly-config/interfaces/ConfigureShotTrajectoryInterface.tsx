739269eca753e96de84711516bfddece4015b4ad
Roushil Satta <94149218+RoushilS@users.noreply.github.com>
2026-06-29 16:12:15 -0700

    Update ConfigureShotTrajectoryInterface.tsx

--- fission/src/ui/panels/configuring/assembly-config/interfaces/ConfigureShotTrajectoryInterface.tsx
+++ fission/src/ui/panels/configuring/assembly-config/interfaces/ConfigureShotTrajectoryInterface.tsx
@@ -169,7 +169,7 @@ const ConfigureShotTrajectoryInterface: React.FC<ConfigEjectorProps> = ({ select
 
     useEffect(() => {
         if (selectedRobot?.ejectorPreferences) {
-            setEjectorVelocity(selectedRobot.ejectorPreferences.ejectorVelocity ?? (MIN_VELOCITY + MAX_VELOCITY) / 2.0)
+            setEjectorVelocity(selectedRobot.ejectorPreferences.ejectorVelocity)
             setSelectedNode(selectedRobot.ejectorPreferences.parentNode)
             setEjectOrder(selectedRobot.ejectorPreferences.ejectOrder)
         } else {
