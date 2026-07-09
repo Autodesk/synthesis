3e1583c60b7c89f73ba77bf9844e080584d39b0d
0xda157 <da157@voidq.com>
2026-07-09 13:40:56 -0700

    flake: disable playwright setup

--- flake.nix
+++ flake.nix
@@ -34,13 +34,13 @@
               nodejs
               bun
               git-lfs
-              playwright-driver.browsers
+              # playwright-driver.browsers
             ];
 
-            env = {
-              PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
-              PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
-            };
+            # env = {
+            #   PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
+            #   PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+            # };
           };
           exporter = pkgs.mkShell {
             packages = with pkgs; [
