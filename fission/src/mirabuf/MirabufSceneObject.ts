b6277926e6e4fb4960a1fa6a46a4894131256064
Azalea Colburn <azaleacolburn@gmail.com>
2026-07-13 22:15:52 -0700

    fix(wip): try offsetting the root node position

--- fission/src/mirabuf/MirabufSceneObject.ts
+++ fission/src/mirabuf/MirabufSceneObject.ts
@@ -52,6 +52,7 @@ import {
     convertJoltMat44ToThreeMatrix4,
     convertJoltRVec3ToJoltVec3,
     convertJoltVec3ToThreeVector3,
+    convertThreeVector3ToJoltRVec3,
     convertThreeVector3ToJoltVec3,
 } from "@/util/TypeConversions"
 import { createMeshForShape } from "@/util/threejs/MeshCreation.ts"
@@ -133,6 +134,7 @@ class MirabufSceneObject extends SceneObject implements ContextSupplier {
     private _collisionUnsubscriber?: () => void
 
     private _furthestVertices?: AxisVertices = undefined
+    private _centerPositionOffsetFromRootNodeTransform?: Jolt.RVec3 = undefined
 
     public get scoringZones(): Readonly<ScoringZoneSceneObject[]> {
         return this._scoringZones
@@ -868,9 +870,18 @@ class MirabufSceneObject extends SceneObject implements ContextSupplier {
         const rootBody = World.physicsSystem.getBody(this.getRootNodeId()!)!
         // Do not destroy
         const rotation = rootBody.GetRotation()
-        const position = convertThreeVector3ToJoltVec3(this.getPositionTransform())
 
-        const transform = JOLT.Mat44.prototype.sRotationTranslation(rotation, position)
+        // Here, we calculate the vector between the center of the robot when axis-aligned (which is should be initially) and the root node transform
+        if (!this._centerPositionOffsetFromRootNodeTransform) {
+            const rootNodeTransform = rootBody.GetPosition()
+            const alignedPosition = convertThreeVector3ToJoltRVec3(this.getPositionTransform())
+
+            this._centerPositionOffsetFromRootNodeTransform = alignedPosition.SubRVec3(rootNodeTransform)
+        }
+
+        // Then, we just always use the root node transform, offsetting it by the hopefully constant vector
+        const position = rootBody.GetPosition().AddRVec3(this._centerPositionOffsetFromRootNodeTransform!)
+        const transform = JOLT.Mat44.prototype.sRotationTranslation(rotation, convertJoltRVec3ToJoltVec3(position))
 
         const orientedBoundingBox = new JOLT.OrientedBox(transform, halfExtent)
 
