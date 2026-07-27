8ba820c4072be07067d617e4ba40cceb72f72a04
Zach Rutman <zachary.rutman@autodesk.com>
2026-07-27 15:45:11 -0700

    fix: biome ignores

--- fission/src/systems/multiplayer/MultiplayerTypes.ts
+++ fission/src/systems/multiplayer/MultiplayerTypes.ts
@@ -1,25 +1,32 @@
-import type {Alliance, FieldPreferences, RobotPreferences, Station} from "@/systems/preferences/PreferenceTypes.ts"
+import type { Alliance, FieldPreferences, RobotPreferences, Station } from "@/systems/preferences/PreferenceTypes.ts"
 import type { MessageType } from "@/systems/multiplayer/MultiplayerMessageTypes.ts"
 
 export type MessageWithTimestamp = {
     [K in keyof MessageType]: {
         recipientId?: string
-        client_id: string
+        clientId: string
         type: K
         data: MessageType[K]
         timestamp: number
     }
 }[keyof MessageType]
-export type Message = Omit<MessageWithTimestamp, "timestamp" | "client_id"> & Partial<MessageWithTimestamp>
+export type Message = Omit<MessageWithTimestamp, "timestamp" | "clientId"> & Partial<MessageWithTimestamp>
 
+// biome-ignore-start lint/style/useNamingConvention: Otherwise types are same
 export type EncodedAssembly = Uint8Array & { __: "encodedassembly" }
 export type RemoteSceneObjectId = number & { __: "remotesceneobject" | "sceneobjectkey" }
 export type LocalSceneObjectId = number & { __: "localsceneobject" | "sceneobjectkey" }
+// biome-ignore-end lint/style/useNamingConvention: Otherwise types are same
 
 export type ClientInfo = {
     displayName: string
     clientId: string
     creationTime: number
+}
+
+export type ClientAndLatencyInfo = ClientInfo & {
+    latency?: number
+    lastUpdateTime?: number
 }
 
 export function shortClientId(info: ClientInfo) {
