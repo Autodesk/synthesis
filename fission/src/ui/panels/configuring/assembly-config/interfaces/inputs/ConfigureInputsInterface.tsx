d19f69175ecfc83fff9913fc122a7973f32353d9
Alexey Dmitriev <157652245+AlexD717@users.noreply.github.com>
2026-07-29 14:47:42 -0700

    Fix Configuration Input Selection

--- fission/src/ui/panels/configuring/assembly-config/interfaces/inputs/ConfigureInputsInterface.tsx
+++ fission/src/ui/panels/configuring/assembly-config/interfaces/inputs/ConfigureInputsInterface.tsx
@@ -63,12 +63,18 @@ const ConfigureInputsInterface: React.FC<PanelImplProps<void, ConfigurePanelCust
         const unsubscribeConfig = EventSystem.listen("ConfigurationSavedEvent", saveEvent)
         const unsubscribeInput = EventSystem.listen("InputSchemeChanged", handleSchemeChange)
         return () => {
-            setSelectedScheme(undefined)
-            setGlobalSelectedScheme(undefined)
             unsubscribeConfig()
             unsubscribeInput()
         }
-    }, [saveEvent, handleSchemeChange, setGlobalSelectedScheme])
+    }, [saveEvent, handleSchemeChange])
+
+    // We want a separate use effect as handleSchemeChange is recreated on selectedScheme change
+    useEffect(() => {
+        return () => {
+            setSelectedScheme(undefined)
+            setGlobalSelectedScheme(undefined)
+        }
+    }, [setGlobalSelectedScheme])
 
     const schemeOptionMap = useMemo(() => {
         const map = new Map<InputScheme, SchemeSelectionOption>()
