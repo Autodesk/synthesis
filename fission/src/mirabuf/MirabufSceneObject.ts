df8b17368a94c6c847a8eff59b5355c8c2f49e08
Roushil Satta <94149218+RoushilS@users.noreply.github.com>
2026-08-20 09:26:10 -0700

    fix: replace any with SynthesisBrain

--- fission/src/mirabuf/MirabufSceneObject.ts
+++ fission/src/mirabuf/MirabufSceneObject.ts
@@ -247,7 +247,7 @@ class MirabufSceneObject extends SceneObject implements ContextSupplier {
                     const name =
                         this.nameOverride ??
                         (this._brain?.isSynthesis()
-                            ? (this._brain as any).inputSchemeLabel
+                            ? (this._brain as SynthesisBrain).inputSchemeLabel
                             : this._brain?.isWPILib()
                               ? "Magic"
                               : "Not Configured!")
